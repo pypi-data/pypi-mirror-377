d3fe1b5a387db4acbc2bda8ab3d1f61106338b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 05:04:34 +0000

    Add package SoundCard/0.4.5/soundcard-0.4.5.tar.gz

---
+++ packages/SoundCard/soundcard-0.4.5.tar.gz/soundcard-0.4.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='SoundCard',
+    version='0.4.5',
+    description='Play and record audio without resorting to CPython extensions',
+    author='Bastian Bechtold',
+    url='https://github.com/bastibe/SoundCard',
+    license='BSD 3-clause',
+    packages=['soundcard', 'soundcard.__pyinstaller'],
+    package_data={'soundcard': ['*.py.h']},
+    install_requires=['numpy>=1.11', 'cffi'],
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    entry_points={
+        "pyinstaller40": [
+            "hook-dirs = soundcard.__pyinstaller:get_hook_dirs",
+            "tests = soundcard.__pyinstaller:get_test_dirs",
+        ],
+    },
+)

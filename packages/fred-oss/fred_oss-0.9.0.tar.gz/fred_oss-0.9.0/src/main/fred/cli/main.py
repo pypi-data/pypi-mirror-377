201fc7bb9e7a3c8b4670199b3504693963ce46e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:05:06 +0000

    Add package fred-oss/0.9.0/fred_oss-0.9.0.tar.gz

---
+++ packages/fred-oss/fred_oss-0.9.0.tar.gz/fred_oss-0.9.0/src/main/fred/cli/main.py
@@ -0,0 +1,26 @@
+from fred.version import version
+from fred.settings import logger_manager
+from fred.cli.interface import AbstractCLI
+
+
+logger = logger_manager.get_logger(name=__name__)
+
+
+class CLIExtensionGroups:
+    """CLI Extensions providing access to various integrations by following a lazy loading pattern."""
+
+    @property
+    def databricks(self):
+        from fred.integrations.databricks.cli_ext import DatabricksExt
+        return DatabricksExt()
+    
+    @property
+    def runpod(self):
+        from fred.integrations.runpod.cli_ext import RunPodExt
+        return RunPodExt()
+
+
+class CLI(AbstractCLI, CLIExtensionGroups):
+
+    def version(self) -> str:
+        return version.value

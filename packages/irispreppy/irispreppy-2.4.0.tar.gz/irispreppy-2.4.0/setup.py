94f88cd26461415dba533d08c5b826d306630640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 18:04:55 +0000

    Add package irispreppy/2.4.0/irispreppy-2.4.0.tar.gz

---
+++ packages/irispreppy/irispreppy-2.4.0.tar.gz/irispreppy-2.4.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md", "r") as f:
+        return f.read()
+
+setup(name='irispreppy',
+      version="2.4.0",
+      url='https://github.com/OfAaron3/irispreppy',
+      author='Aaron W. Peat',
+      author_email='aaron.peat@uwr.edu.pl',
+      license='MIT',
+      packages=['irispreppy.psf', 'irispreppy.radcal', 'irispreppy', 'irispreppy.radcal.responses'],
+      install_requires=[
+          'numpy',
+          'astropy',
+          'scipy',
+          'beautifulsoup4',
+          'weno4'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+)

82240a6d58e5a5ed287f686c45534b376f558813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:14:39 +0000

    Add package skellycam/2025.9.1097/skellycam-2025.9.1097-py3-none-any.whl

---
+++ packages/skellycam/skellycam-2025.9.1097-py3-none-any.whl/skellycam/__main__.py
@@ -0,0 +1,39 @@
+# __main__.py
+import platform
+import sys
+from pathlib import Path
+import argparse
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="SkellyCam")
+    return parser.parse_args()
+
+def run():
+    parse_args()
+    
+    try:
+        from skellycam.gui.qt.main import qt_gui_main
+    except Exception as e:
+        base_package_path = Path(__file__).parent.parent
+        print(f"adding base_package_path: {base_package_path} : to sys.path")
+        sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
+        from skellycam.gui.qt.main import qt_gui_main
+    
+    
+    qt_gui_main()
+
+
+if __name__ == "__main__":
+    print(f"Running `skellycam.__main__` from - {__file__}")
+
+    if platform.system() == "Windows":
+        # set up so you can change the taskbar icon - https://stackoverflow.com/a/74531530/14662833
+        import ctypes
+        import skellycam
+        myappid = f"{skellycam.__package_name__}_{skellycam.__version__}"  # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+    run()
+

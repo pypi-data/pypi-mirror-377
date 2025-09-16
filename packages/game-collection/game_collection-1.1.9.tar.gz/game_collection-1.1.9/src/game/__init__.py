dc4321d5562929ba6dd8505d3811998647a9fea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:04:59 +0000

    Add package game-collection/1.1.9/game_collection-1.1.9.tar.gz

---
+++ packages/game-collection/game_collection-1.1.9.tar.gz/game_collection-1.1.9/src/game/__init__.py
@@ -0,0 +1,9 @@
+"""Game Collection - A collection of classic arcade games."""
+
+__version__ = "1.0.1"
+__author__ = "Game Collection Team"
+__email__ = "game-collection@example.com"
+
+from .main import main
+
+__all__ = ["main", "__version__"]

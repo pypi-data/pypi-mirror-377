5eb5b348b781e6b5e11dbc25442c76b0bd316c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 05:05:10 +0000

    Add package comfyui-frontend-package/1.27.4/comfyui_frontend_package-1.27.4-py3-none-any.whl

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.27.4-py3-none-any.whl/comfyui_frontend_package/static/assets/DesktopStartView-B-L-imE-.js
@@ -0,0 +1,79 @@
+import { defineComponent, openBlock, createBlock, withCtx, createVNode, unref } from "vue";
+import ProgressSpinner from "primevue/progressspinner";
+import { _ as _sfc_main$1 } from "./BaseViewTemplate-lDALG4qH.js";
+import "./index-DJG8XLpz.js";
+import "@primevue/themes";
+import "@primevue/themes/aura";
+import "primevue/config";
+import "primevue/confirmationservice";
+import "primevue/toastservice";
+import "primevue/tooltip";
+import "primevue/blockui";
+import "primevue/dialog";
+import "vue-i18n";
+import "primevue/button";
+import "primevue/scrollpanel";
+import "primevue/skeleton";
+import "primevue/checkbox";
+import "primevue/message";
+import "primevue/divider";
+import "primevue/usetoast";
+import "primevue/card";
+import "primevue/listbox";
+import "primevue/panel";
+import "primevue/progressbar";
+import "primevue/floatlabel";
+import "primevue/inputtext";
+import "@primevue/forms";
+import "@primevue/forms/resolvers/zod";
+import "primevue/password";
+import "primevue/tag";
+import "primevue/inputnumber";
+import "primevue/popover";
+import "primevue/toggleswitch";
+import "primevue/tab";
+import "primevue/tablist";
+import "primevue/tabpanel";
+import "primevue/tabpanels";
+import "primevue/tabs";
+import "primevue/multiselect";
+import "primevue/autocomplete";
+import "primevue/dropdown";
+import "primevue/tabmenu";
+import "primevue/dataview";
+import "primevue/selectbutton";
+import "primevue/column";
+import "primevue/datatable";
+import "primevue/iconfield";
+import "primevue/inputicon";
+import "primevue/badge";
+import "primevue/chip";
+import "primevue/select";
+import "primevue/colorpicker";
+import "primevue/radiobutton";
+import "primevue/knob";
+import "primevue/slider";
+import "primevue/contextmenu";
+import "primevue/tree";
+import "primevue/toolbar";
+import "primevue/confirmpopup";
+import "primevue/useconfirm";
+import "primevue/galleria";
+import "primevue/confirmdialog";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "DesktopStartView",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(_sfc_main$1, { dark: "" }, {
+        default: withCtx(() => [
+          createVNode(unref(ProgressSpinner), { class: "m-8 w-48 h-48" })
+        ]),
+        _: 1
+      });
+    };
+  }
+});
+export {
+  _sfc_main as default
+};
+//# sourceMappingURL=DesktopStartView-B-L-imE-.js.map

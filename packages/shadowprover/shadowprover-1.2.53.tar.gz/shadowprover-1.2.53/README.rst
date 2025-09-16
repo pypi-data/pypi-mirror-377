1f2455ca5975287cc7f2635ad26e1f469f844e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 15:04:37 +0000

    Add package shadowprover/1.2.53/shadowprover-1.2.53.tar.gz

---
+++ packages/shadowprover/shadowprover-1.2.53.tar.gz/shadowprover-1.2.53/README.rst
@@ -0,0 +1,7 @@
+
+## ShadowProver
+ShadowProver is a theorem prover (multi-modal logic with quantifiers). 
+
+There is a Python API for the prover. (Requires docker to run the prover in a clean container.)
+
+
---
+++ packages/shadowprover/shadowprover-1.2.53.tar.gz/shadowprover-1.2.53/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "Click>=7.0",
+    "edn_format",
+    "pydantic",
+    "pyparsing",
+    "fuzzywuzzy",
+    "loguru",
+    "colorama",
+]
+
+test_requirements = []
+
+setup(
+    author="Naveen Sundar Govindarajulu",
+    author_email="naveensundarg@gmail.com",
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points={
+        "console_scripts": [
+            "shadowprover=shadowprover.cli:main",
+        ],
+    },
+    install_requires=requirements,
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    packages=find_packages(include=["shadowprover", "shadowprover.*"]),
+    include_package_data=True,  # Pull in files from MANIFEST.in
+    keywords="shadowprover",
+    name="shadowprover",
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/naveensundarg/py_laser",
+    version="1.2.53",
+    zip_safe=False,
+)

bd27c557ebb16621c0c67ffab29e8b6e44145d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 17:05:02 +0000

    Add package gestion_fechas/4.0.0/gestion_fechas-4.0.0.tar.gz

---
+++ packages/gestion_fechas/gestion_fechas-4.0.0.tar.gz/gestion_fechas-4.0.0/gestion_fechas/proceso_etl.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+from dateutil.relativedelta import relativedelta
+
+
+@dataclass
+class ProcesoETL:
+    etl_id: int
+    etl_nombre: str
+    frecuencia: relativedelta
+    offset: Optional[relativedelta] = None
+    fen: Optional[datetime] = None
+    fen_inicio: Optional[datetime] = None
+    fen_fin: Optional[datetime] = None
+    estado: str = "OK"
+    formato_fen: str = None

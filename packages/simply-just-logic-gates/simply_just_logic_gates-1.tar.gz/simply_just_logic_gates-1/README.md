5bbaef43d4f45068253187fa53dbcba3b823220e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:05:08 +0000

    Add package simply-just-logic-gates/1/simply_just_logic_gates-1.tar.gz

---
+++ packages/simply-just-logic-gates/simply_just_logic_gates-1.tar.gz/simply_just_logic_gates-1/README.md
@@ -0,0 +1,19 @@
+Simply Just Logic Gates!
+
+---
+
+**Package name:** simply_just_logic_gates
+
+**Install with:** `pip install simply-just-logic-gates`
+
+**Update with:** `pip install --upgrade simply-just-logic-gates`
+
+**Use with:** `import lg`
+
+---
+
+For a list of logic gates, use `lg.gates()`. For credits, use `lg.credits()`.
+
+---
+
+**Created by:** Error Dev **|** [https://devicals.github.io/](https://devicals.github.io/)
---
+++ packages/simply-just-logic-gates/simply_just_logic_gates-1.tar.gz/simply_just_logic_gates-1/lg/__init__.py
@@ -0,0 +1,96 @@
+#  ___                   ___           
+# | __|_ _ _ _ ___ _ _  |   \ _____ __ 
+# | _|| '_| '_/ _ \ '_| | |) / -_) V / 
+# |___|_| |_| \___/_|   |___/\___|\_/  
+#                                      
+
+# Functions =+--
+
+def credits():
+    """
+    Shows credits for simply-just-logic-gates.
+    """
+
+    print("# simply just logic gates credits")
+    print("|")
+    print("| author: Error Dev")
+    print("|")
+    print("> all code and comments in this module/package are created by the author, Error Dev")
+
+def author():
+    """
+    Lists the author and his contact information.
+    """
+
+    print("# contact information")
+    print("| name: Error Dev")
+    print("> discord: @error_dev")
+    print("> email: 3rr0r.d3v@gmail.com")
+
+def gates():
+    """
+    Lists all the logic gates avaliable.
+    """
+
+    print("# logic gates list")
+    print("| lg.and()")
+    print("|")
+    print("| lg.or()")
+    print("|")
+    print("| lg.not()")
+    print("|")
+    print("| lg.nand()")
+    print("|")
+    print("| lg.nor()")
+    print("|")
+    print("| lg.xor()")
+    print("| lg.eor()")
+    print("| (eor is the same as xor)")
+
+def and(x, y):
+    if x and y:
+        return True
+    else:
+        return False
+
+def or(x, y):
+    if x or y:
+        return True
+    else:
+        return False
+
+def not(x):
+    return not x
+
+def nand(x, y):
+    if x and y:
+        return not True
+    else:
+        return not False
+
+def nor(x, y):
+    if x or y:
+        return not True
+    else:
+        return not False
+
+def xor(x, y):
+    if x and y:
+        return False
+    elif not x and not y:
+        return False
+    else:
+        return True
+def eor(x, y):
+    if x and y:
+        return False
+    elif not x and not y:
+        return False
+    else:
+        return True
+
+#  ___                   ___           
+# | __|_ _ _ _ ___ _ _  |   \ _____ __ 
+# | _|| '_| '_/ _ \ '_| | |) / -_) V / 
+# |___|_| |_| \___/_|   |___/\___|\_/  
+#                                      

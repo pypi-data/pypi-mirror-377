f357cba0d82db15bcd57683236d6172cab776621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 17:05:10 +0000

    Add package dataio-artpark/0.4.0b4/dataio_artpark-0.4.0b4.tar.gz

---
+++ packages/dataio-artpark/dataio_artpark-0.4.0b4.tar.gz/dataio_artpark-0.4.0b4/src/dataio/__init__.py
@@ -0,0 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
+from dataio.sdk import DataIOAPI
+
+__all__ = ["DataIOAPI"]
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = version("dataio-artpark")

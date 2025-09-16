75c2abc1dbf670304e1e9da743c32567f3343419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:03:35 +0000

    Add package retrack/3.2.0/retrack-3.2.0.tar.gz

---
+++ packages/retrack/retrack-3.2.0.tar.gz/retrack-3.2.0/retrack/nodes/datetime.py
@@ -0,0 +1,35 @@
+import datetime as dt
+import typing
+
+import pandas as pd
+import pydantic
+
+from retrack.nodes.base import BaseNode, InputConnectionModel, OutputConnectionModel
+
+###############################################################
+# CurrentYear Inputs and Outputs
+###############################################################
+
+
+class CurrentYearInputsModel(pydantic.BaseModel):
+    input_void: typing.Optional[InputConnectionModel] = None
+
+
+class CurrentYearOutputsModel(pydantic.BaseModel):
+    output_value: OutputConnectionModel
+
+
+###############################################################
+# CurrentYear Node
+###############################################################
+
+
+class CurrentYear(BaseNode):
+    inputs: CurrentYearInputsModel
+    outputs: CurrentYearOutputsModel
+
+    async def run(
+        self,
+        input_void: typing.Optional[pd.Series] = None,
+    ) -> typing.Dict[str, str]:
+        return {"output_value": dt.datetime.now().year}

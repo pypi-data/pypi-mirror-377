f5eeaed5dbcb54028a1b914042d92519b684e236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:05:33 +0000

    Add package day2/0.2.7/day2-0.2.7.tar.gz

---
+++ packages/day2/day2-0.2.7.tar.gz/day2-0.2.7/day2/models/__init__.py
@@ -0,0 +1,70 @@
+"""Data models for the MontyCloud DAY2 SDK."""
+
+from day2.models.assessment import (
+    Assessment,
+    CreateAssessmentInput,
+    CreateAssessmentOutput,
+    GenerateAssessmentReportInput,
+    GenerateAssessmentReportOutput,
+    GetAssessmentOutput,
+    ListAssessmentsOutput,
+    RunAssessmentInput,
+    RunAssessmentOutput,
+)
+from day2.models.bot import (
+    ComplianceBotFinding,
+    ListComplianceBotFindingsOutput,
+    ListComplianceBotPolicyGroupsOutput,
+    ListComplianceBotResourceTypesOutput,
+    Policy,
+    ResourceType,
+)
+from day2.models.cost import GetCostByChargeTypeOutput
+from day2.models.report import (
+    DeleteReportInput,
+    DeleteReportOutput,
+    GetReportDetailsOutput,
+    GetReportOutput,
+    ListReportsOutput,
+)
+from day2.models.resource import (
+    GetInventorySummaryOutput,
+    InventorySummaryByAccountNumber,
+    InventorySummaryByRegion,
+    InventorySummaryByResourceType,
+    ListRegionsOutput,
+    ListResourceTypesOutput,
+)
+from day2.models.tenant import ListTenantsOutput, TenantDetails
+
+__all__ = [
+    "TenantDetails",
+    "ListTenantsOutput",
+    "Assessment",
+    "ListAssessmentsOutput",
+    "CreateAssessmentInput",
+    "CreateAssessmentOutput",
+    "GetAssessmentOutput",
+    "GetCostByChargeTypeOutput",
+    "RunAssessmentInput",
+    "RunAssessmentOutput",
+    "GenerateAssessmentReportInput",
+    "GenerateAssessmentReportOutput",
+    "GetReportDetailsOutput",
+    "GetReportOutput",
+    "ListReportsOutput",
+    "Policy",
+    "ResourceType",
+    "ComplianceBotFinding",
+    "ListComplianceBotFindingsOutput",
+    "ListComplianceBotResourceTypesOutput",
+    "ListComplianceBotPolicyGroupsOutput",
+    "GetInventorySummaryOutput",
+    "ListRegionsOutput",
+    "ListResourceTypesOutput",
+    "InventorySummaryByAccountNumber",
+    "InventorySummaryByRegion",
+    "InventorySummaryByResourceType",
+    "DeleteReportInput",
+    "DeleteReportOutput",
+]

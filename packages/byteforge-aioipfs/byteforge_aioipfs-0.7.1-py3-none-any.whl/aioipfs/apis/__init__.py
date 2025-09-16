e4a909047c4c2ff257803d8bd9b4631dbb591f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:14:07 +0000

    Add package byteforge-aioipfs/0.7.1/byteforge_aioipfs-0.7.1-py3-none-any.whl

---
+++ packages/byteforge-aioipfs/byteforge_aioipfs-0.7.1-py3-none-any.whl/aioipfs/apis/__init__.py
@@ -0,0 +1,240 @@
+import json
+import asyncio
+import sys
+
+from typing import Union
+
+from aiohttp.web_exceptions import (HTTPInternalServerError,
+                                    HTTPForbidden,
+                                    HTTPNotFound,
+                                    HTTPMethodNotAllowed,
+                                    HTTPBadRequest)
+from aiohttp.client_exceptions import (ClientConnectorError, ClientPayloadError,
+                                       ServerDisconnectedError)
+
+from aioipfs.helpers import decode_json
+from aioipfs.exceptions import (APIError, EndpointNotFoundError, IPFSConnectionError,
+                                InvalidCIDError, IpnsKeyError, NoSuchLinkError, NotPinnedError,
+                                PinRemoteError, RPCAccessDenied, UnknownAPIError)
+from aioipfs.util import car_decoder, have_car_decoder
+
+
+DEFAULT_TIMEOUT = 60 * 60
+
+HTTP_ERROR_CODES = [
+    HTTPInternalServerError.status_code,
+    HTTPBadRequest.status_code,
+    HTTPForbidden.status_code,
+    HTTPNotFound.status_code,
+    HTTPMethodNotAllowed.status_code
+]
+
+
+class SubAPI:
+    """
+    Master class for all classes implementing API functions
+
+    :param driver: the AsyncIPFS instance
+    """
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def url(self, path):
+        return self.driver.api_endpoint(path)
+
+    def decode_error(self, errormsg):
+        """
+        Decode a (JSON) error message from the daemon and
+        return a tuple (text_message, error_code)
+        """
+        try:
+            decoded_json = json.loads(errormsg)
+            mtype = decoded_json.get('Type')
+            assert mtype.lower() == 'error'
+            return decoded_json['Message'], decoded_json['Code']
+        except Exception:
+            return None, None
+
+    def handle_error(self, response, data: Union[str, None]):
+        """
+        When the daemon returns an HTTP status code != 200, this
+        method is called to raise an API exception.
+        """
+
+        if response.status == HTTPNotFound.status_code:
+            # 404
+            raise EndpointNotFoundError(
+                f'Endpoint for URL: {response.url} NOT FOUND'
+            )
+
+        msg, code = self.decode_error(data)
+
+        if response.status == HTTPForbidden.status_code:
+            # 403
+            raise RPCAccessDenied(str(msg))
+
+        if isinstance(msg, str) and code is not None:
+            # Check for specific errors
+
+            for errclass in [NotPinnedError,
+                             InvalidCIDError,
+                             NoSuchLinkError,
+                             IpnsKeyError,
+                             PinRemoteError]:
+                if errclass.match(msg) is True:
+                    raise errclass(
+                        code=code,
+                        message=msg,
+                        http_status=response.status
+                    )
+
+            # Otherwise raise a generic error
+            raise APIError(code=code,
+                           message=msg,
+                           http_status=response.status)
+        else:
+            raise UnknownAPIError()
+
+    async def fetch_text(self, url, params={}, timeout=DEFAULT_TIMEOUT):
+        async with self.driver.session.post(url, params=params,
+                                            auth=self.driver.auth) as response:
+            status, textdata = response.status, await response.text()
+            if status in HTTP_ERROR_CODES:
+                self.handle_error(response, textdata)
+
+            return textdata
+
+    async def fetch_raw(self, url, params={}, timeout=DEFAULT_TIMEOUT):
+        async with self.driver.session.post(url, params=params,
+                                            auth=self.driver.auth) as response:
+            status, data = response.status, await response.read()
+            if status in HTTP_ERROR_CODES:
+                self.handle_error(response, data)
+
+            return data
+
+    async def car_stream(self, url, params={}):
+        if not have_car_decoder:
+            raise Exception('The CAR decoding library is not available')
+
+        stream = car_decoder.ChunkedMemoryByteStream()
+
+        async with self.driver.session.post(url, params=params,
+                                            auth=self.driver.auth) as resp:
+            async for chunk, _ in resp.content.iter_chunks():
+                await stream.append_bytes(chunk)
+
+        await stream.mark_complete()
+
+        return stream
+
+    async def fetch_json(self, url, params={}, timeout=DEFAULT_TIMEOUT):
+        return await self.post(url, params=params, outformat='json')
+
+    async def post(self, url, data=None, headers={}, params={},
+                   outformat='text'):
+        try:
+            async with self.driver.session.post(url, data=data,
+                                                auth=self.driver.auth,
+                                                headers=headers,
+                                                params=params) as response:
+                if response.status in HTTP_ERROR_CODES:
+                    return self.handle_error(
+                        response, await response.read()
+                    )
+
+                if outformat == 'text':
+                    return await response.text()
+                elif outformat == 'json':
+                    return await response.json()
+                elif outformat == 'raw':
+                    return await response.read()
+                else:
+                    raise Exception(
+                        f'Unknown output format {outformat}')
+        except (APIError, UnknownAPIError) as apierr:
+            if self.driver.debug:
+                print(f'{url}: Post API error: {apierr}',
+                      file=sys.stderr)
+
+            raise apierr
+        except (ClientPayloadError,
+                ClientConnectorError,
+                ServerDisconnectedError) as err:
+            if self.driver.debug:
+                print(f'{url}: aiohttp error: {err}',
+                      file=sys.stderr)
+
+            return None
+
+    async def mjson_decode(self, url, method='post', data=None,
+                           params=None, headers=None,
+                           new_session=False,
+                           timeout=60.0 * 60,
+                           read_timeout=60.0 * 10):
+        """
+        Multiple JSON objects response decoder (async generator), used for
+        the API endpoints which return multiple JSON messages
+
+        :param str method: http method, get or post
+        :param data: data, for POST only
+        :param params: http params
+        """
+
+        kwargs = {'params': params if params else {}, 'auth': self.driver.auth}
+
+        if new_session is True:
+            session = self.driver.get_session(
+                conn_timeout=timeout,
+                read_timeout=read_timeout
+            )
+        else:
+            session = self.driver.session
+
+        if method not in ['get', 'post']:
+            raise ValueError('mjson_decode: unknown method')
+
+        if method == 'post':
+            if data is not None:
+                kwargs['data'] = data
+
+        if isinstance(headers, dict):
+            kwargs['headers'] = headers
+
+        try:
+            async with getattr(session, method)(url, **kwargs) as response:
+                if response.status in HTTP_ERROR_CODES:
+                    self.handle_error(response, await response.text())
+
+                async for raw_message in response.content:
+                    message = decode_json(raw_message)
+
+                    if message is not None:
+                        if 'Message' in message and 'Code' in message:
+                            self.handle_error(response, raw_message)
+                        else:
+                            yield message
+
+                    await asyncio.sleep(0)
+        except (asyncio.CancelledError,
+                asyncio.TimeoutError) as err:
+            if new_session is True:
+                await session.close()
+
+            raise err
+        except APIError as e:
+            if new_session is True:
+                await session.close()
+
+            raise e
+        except (ClientPayloadError,
+                ClientConnectorError,
+                ServerDisconnectedError):
+            if new_session is True:
+                await session.close()
+
+            raise IPFSConnectionError('Connection/payload error')
+
+        if new_session is True:
+            await session.close()
---
+++ packages/byteforge-aioipfs/byteforge_aioipfs-0.7.1-py3-none-any.whl/aioipfs/helpers.py
@@ -0,0 +1,207 @@
+import functools
+import json
+import re
+
+from multiaddr import Multiaddr  # type: ignore
+from packaging.version import Version
+from contextlib import closing
+from typing import Union
+import socket
+import posixpath
+
+from urllib.parse import quote
+
+from multiformats_cid import make_cid
+from multiformats_cid import CIDv1
+from multiformats_cid import CIDv0
+
+
+try:
+    import orjson  # type: ignore
+except ImportError:
+    have_orjson = False
+else:
+    have_orjson = True
+
+
+ARG_PARAM = 'arg'
+
+
+def boolarg(arg):
+    return str(arg).lower()
+
+
+def quote_args(*args):
+    # Used in the few cases where there are multiple 'arg=' URL params
+    # that yarl can't handle at the moment
+    quoted = ''
+
+    if len(args) > 0:
+        for arg in args:
+            quoted += '&{0}={1}'.format(ARG_PARAM, quote(str(arg)))
+        return quoted[1:]
+
+
+def quote_dict(data):
+    quoted = ''
+
+    if not isinstance(data, dict):  # pragma: no cover
+        raise ValueError('quote_dict: need dictionary')
+
+    for arg, value in data.items():
+        if isinstance(value, list):
+            for lvalue in value:
+                quoted += '&{0}={1}'.format(arg, quote(str(lvalue)))
+        elif isinstance(value, str):
+            quoted += '&{0}={1}'.format(arg, quote(value))
+        elif isinstance(value, int):
+            quoted += '&{0}={1}'.format(arg, quote(str(value)))
+        elif isinstance(value, bool):
+            quoted += '&{0}={1}'.format(arg, quote(boolarg(value)))
+
+    if len(quoted) > 0:
+        return quoted[1:]
+
+
+def decode_json(data: bytes):
+    if not isinstance(data, bytes):  # pragma: no cover
+        raise TypeError('decode_json: invalid value type')
+
+    if have_orjson:
+        return orjson.loads(data.decode())
+    else:
+        return json.loads(data.decode())
+
+
+def async_enterable(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        class AsyncEnterableInstance:
+            async def __aenter__(self):
+                self.context = await f(*args, **kwargs)
+                return await self.context.__aenter__()
+
+            async def __aexit__(self, *args, **kwargs):
+                await self.context.__aexit__(*args, **kwargs)
+
+            def __await__(self):
+                return f(*args, **kwargs).__await__()
+
+        return AsyncEnterableInstance()
+
+    return wrapper
+
+
+def maddr_tcp_explode(multi: Multiaddr) -> tuple:
+    host, port = None, 0
+
+    for proto in multi.protocols():
+        if proto.name in ['ip4', 'ip6', 'dns4', 'dns6']:
+            host = multi.value_for_protocol(proto.code)
+        if proto.name == 'tcp':
+            port = int(multi.value_for_protocol(proto.code))
+
+    return host, port
+
+
+def unusedTcpPort():
+    try:
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+            s.bind(('', 0))
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            return s.getsockname()[1]
+    except Exception:  # pragma: no cover
+        return None
+
+
+def p2p_addr_explode(addr: str) -> tuple:
+    """
+    Explode a P2P service endpoint address such as :
+
+    /p2p/12D3KooWD3bfmNbuuuVCYwkjnFt3ukm3qaB3hDED3peHHXawvRAi/x/videocall/room1/1.0.0
+    /p2p/12D3KooWD3bfmNbuuuVCYwkjnFt3ukm3qaB3hDED3peHHXawvRAi/x/test
+
+    into its components, returning a tuple in the form
+
+    (peer_id, proto_full, proto_version)
+
+    proto_full can be passed to 'ipfs p2p dial'
+    """
+
+    peer_id_re = re.compile(r'([\w]){46,59}$')
+
+    parts = addr.lstrip('/').split('/')
+    try:
+        assert parts.pop(0) == 'p2p'
+        peer_id = parts.pop(0)
+        prefix = parts.pop(0)
+        assert peer_id_re.match(peer_id)
+        assert prefix in ['x', 'y', 'z']
+
+        p_version = None
+        proto_a = [prefix]
+        proto_part = parts.pop(0)
+        proto_a.append(proto_part)
+
+        while proto_part:
+            try:
+                proto_part = parts.pop(0)
+            except IndexError:
+                break
+
+            proto_a.append(proto_part)
+
+            try:
+                v = Version(proto_part)
+            except Exception:
+                # No version
+                pass
+            else:
+                # Found a version, should be last element
+                p_version = v
+                assert len(parts) == 0
+                break
+
+        return peer_id, '/' + posixpath.join(*proto_a), p_version
+    except Exception as err:
+        raise ValueError(f'Invalid p2p endpoint addr: {err}')
+
+
+def peerid_reencode(peer_id: str,
+                    base: str = 'base36',
+                    multicodec: str = 'libp2p-key') -> Union[str, None]:
+    """
+    Encode a PeerId to a specific base
+    """
+
+    cid = make_cid(peer_id)
+    if not cid:
+        return None
+
+    if base in ['base32', 'base36']:
+        return CIDv1(multicodec, cid.multihash).encode(base).decode()
+    elif base in ['base58']:
+        return str(CIDv0(cid.multihash))
+
+    return None
+
+
+def peerid_base32(peer_id: str) -> Union[str, None]:  # pragma: no cover
+    """
+    Convert any PeerId to a CIDv1 (base32)
+    """
+    return peerid_reencode(peer_id, base='base32')
+
+
+def peerid_base36(peer_id: str) -> Union[str, None]:  # pragma: no cover
+    """
+    Convert any PeerId to a CIDv1 (base36)
+    """
+    return peerid_reencode(peer_id, base='base36')
+
+
+def peerid_base58(peer_id: str) -> Union[str, None]:  # pragma: no cover
+    """
+    Convert any PeerId to a CIDv0
+    """
+    return peerid_reencode(peer_id, base='base58')

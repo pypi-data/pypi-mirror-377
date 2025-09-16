e934cc047aabbf897f4945d0aec546382d6d8aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 06:04:12 +0000

    Add package mphot/1.2.1/mphot-1.2.1.tar.gz

---
+++ packages/mphot/mphot-1.2.1.tar.gz/mphot-1.2.1/docs/source/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+# add path to source code
+sys.path.insert(0, os.path.abspath("../src/"))
+
+project = "mphot"
+copyright = "2024, Peter Pihlmann Pedersen"
+author = "Peter Pihlmann Pedersen"
+release = "1.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_title = "mphot"
+html_static_path = ["_static"]
+html_context = {
+    # ...
+    "default_mode": "light"
+}

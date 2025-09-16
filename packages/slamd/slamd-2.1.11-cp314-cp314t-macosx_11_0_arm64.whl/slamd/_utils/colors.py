e2bae9200737404ab791ff59704041a83ebd588d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 05:03:48 +0000

    Add package slamd/2.1.11/slamd-2.1.11-cp314-cp314t-macosx_11_0_arm64.whl

---
+++ packages/slamd/slamd-2.1.11-cp314-cp314t-macosx_11_0_arm64.whl/slamd/_utils/colors.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Color:
+    """Namespace for colors in RGB format.
+
+    Not meant to be initialized.
+    """
+
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+    yellow = (255, 255, 0)
+    cyan = (0, 255, 255)
+    magenta = (255, 0, 255)
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    gray = (128, 128, 128)
+    dark_gray = (64, 64, 64)
+    light_gray = (192, 192, 192)
+    orange = (255, 128, 0)
+    pink = (255, 0, 128)
+    purple = (128, 0, 255)
+    brown = (128, 64, 0)
+    dark_green = (0, 128, 0)
+    dark_blue = (0, 0, 128)
+    dark_red = (128, 0, 0)
+    dark_yellow = (128, 128, 0)
+    dark_cyan = (0, 128, 128)
+    dark_magenta = (128, 0, 128)
+
+
+

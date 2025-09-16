1710c12da165fd5451ee44aa405203b8b84315fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:05:39 +0000

    Add package danielchen-utils/0.1.2/danielchen_utils-0.1.2.tar.gz

---
+++ packages/danielchen-utils/danielchen_utils-0.1.2.tar.gz/danielchen_utils-0.1.2/danielchen_utils/data/config.py
@@ -0,0 +1,25 @@
+import os
+# 当前文件夹上层的.evn文件
+
+EOS_APPID=os.getenv("EOS_APPID")
+EOS_SECRET=os.getenv("EOS_SECRET")
+EOS_BUCKETID=os.getenv("EOS_BUCKETID")
+EOS_URL=os.getenv("EOS_URL")
+EOS_TOKEN_URL=os.getenv("EOS_TOKEN_URL")
+EOS_SAVE_DATA_URL=os.getenv("EOS_SAVE_DATA_URL")
+EOS_ONLINE_URL=os.getenv("EOS_ONLINE_URL")
+UPLOAD_TEMP_PATH=os.getenv("UPLOAD_TEMP_PATH", '/tmp/upload_temp')
+
+MODE = os.getenv('MODE', "dev")
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT= os.getenv("REDIS_PORT")
+REDIS_PASSWORD= os.getenv("REDIS_PASSWORD", "123456")
+
+# STATIC_URL = os.getenv()
+PORT = os.getenv("PORT", "8188")
+UPLOAD_DIR = os.getenv("UPLOAD_DIR", "input")
+# LOG_DIR = os.getenv("LOG_DIR", "logs")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "info")
+
+TASK_STATUS_PREFIX = os.getenv("TASK_STATUS_PREFIX", "aigc_comfy_task_status:")

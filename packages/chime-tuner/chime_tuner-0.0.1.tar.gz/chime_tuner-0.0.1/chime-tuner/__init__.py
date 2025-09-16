ef48973b93f3ddcafbe0c57dc16a0b02a6a9ca94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:05:24 +0000

    Add package chime-tuner/0.0.1/chime_tuner-0.0.1.tar.gz

---
+++ packages/chime-tuner/chime_tuner-0.0.1.tar.gz/chime_tuner-0.0.1/chime-tuner/__init__.py
@@ -0,0 +1,4 @@
+
+print("WARNING: This import is vulnerable to dependency confusion. This attack has been prevented.")
+def hello():
+    print("HELLO WORLD")
---
+++ packages/chime-tuner/chime_tuner-0.0.1.tar.gz/chime_tuner-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-tuner',
+    packages=find_packages(include=['chime-tuner']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)

1942c66d08d3185d014e597c962e9960c834f20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:05:01 +0000

    Add package alphalens-tej/2.0.3/alphalens_tej-2.0.3.tar.gz

---
+++ packages/alphalens-tej/alphalens_tej-2.0.3.tar.gz/alphalens_tej-2.0.3/src/alphalens/__init__.py
@@ -0,0 +1,10 @@
+from . import performance
+from . import plotting
+from . import tears
+from . import utils
+
+from ._version import __version__
+
+
+
+__all__ = ["performance", "plotting", "tears", "utils"]

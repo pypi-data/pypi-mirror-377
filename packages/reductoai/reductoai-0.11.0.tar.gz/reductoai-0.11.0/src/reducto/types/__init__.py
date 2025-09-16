f4afc45020c659b6f16f22e611560ec263e8c98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:04:33 +0000

    Add package reductoai/0.11.0/reductoai-0.11.0.tar.gz

---
+++ packages/reductoai/reductoai-0.11.0.tar.gz/reductoai-0.11.0/src/reducto/types/__init__.py
@@ -0,0 +1,39 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import (
+    Upload as Upload,
+    PageRange as PageRange,
+    ParseUsage as ParseUsage,
+    BoundingBox as BoundingBox,
+    EditResponse as EditResponse,
+    ParseResponse as ParseResponse,
+    SplitCategory as SplitCategory,
+    SplitResponse as SplitResponse,
+    ExtractResponse as ExtractResponse,
+    PipelineResponse as PipelineResponse,
+    WebhookConfigNew as WebhookConfigNew,
+    ArrayExtractConfig as ArrayExtractConfig,
+    BaseProcessingOptions as BaseProcessingOptions,
+    AdvancedProcessingOptions as AdvancedProcessingOptions,
+    ExperimentalProcessingOptions as ExperimentalProcessingOptions,
+)
+from .edit_run_params import EditRunParams as EditRunParams
+from .job_get_response import JobGetResponse as JobGetResponse
+from .parse_run_params import ParseRunParams as ParseRunParams
+from .split_run_params import SplitRunParams as SplitRunParams
+from .extract_run_params import ExtractRunParams as ExtractRunParams
+from .edit_run_job_params import EditRunJobParams as EditRunJobParams
+from .pipeline_run_params import PipelineRunParams as PipelineRunParams
+from .client_upload_params import ClientUploadParams as ClientUploadParams
+from .parse_run_job_params import ParseRunJobParams as ParseRunJobParams
+from .split_run_job_params import SplitRunJobParams as SplitRunJobParams
+from .webhook_run_response import WebhookRunResponse as WebhookRunResponse
+from .edit_run_job_response import EditRunJobResponse as EditRunJobResponse
+from .extract_run_job_params import ExtractRunJobParams as ExtractRunJobParams
+from .parse_run_job_response import ParseRunJobResponse as ParseRunJobResponse
+from .split_run_job_response import SplitRunJobResponse as SplitRunJobResponse
+from .pipeline_run_job_params import PipelineRunJobParams as PipelineRunJobParams
+from .extract_run_job_response import ExtractRunJobResponse as ExtractRunJobResponse
+from .pipeline_run_job_response import PipelineRunJobResponse as PipelineRunJobResponse
---
+++ packages/reductoai/reductoai-0.11.0.tar.gz/reductoai-0.11.0/src/reducto/types/shared_params/__init__.py
@@ -0,0 +1,11 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .upload import Upload as Upload
+from .page_range import PageRange as PageRange
+from .bounding_box import BoundingBox as BoundingBox
+from .split_category import SplitCategory as SplitCategory
+from .webhook_config_new import WebhookConfigNew as WebhookConfigNew
+from .array_extract_config import ArrayExtractConfig as ArrayExtractConfig
+from .base_processing_options import BaseProcessingOptions as BaseProcessingOptions
+from .advanced_processing_options import AdvancedProcessingOptions as AdvancedProcessingOptions
+from .experimental_processing_options import ExperimentalProcessingOptions as ExperimentalProcessingOptions

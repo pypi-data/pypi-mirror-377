e2ba25ee19b5640937625a28b3d372bc5c3097be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:05:11 +0000

    Add package great-expectations-cloud/20250915.1.dev1/great_expectations_cloud-20250915.1.dev1.tar.gz

---
+++ packages/great-expectations-cloud/great_expectations_cloud-20250915.1.dev1.tar.gz/great_expectations_cloud-20250915.1.dev1/great_expectations_cloud/agent/actions/agent_action.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import datetime
+from abc import abstractmethod
+from collections.abc import Sequence
+from typing import TYPE_CHECKING, Generic, Optional, TypeVar, Union
+from uuid import UUID
+
+from pydantic.v1 import BaseModel
+
+from great_expectations_cloud.agent.models import (
+    AgentBaseExtraForbid,
+    AgentBaseExtraIgnore,
+    CreatedResource,
+)
+
+if TYPE_CHECKING:
+    from great_expectations.data_context import CloudDataContext
+
+
+class ActionResult(BaseModel):
+    id: str
+    type: str
+    created_resources: Sequence[CreatedResource]
+    job_duration: Optional[datetime.timedelta] = (  # noqa: UP045
+        None  # Python 3.8 doesn't support `X | Y` for type annotation
+    )
+
+
+_EventT = TypeVar("_EventT", bound=Union[AgentBaseExtraForbid, AgentBaseExtraIgnore])
+
+
+class AgentAction(Generic[_EventT]):
+    def __init__(
+        self, context: CloudDataContext, base_url: str, organization_id: UUID, auth_key: str
+    ):
+        self._context = context
+        self._base_url = base_url
+        self._organization_id = organization_id
+        self._auth_key = auth_key
+
+    @abstractmethod
+    def run(self, event: _EventT, id: str) -> ActionResult: ...

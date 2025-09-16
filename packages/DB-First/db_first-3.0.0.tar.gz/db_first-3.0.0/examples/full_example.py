269d1f0c827caad75c87d6a0dfdbb483151c6f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:14:12 +0000

    Add package DB-First/3.0.0/db_first-3.0.0.tar.gz

---
+++ packages/DB-First/db_first-3.0.0.tar.gz/db_first-3.0.0/examples/full_example.py
@@ -0,0 +1,83 @@
+from uuid import UUID
+
+from db_first import BaseCRUD
+from db_first.base_model import ModelMixin
+from db_first.mixins import CreateMixin
+from db_first.mixins import DeleteMixin
+from db_first.mixins import ReadMixin
+from db_first.mixins import UpdateMixin
+from marshmallow import fields
+from marshmallow import Schema
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Session
+
+engine = create_engine('sqlite://', echo=True, future=True)
+session = Session(engine)
+Base = declarative_base()
+
+
+class Items(ModelMixin, Base):
+    __tablename__ = 'items'
+    data: Mapped[str] = mapped_column(comment='Data of item.')
+
+
+Base.metadata.create_all(engine)
+
+
+class InputSchemaOfCreate(Schema):
+    data = fields.String()
+
+
+class InputSchemaOfUpdate(InputSchemaOfCreate):
+    id = fields.UUID()
+
+
+class InputSchemaOfRead(Schema):
+    id = fields.UUID()
+
+
+class OutputSchema(InputSchemaOfUpdate):
+    created_at = fields.DateTime()
+
+
+class ItemController(CreateMixin, ReadMixin, UpdateMixin, DeleteMixin, BaseCRUD):
+    class Meta:
+        session = session
+        model = Items
+        input_schema_of_create = InputSchemaOfCreate
+        input_schema_of_update = InputSchemaOfUpdate
+        output_schema_of_create = OutputSchema
+        input_schema_of_read = InputSchemaOfRead
+        output_schema_of_read = OutputSchema
+        output_schema_of_update = OutputSchema
+        schema_of_paginate = OutputSchema
+        sortable = ['created_at']
+
+
+if __name__ == '__main__':
+    item = ItemController()
+
+    first_new_item = item.create({'data': 'first'}, deserialize=True)
+    print('Item as object:', first_new_item)
+    second_new_item = item.create({'data': 'second'}, deserialize=True, serialize=True)
+    print('Item as dict:', second_new_item)
+
+    first_item = item.read({'id': first_new_item.id})
+    print('Item as object:', first_item)
+    first_item = item.read({'id': first_new_item.id})
+    print('Item as dict:', first_item)
+
+    updated_first_item = item.update(data={'id': first_new_item.id, 'data': 'updated_first'})
+    print('Item as object:', updated_first_item)
+    updated_second_item = item.update(
+        data={'id': UUID(second_new_item['id']), 'data': 'updated_second'}, serialize=True
+    )
+    print('Item as dict:', updated_second_item)
+
+    items = item.paginate(sort_created_at='desc')
+    print('Items as objects:', items)
+    items = item.paginate(sort_created_at='desc', serialize=True)
+    print('Items as dicts:', items)
---
+++ packages/DB-First/db_first-3.0.0.tar.gz/db_first-3.0.0/tests/test_crud_mixin.py
@@ -0,0 +1,139 @@
+from copy import deepcopy
+from uuid import UUID
+from uuid import uuid4
+
+import pytest
+from db_first import BaseCRUD
+from db_first import ModelMixin
+from db_first.exc import MetaNotFound
+from db_first.exc import OptionNotFound
+from db_first.mixins.crud import CreateMixin
+from db_first.mixins.crud import DeleteMixin
+from db_first.mixins.crud import ReadMixin
+from db_first.mixins.crud import UpdateMixin
+from marshmallow import fields
+from marshmallow import Schema
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from .conftest import UNIQUE_STRING
+
+
+def test_crud_mixin(fx_db_connection):
+    Base, engine, db_session = fx_db_connection
+
+    class TestModel(Base, ModelMixin):
+        __tablename__ = 'test_model'
+
+        first: Mapped[str] = mapped_column()
+
+    Base.metadata.create_all(engine)
+
+    class SchemaOfCreate(Schema):
+        first = fields.String()
+
+    class SchemaOfResultCreate(Schema):
+        id = fields.UUID()
+        first = fields.String()
+
+    class SchemaOfRead(Schema):
+        id = fields.UUID()
+
+    class TestCreate(CreateMixin, BaseCRUD):
+        class Meta:
+            model = TestModel
+            session = db_session
+            input_schema_of_create = SchemaOfCreate
+            output_schema_of_create = SchemaOfResultCreate
+
+    data_for_create = {'first': next(UNIQUE_STRING)}
+    TestCreate().create(data_for_create, serialize=True)
+    new_data = TestCreate().create(data_for_create, serialize=True)
+    new_data_for_assert = deepcopy(new_data)
+    assert new_data_for_assert.pop('id')
+    assert new_data_for_assert == data_for_create
+
+    class TestRead(ReadMixin, BaseCRUD):
+        class Meta:
+            model = TestModel
+            session = db_session
+            filterable = ['id']
+            input_schema_of_read = SchemaOfRead
+            output_schema_of_read = SchemaOfResultCreate
+
+    data_for_read = TestRead().read({'id': UUID(new_data['id'])}, serialize=True)
+    assert new_data == data_for_read
+
+    class TestUpdate(UpdateMixin, BaseCRUD):
+        class Meta:
+            model = TestModel
+            session = db_session
+            input_schema_of_update = SchemaOfResultCreate
+            output_schema_of_update = SchemaOfResultCreate
+
+    data_for_update = {'id': UUID(new_data['id']), 'first': next(UNIQUE_STRING)}
+    updated_data = TestUpdate().update(data=data_for_update, serialize=True)
+    data_for_update['id'] = str(data_for_update['id'])
+    assert updated_data == data_for_update
+
+    class TestDelete(DeleteMixin, BaseCRUD):
+        class Meta:
+            model = TestModel
+            session = db_session
+            input_schema_of_update = SchemaOfResultCreate
+            output_schema_of_update = SchemaOfResultCreate
+
+    TestDelete().delete({'id': UUID(new_data['id'])})
+
+    with pytest.raises(NoResultFound):
+        assert not TestRead().read({'id': UUID(new_data['id'])})
+
+
+def test_crud_mixin__wrong_meta(fx_db_connection):
+
+    class TestController(CreateMixin, ReadMixin, UpdateMixin, DeleteMixin, BaseCRUD):
+        pass
+
+    data_for_create = {'first': next(UNIQUE_STRING)}
+    with pytest.raises(MetaNotFound) as e:
+        TestController().create(data=data_for_create, serialize=True)
+    assert e.value.args[0] == 'You need add class Meta with options.'
+
+    with pytest.raises(MetaNotFound) as e:
+        TestController().read({'id': uuid4()}, serialize=True)
+    assert e.value.args[0] == 'You need add class Meta with options.'
+
+    data_for_update = {'id': uuid4(), 'first': next(UNIQUE_STRING)}
+    with pytest.raises(MetaNotFound) as e:
+        TestController().update(data=data_for_update, serialize=True)
+    assert e.value.args[0] == 'You need add class Meta with options.'
+
+    with pytest.raises(MetaNotFound) as e:
+        TestController().delete({'id': uuid4()})
+    assert e.value.args[0] == 'You need add class Meta with options.'
+
+
+def test_crud_mixin__wrong_options_in_meta(fx_db_connection):
+
+    class TestController(CreateMixin, ReadMixin, UpdateMixin, DeleteMixin, BaseCRUD):
+        class Meta:
+            pass
+
+    data_for_create = {'first': next(UNIQUE_STRING)}
+    with pytest.raises(OptionNotFound) as e:
+        TestController().create(data=data_for_create, deserialize=True)
+    assert e.value.args[0] == 'Option <input_schema_of_create> not set in Meta class.'
+
+    with pytest.raises(OptionNotFound) as e:
+        TestController().read({'id': uuid4()}, serialize=True)
+    assert e.value.args[0] == 'Option <session> not set in Meta class.'
+
+    data_for_update = {'id': uuid4(), 'first': next(UNIQUE_STRING)}
+    with pytest.raises(OptionNotFound) as e:
+        TestController().update(data=data_for_update, deserialize=True)
+    assert e.value.args[0] == 'Option <input_schema_of_update> not set in Meta class.'
+
+    with pytest.raises(OptionNotFound) as e:
+        TestController().delete({'id': uuid4()})
+    assert e.value.args[0] == 'Option <session> not set in Meta class.'

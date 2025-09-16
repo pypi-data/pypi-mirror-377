1d5456eb014c1e50b9eb32fc97bb8ede567ed4e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 18:05:07 +0000

    Add package mosamatic2/2.0.6/mosamatic2-2.0.6.tar.gz

---
+++ packages/mosamatic2/mosamatic2-2.0.6.tar.gz/mosamatic2-2.0.6/src/mosamatic2/core/data/dicomimage.py
@@ -0,0 +1,18 @@
+from mosamatic2.core.data.filedata import FileData
+from mosamatic2.core.utils import (
+    is_dicom,
+    load_dicom,
+    is_jpeg2000_compressed,
+)
+
+
+class DicomImage(FileData):
+    def load(self):
+        if self.path():
+            if is_dicom(self.path()):
+                p = load_dicom(self.path())
+                if is_jpeg2000_compressed(p):
+                    p.decompress()
+                self.set_object(p)
+                return True
+        return False

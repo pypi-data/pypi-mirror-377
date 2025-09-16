361ee78dd0d91cf4b1250456a32d4f3398f87c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 15:04:34 +0000

    Add package bluer-algo/4.402.1/bluer_algo-4.402.1.tar.gz

---
+++ packages/bluer-algo/bluer_algo-4.402.1.tar.gz/bluer_algo-4.402.1/bluer_algo/README/items.py
@@ -0,0 +1,25 @@
+from bluer_objects import README
+
+
+items = README.Items(
+    [
+        {
+            "name": "yolo",
+            "description": "a yolo interface.",
+            "marquee": "https://github.com/kamangir/assets/raw/main/coco128-2025-08-14-myrsky/review.png?raw=true",
+            "url": "./bluer_algo/docs/yolo",
+        },
+        {
+            "name": "tracker",
+            "marquee": "https://github.com/kamangir/assets/raw/main/tracker-camshift-2025-07-16-11-07-52-4u3nu4/tracker.gif?raw=true",
+            "description": "a visual tracker.",
+            "url": "./bluer_algo/docs/tracker",
+        },
+        {
+            "name": "image classifier",
+            "marquee": "https://github.com/kamangir/assets/raw/main/swallow-model-2025-07-11-15-04-03-2glcch/evaluation.png?raw=true",
+            "description": "an image classifier.",
+            "url": "./bluer_algo/docs/image_classifier",
+        },
+    ]
+)

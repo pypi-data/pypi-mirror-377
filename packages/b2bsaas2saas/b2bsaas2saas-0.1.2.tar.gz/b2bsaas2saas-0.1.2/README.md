ec5cd6e46fa370c15c94ec0e92bebfa920d0017f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 15:04:39 +0000

    Add package b2bsaas2saas/0.1.2/b2bsaas2saas-0.1.2.tar.gz

---
+++ packages/b2bsaas2saas/b2bsaas2saas-0.1.2.tar.gz/b2bsaas2saas-0.1.2/README.md
@@ -0,0 +1,22 @@
+# B2B SaaS to SaaS
+
+Yes, this package generates random SaaS ideas. It can be used as a command-line tool or as a library in your Python projects. 
+
+https://pypi.org/project/b2bsaas2saas/0.1.1/
+
+## Installation
+
+```bash
+pip install b2bsaas2saas
+```
+
+## Usage
+
+```python
+from b2bsaas2saas.main import get_random_saas_idea
+
+idea = get_random_saas_idea()
+print(idea)
+```
+
+License: MIT
---
+++ packages/b2bsaas2saas/b2bsaas2saas-0.1.2.tar.gz/b2bsaas2saas-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="b2bsaas2saas",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "openai"
+    ],
+    entry_points={
+        "console_scripts": [
+            "b2bsaas2saas=b2bsaas2saas.main:get_random_saas_idea"
+        ]
+    },
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A package to generate random SaaS ideas using ChatGPT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/b2bsaas2saas",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)

95bffb6973f9f940e26dd2583f978a32549416c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 15:04:47 +0000

    Add package ncnn/1.0.20250916/ncnn-1.0.20250916-cp311-cp311-win_amd64.whl

---
+++ packages/ncnn/ncnn-1.0.20250916-cp311-cp311-win_amd64.whl/ncnn/__init__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Tencent
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_11_1():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'ncnn.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_1()
+del _delvewheel_patch_1_11_1
+# end delvewheel patch
+
+from .ncnn import *
+
+__version__ = ncnn.__version__

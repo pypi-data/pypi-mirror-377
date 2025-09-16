b3986321027c69ea065532e35791dd0bc862e8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 18:05:06 +0000

    Add package ansible-creator/25.9.0/ansible_creator-25.9.0.tar.gz

---
+++ packages/ansible-creator/ansible_creator-25.9.0.tar.gz/ansible_creator-25.9.0/tests/defaults.py
@@ -0,0 +1,10 @@
+"""Constants with default values used throughout the tests."""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+
+FIXTURES_DIR = (Path(__file__).parent / "fixtures").resolve()
+
+UUID_LENGTH = 8
---
+++ packages/ansible-creator/ansible_creator-25.9.0.tar.gz/ansible_creator-25.9.0/tests/units/test_compat.py
@@ -0,0 +1,17 @@
+"""Tests for compat module."""
+
+from __future__ import annotations
+
+from ansible_creator.compat import Traversable
+
+
+def test_import() -> None:
+    """Test the import of traversable.
+
+    This is a simple test to ensure that the import of Traversable is working.
+    Since it is only imported for type checking.
+
+    Raises:
+        AssertionError: If the assertion fails.
+    """
+    assert Traversable is not None

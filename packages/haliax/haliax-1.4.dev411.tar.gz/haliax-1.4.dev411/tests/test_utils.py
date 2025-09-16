a07fc738a9341061319152f1fde701c4a27922cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 05:03:50 +0000

    Add package haliax/1.4.dev411/haliax-1.4.dev411.tar.gz

---
+++ packages/haliax/haliax-1.4.dev411.tar.gz/haliax-1.4.dev411/tests/test_utils.py
@@ -0,0 +1,28 @@
+# Copyright 2025 The Levanter Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+from typing import TypeVar
+
+import jax
+import pytest
+
+T = TypeVar("T")
+
+
+def skip_if_not_enough_devices(count: int):
+    return pytest.mark.skipif(len(jax.devices()) < count, reason=f"Not enough devices ({len(jax.devices())})")
+
+
+def has_torch():
+    try:
+        import torch  # noqa F401
+
+        return True
+    except ImportError:
+        return False
+
+
+def skip_if_no_torch(f):
+    return pytest.mark.skipif(not has_torch(), reason="torch not installed")(f)

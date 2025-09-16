1cd0d8eb94856439294758d83bce83e3444846b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:05:07 +0000

    Add package CordForge/0.3.2/cordforge-0.3.2-py3-none-any.whl

---
+++ packages/CordForge/cordforge-0.3.2-py3-none-any.whl/CordForge/__init__.py
@@ -0,0 +1,16 @@
+# Generalized imports for dev utility
+from PIL import Image
+from discord import *
+from discord.ext.commands import *
+from discord.abc import *
+from asyncio import sleep, create_task
+
+# CordForge specific
+from .cord import Cord
+from .card import Card
+from .vector2 import Vector2
+from .colors import *
+from .font import Font
+from .components import *
+from .utilities import *
+from .user import User

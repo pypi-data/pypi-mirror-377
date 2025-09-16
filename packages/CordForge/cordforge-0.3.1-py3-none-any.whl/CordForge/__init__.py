7ebd4300c17deeb1c09778c33c6e143e74fab387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:04:24 +0000

    Add package CordForge/0.3.1/cordforge-0.3.1-py3-none-any.whl

---
+++ packages/CordForge/cordforge-0.3.1-py3-none-any.whl/CordForge/__init__.py
@@ -0,0 +1,12 @@
+from sys import dont_write_bytecode
+from discord import Interaction
+from discord.ext.commands import Context
+dont_write_bytecode = True
+from .cord import Cord
+from .card import Card
+from .vector2 import Vector2
+from .colors import *
+from .font import Font
+from .components import *
+from .utilities import *
+from .user import User
---
+++ packages/CordForge/cordforge-0.3.1-py3-none-any.whl/CordForge/user.py
@@ -0,0 +1,37 @@
+from discord import Member
+from typing import Any
+
+
+class User:
+    account:Member
+    name:str
+    nickname:str
+    id:int
+
+    def __init__(_, account:Member) -> None:
+        object.__setattr__(_, "account", account)
+        object.__setattr__(_, "id", account.id)
+        _.name = account.name
+        _.nickname = account.nick
+        _.data = {}
+
+        for key, value in User.__dict__.items():
+            if not key.startswith("__") and key not in ["account", "id", "name", "nickname", "data", "add_player_trait"]:
+                _.data[key] = value
+
+
+    def __setattr__(_, name, value):
+        if name in ["account", "id"]:
+            raise AttributeError(f"Cannot modify Player.{name}. These are determined by the user's Discord profile,\
+                                 and are used by CordForge for various validations, and utilities.")
+        super().__setattr__(name, value)
+        if name not in  ["name", "nickname", "data"]:
+            print(name)
+            _.data.update({name:value})
+
+
+    @staticmethod
+    def add_trait(trait_name:str, value:Any) -> None:    
+        if not hasattr(User, trait_name):
+            print(f"Adding trait {trait_name}")
+            setattr(User, trait_name, value)

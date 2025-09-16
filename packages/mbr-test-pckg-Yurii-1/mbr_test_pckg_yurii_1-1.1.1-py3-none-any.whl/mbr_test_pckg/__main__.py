1f729e983bc11c962bf2066ba7f7862e7912bd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:05:26 +0000

    Add package mbr-test-pckg-Yurii-1/1.1.1/mbr_test_pckg_yurii_1-1.1.1-py3-none-any.whl

---
+++ packages/mbr-test-pckg-Yurii-1/mbr_test_pckg_yurii_1-1.1.1-py3-none-any.whl/mbr_test_pckg/__main__.py
@@ -0,0 +1,9 @@
+from .show import python_path
+
+def main() -> None:
+    print("=== __main__.main() - start ===")
+    python_path()
+    print("=== __main__.main() - end ===")
+
+if __name__ == "__main__":
+    main()

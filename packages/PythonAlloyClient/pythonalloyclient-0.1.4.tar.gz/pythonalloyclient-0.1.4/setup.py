0c7ed895f6980f8741e6b92fb8684bb8d9c859a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:04:17 +0000

    Add package PythonAlloyClient/0.1.4/pythonalloyclient-0.1.4.tar.gz

---
+++ packages/PythonAlloyClient/pythonalloyclient-0.1.4.tar.gz/pythonalloyclient-0.1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PythonAlloyClient',
+    version='0.1.4',
+    author='Mrigank Pawagi',
+    author_email='mrigankpawagi@gmail.com',
+    description='Python Client for the Alloy Language Server',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mrigankpawagi/PythonAlloyClient',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        dep.strip() for dep in open('requirements.txt').readlines()
+    ],
+    package_data={'PythonAlloyClient': ['resources/*']}
+)

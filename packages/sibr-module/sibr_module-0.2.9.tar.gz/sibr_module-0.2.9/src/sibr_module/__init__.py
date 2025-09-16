0e0ff8ecbb47f13472e740b051cd1ba8e0f829d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:04:49 +0000

    Add package sibr-module/0.2.9/sibr_module-0.2.9.tar.gz

---
+++ packages/sibr-module/sibr_module-0.2.9.tar.gz/sibr_module-0.2.9/src/sibr_module/__init__.py
@@ -0,0 +1,9 @@
+from .google_helpers import BigQuery, Logger, SecretsManager, CStorage,LoggerV2
+
+__all__ = [
+    "BigQuery",
+    "Logger",
+    "LoggerV2",
+    "SecretsManager",
+    "CStorage"
+]

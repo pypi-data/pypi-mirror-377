d26b525cb12bdfd1cb051db1a6130a1f4be50d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 17:05:22 +0000

    Add package draive/0.84.7/draive-0.84.7.tar.gz

---
+++ packages/draive/draive-0.84.7.tar.gz/draive-0.84.7/src/draive/openai/utils.py
@@ -0,0 +1,17 @@
+from typing import cast
+
+from haiway import MISSING, Missing
+from openai import NOT_GIVEN, NotGiven
+
+__all__ = ("unwrap_missing",)
+
+
+def unwrap_missing[Value](
+    value: Value | Missing,
+    /,
+) -> Value | NotGiven:
+    if value is MISSING:
+        return NOT_GIVEN
+
+    else:
+        return cast(Value, value)

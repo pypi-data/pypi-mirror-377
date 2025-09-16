0e2437eafeeec8baaa749947adbdaf5ffdcd5740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 15:04:22 +0000

    Add package gui-dictionary/0.1.0/gui_dictionary-0.1.0-py3-none-any.whl

---
+++ packages/gui-dictionary/gui_dictionary-0.1.0-py3-none-any.whl/gui_dictionary/main.py
@@ -0,0 +1,41 @@
+import tkinter
+from tkinter import ttk
+
+from utils.dictionary import get_antonyms, get_definition, get_synonyms
+
+
+def main():
+    root = tkinter.Tk()
+
+    root.title("Dictionary")
+    root.geometry("1080x720")
+
+    definition = ttk.Label(root, wraplength=300, justify="left")
+
+    thesaurus = ttk.Label(root)
+
+    word = tkinter.Variable()
+
+    word_input = ttk.Entry(root, textvariable=word)
+    word_input.grid(row=0, column=0, padx=5, pady=5)
+
+    define_button = ttk.Button(
+        root, text="Define!", command=lambda: get_definition(definition, word.get())
+    )
+    define_button.grid(row=0, column=1, padx=5, pady=5)
+
+    synonym_button = ttk.Button(
+        root, text="Get Synonyms!", command=lambda: get_synonyms(thesaurus, word.get())
+    )
+    synonym_button.grid(row=2, column=1, padx=5, pady=5)
+
+    antonym_button = ttk.Button(
+        root, text="Get Antonyms!", command=lambda: get_antonyms(thesaurus, word.get())
+    )
+    antonym_button.grid(row=2, column=2, padx=5, pady=5)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()

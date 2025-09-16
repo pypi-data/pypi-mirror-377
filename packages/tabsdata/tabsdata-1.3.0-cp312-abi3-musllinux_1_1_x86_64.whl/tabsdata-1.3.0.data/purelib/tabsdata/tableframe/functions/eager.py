d991debe799142761206fe266d147004d604abab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 17:05:06 +0000

    Add package tabsdata/1.3.0/tabsdata-1.3.0-cp312-abi3-musllinux_1_1_x86_64.whl

---
+++ packages/tabsdata/tabsdata-1.3.0-cp312-abi3-musllinux_1_1_x86_64.whl/tabsdata-1.3.0.data/purelib/tabsdata/tableframe/functions/eager.py
@@ -0,0 +1,125 @@
+#
+# Copyright 2025 Tabs Data Inc.
+#
+
+from __future__ import annotations
+
+from typing import Iterable
+
+import polars as pl
+
+# noinspection PyProtectedMember
+import polars._typing as pl_typing
+
+import tabsdata.tableframe.expr.expr as td_expr
+import tabsdata.tableframe.lazyframe.frame as td_frame
+
+# noinspection PyProtectedMember
+from tabsdata._utils.annotations import pydoc
+
+
+@pydoc(categories="union")
+def concat(
+    items: Iterable[td_frame.TdType],
+) -> td_frame.TdType:
+    """
+    Combine multiple TableFrames by stacking their rows.
+
+    Args:
+        items: The TableFrames to concatenate.
+
+    Example:
+
+    >>> import tabsdata as td
+    >>>
+    >>> tf1: td.TableFrame ...
+    >>>
+    ┌──────┬──────┐
+    │ a    ┆ b    │
+    │ ---  ┆ ---  │
+    │ str  ┆ i64  │
+    ╞══════╪══════╡
+    │ a    ┆ 1    │
+    │ b    ┆ 2    │
+    └──────┴──────┘
+    >>>
+    >>> tf2: td.TableFrame ...
+    >>>
+    ┌──────┬──────┐
+    │ a    ┆ b    │
+    │ ---  ┆ ---  │
+    │ str  ┆ i64  │
+    ╞══════╪══════╡
+    │ x    ┆ 10   │
+    │ y    ┆ 20   │
+    └──────┴──────┘
+    >>>
+    >>> tf = td.concat(tf1, tf2)
+    >>>
+    ┌──────┬──────┐
+    │ a    ┆ b    │
+    │ ---  ┆ ---  │
+    │ str  ┆ i64  │
+    ╞══════╪══════╡
+    │ a    ┆ 1    │
+    │ b    ┆ 2    │
+    │ x    ┆ 10   │
+    │ y    ┆ 20   │
+    └──────┴──────┘
+    """
+    unwrapped_items = [_unwrap_td_ype(item) for item in items]
+
+    # First, perform a no-op concatenation of empty LazyFrames built from the
+    # schemas of the original frames. This ensures that all schemas are compatible.
+    # Without this step, a mismatch might only surface later with a less clear error.
+    schemas = [lf.collect_schema() for lf in unwrapped_items]
+    empties = [
+        pl.DataFrame(schema={name: schema[name] for name in schema.names()})
+        for schema in schemas
+    ]
+    pl.concat(
+        empties,
+        how="vertical",
+        rechunk=False,
+        parallel=True,
+    )
+
+    polars_type = pl.concat(
+        unwrapped_items,
+        how="vertical",
+        rechunk=False,
+        parallel=True,
+    )
+    wrapped_item = _wrap_polars_type(polars_type)
+    return wrapped_item
+
+
+def _wrap_polars_type(
+    obj: pl_typing.PolarsType,
+) -> td_frame.TdType:
+    if isinstance(obj, pl.LazyFrame):
+        # noinspection PyProtectedMember
+        return td_frame.TableFrame.__build__(
+            df=obj,
+            mode="tab",
+            idx=None,
+        )
+    elif isinstance(obj, pl.Expr):
+        # noinspection PyProtectedMember
+        return td_expr.Expr(obj)
+    else:
+        return obj
+
+
+# noinspection PyTypeChecker
+def _unwrap_td_ype(
+    obj: td_frame.TdType,
+) -> pl_typing.PolarsType:
+    if isinstance(obj, td_frame.TableFrame):
+        # noinspection PyProtectedMember
+        return obj._lf
+    elif isinstance(obj, td_expr.Expr):
+        # noinspection PyProtectedMember
+        return obj._expr
+    else:
+        return obj

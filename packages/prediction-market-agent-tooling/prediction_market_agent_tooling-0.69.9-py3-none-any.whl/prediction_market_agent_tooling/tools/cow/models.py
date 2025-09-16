7b4c52e6b9ae6830c9977eda9f603e32e5b736df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:13:44 +0000

    Add package prediction-market-agent-tooling/0.69.9/prediction_market_agent_tooling-0.69.9-py3-none-any.whl

---
+++ packages/prediction-market-agent-tooling/prediction_market_agent_tooling-0.69.9-py3-none-any.whl/prediction_market_agent_tooling/tools/cow/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from sqlmodel import Field, SQLModel
+
+from prediction_market_agent_tooling.gtypes import ChecksumAddress, HexBytes
+from prediction_market_agent_tooling.tools.datetime_utc import DatetimeUTC
+from prediction_market_agent_tooling.tools.utils import utcnow
+
+
+class MinimalisticTrade(BaseModel):
+    sellToken: ChecksumAddress
+    buyToken: ChecksumAddress
+    orderUid: HexBytes
+    txHash: HexBytes
+
+
+class Order(BaseModel):
+    uid: str
+    sellToken: str
+    buyToken: str
+    creationDate: DatetimeUTC
+
+
+class RateLimit(SQLModel, table=True):
+    __tablename__ = "rate_limit"
+    __table_args__ = {"extend_existing": True}
+    id: str = Field(primary_key=True)
+    last_called_at: DatetimeUTC = Field(default_factory=utcnow)

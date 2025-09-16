078a1dca10b04bf27caf149b21b3ed1e9e4a3af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:05:38 +0000

    Add package ecl-logging-utility/1.0.17/ecl_logging_utility-1.0.17-py3-none-any.whl

---
+++ packages/ecl-logging-utility/ecl_logging_utility-1.0.17-py3-none-any.whl/ecl_logging_utility/__init__.py
@@ -0,0 +1,4 @@
+from .logging_utility import configure_logging, refresh_logger, logger, clear_contextvars, bind_contextvars
+
+__all__ = ['configure_logging', 'refresh_logger', 'logger', 'clear_contextvars', 'bind_contextvars']
+__version__ = '1.0.17'

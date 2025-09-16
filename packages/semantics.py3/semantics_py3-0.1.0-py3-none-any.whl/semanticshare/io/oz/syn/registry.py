54bc2d27ed1db03dd5c9cea1ef141ddca02efcd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 17:04:45 +0000

    Add package semantics.py3/0.1.0/semantics_py3-0.1.0-py3-none-any.whl

---
+++ packages/semantics.py3/semantics_py3-0.1.0-py3-none-any.whl/semanticshare/io/oz/syn/registry.py
@@ -0,0 +1,220 @@
+import json
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import Union, cast, Optional
+
+from anson.io.odysz.anson import Anson
+from anson.io.odysz.common import LangExt
+from semanticshare.io.odysz.semantic.jprotocol import AnsonBody, AnsonMsg, AnsonResp, JServUrl, JProtocol
+from semanticshare.io.oz.jserv.docs.syn.singleton import AppSettings
+
+from . import Synode, SyncUser
+
+
+@dataclass
+class SynOrg(Anson):
+    meta: str
+
+    orgId: str
+    orgName: str
+    # edu | org | com | ...
+    orgType: str
+    ''' This is a tree table. '''
+    parent: Optional[AnsonMsg]
+
+    fullpath: str
+    """
+    Ignored by toJson / toBlock in java
+    """
+
+    market: str
+    # web server url, configured in dictionary like: $WEB-ROOT:8888
+    webroot: str
+    # The home page url (landing page)
+    homepage: str
+    # The default resources collection, usually a group / tree of documents.
+    album0: str
+
+    def __init__(self, orgtype:str='', orgid:str='', orgname:str='', webroot:str='', homepage:str='', albumid=''):
+        super().__init__()
+        self.parent = None
+        self.fullpath = ""
+        self.orgId = orgid
+        self.orgName = orgname
+        self.orgType = orgtype
+        self.webroot = webroot
+        self.homepage = homepage
+        self.album0 = albumid
+
+
+@dataclass
+class SynodeConfig(Anson):
+    debug: bool
+    '''
+    Java but not used
+    '''
+    chsize: int
+    '''
+    Java but not used
+    '''
+
+    sysconn: str
+    synconn: str
+
+    synid: str
+    domain: str
+    mode: Union[str, None]
+    admin: str
+    org: SynOrg
+    ''' Market, organization or so? '''
+    syncIns: float
+    '''
+     * Synchronization interval, initially, in seconds.
+     * No worker thread started if less or equals 0.
+    '''
+
+    peers: list[Synode]
+
+    https: bool
+
+    def __init__(self, domain:str=None, synode:str=None,
+                 synconn:str=None, sysconn:str=None, admin:str='admin',
+                 org:SynOrg=None, peers:list=None, mode:str=None):
+        super().__init__()
+        self.https = False
+        self.synconn = synconn
+        self.sysconn = sysconn
+        self.synid = synode
+        self.admin = admin
+        self.peers = peers
+        self.mode  = mode
+        self.org   = org
+
+
+@dataclass
+class AnRegistry(Anson):
+    config: SynodeConfig
+    synusers: list[SyncUser]
+
+    def __init__(self):
+        super().__init__()
+        self.config = cast('SynodeConfig', None)
+        self.synusers = []
+
+    @staticmethod
+    def load(path) -> 'AnRegistry':
+        if Path(path).is_file():
+            with open(path, 'r', encoding="utf-8") as file:
+                obj = json.load(file)
+                obj['__type__'] = AnRegistry().__type__
+                return Anson.from_envelope(obj)
+        else:
+            raise FileNotFoundError(f"File doesn't exist: {path}")
+        
+    def find_peer(self, peerid: str):
+        return None if LangExt.isblank(peerid) or self.config.peers is None \
+            else AnRegistry.find_synode(self.config.peers, peerid) \
+    
+    @classmethod
+    def find_synode(cls, synodes: list[Synode], id):
+        if synodes is not None:
+            for peer in synodes:
+                if peer.synid == id:
+                    return peer
+        return None
+
+    @classmethod
+    def find_synuser(cls, users: list[SyncUser], id):
+        if users is not None:
+            for u in users:
+                if u.userId == id:
+                    return u
+        return None
+
+
+@dataclass()
+class Centralport(Enum):
+    heartbeat:str = "ping.serv"
+    session  :str = "login.serv"
+    register :str = "regist.serv"
+    menu     :str = "menu.serv"
+
+
+@dataclass
+class CynodeStats:
+    create :str = "c"
+    asHub  :str = "h"
+    asPeer :str = "p"
+    die    :str = "d"
+
+
+@dataclass
+class RegistReq(AnsonBody):
+    
+    class A:
+        queryDomx      = "r/domx"
+        queryDomConfig = "r/dom-config"
+        registDom      = "c/domx"
+        updateDom      = "u/domx"
+        submitSettings = "u/settings"
+
+    market: str
+    diction: Optional[SynodeConfig]
+    myjserv: Optional[JServUrl]
+    
+    def __init__(self, act: str=None, market:str=None):
+        super().__init__()
+        self.market = market
+        self.a = act
+        self.diction = None
+    
+    def dictionary(self, d: SynodeConfig):
+        self.diction = d
+        return self
+    
+    def domain(self):
+        return None if self.diction is None else \
+               self.diction.domain
+
+    def jserurl(self, https: bool, settings: AppSettings, iport: tuple[str, int]):
+        self.myjserv = JServUrl(
+            https=https,
+            ip=iport[0],
+            port=iport[1],
+            subpaths=[JProtocol.urlroot])
+
+@dataclass
+class RegistResp(AnsonResp):
+    class R:
+        ok = "ok"
+        domexists = "domexists"
+        invalid = "invalid"
+        error = "error"
+
+    r: str
+    orgDomains: list[str]
+    
+    diction: SynodeConfig
+    
+    def __init__(self):
+        super().__init__()
+        self.orgDomains = []
+    
+    def peer_ids(self):
+        return self.diction.peers if self.diction is not None else None
+    
+    def next_installing(self):
+        for p in self.diction.peers:
+            if p is not None and p.stat == CynodeStats.create:
+                return p.synid
+        return None
+
+    def domains(self):
+        return self.orgDomains if self.orgDomains is not None else []
+
+
+def loadYellowPages():
+    path = ""
+    registry = AnRegistry().load(path)
+    return registry

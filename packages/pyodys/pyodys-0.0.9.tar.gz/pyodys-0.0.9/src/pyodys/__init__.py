697cc2328e1d527a8b08325bc7714fb46a695c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:04:24 +0000

    Add package pyodys/0.0.9/pyodys-0.0.9.tar.gz

---
+++ packages/pyodys/pyodys-0.0.9.tar.gz/pyodys-0.0.9/src/pyodys/__init__.py
@@ -0,0 +1,4 @@
+from .solveurs.runge_kutta.ButcherTableau import ButcherTableau
+from .solveurs.runge_kutta.RKSolver import RKSolver, PyOdysError
+from .systemes.ODEProblem import ODEProblem 
+from .utils.interpolation import *

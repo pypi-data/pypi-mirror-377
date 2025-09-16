03c18b5b8f9bfa3d5e4a223e68ce3b3a53f59ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:05:46 +0000

    Add package lectes/0.1.1/lectes-0.1.1-py3-none-any.whl

---
+++ packages/lectes/lectes-0.1.1-py3-none-any.whl/lectes/__init__.py
@@ -0,0 +1,4 @@
+from .config.models import Rule as Rule, Configuration as Configuration
+from .engine.models import Regex as Regex
+from .scanner.scanner import Scanner as Scanner
+from .scanner.logger import LogLevel as LogLevel

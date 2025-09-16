7ada08216eef2a2a9ae3037a02641ca9e18fc492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:05:19 +0000

    Add package adv-optm/0.1.4/adv_optm-0.1.4-py3-none-any.whl

---
+++ packages/adv-optm/adv_optm-0.1.4-py3-none-any.whl/adv_optm/__init__.py
@@ -0,0 +1,17 @@
+from .optim import (
+    AdamW_adv,
+    Prodigy_adv,
+    Adopt_adv,
+    Lion_adv,
+    Lion_Prodigy_adv,
+)
+
+__all__ = [
+    "AdamW_adv",
+    "Prodigy_adv",
+    "Adopt_adv",
+    "Lion_adv",
+    "Lion_Prodigy_adv",
+]
+
+__version__ = "0.1.4"

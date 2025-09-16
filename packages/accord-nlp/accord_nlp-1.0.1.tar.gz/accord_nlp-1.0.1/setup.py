670f69d639d834de03d4e176267f870e4021ace7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 18:04:48 +0000

    Add package accord-nlp/1.0.1/accord_nlp-1.0.1.tar.gz

---
+++ packages/accord-nlp/accord_nlp-1.0.1.tar.gz/accord_nlp-1.0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    distname="accord-nlp",
+    version="1.0.1",
+    author="Hansi Hettiarachchi",
+    author_email="hansi.h.hettiarachchi@gmail.com",
+    description="ACCORD-NLP: Transformer/language model-based information extraction from regulatory text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Accord-Project/accord-nlp",
+    keywords=['NLP', 'NER', 'Relation Extraction', 'Information Extraction'],
+    packages=find_packages(exclude=("data", "data_annotation", "experiments", "data_preparation", "data_processing",
+                                    "rase-to-json", "sentence-classification", "text-to-rase")),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "scikit-learn",
+        "statsmodels",
+        "matplotlib",
+        "openpyxl",
+        "nltk",
+        "transformers==4.40.2",
+        "seqeval",
+        "tensorboardX",
+        "wandb",
+        "sentencepiece",
+        "datasets"
+        "graphviz"
+    ],
+)

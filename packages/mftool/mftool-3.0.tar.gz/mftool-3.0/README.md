c51f3c105e54e151abcec3315e5b707234996a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 15:05:04 +0000

    Add package mftool/3.0/mftool-3.0.tar.gz

---
+++ packages/mftool/mftool-3.0.tar.gz/mftool-3.0/README.md
@@ -0,0 +1,18 @@
+mftool is a library getting publicly available Mutual Funds data in India. It can be used in various types of projects which requires getting live quotes for a given mutual fund scheme or build large data sets for further data analytics.
+
+Features
+=============
+
+* Getting last updated quotes for Mutual fund scheme using scheme codes.
+* Return data in Dataframe, JSON and dictionary formats.
+* Getting quotes for all the schemes available with AMFI
+* Helper APIs to check whether a given Scheme code is correct.
+* Getting all historical NAVs for a schemes.
+* Getting list of all schemes with there Scheme codes.
+* Get daily scheme performance.
+
+
+Project Page
+=============
+https://github.com/NayakwadiS/mftool
+
---
+++ packages/mftool/mftool-3.0.tar.gz/mftool-3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="mftool",
+    version="3.0",
+    author="SujitN",
+    author_email="nayakwadi_sujit@rediffmail.com",
+    description="Library for getting real time Mutual funds info",
+    license="MIT",
+    keywords="amfi, quote, mutual-funds, funds, bse, nse, market, stock, stocks",
+    install_requires=['requests', 'bs4', 'httpx', 'pandas', 'yfinance', 'matplotlib'],
+    url="https://github.com/NayakwadiS/mftool",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_data={'': ['*.json']}
+)

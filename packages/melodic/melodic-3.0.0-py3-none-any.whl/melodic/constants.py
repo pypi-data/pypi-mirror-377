5a74cb3cc3bc32c8247a3c3507f0ac1cdd982a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:03:55 +0000

    Add package melodic/3.0.0/melodic-3.0.0-py3-none-any.whl

---
+++ packages/melodic/melodic-3.0.0-py3-none-any.whl/melodic/constants.py
@@ -0,0 +1,8 @@
+BASE_URL = "https://www.azlyrics.com"
+
+DEFAULT_USER_AGENT = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+)
+
+HTTP_STATUSES = {"NOT_FOUND": 404, "FORBIDDEN": 403}

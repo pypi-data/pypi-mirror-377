9e2f0e7ccb8dbf0973c890ec613838af85c8842f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 15:04:37 +0000

    Add package b2b-saas2saas/1.0.0/b2b_saas2saas-1.0.0-py3-none-any.whl

---
+++ packages/b2b-saas2saas/b2b_saas2saas-1.0.0-py3-none-any.whl/b2b_saas2saas/main.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+import os
+
+def get_random_saas_idea():
+
+    prompt = """
+    Generate a B2B SaaS idea and describe it with 6-8 sentences. It should be as hype as possible, I don't really care 
+    if it's technically very straightforward to do (e.g. a chatgpt wrapper). However, it should claim to solve a specific problem
+    and be coherent enough to understand. The idea should have as many of the following qualities/keywords as possible to attract
+    that sweet venture capital money (NOT necessarily ordered by importance):
+    
+    artificial intelligence (AI), machine learning (ML), deep learning (DL), natural language processing (NLP), 
+    retrieval augmented generation (RAG), large language models (LLMs), foundational models, agentic AI, generative AI,
+    multimodal AI, disruptive innovation, possibility to create shareholder value, context engineering
+    """
+
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are an expert at B2B SaaS ideation."},
+        {"role": "user", "content": prompt}
+    ],
+    temperature=1.2
+    )
+
+    return completion.choices[0].message.content

8739230581309e3ce1bd5b33a062abd1de661de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 17:05:20 +0000

    Add package refpy/0.1.26/refpy-0.1.26.tar.gz

---
+++ packages/refpy/refpy-0.1.26.tar.gz/refpy-0.1.26/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="refpy",
+    version="0.1.26",
+    description="Package for subsea pipelines and risers design in Python",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/refpy/refpy",
+    author="ismael-ripoll",
+    license="MIT License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires='>=3.10',
+    install_requires=[],
+    project_urls={
+        "Documentation": "https://refpy.github.io/refpy",
+        "Source": "https://github.com/refpy/refpy",
+        "Tracker": "https://github.com/refpy/refpy/issues",
+    }
+)

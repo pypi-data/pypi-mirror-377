75336309f4b1ad2a7dfa48ae7c135165f4ed3404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:04:52 +0000

    Add package transformer-thermal-model/0.2.0/transformer_thermal_model-0.2.0-py3-none-any.whl

---
+++ packages/transformer-thermal-model/transformer_thermal_model-0.2.0-py3-none-any.whl/transformer_thermal_model/transformer/enums/transformer_type.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: Contributors to the Transformer Thermal Model project
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from enum import StrEnum
+
+
+class TransformerType(StrEnum):
+    """A type of transformer.
+
+    Can be handy to quickly identify the type of transformer supported by the
+    Transformer Thermal Model.
+
+    Attributes:
+        POWER (str): Power transformer.
+        DISTRIBUTION (str): Distribution transformer.
+    """
+
+    POWER = "power"
+    DISTRIBUTION = "distribution"

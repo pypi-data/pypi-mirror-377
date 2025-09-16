29ad434d81add07eb26df69577976b4074d5c8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 15:04:41 +0000

    Add package maleo-types-enums/0.0.9/maleo_types_enums-0.0.9-py3-none-any.whl

---
+++ packages/maleo-types-enums/maleo_types_enums-0.0.9-py3-none-any.whl/maleo/types/enums/status.py
@@ -0,0 +1,9 @@
+from typing import List, Optional, Sequence
+from maleo.enums.status import DataStatus
+
+
+OptionalDataStatus = Optional[DataStatus]
+ListOfDataStatuses = List[DataStatus]
+OptionalListOfDataStatuses = Optional[ListOfDataStatuses]
+SequenceOfDataStatuses = Sequence[DataStatus]
+OptionalSequenceOfDataStatuses = Optional[SequenceOfDataStatuses]

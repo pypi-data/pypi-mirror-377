6fc5ce05c3f27ae7b4464b3581a8a23a915a20ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 16:05:19 +0000

    Add package lectes/0.1.0/lectes-0.1.0.tar.gz

---
+++ packages/lectes/lectes-0.1.0.tar.gz/lectes-0.1.0/README.md
@@ -0,0 +1,2 @@
+# lectes
+Python lexer generator
---
+++ packages/lectes/lectes-0.1.0.tar.gz/lectes-0.1.0/src/lectes/config/models.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from lectes.engine.models import Regex
+
+
+@dataclass
+class Rule:
+    """
+    Represents a scanner configuration rule.
+
+    Is actually a proxy for a regular expression.
+
+    ## Example
+
+    ```python
+    from lectes import Regex
+
+    Rule(name="INT_LITERAL", regex=Regex("0|([-]?[1-9]+[0-9]*))
+    ```
+    """
+
+    name: str
+    regex: Regex
+
+
+@dataclass
+class Configuration:
+    """
+    Represents the configured rules of the scanner.
+    """
+
+    rules: list[Rule]

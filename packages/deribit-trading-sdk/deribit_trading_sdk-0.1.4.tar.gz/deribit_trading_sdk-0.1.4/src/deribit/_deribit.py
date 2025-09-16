574f85b0a828acddbb75214c098fcdf91772935d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:14:22 +0000

    Add package deribit-trading-sdk/0.1.4/deribit_trading_sdk-0.1.4.tar.gz

---
+++ packages/deribit-trading-sdk/deribit_trading_sdk-0.1.4.tar.gz/deribit_trading_sdk-0.1.4/src/deribit/_deribit.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from functools import cached_property
+
+from . import MarketData, Trading, Account, Wallet, Subscriptions
+
+@dataclass(frozen=True)
+class Deribit(
+  MarketData,
+  Trading,
+  Account,
+  Wallet,
+):
+  @cached_property
+  def subscriptions(self) -> Subscriptions:
+    from deribit.core import AuthedSocketClient, UserError
+    if isinstance(self.client, AuthedSocketClient):
+      return Subscriptions(self.client)
+    else:
+      raise UserError('Use WebSockets to access subscriptions')
+
+  

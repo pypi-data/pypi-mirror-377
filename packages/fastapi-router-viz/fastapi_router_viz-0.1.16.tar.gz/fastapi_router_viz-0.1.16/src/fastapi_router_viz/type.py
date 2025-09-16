baa1997923a467608378a53198313f01da706620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:05:04 +0000

    Add package fastapi-router-viz/0.1.16/fastapi_router_viz-0.1.16.tar.gz

---
+++ packages/fastapi-router-viz/fastapi_router_viz-0.1.16.tar.gz/fastapi_router_viz-0.1.16/src/fastapi_router_viz/type.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class FieldInfo:
+    name: str
+    type_name: str
+
+@dataclass
+class Tag:
+    id: str
+    name: str
+
+@dataclass
+class Route:
+    id: str
+    name: str
+
+@dataclass
+class SchemaNode:
+    id: str
+    module: str
+    name: str
+    fields: list[FieldInfo]
+
+@dataclass
+class ModuleNode:
+    name: str
+    fullname: str
+    schema_nodes: list[SchemaNode]
+    modules: list['ModuleNode']
+
+@dataclass
+class Link:
+    source: str
+    source_origin: str  # internal relationship
+    target: str
+    target_origin: str
+    type: Literal['child', 'parent', 'entry', 'subset']

537723358eb6272a769b22fb67cfb8f3ef3dc3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:14:47 +0000

    Add package django-spire/0.15.11/django_spire-0.15.11.tar.gz

---
+++ packages/django-spire/django_spire-0.15.11.tar.gz/django_spire-0.15.11/django_spire/core/management/commands/spire_startapp_pkg/template/app/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SpireChildAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    label = 'spireparentapp_spirechildapp'
+    name = 'module'

9b988516989489ba386c23a0b664cea18618990f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 03:14:08 +0000

    Add package aiobmsble/0.4/aiobmsble-0.4.tar.gz

---
+++ packages/aiobmsble/aiobmsble-0.4.tar.gz/aiobmsble-0.4/tests/test_plugins.py
@@ -0,0 +1,40 @@
+"""Test the aiobmsble library base class functions."""
+
+from types import ModuleType
+
+from aiobmsble.basebms import BaseBMS
+from aiobmsble.utils import bms_supported, load_bms_plugins
+
+
+def test_device_info(plugin_fixture: ModuleType) -> None:
+    """Test that the BMS returns valid device information."""
+    bms_class: type[BaseBMS] = plugin_fixture.BMS
+    result: dict[str, str] = bms_class.device_info()
+    assert "manufacturer" in result
+    assert "model" in result
+
+
+def test_matcher_dict(plugin_fixture: ModuleType) -> None:
+    """Test that the BMS returns BT matcher."""
+    bms_class: type[BaseBMS] = plugin_fixture.BMS
+    assert len(bms_class.matcher_dict_list())
+
+
+def test_advertisements_unique(bms_advertisements) -> None:
+    """Check that each advertisement only matches one, the right BMS."""
+    for adv, bms_real, _comments in bms_advertisements:
+        for bms_under_test in load_bms_plugins():
+            supported: bool = bms_supported(bms_under_test.BMS, adv)
+            assert supported == (
+                f"aiobmsble.bms.{bms_real}" == bms_under_test.__name__
+            ), f"{adv} {"incorrectly matches"if supported else "does not match"} {bms_under_test}!"
+
+
+def test_advertisements_ignore(ignore_advertisements) -> None:
+    """Check that each advertisement only matches one, the right BMS."""
+    for adv, reason, _comments in ignore_advertisements:
+        for bms_under_test in load_bms_plugins():
+            supported: bool = bms_supported(bms_under_test.BMS, adv)
+            assert (
+                not supported
+            ), f"{adv} incorrectly matches {bms_under_test}! {reason=}"

d59d001cb197f775e12f1daacd4da442c8322a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 04:03:37 +0000

    Add package kwimage-ext/0.3.1/kwimage_ext-0.3.1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/kwimage-ext/kwimage_ext-0.3.1-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/kwimage_ext/__init__.py
@@ -0,0 +1,21 @@
+"""
+Binary extensions for kwimage
+
++------------------+-----------------------------------------------------------+
+| Read the docs    | https://kwimage_ext.readthedocs.io                        |
++------------------+-----------------------------------------------------------+
+| Gitlab (main)    | https://gitlab.kitware.com/computer-vision/kwimage_ext    |
++------------------+-----------------------------------------------------------+
+| Pypi             | https://pypi.org/project/kwimage_ext                      |
++------------------+-----------------------------------------------------------+
+
+"""
+__version__ = '0.3.1'
+
+__author__ = 'Kitware Inc., Jon Crall'
+__author_email__ = 'kitware@kitware.com, jon.crall@kitware.com'
+__url__ = 'https://gitlab.kitware.com/computer-vision/kwimage_ext'
+
+__mkinit__ = """
+mkinit -m kwimage_ext
+"""

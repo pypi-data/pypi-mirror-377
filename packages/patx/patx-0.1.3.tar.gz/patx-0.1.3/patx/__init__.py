54993de07136afb0d77ea2131df9fd234f5c6ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 17:04:46 +0000

    Add package patx/0.1.3/patx-0.1.3.tar.gz

---
+++ packages/patx/patx-0.1.3.tar.gz/patx-0.1.3/patx/__init__.py
@@ -0,0 +1,24 @@
+"""
+PatX - Pattern eXtraction for Time Series Feature Engineering
+
+A Python package for extracting polynomial patterns from time series data
+to create meaningful features for machine learning models.
+"""
+
+from .core import PatternOptimizer
+from .models import LightGBMModel, XGBoostModel, get_model, evaluate_model_performance
+from .data import load_mitbih_data, load_remc_data
+
+__version__ = "0.1.4"
+__author__ = "Jonas Wolber"
+__email__ = "jonascw@web.de"
+
+__all__ = [
+    "PatternOptimizer",
+    "LightGBMModel",
+    "XGBoostModel",
+    "get_model",
+    "evaluate_model_performance",
+    "load_mitbih_data",
+    "load_remc_data"
+]

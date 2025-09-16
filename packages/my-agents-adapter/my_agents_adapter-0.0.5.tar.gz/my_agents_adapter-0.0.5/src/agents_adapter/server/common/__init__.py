4919066e2294b21e7e763eab6ddd73335fa1b3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-16 14:04:08 +0000

    Add package my-agents-adapter/0.0.5/my_agents_adapter-0.0.5.tar.gz

---
+++ packages/my-agents-adapter/my_agents_adapter-0.0.5.tar.gz/my_agents_adapter-0.0.5/src/agents_adapter/server/common/__init__.py
@@ -0,0 +1,7 @@
+# Package initializer for agents_adapter.server.common
+# This file makes the 'common' folder a Python package so it is included in distributions.
+
+__all__ = [
+    "agent_run_context",
+    "id_generator",
+]

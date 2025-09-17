fe07fc64cde082f98edbe482df85e99693677d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:14:35 +0000

    Add package picterra/2.1.6/picterra-2.1.6.tar.gz

---
+++ packages/picterra/picterra-2.1.6.tar.gz/picterra-2.1.6/src/picterra/tracer_client.py
@@ -0,0 +1,171 @@
+"""
+Handles interfacing with the API documented at https://app.picterra.ch/public/apidocs/plots_analysis/v1/
+
+Note that Tracer is separate from Forge and so an API key which is valid for
+one may encounter permissions issues if used with the other
+"""
+import datetime
+import json
+import os.path
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Any, Dict, List, Literal, Optional
+else:
+    from typing_extensions import Literal
+    from typing import Dict, List, Any, Optional
+
+import requests
+
+from picterra.base_client import APIError, BaseAPIClient
+
+AnalysisMethodology = Literal["eudr_cocoa", "eudr_soy"]
+
+
+def _check_resp_is_ok(resp: requests.Response, msg: str) -> None:
+    if not resp.ok:
+        raise APIError("%s (status %d): %s" % (msg, resp.status_code, resp.text))
+
+
+class TracerClient(BaseAPIClient):
+    def __init__(self, **kwargs):
+        super().__init__("public/api/plots_analysis/v1/", **kwargs)
+
+    def create_plots_group(
+            self,
+            plots_group_name: str,
+            methodology: AnalysisMethodology,
+            plots_geometries_filenames: List[str],
+            columns: Optional[Dict[str, str]] = None
+    ) -> str:
+        """
+        Creates a new plots group.
+
+        Args:
+            plots_group_name: user-friendly name for the group
+            methodology: plots group methodology
+            plots_geometries_filenames: Paths to files containing the geometries of the plots the group will have
+            columns: columns to add to the group. if any
+
+        Returns:
+            str: the id of the new group.
+        """
+        data = {
+            "name": plots_group_name,
+            "methodology": methodology,
+            "custom_columns_values": columns
+        }
+        resp = self.sess.post(self._full_url("plots_groups/"), json=data)
+        _check_resp_is_ok(resp, "Failure starting plots group commit")
+        op_result = self._wait_until_operation_completes(resp.json())["results"]
+        self.update_plots_group_plots(op_result["plots_group_id"], plots_geometries_filenames)
+        return op_result["plots_group_id"]
+
+    def update_plots_group_plots(self, plots_group_id: str, plots_geometries_filenames: List[str], delete_existing_plots: bool = False) -> Dict[str, Any]:
+        """
+        Updates the geometries of a given plots group
+
+        Args:
+            plots_group_id: identifier for the plots group to replace
+            plots_geometries_filenames: List of paths to files containing the geometries of the plots the group will have
+            delete_existing_plots: If true, will remove all existing plots in the plots group before uploading new ones.
+                If False (default), plot data uploaded is merged with existing plots.
+
+        Returns:
+            dict: The creation operation result, which includes the plot group id
+        """
+        files = []
+        for filename in plots_geometries_filenames:
+            resp = self.sess.post(self._full_url("upload/file/"))
+            _check_resp_is_ok(resp, "Failure obtaining upload URL and ID")
+            upload_id = resp.json()["upload_id"]
+            upload_url = resp.json()["upload_url"]
+            with open(filename, "rb") as fh:
+                resp = requests.put(upload_url, data=fh.read())
+                _check_resp_is_ok(resp, "Failure uploading plots file for group")
+                files.append({"filename": os.path.basename(filename), "upload_id": upload_id})
+        data = {"files": files, "overwrite": delete_existing_plots}
+        resp = self.sess.post(self._full_url(f"plots_groups/{plots_group_id}/upload/commit/"), json=data)
+        _check_resp_is_ok(resp, "Failure starting plots group update:")
+        return self._wait_until_operation_completes(resp.json())
+
+    def analyze_plots_precheck(
+        self,
+        plots_group_id: str,
+        plots_analysis_name: str,
+        plot_ids: List[str],
+        date_from: datetime.date,
+        date_to: datetime.date
+    ) -> str:
+        """
+        Check the analysis for a given date over the plot ids of the specified plot group has no errors
+
+        Args:
+            plots_group_id: id of the plots group on which we want to run the new analysis
+            plots_analysis_name: name to give to the new analysis
+            plot_ids: list of the plot ids of the plots group to select for the analysis
+            date_from: start point in time at which the analysis should be evaluated.
+            date_to: end point in time at which the analysis should be evaluated.
+
+        Returns:
+            str: the analysis precheck data URL.
+        """
+        resp = self.sess.post(self._full_url("upload/file/"))
+        _check_resp_is_ok(resp, "Failure obtaining an upload")
+        upload_id, upload_url = resp.json()["upload_id"], resp.json()["upload_url"]
+        resp = requests.put(upload_url, data=json.dumps({"plot_ids": plot_ids}))
+        _check_resp_is_ok(resp, "Failure uploading plots file for analysis")
+        data = {
+            "analysis_name": plots_analysis_name,
+            "upload_id": upload_id,
+            "date_from": date_from.isoformat(),
+            "date_to": date_to.isoformat()
+        }
+        resp = self.sess.post(self._full_url(f"plots_groups/{plots_group_id}/analysis/precheck/"), json=data)
+        _check_resp_is_ok(resp, "Failure starting analysis precheck")
+        op_result = self._wait_until_operation_completes(resp.json())
+        return op_result["results"]["precheck_data_url"]
+
+    def analyze_plots(
+        self,
+        plots_group_id: str,
+        plots_analysis_name: str,
+        plot_ids: List[str],
+        date_from: datetime.date,
+        date_to: datetime.date
+    ) -> dict:
+        """
+        Runs the analysis for a given date over the plot ids of the specified plot group,
+        and returns the URL where we can see the analysis in the Picterra platform.
+
+        Args:
+            plots_group_id: id of the plots group on which we want to run the new analysis
+            plots_analysis_name: name to give to the new analysis
+            plot_ids: list of the plot ids of the plots group to select for the analysis
+            date_from: start point in time at which the analysis should be evaluated.
+            date_to: end point in time at which the analysis should be evaluated.
+
+        Returns:
+            dict: the analysis metadata.
+        """
+        resp = self.sess.post(self._full_url("upload/file/"))
+        _check_resp_is_ok(resp, "Failure obtaining an upload")
+        upload_id, upload_url = resp.json()["upload_id"], resp.json()["upload_url"]
+        resp = requests.put(upload_url, data=json.dumps({"plot_ids": plot_ids}))
+        _check_resp_is_ok(resp, "Failure uploading plots file for analysis")
+        data = {
+            "analysis_name": plots_analysis_name,
+            "upload_id": upload_id,
+            "date_from": date_from.isoformat(),
+            "date_to": date_to.isoformat()
+        }
+        resp = self.sess.post(self._full_url(f"plots_groups/{plots_group_id}/analysis/"), json=data)
+        _check_resp_is_ok(resp, "Couldn't start analysis")
+        op_result = self._wait_until_operation_completes(resp.json())
+        analysis_id = op_result["results"]["analysis_id"]
+        resp = self.sess.get(
+            self._full_url(f"plots_groups/{plots_group_id}/analysis/{analysis_id}/")
+        )
+        _check_resp_is_ok(resp, f"Failure to get analysis {analysis_id}")
+        analysis_data = resp.json()
+        return analysis_data
---
+++ packages/picterra/picterra-2.1.6.tar.gz/picterra-2.1.6/tests/test_tracer_client.py
@@ -0,0 +1,201 @@
+import datetime
+import json
+import os
+import tempfile
+
+import responses
+
+from picterra import TracerClient
+from tests.utils import (
+    OP_RESP,
+    OPERATION_ID,
+    _add_api_response,
+    _client,
+    plots_analysis_api_url,
+)
+
+
+def test_plots_analysis_platform_client_base_url(monkeypatch):
+    """
+    Sanity-check that the client defaults to the correct base url
+    """
+    monkeypatch.setenv("PICTERRA_API_KEY", "1234")
+    client = TracerClient()
+    assert client.base_url == "https://app.picterra.ch/public/api/plots_analysis/v1/"
+
+
+@responses.activate
+def test_create_plots_group(monkeypatch):
+    _add_api_response(
+        plots_analysis_api_url("upload/file/"),
+        responses.POST,
+        {
+            "upload_id": "an-upload",
+            "upload_url": "https://upload.example.com/",
+        },
+    )
+    responses.put("https://upload.example.com/")
+    _add_api_response(
+        plots_analysis_api_url("plots_groups/"),
+        responses.POST,
+        OP_RESP,
+        match=responses.matchers.json_params_matcher({
+            "name": "name of my plot group",
+            "methodology": "eudr_cocoa",
+            "custom_columns_values": {"foo": "bar"}
+        }),
+    )
+    _add_api_response(plots_analysis_api_url(f"operations/{OPERATION_ID}/"), responses.GET, {
+        "status": "success",
+        "results": {"plots_group_id": "a-plots-group"}
+    })
+    client: TracerClient = _client(monkeypatch, platform="plots_analysis")
+    with tempfile.NamedTemporaryFile() as tmp:
+        _add_api_response(plots_analysis_api_url(
+            "plots_groups/a-plots-group/upload/commit/"),
+            responses.POST,
+            OP_RESP,
+            match=responses.matchers.json_params_matcher({
+                "files": [
+                    {
+                        "upload_id": "an-upload",
+                        "filename": os.path.basename(tmp.name),
+                    }
+                ],
+                "overwrite": False,
+            }),
+        )
+        with open(tmp.name, "w") as f:
+            json.dump({"type": "FeatureCollection", "features": []}, f)
+        assert client.create_plots_group(
+            "name of my plot group",
+            "eudr_cocoa",
+            [tmp.name],
+            {"foo": "bar"},
+        ) == "a-plots-group"
+
+
+@responses.activate
+def test_update_plots_group_plots(monkeypatch):
+    _add_api_response(
+        plots_analysis_api_url("upload/file/"),
+        responses.POST,
+        {
+            "upload_id": "an-upload",
+            "upload_url": "https://upload.example.com/",
+        },
+    )
+    responses.put("https://upload.example.com/")
+    _add_api_response(plots_analysis_api_url(f"operations/{OPERATION_ID}/"), responses.GET, {
+        "status": "success",
+        "results": {
+            "plots_group_id": "group-id",
+        }
+    })
+    client: TracerClient = _client(monkeypatch, platform="plots_analysis")
+    with tempfile.NamedTemporaryFile() as tmp:
+        _add_api_response(plots_analysis_api_url(
+            "plots_groups/group-id/upload/commit/"),
+            responses.POST,
+            OP_RESP,
+            match=responses.matchers.json_params_matcher({
+                "files": [
+                    {
+                        "filename": os.path.basename(tmp.name),
+                        "upload_id": "an-upload",
+                    }
+                ],
+                "overwrite": False,
+            }),
+        )
+        with open(tmp.name, "w") as f:
+            json.dump({"type": "FeatureCollection", "features": []}, f)
+        client.update_plots_group_plots("group-id", [tmp.name])
+
+
+@responses.activate
+def test_analyse_plots(monkeypatch):
+    _add_api_response(
+        plots_analysis_api_url("upload/file/"),
+        responses.POST,
+        {
+            "upload_id": "an-upload-id",
+            "upload_url": "https://upload.example.com/",
+        },
+    )
+    responses.put("https://upload.example.com/", match=[responses.matchers.json_params_matcher({
+        "plot_ids": ["uno", "dos"],
+    })])
+    _add_api_response(plots_analysis_api_url(
+        "plots_groups/a-group-id/analysis/"),
+        responses.POST,
+        OP_RESP,
+        match=responses.matchers.json_params_matcher({
+            "analysis_name": "foobar",
+            "upload_id": "an-upload-id",
+            "date_from": "2023-01-01",
+            "date_to": "2025-01-01",
+        }),
+    )
+    _add_api_response(plots_analysis_api_url(f"operations/{OPERATION_ID}/"), responses.GET, {
+        "status": "success",
+        "results": {"analysis_id": "an-analysis-id"}
+    })
+    _add_api_response(
+        plots_analysis_api_url("plots_groups/a-group-id/analysis/an-analysis-id/"),
+        responses.GET,
+        {"url": "http://analysis.example.com"}
+    )
+    client: TracerClient = _client(monkeypatch, platform="plots_analysis")
+    with tempfile.NamedTemporaryFile() as tmp:
+        with open(tmp.name, "w") as f:
+            json.dump({"type": "FeatureCollection", "features": []}, f)
+    assert client.analyze_plots(
+        "a-group-id",
+        "foobar",
+        ["uno", "dos"],
+        datetime.date.fromisoformat("2023-01-01"),
+        datetime.date.fromisoformat("2025-01-01")
+    )["url"] == "http://analysis.example.com"
+
+
+
+@responses.activate
+def test_analyse_precheck(monkeypatch):
+    _add_api_response(
+        plots_analysis_api_url("upload/file/"),
+        responses.POST,
+        {
+            "upload_id": "an-upload-id",
+            "upload_url": "https://upload.example.com/",
+        },
+    )
+    responses.put("https://upload.example.com/", match=[responses.matchers.json_params_matcher({
+        "plot_ids": ["uno", "dos"],
+    })])
+    _add_api_response(plots_analysis_api_url(
+        "plots_groups/a-group-id/analysis/precheck/"),
+        responses.POST,
+        OP_RESP,
+        match=responses.matchers.json_params_matcher({
+            "analysis_name": "foobar",
+            "upload_id": "an-upload-id",
+            "date_from": "2023-01-01",
+            "date_to": "2025-01-01",
+        }),
+    )
+    _add_api_response(plots_analysis_api_url(f"operations/{OPERATION_ID}/"), responses.GET, {
+        "status": "success",
+        "results": {"precheck_data_url": "https://precheck_data_url.example.com/"}
+    })
+    client: TracerClient = _client(monkeypatch, platform="plots_analysis")
+    with tempfile.NamedTemporaryFile() as tmp:
+        with open(tmp.name, "w") as f:
+            json.dump({"type": "FeatureCollection", "features": []}, f)
+    assert client.analyze_plots_precheck(
+        "a-group-id",
+        "foobar",
+        ["uno", "dos"],
+        datetime.date.fromisoformat("2023-01-01"),
+        datetime.date.fromisoformat("2025-01-01")
+    ) == "https://precheck_data_url.example.com/"

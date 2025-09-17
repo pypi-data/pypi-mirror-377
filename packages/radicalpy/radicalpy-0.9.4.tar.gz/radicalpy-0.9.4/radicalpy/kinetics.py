e34431d367d1cf8bc7385541aaed03bb1c22b1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:05:25 +0000

    Add package radicalpy/0.9.4/radicalpy-0.9.4.tar.gz

---
+++ packages/radicalpy/radicalpy-0.9.4.tar.gz/radicalpy-0.9.4/radicalpy/kinetics.py
@@ -0,0 +1,166 @@
+"""Kinetics classes."""
+
+import numpy as np
+
+from .simulation import (
+    HilbertIncoherentProcessBase,
+    LiouvilleIncoherentProcessBase,
+    LiouvilleSimulation,
+    State,
+)
+
+
+class HilbertKineticsBase(HilbertIncoherentProcessBase):
+    """Base class for kinetics operators (Hilbert space)."""
+
+    def _name(self):
+        name = super()._name()
+        return f"Kinetics: {name}"
+
+
+class LiouvilleKineticsBase(LiouvilleIncoherentProcessBase):
+    """Base class for kinetics superoperators (Liouville space)."""
+
+    def _name(self):
+        name = super()._name()
+        return f"Kinetics: {name}"
+
+
+class Exponential(HilbertKineticsBase):
+    """Exponential model kinetics operator.
+
+    Source: `Kaptein et al. Chem. Phys. Lett. 4, 4, 195-197 (1969)`_.
+
+    >>> Exponential(rate_constant=1e6)
+    Kinetics: Exponential
+    Rate constant: 1000000.0
+
+    .. _Kaptein et al. Chem. Phys. Lett. 4, 4, 195-197 (1969):
+       https://doi.org/10.1016/0009-2614(69)80098-9
+    """
+
+    def adjust_product_probabilities(
+        self,
+        product_probabilities: np.ndarray,
+        time: np.ndarray,
+    ):
+        """See `radicalpy.simulation.HilbertIncoherentProcessBase.adjust_product_probabilities`."""
+        product_probabilities *= np.exp(-self.rate * time)
+
+
+class Haberkorn(LiouvilleKineticsBase):
+    """Haberkorn kinetics superoperator for singlet/triplet recombination/product formation.
+
+    Source: `Haberkorn, Mol. Phys. 32:5, 1491-1493 (1976)`_.
+
+    Args:
+        rate_constant (float): The kinetic rate constant (1/s).
+        target (State): The target state of the reaction pathway
+            (singlet or triplet states only).
+
+    >>> Haberkorn(rate_constant=1e6, target=State.SINGLET)
+    Kinetics: Haberkorn
+    Rate constant: 1000000.0
+    Target: S
+
+    >>> Haberkorn(rate_constant=1e6, target=State.TRIPLET)
+    Kinetics: Haberkorn
+    Rate constant: 1000000.0
+    Target: T
+
+    .. _Haberkorn, Mol. Phys. 32:5, 1491-1493 (1976):
+       http://dx.doi.org/10.1080/00268977600102851
+    """
+
+    def __init__(self, rate_constant: float, target: State):
+        super().__init__(rate_constant)
+        self.target = target
+        if target not in {
+            State.SINGLET,
+            State.TP_SINGLET,
+            State.TRIPLET,
+            State.TRIPLET_MINUS,
+            State.TRIPLET_PLUS,
+            State.TRIPLET_PLUS_MINUS,
+            State.TRIPLET_ZERO,
+        }:
+            raise ValueError(
+                "Haberkorn kinetics supports only SINGLET and TRIPLET targets"
+            )
+
+    def init(self, sim: LiouvilleSimulation):
+        """See `radicalpy.simulation.HilbertIncoherentProcessBase.init`."""
+        Q = sim.projection_operator(self.target)
+        self.subH = 0.5 * self.rate * sim._convert(Q)
+
+    def __repr__(self):
+        lines = [
+            super().__repr__(),
+            f"Target: {self.target.value}",
+        ]
+        return "\n".join(lines)
+
+
+class HaberkornFree(LiouvilleKineticsBase):
+    """Haberkorn kinetics superoperator for free radical/RP2 formation.
+
+    Source: `Haberkorn, Mol. Phys. 32:5, 1491-1493 (1976)`_.
+
+    >>> HaberkornFree(rate_constant=1e6)
+    Kinetics: HaberkornFree
+    Rate constant: 1000000.0
+
+    .. _Haberkorn, Mol. Phys. 32:5, 1491-1493 (1976):
+       http://dx.doi.org/10.1080/00268977600102851
+    """
+
+    def init(self, sim: LiouvilleSimulation):
+        """See `radicalpy.simulation.HilbertIncoherentProcessBase.init`."""
+        self.subH = self.rate * np.eye(sim.hamiltonian_size)
+
+
+class JonesHore(LiouvilleKineticsBase):
+    """Jones-Hore kinetics superoperator for two-site models.
+
+    Source: `Jones et al. Chem. Phys. Lett. 507, 269-273 (2011)`_.
+
+    Args:
+        singlet_rate (float): Singlet recombination rate constant (1/s).
+        triplet_rate (float): Triplet product formation rate constant (1/s).
+
+    >>> JonesHore(1e7, 1e6)
+    Kinetics: JonesHore
+    Singlet rate: 10000000.0
+    Triplet rate: 1000000.0
+
+    .. _Jones et al. Chem. Phys. Lett. 507, 269-273 (2011):
+       https://doi.org/10.1016/j.cplett.2011.03.082
+    """
+
+    def __init__(self, singlet_rate: float, triplet_rate: float):
+        self.singlet_rate = singlet_rate
+        self.triplet_rate = triplet_rate
+
+    def init(self, sim: LiouvilleSimulation):
+        """See `radicalpy.simulation.HilbertIncoherentProcessBase.init`."""
+        QS = sim.projection_operator(State.SINGLET)
+        QT = sim.projection_operator(State.TRIPLET)
+        self.subH = (
+            0.5 * self.singlet_rate * sim._convert(QS)
+            + 0.5 * self.triplet_rate * sim._convert(QT)
+            + (0.5 * (self.singlet_rate + self.triplet_rate))
+            * (np.kron(QS, QT) + np.kron(QT, QS))
+        )
+
+    def __repr__(self):
+        lines = [
+            self._name(),
+            f"Singlet rate: {self.singlet_rate}",
+            f"Triplet rate: {self.triplet_rate}",
+        ]
+        return "\n".join(lines)
+
+    @property
+    def rate_constant(self) -> float:
+        """Average rate of the kinetic processes."""
+        return (self.singlet_rate + self.triplet_rate) / 2
---
+++ packages/radicalpy/radicalpy-0.9.4.tar.gz/radicalpy-0.9.4/radicalpy/plot.py
@@ -0,0 +1,373 @@
+#! /usr/bin/env python
+
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+from .simulation import HilbertSimulation, State
+from .utils import spherical_to_cartesian
+
+ELEMENT_COLORS = {
+    "H": "lightcoral",
+    "C": "black",
+    "N": "blue",
+    "O": "red",
+    "F": "green",
+    "Cl": "green",
+    "Br": "brown",
+    "I": "purple",
+    "S": "gold",
+    "P": "orange",
+}
+
+
+def anisotropy_surface(theta, phi, Y):
+    # TODO(vatai): clean up
+    PH, TH = np.meshgrid(phi, theta)
+    xyz = np.array([np.sin(TH) * np.cos(PH), np.sin(TH) * np.sin(PH), np.cos(TH)])
+
+    Yx, Yy, Yz = Y.real * spherical_to_cartesian(TH, PH).T
+
+    # Colour the plotted surface according to the sign of Y
+    # cmap = plt.cm.ScalarMappable(cmap=plt.get_cmap("Accent_r"))
+    cmap = plt.cm.ScalarMappable(cmap=plt.get_cmap("viridis"))
+    cmap.set_clim(-0.01, 0.01)
+
+    fig = plt.figure(figsize=plt.figaspect(1.0))
+    ax = fig.add_subplot(projection="3d")
+    ax.set_facecolor("none")
+    ax.plot_surface(Yx, Yy, Yz, facecolors=cmap.to_rgba(Y.real), rstride=2, cstride=2)
+
+    # Draw a set of x, y, z axes for reference
+    ax_lim = np.max(Y.real)
+    ax.plot([-ax_lim, ax_lim], [0, 0], [0, 0], c="0.5", lw=1, zorder=10)
+    ax.plot([0, 0], [-ax_lim, ax_lim], [0, 0], c="0.5", lw=1, zorder=10)
+    ax.plot([0, 0], [0, 0], [-ax_lim, ax_lim], c="0.5", lw=1, zorder=10)
+    # Set the Axes limits alpha and title, turn off the Axes frame.
+    ax_lim = np.max(Y.real)
+    ax.set_xlim(-ax_lim, ax_lim)
+    ax.set_ylim(-ax_lim, ax_lim)
+    ax.set_zlim(-ax_lim, ax_lim)
+    ax.axis("off")
+    fig.set_size_inches(20, 10)
+
+
+def density_matrix_animation(rhos, frames, bar3d_kwargs, axes_kwargs):
+    fig = plt.figure()
+    ax = plt.axes(projection="3d", aspect="auto")
+
+    def anim_func(t):
+        Z = np.abs(rhos[t])
+        X, Y = np.meshgrid(range(len(Z)), range(len(Z)))
+        X, Y, Z = X.flatten(), Y.flatten(), Z.flatten()
+
+        fracs = Z.astype(float) / Z.max()
+        norm = colors.Normalize(fracs.min(), fracs.max())
+        color_values = cm.jet(norm(fracs.tolist()))
+
+        ax.cla()
+        # ax.axis("off")
+
+        ax.set(**axes_kwargs)
+        frame = ax.bar3d(
+            X,
+            Y,
+            np.zeros_like(X),
+            np.ones_like(X),
+            np.ones_like(Y),
+            Z,
+            color=color_values,
+            **bar3d_kwargs,
+        )
+        return frame
+
+    return FuncAnimation(fig, anim_func, frames=frames)
+
+
+def linear_energy_levels(H, B, linecolour, title):
+    # todo(vatai): clean up
+    eigval = np.linalg.eigh(H)  # try eig(H)
+    E = np.real(eigval[0])  # 0 = eigenvalues, 1 = eigenvectors
+
+    fig = plt.figure(figsize=(4, 8))
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.eventplot(E, orientation="vertical", color=linecolour, linewidth=3)
+    ax.set_title(title, size=18)
+    ax.set_ylabel("Spin state energy / J", size=14)
+    plt.tick_params(labelsize=14)
+
+
+def energy_levels(sim: HilbertSimulation, B: np.ndarray, J=0, D=0):
+    # TODO(VATAI): DO THIS PROPERLY
+    # TODO(VATAI): use tick labels
+    assert (
+        type(sim) == HilbertSimulation
+    ), "plot.energy_levels assumes Hilbert space simulation"
+    H_base = sim.total_hamiltonian(0, J, D)
+    H_zee = sim.zeeman_hamiltonian(1)
+
+    E = np.zeros([len(B), len(H_base)], dtype=np.complex128)
+
+    for i, B0 in enumerate(B):
+        H = H_base + B0 * H_zee
+        eigval = np.linalg.eig(H)
+        E[i] = eigval[0]  # 0 = eigenvalues, 1 = eigenvectors
+
+    fig = plt.figure()
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.plot(B, np.real(E[:, ::-1]), linewidth=2)
+    # ax.set_title(title, size=18)
+    ax.set_xlabel("$B_0 / T$", size=14)
+    ax.set_ylabel("Spin state energy / J", size=14)
+    plt.tick_params(labelsize=14)
+
+
+def monte_carlo_free(pos):
+    f = 1e9
+    pos *= f
+
+    fig, ax = plt.subplots(1, 1, subplot_kw={"projection": "3d", "aspect": "auto"})
+    ax.set_facecolor("none")
+    ax.grid(False)
+    plt.axis("on")
+    ax.plot(*pos.T, alpha=0.9, color="cyan")
+    ax.plot(*pos[0], "bo", markersize=15)
+    ax.plot(0, 0, 0, "mo", markersize=15)
+    ax.set_title(
+        "3D Monte Carlo random walk simulation for a radical pair in water", size=16
+    )
+    ax.set_xlabel("$X$ / nm", size=14)
+    ax.set_ylabel("$Y$ / nm", size=14)
+    ax.set_zlabel("$Z$ / nm", size=14)
+    # plt.xlim([-1, 1]); plt.ylim([-1, 1])
+    plt.tick_params(labelsize=14)
+    fig.set_size_inches(10, 10)
+
+
+def monte_carlo_caged(pos, r_max):
+    phi = np.linspace(0, np.pi, 20)
+    theta = np.linspace(0, 2 * np.pi, 40)
+    x_frame = r_max * np.outer(np.sin(theta), np.cos(phi))
+    y_frame = r_max * np.outer(np.sin(theta), np.sin(phi))
+    z_frame = r_max * np.outer(np.cos(theta), np.ones_like(phi))
+
+    f = 1e9
+
+    fig, ax = plt.subplots(1, 1, subplot_kw={"projection": "3d", "aspect": "auto"})
+    ax.set_facecolor("none")
+    ax.grid(False)
+    plt.axis("on")
+    ax.plot_wireframe(
+        x_frame * f,
+        y_frame * f,
+        z_frame * f,
+        color="k",
+        alpha=0.1,
+        rstride=1,
+        cstride=1,
+    )
+    pos = f * pos
+    ax.plot(*pos.T, alpha=0.9, color="cyan")
+    ax.plot(*pos[0], "bo", markersize=15)
+    ax.plot(0, 0, 0, "ro", markersize=15)
+    #     ax.set_title("3D Monte Carlo random walk simulation for an encapsulated radical pair", size=16)
+    ax.set_xlabel("$X$ / nm", size=14)
+    ax.set_ylabel("$Y$ / nm", size=14)
+    ax.set_zlabel("$Z$ / nm", size=14)
+    # plt.xlim([-1, 1]); plt.ylim([-1, 1])
+    plt.tick_params(labelsize=14)
+    fig.set_size_inches(10, 10)
+
+
+def plot_3d_results(
+    xdata, ydata, zdata, xlabel, ylabel, zlabel, azim=-135, dist=10, elev=35, factor=1e6
+):
+    fig = plt.figure(figsize=plt.figaspect(1.0))
+    ax = fig.add_subplot(projection="3d")
+    cmap = plt.cm.ScalarMappable(cmap=plt.get_cmap("viridis"))
+    ax.set_facecolor("none")
+    ax.grid(False)
+    X, Y = np.meshgrid(xdata, ydata)
+    ax.plot_surface(
+        X,
+        Y * factor,
+        zdata,
+        facecolors=cmap.to_rgba(zdata.real),
+        rstride=1,
+        cstride=1,
+    )
+    ax.set_xlabel(xlabel, size=24, labelpad=15)
+    ax.set_ylabel(ylabel, size=24, labelpad=15)
+    ax.set_zlabel(zlabel, size=24, labelpad=18)
+    # ax.set_proj_type("ortho")
+    ax.azim = azim
+    ax.dist = dist
+    ax.elev = elev
+    plt.tick_params(labelsize=18)
+    fig.set_size_inches(10, 10)
+    # plt.show()
+
+
+def plot_autocorrelation_fit(t_j, acf_j, acf_j_fit, zero_point_crossing_j):
+    fig = plt.figure()
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.set_facecolor("none")
+    ax.grid(False)
+    plt.axis("on")
+    plt.xscale("log")
+    # .rc("axes", edgecolor="black")
+    plt.plot(t_j, acf_j[0:zero_point_crossing_j], color="tab:blue", linewidth=3)
+    plt.plot(t_j, acf_j_fit["fit"], color="black", linestyle="dashed", linewidth=2)
+    ax.set_xlabel(r"$\tau$ / s", size=24)
+    ax.set_ylabel(r"$g_J(\tau)$", size=24)
+    plt.tick_params(labelsize=18)
+    fig.set_size_inches(5, 5)
+    # plt.show()
+
+
+def plot_bhalf_time(ts, bhalf_time, fit_error_time, style="ro", factor=1e6):
+    plt.figure()
+    for i in range(2, len(ts), 10):
+        plt.plot(ts[i] * factor, bhalf_time[i], style, linewidth=3)
+        plt.errorbar(
+            ts[i] * factor,
+            bhalf_time[i],
+            fit_error_time[1, i],
+            color="k",
+            linewidth=2,
+        )
+    plt.xlabel(r"Time / $\mu s$", size=24)
+    plt.ylabel("$B_{1/2}$ / mT", size=24)
+    plt.tick_params(labelsize=18)
+    plt.gcf().set_size_inches(5, 5)
+    # plt.show()
+
+
+def plot_exchange_interaction_in_solution(ts, trajectory_data, j):
+    fig = plt.figure()
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.set_facecolor("none")
+    ax.grid(False)
+    plt.axis("on")
+    plt.rc("axes", edgecolor="black")
+    color = "tab:red"
+    plt.plot(ts, trajectory_data[:, 1] * 1e9, color=color)
+    ax2 = ax.twinx()
+    color2 = "tab:blue"
+    plt.plot(ts, -j, color=color2)
+    ax.set_xlabel("Time / ns", size=24)
+    ax.set_ylabel("Radical pair separation / nm", size=24, color=color)
+    ax2.set_ylabel("Exchange interaction / mT", size=24, color=color2)
+    ax.tick_params(axis="y", labelsize=18, labelcolor=color)
+    ax.tick_params(axis="x", labelsize=18, labelcolor="k")
+    ax2.tick_params(labelsize=18, labelcolor=color2)
+    fig.set_size_inches(5, 5)
+    # plt.show()
+
+
+def plot_general(
+    xdata, ydata, xlabel, ylabel, style="-", label=[], colors="r", factor=1
+):
+    # plt.figure()
+    plt.plot(xdata * factor, ydata, style, linewidth=3, label=label, color=colors)
+    plt.xlabel(xlabel, size=24)
+    plt.ylabel(ylabel, size=24)
+    plt.legend()
+    plt.tick_params(labelsize=18)
+    plt.gcf().set_size_inches(10, 5)
+
+
+def plot_molecule(
+    ax, labels, elements, coords, bonds, show_labels=True, show_atoms=False
+):
+    colors = [ELEMENT_COLORS.get(e, "gray") for e in elements]
+    X, Y, Z = coords[:, 0], coords[:, 1], coords[:, 2]
+    if show_atoms:
+        ax.scatter(X, Y, Z, s=100, c=colors, depthshade=True)
+    if show_labels:
+        for lab, x, y, z in zip(labels, X, Y, Z):
+            ax.text(x + 0.4, y + 0.4, z + 0.4, lab, fontsize=10)
+    for i, j in bonds:
+        ax.plot([X[i], X[j]], [Y[i], Y[j]], [Z[i], Z[j]], "k-", linewidth=4)
+    set_equal_aspect(ax, X, Y, Z)
+
+
+def set_equal_aspect(ax, X, Y, Z):
+    rng = np.array([X.max() - X.min(), Y.max() - Y.min(), Z.max() - Z.min()]).max()
+    xb = yb = zb = 0.5 * rng
+    ax.set_xlim(X.mean() - xb, X.mean() + xb)
+    ax.set_ylim(Y.mean() - yb, Y.mean() + yb)
+    ax.set_zlim(Z.mean() - zb, Z.mean() + zb)
+
+
+def spin_state_labels(sim: HilbertSimulation):
+    if len(sim.radicals) != 2:
+        raise ValueError(
+            "Density matrix plotting make little sense for non-radical pairs!"
+        )
+    # multiplicities = sim.multiplicities[len(sim.radicals) :]
+    multiplicities = [r.multiplicity for r in sim.radicals]
+    old_labels = [
+        State.TRIPLET_PLUS.value,
+        State.TRIPLET_ZERO.value,
+        State.SINGLET.value,
+        State.TRIPLET_MINUS.value,
+    ]
+    for m in multiplicities:
+        labels = []
+        for label in old_labels:
+            for t in range(m):
+                tt = int(2 * ((m - t - 1) - (m - 1) / 2))
+                tt = f"{tt}/2" if tt % 2 else str(tt // 2)
+                if tt[0] not in {"-", "0"}:
+                    tt = f"+{tt}"
+                labels.append(f"{label}, {tt}")
+
+        old_labels = labels
+    return list(map(_format_label, labels))
+
+
+def _format_label(t):
+    return f"$\\vert {t} \\rangle$"
+
+
+def visualise_tensor(ax, tensor, rot_matrix, coords, colour):
+
+    resolution = 30
+    theta = np.linspace(0, np.pi, resolution)
+    phi = np.linspace(0, 2 * np.pi, resolution)
+
+    tensor_vis = np.zeros([len(theta), len(phi), 3])
+
+    for i in range(0, len(theta)):
+        for j in range(0, len(phi)):
+
+            xyz = np.array(
+                [
+                    np.sin(theta[i]) * np.cos(phi[j]),
+                    np.sin(theta[i]) * np.sin(phi[j]),
+                    np.cos(theta[i]),
+                ]
+            )
+
+            tensor_vis[i, j] = (
+                np.dot(
+                    np.dot(
+                        xyz.T, np.array(rot_matrix) @ tensor @ np.array(rot_matrix).T
+                    ),
+                    xyz,
+                )
+                * xyz.T
+                + coords
+            )
+
+    ax.plot_surface(
+        tensor_vis[:, :, 0],
+        tensor_vis[:, :, 1],
+        tensor_vis[:, :, 2],
+        color=colour,
+        edgecolor="none",
+    )
+    return tensor_vis

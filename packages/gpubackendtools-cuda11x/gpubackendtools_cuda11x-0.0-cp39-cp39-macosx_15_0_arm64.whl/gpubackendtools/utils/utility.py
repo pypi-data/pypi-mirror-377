20370b87e509b864f5d030c5568c3c3299decb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 15:06:49 +0000

    Add package gpubackendtools-cuda11x/0.0/gpubackendtools_cuda11x-0.0-cp39-cp39-macosx_15_0_arm64.whl

---
+++ packages/gpubackendtools-cuda11x/gpubackendtools_cuda11x-0.0-cp39-cp39-macosx_15_0_arm64.whl/gpubackendtools/utils/utility.py
@@ -0,0 +1,24 @@
+from ast import Mod
+import numpy as np
+import warnings
+
+
+try:
+    from cupy.cuda.runtime import setDevice
+
+
+except (ModuleNotFoundError, ImportError):
+    setDevice = None
+
+
+def cuda_set_device(dev):
+    """Globally sets CUDA device
+
+    Args:
+        dev (int): CUDA device number.
+
+    """
+    if setDevice is not None:
+        setDevice(dev)
+    else:
+        warnings.warn("Setting cuda device, but cupy/cuda not detected.")

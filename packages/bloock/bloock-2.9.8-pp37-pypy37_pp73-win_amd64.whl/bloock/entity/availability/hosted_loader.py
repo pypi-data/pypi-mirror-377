5dd36895aa61a0388115f040c294164cec9b1356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:11 +0000

    Add package bloock/2.9.8/bloock-2.9.8-pp37-pypy37_pp73-win_amd64.whl

---
+++ packages/bloock/bloock-2.9.8-pp37-pypy37_pp73-win_amd64.whl/bloock/entity/availability/hosted_loader.py
@@ -0,0 +1,19 @@
+import bloock._bridge.proto.bloock_availability_entities_pb2 as proto
+from bloock.entity.availability.loader import Loader
+from bloock.entity.availability.loader_args import LoaderArgs
+
+
+class HostedLoader(Loader):
+    """
+    Represents a loader for hosted data availability.
+    """
+    def __init__(self, hash: str) -> None:
+        """
+        Constructs a HostedLoader object with the specified parameters.
+        :type hash: object
+        :rtype: object
+        """
+        super().__init__(type=proto.HOSTED, args=LoaderArgs(hash))
+
+    def to_proto(self) -> proto.Loader:
+        return proto.Loader(type=self.type, args=self.args.to_proto())
---
+++ packages/bloock/bloock-2.9.8-pp37-pypy37_pp73-win_amd64.whl/bloock/entity/identity/credential_schema.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import bloock._bridge.proto.bloock_identity_entities_pb2 as proto
+
+
+class CredentialSchema:
+    """
+    Represents the schema information for a credential, including its ID and type.
+    """
+    def __init__(self, id: str, type: str) -> None:
+        """
+        Constructs an CredentialSchema object with the specified parameters.
+        :type type: object
+        :type id: object
+        :rtype: object
+        """
+        self.id = id
+        self.type = type
+
+    @staticmethod
+    def from_proto(c: proto.CredentialSchema) -> CredentialSchema:
+        return CredentialSchema(
+            id=c.id,
+            type=c.type
+        )
+
+    def to_proto(self) -> proto.CredentialSchema:
+        return proto.CredentialSchema(
+            id=self.id,
+            type=self.type
+        )

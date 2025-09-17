0a4a66c83a68554a37159d37ff02c390bf90d63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:06:20 +0000

    Add package uk_address_matcher/1.0.0.dev20/uk_address_matcher-1.0.0.dev20-py3-none-any.whl

---
+++ packages/uk_address_matcher/uk_address_matcher-1.0.0.dev20-py3-none-any.whl/uk_address_matcher/__init__.py
@@ -0,0 +1,34 @@
+__version__ = "1.0.0.dev19"
+
+from uk_address_matcher.linking_model.splink_model import get_linker
+from uk_address_matcher.cleaning.cleaning_pipelines import (
+    clean_data_on_the_fly,
+    clean_data_using_precomputed_rel_tok_freq,
+    get_numeric_term_frequencies_from_address_table,
+    get_address_token_frequencies_from_address_table,
+)
+from uk_address_matcher.post_linkage.identify_distinguishing_tokens import (
+    improve_predictions_using_distinguishing_tokens,
+)
+
+from uk_address_matcher.post_linkage.analyse_results import (
+    best_matches_with_distinguishability,
+    best_matches_summary,
+)
+from uk_address_matcher.post_linkage.accuracy_from_labels import (
+    inspect_match_results_vs_labels,
+    evaluate_predictions_against_labels,
+)
+
+__all__ = [
+    "get_linker",
+    "clean_data_on_the_fly",
+    "clean_data_using_precomputed_rel_tok_freq",
+    "get_numeric_term_frequencies_from_address_table",
+    "get_address_token_frequencies_from_address_table",
+    "improve_predictions_using_distinguishing_tokens",
+    "best_matches_with_distinguishability",
+    "best_matches_summary",
+    "inspect_match_results_vs_labels",
+    "evaluate_predictions_against_labels",
+]

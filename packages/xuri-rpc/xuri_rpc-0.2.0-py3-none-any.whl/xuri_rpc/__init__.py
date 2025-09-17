32b2dc7f76e13f4d532f1ec147a9d27d430a4c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:13:53 +0000

    Add package xuri-rpc/0.2.0/xuri_rpc-0.2.0-py3-none-any.whl

---
+++ packages/xuri-rpc/xuri_rpc-0.2.0-py3-none-any.whl/xuri_rpc/__init__.py
@@ -0,0 +1,2 @@
+from . core import  dict2obj,Message,MessageReceiverOptions,RunnableProxyManager,PlainProxyManager,setHostId,Client,ISender,asProxy,getMessageReceiver,MessageReceiver 
+from .core import setDebugFlag

c9dcd2bcf6dfc1c52cfe61ab4eae6faad787ce5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:07:37 +0000

    Add package split-datalawyer/0.1.95/split_datalawyer-0.1.95-py3-none-any.whl

---
+++ packages/split-datalawyer/split_datalawyer-0.1.95-py3-none-any.whl/split_datalawyer/utils/__init__.py
@@ -0,0 +1,61 @@
+import re
+from split_datalawyer.utils.file_utils import *
+from split_datalawyer.utils.hyphen_utils import PortugueseHyphenDetector, PortugueseCompoundDetector
+
+
+def _get_local_directory():
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+
+    return dname
+
+
+def get_stop_patterns(stop_patterns_path=None):
+    if stop_patterns_path is None:
+        stop_patterns_path = os.path.join(_get_local_directory(), "stoppatterns.txt")
+
+    f = open(stop_patterns_path, encoding='utf-8')
+    phrase_list = f.readlines()
+    f.close()
+
+    return [re.compile(x.replace("\n", ""), re.IGNORECASE) for x in phrase_list]
+
+
+def _get_abbreviations(add_dot=True):
+    abbreviations_path = os.path.join(_get_local_directory(), "abbreviations.csv")
+    with open(abbreviations_path, "r", encoding='utf-8') as f:
+        abbreviations = f.readlines()
+
+    if add_dot:
+        return [x.replace("\n", "").lower() + "." for x in abbreviations]
+    else:
+        return [x.replace("\n", "").lower() for x in abbreviations]
+
+
+def _get_continuation_terms():
+    continuation_terms_path = os.path.join(_get_local_directory(), "continuation_terms.csv")
+    f = open(continuation_terms_path, "r", encoding='utf-8')
+    continuation_terms = f.readlines()
+    f.close()
+
+    return [re.compile(x.replace("\n", "")) for x in continuation_terms]
+
+detector = PortugueseHyphenDetector()
+compound_detector = PortugueseCompoundDetector()
+
+def remove_invalid_hyphen(sentence: str) -> str:
+    words = []
+    for word in sentence.split():
+        base_result = detector.should_preserve_hyphen(word)
+        compound_result = compound_detector.is_compound_adjective(word)
+        if base_result is False and compound_result is False and not detector.ends_with_hyphen(word):
+            words.append(word.replace('-', ''))
+        else:
+            words.append(word)
+    return ' '.join(words)
+
+
+ABBREVIATION_LIST = _get_abbreviations()
+ABBREVIATION_LIST_WITHOUT_DOTS = _get_abbreviations(False)
+CONTINUATION_TERMS = _get_continuation_terms()
+DEFAULT_STOP_PATTERNS = get_stop_patterns()

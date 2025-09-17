f64889b682dfbe4c8480f4a442e0eb435ec03eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:14:06 +0000

    Add package hic-io/1.0.2/hic_io-1.0.2.tar.gz

---
+++ packages/hic-io/hic_io-1.0.2.tar.gz/hic_io-1.0.2/setup.py
@@ -0,0 +1,146 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+import os
+import glob
+
+__version__ = '1.0.2'
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class GetPybindInclude(object):
+    """Helper class to determine the pybind11 include path
+
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
+ext_modules = [
+    Extension(
+        'hic_io',
+        ['hic_io/straw.cpp'],
+        include_dirs=[
+            # Path to pybind11 headers
+            GetPybindInclude(),
+            GetPybindInclude(user=True)
+        ],
+        language='c++'
+    ),
+]
+
+
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+def cpp_flag(compiler):
+    """Return the -std=c++[11/14/17] compiler flag.
+
+    The newer version is prefered over c++11 (when it is available).
+    """
+    flags = ['-std=c++14', '-std=c++11']
+
+    for flag in flags:
+        if has_flag(compiler, flag): return flag
+
+    raise RuntimeError('Unsupported compiler -- at least C++11 support '
+                       'is needed!')
+
+
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': [],
+    }
+    l_opts = {
+        'msvc': [],
+        'unix': ['-lcurl', '-lz'],
+    }
+
+    if sys.platform == 'darwin':
+        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        c_opts['unix'] += darwin_opts
+        l_opts['unix'] += darwin_opts
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        link_opts = self.l_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, '-fvisibility=hidden'):
+                opts.append('-fvisibility=hidden')
+        elif ct == 'msvc':
+            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+            ext.extra_link_args = link_opts
+        build_ext.build_extensions(self)
+
+
+"""
+setup(
+    name='hic-straw',
+    version=__version__,
+    author='Neva C. Durand, Muhammad S Shamim',
+    author_email='neva@broadinstitute.org',
+    license='MIT',
+    keywords=['Hi-C', '3D Genomics', 'Chromatin', 'ML'],
+    url='https://github.com/aidenlab/straw',
+    description='Straw bound with pybind11',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    ext_modules=ext_modules,
+    install_requires=['pybind11>=2.4'],
+    setup_requires=['pybind11>=2.4'],
+    python_requires='>3.3',
+    cmdclass={'build_ext': BuildExt},
+    zip_safe=False,
+)
+"""
+setup(
+    name='hic_io',
+    version=__version__,
+    author='Arthur Gouhier',
+    author_email='ajgouhier@gmail.com',
+    license='MIT',
+    url="https://github.com/ajgouhier/hic_io",
+    description='Straw bound with pybind11 from hic-straw 1.3.1',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    ext_modules=ext_modules,
+    install_requires=['numpy', 'pybind11>=2.4'],
+    setup_requires=['numpy', 'pybind11>=2.4'],
+    python_requires='>3.3',
+    cmdclass={'build_ext': BuildExt},
+    zip_safe=False,
+    data_files=[
+        ('hic_io', glob.glob('hic_io/*.h') + glob.glob('hic_io/*.hpp'))
+    ],
+)

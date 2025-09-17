037d3b4a08588589a6dab1d20d49c278aa99204c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:05:06 +0000

    Add package vecx/0.34.0b3/vecx-0.34.0b3.tar.gz

---
+++ packages/vecx/vecx-0.34.0b3.tar.gz/vecx-0.34.0b3/setup.py
@@ -0,0 +1,31 @@
+# myproject/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="vecx",
+    version="0.34.0b3",
+    packages=find_packages(),
+    package_data={
+        '': ['libvx/*'],  # Include all files in the libvx directory
+    },
+    install_requires=[
+        # List your dependencies here
+        "requests>=2.28.0",
+        "numpy>=2.2.4",
+        "msgpack>=1.1.0",
+        "cffi>=1.17.1",
+    ],
+    author="LaunchX Labs",
+    author_email="support@vectorxdb.ai",
+    description="Encrypted Vector Database for Secure and Fast ANN Searches",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://vectorxdb.ai",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

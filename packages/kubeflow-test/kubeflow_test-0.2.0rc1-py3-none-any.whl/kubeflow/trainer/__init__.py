731adf106a38172e37f87197196ec3c8dea851ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 14:05:38 +0000

    Add package kubeflow-test/0.2.0rc1/kubeflow_test-0.2.0rc1-py3-none-any.whl

---
+++ packages/kubeflow-test/kubeflow_test-0.2.0rc1-py3-none-any.whl/kubeflow/trainer/__init__.py
@@ -0,0 +1,58 @@
+# Copyright 2024 The Kubeflow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from __future__ import absolute_import
+
+# Import the Kubeflow Trainer client.
+from kubeflow.trainer.api.trainer_client import TrainerClient  # noqa: F401
+
+# Import the Kubeflow Trainer constants.
+from kubeflow.trainer.constants.constants import DATASET_PATH, MODEL_PATH  # noqa: F401
+
+# Import the Kubeflow Trainer types.
+from kubeflow.trainer.types.types import (
+    BuiltinTrainer,
+    CustomTrainer,
+    DataFormat,
+    DataType,
+    HuggingFaceDatasetInitializer,
+    HuggingFaceModelInitializer,
+    Initializer,
+    Loss,
+    Runtime,
+    TorchTuneConfig,
+    TorchTuneInstructDataset,
+    RuntimeTrainer,
+    TrainerType,
+)
+
+__all__ = [
+    "BuiltinTrainer",
+    "CustomTrainer",
+    "DataFormat",
+    "DATASET_PATH",
+    "DataType",
+    "HuggingFaceDatasetInitializer",
+    "HuggingFaceModelInitializer",
+    "Initializer",
+    "Loss",
+    "MODEL_PATH",
+    "Runtime",
+    "TorchTuneConfig",
+    "TorchTuneInstructDataset",
+    "RuntimeTrainer",
+    "TrainerClient",
+    "TrainerType",
+]
---
+++ packages/kubeflow-test/kubeflow_test-0.2.0rc1-py3-none-any.whl/kubeflow/trainer/api/__init__.py
@@ -0,0 +1,4 @@
+# ruff: noqa
+
+# import apis into api package
+

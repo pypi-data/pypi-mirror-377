48fbcc087fcf16b07536e5674a602ca41016cf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 07:05:26 +0000

    Add package halide/21.0.0/halide-21.0.0-cp313-cp313-macosx_11_0_x86_64.whl

---
+++ packages/halide/halide-21.0.0-cp313-cp313-macosx_11_0_x86_64.whl/halide/lib/cmake/Halide_Python/Halide_PythonConfig.cmake
@@ -0,0 +1,26 @@
+cmake_minimum_required(VERSION 3.28)
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was Halide_PythonConfig.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+include(CMakeFindDependencyMacro)
+find_dependency(Python 3 COMPONENTS Interpreter Development.Module)
+
+include("${CMAKE_CURRENT_LIST_DIR}/Halide_Python-targets.cmake")
+
+check_required_components(${CMAKE_FIND_PACKAGE_NAME})

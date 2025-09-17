dc2e4590f3a4fc6578fab3495d11041db650ae90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:14:30 +0000

    Add package osc-data/0.2.6/osc_data-0.2.6.tar.gz

---
+++ packages/osc-data/osc_data-0.2.6.tar.gz/osc_data-0.2.6/src/lib.rs
@@ -0,0 +1,13 @@
+mod audio;
+mod text;
+mod text_stream;
+use pyo3::prelude::*;
+
+/// A Python module implemented in Rust.
+#[pymodule(name = "_core")]
+fn rust_ext<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
+    let _ = audio::register_module(m)?;
+    let _ = text_stream::register_module(m)?;
+    let _ = text::register_module(m)?;
+    Ok(())
+}

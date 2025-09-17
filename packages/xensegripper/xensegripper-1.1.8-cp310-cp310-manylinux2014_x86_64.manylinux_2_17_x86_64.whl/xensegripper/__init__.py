dd61b88860cd445a99d5ce41644e36347d446d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 15:06:59 +0000

    Add package xensegripper/1.1.8/xensegripper-1.1.8-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/xensegripper/xensegripper-1.1.8-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/xensegripper/__init__.py
@@ -0,0 +1,8 @@
+﻿import cypack; cypack.init(__name__, set([]));
+from .xense_gripper import XenseGripper
+from .node.camera_client import CameraNode as XenseCamera
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent
+
+from ._version import __version__
+__all__ = ["__version__"]

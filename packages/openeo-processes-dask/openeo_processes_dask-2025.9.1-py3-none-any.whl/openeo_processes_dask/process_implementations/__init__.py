7a9b4908b30f8701a502d6207e5a5ce0822516cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:05:29 +0000

    Add package openeo-processes-dask/2025.9.1/openeo_processes_dask-2025.9.1-py3-none-any.whl

---
+++ packages/openeo-processes-dask/openeo_processes_dask-2025.9.1-py3-none-any.whl/openeo_processes_dask/process_implementations/__init__.py
@@ -0,0 +1,28 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+from .arrays import *
+from .comparison import *
+from .cubes import *
+from .inspect import *
+from .logic import *
+from .math import *
+from .text import *
+from .udf import *
+
+try:
+    from .ml import *
+except ImportError as e:
+    logger.warning(
+        "Did not load machine learning processes due to missing dependencies: Install them like this: `pip install openeo-processes-dask[implementations, ml]`"
+    )
+
+try:
+    from .experimental import *
+except ImportError as e:
+    logger.warning("Did not load experimental processes.")
+
+import rioxarray as rio  # Required for the .rio accessor on xarrays.
+
+import openeo_processes_dask.process_implementations.cubes._xr_interop

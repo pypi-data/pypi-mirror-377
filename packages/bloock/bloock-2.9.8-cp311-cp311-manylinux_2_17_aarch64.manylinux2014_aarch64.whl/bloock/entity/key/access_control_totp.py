b9e485be1491cbfc4d350cd47d4ecc0c4c6b7a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:09 +0000

    Add package bloock/2.9.8/bloock-2.9.8-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/bloock/bloock-2.9.8-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/bloock/entity/key/access_control_totp.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import bloock._bridge.proto.bloock_keys_entities_pb2 as proto
+
+
+class AccessControlTotp:
+    """
+    Represents a Time-based One-Time Password ([TOTP](https://datatracker.ietf.org/doc/html/rfc6238)) code used for access control.
+    """
+    code = None
+
+    def __init__(self, code) -> None:
+        """
+        Constructs an AccessControlTotp object with the specified parameters.
+        :type code: object
+        :rtype: object
+        """
+        self.code = code
+
+    def to_proto(self) -> proto.AccessControlTotp:
+        return proto.AccessControlTotp(
+            code=self.code
+        )

4180b15c5c56b5cf5f96ddd688f6c02f78c36d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 16:05:37 +0000

    Add package nuki-web-api/0.1.0/nuki_web_api-0.1.0-py3-none-any.whl

---
+++ packages/nuki-web-api/nuki_web_api-0.1.0-py3-none-any.whl/nukiwebapi/company.py
@@ -0,0 +1,10 @@
+class Company:
+    """Sub-client for managing companies."""
+
+    def __init__(self, client):
+        self.client = client
+
+    def list_companies(self):
+        """Get a list of companies."""
+        return self.client._request("GET", "/company")
+        

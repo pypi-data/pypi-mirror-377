fd7b3619e6e4cb167f3c93ab1c8de30b471258a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 08:05:16 +0000

    Add package UW-RestClients-Libraries/1.3.3/uw_restclients_libraries-1.3.3.tar.gz

---
+++ packages/UW-RestClients-Libraries/uw_restclients_libraries-1.3.3.tar.gz/uw_restclients_libraries-1.3.3/uw_libraries/util.py
@@ -0,0 +1,11 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from commonconf import override_settings
+
+
+fdao_mylib_override = override_settings(
+    RESTCLIENTS_LIBRARIES_DAO_CLASS='Mock')
+
+fdao_subject_guide_override = override_settings(
+    RESTCLIENTS_LIBCURRICS_DAO_CLASS='Mock')

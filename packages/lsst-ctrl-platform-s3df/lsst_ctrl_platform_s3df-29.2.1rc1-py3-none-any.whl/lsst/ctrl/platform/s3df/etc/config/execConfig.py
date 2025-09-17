dca393f88fa9a0f25623f07d7fd9603a7f58e3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 14:06:29 +0000

    Add package lsst-ctrl-platform-s3df/29.2.1rc1/lsst_ctrl_platform_s3df-29.2.1rc1-py3-none-any.whl

---
+++ packages/lsst-ctrl-platform-s3df/lsst_ctrl_platform_s3df-29.2.1rc1-py3-none-any.whl/lsst/ctrl/platform/s3df/etc/config/execConfig.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+config.platform.nodeSetRequired = False
+config.platform.localScratch = "$USER_SCRATCH/condor_scratch"
+config.platform.fileSystemDomain = "slac.stanford.edu"
+config.platform.scheduler = "slurm"

ef5775de63326c478c5a7c7eff016b32c70ccd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 15:06:49 +0000

    Add package gpubackendtools/0.0/gpubackendtools-0.0-cp39-cp39-macosx_15_0_arm64.whl

---
+++ packages/gpubackendtools/gpubackendtools-0.0-cp39-cp39-macosx_15_0_arm64.whl/gpubackendtools/exceptions.py
@@ -0,0 +1,30 @@
+"""Definition of GPUBACKENDTOOLS package common exceptions"""
+
+try:
+    from exceptiongroup import ExceptionGroup
+except (ImportError, ModuleNotFoundError):
+    ExceptionGroup = ExceptionGroup
+
+
+class GPUBACKENDTOOLSException(Exception):
+    """Base class for GPUBACKENDTOOLS package exceptions."""
+
+    pass
+
+
+class CudaException(GPUBACKENDTOOLSException):
+    """Base class for CUDA-related exceptions."""
+
+    pass
+
+
+class CuPyException(GPUBACKENDTOOLSException):
+    """Base class for CuPy-related exceptions."""
+
+    pass
+
+
+class MissingDependency(GPUBACKENDTOOLSException):
+    """Exception raised when a required dependency is missing."""
+
+    pass

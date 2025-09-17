59d35bfc5f758c2a588994931c5210591e57f41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 07:05:10 +0000

    Add package lalamo/0.3.4/lalamo-0.3.4.tar.gz

---
+++ packages/lalamo/lalamo-0.3.4.tar.gz/lalamo-0.3.4/lalamo/modules/__init__.py
@@ -0,0 +1,86 @@
+from .activations import Activation
+from .attention import Attention, AttentionConfig
+from .common import LalamoModule, WeightLayout, config_converter
+from .decoder import Decoder, DecoderActivationTrace, DecoderConfig, DecoderResult
+from .decoder_layer import DecoderLayer, DecoderLayerActivationTrace, DecoderLayerConfig, DecoderLayerResult
+from .embedding import (
+    EmbeddingBase,
+    EmbeddingConfig,
+    QuantizedTiedEmbedding,
+    QuantizedTiedEmbeddingConfig,
+    TiedEmbedding,
+    TiedEmbeddingConfig,
+    UntiedEmbedding,
+    UntiedEmbeddingConfig,
+)
+from .kv_cache import DynamicKVCacheLayer, KVCache, KVCacheLayer, StaticKVCacheLayer
+from .linear import (
+    FullPrecisionLinear,
+    FullPrecisionLinearConfig,
+    GroupQuantizedLinear,
+    GroupQuantizedLinearConfig,
+    LinearBase,
+    LinearConfig,
+    QLoRALinear,
+    QLoRALinearConfig,
+)
+from .mlp import MLP, MLPConfig
+from .normalization import RMSNorm, RMSNormConfig, UpcastMode
+from .rope import (
+    LinearScalingRoPEConfig,
+    LlamaRoPEConfig,
+    PositionalEmbeddings,
+    RoPE,
+    RoPEConfig,
+    UnscaledRoPEConfig,
+    YARNRoPEConfig,
+)
+
+__all__ = [
+    "MLP",
+    "Activation",
+    "Attention",
+    "AttentionConfig",
+    "Decoder",
+    "DecoderActivationTrace",
+    "DecoderConfig",
+    "DecoderLayer",
+    "DecoderLayerActivationTrace",
+    "DecoderLayerConfig",
+    "DecoderLayerResult",
+    "DecoderResult",
+    "DynamicKVCacheLayer",
+    "EmbeddingBase",
+    "EmbeddingConfig",
+    "FullPrecisionLinear",
+    "FullPrecisionLinearConfig",
+    "GroupQuantizedLinear",
+    "GroupQuantizedLinearConfig",
+    "KVCache",
+    "KVCacheLayer",
+    "LalamoModule",
+    "LinearBase",
+    "LinearConfig",
+    "LinearScalingRoPEConfig",
+    "LlamaRoPEConfig",
+    "MLPConfig",
+    "PositionalEmbeddings",
+    "QLoRALinear",
+    "QLoRALinearConfig",
+    "QuantizedTiedEmbedding",
+    "QuantizedTiedEmbeddingConfig",
+    "RMSNorm",
+    "RMSNormConfig",
+    "RoPE",
+    "RoPEConfig",
+    "StaticKVCacheLayer",
+    "TiedEmbedding",
+    "TiedEmbeddingConfig",
+    "UnscaledRoPEConfig",
+    "UntiedEmbedding",
+    "UntiedEmbeddingConfig",
+    "UpcastMode",
+    "WeightLayout",
+    "YARNRoPEConfig",
+    "config_converter",
+]

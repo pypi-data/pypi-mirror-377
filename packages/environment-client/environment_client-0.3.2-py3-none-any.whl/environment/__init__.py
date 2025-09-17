73cf148997eff15c9fe65b91f7ea278d2aca0763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:14:31 +0000

    Add package environment-client/0.3.2/environment_client-0.3.2-py3-none-any.whl

---
+++ packages/environment-client/environment_client-0.3.2-py3-none-any.whl/environment/__init__.py
@@ -0,0 +1,11 @@
+from .flood_monitoring import FloodClient
+from .bathing_waters import BathingWatersClient
+from .asset_management import AssetManagementClient
+from .catchment_data import CatchmentDataClient
+
+__all__ = [
+    "FloodClient",
+    "BathingWatersClient",
+    "AssetManagementClient",
+    "CatchmentDataClient",
+]

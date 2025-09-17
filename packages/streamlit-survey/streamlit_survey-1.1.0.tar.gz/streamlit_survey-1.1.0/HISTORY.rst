d651bc23d7bfffcc88d22a69289e6066c46fab89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 07:06:57 +0000

    Add package streamlit-survey/1.1.0/streamlit_survey-1.1.0.tar.gz

---
+++ packages/streamlit-survey/streamlit_survey-1.1.0.tar.gz/streamlit_survey-1.1.0/HISTORY.rst
@@ -0,0 +1,24 @@
+=======
+HISTORY
+=======
+
+1.1.0 (2025-09-16)
+------------------
+
+* Add support for feedback component.
+* Add support for segmented_control component.
+* Update Streamlit dependency to 1.40.0.
+
+1.0.0 (2024-08-08)
+------------------
+
+* Fix button customization in paged surveys.
+* Add progress bar option to pages.
+* Add option to pass a list to Page's constructor, in order to both have the current page number and an associated value.
+* Add survey download button and file importer.
+* Add "widget_key" to survey data to help restore survey data for displayed widgets.
+
+0.1.0 (2023-03-23)
+------------------
+
+* First release.
---
+++ packages/streamlit-survey/streamlit_survey-1.1.0.tar.gz/streamlit_survey-1.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = ["streamlit>=1.40.0"]
+
+setup(
+    author="Olivier Binette",
+    author_email="olivier.binette@gmail.com",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+    ],
+    description="Survey components for Streamlit apps",
+    install_requires=requirements,
+    license="Commons Clause + Apache License 2.0",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="streamlit_survey",
+    name="streamlit_survey",
+    packages=find_packages(include=["streamlit_survey", "streamlit_survey.*"]),
+    url="https://github.com/OlivierBinette/streamlit_survey",
+    version="1.1.0",
+    zip_safe=False,
+)

81a91a501a1641b08293934f81c97e7673da51eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:05 +0000

    Add package bloock/2.9.8/bloock-2.9.8-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/bloock/bloock-2.9.8-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/bloock/entity/integrity/record_receipt.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import bloock._bridge.proto.bloock_integrity_entities_pb2 as proto
+
+
+class RecordReceipt:
+    """
+    Represents a receipt for a record, including anchor ID, client, record, and status information.
+    """
+    def __init__(self, anchor: int, client: str, record: str, status: str) -> None:
+        """
+        Constructs a RecordReceipt object with the specified parameters.
+        :type status: object
+        :type record: object
+        :type client: object
+        :type anchor: object
+        :rtype: object
+        """
+        self.anchor = anchor
+        self.client = client
+        self.record = record
+        self.status = status
+
+    @staticmethod
+    def from_proto(receipt: proto.RecordReceipt) -> RecordReceipt:
+        return RecordReceipt(
+            anchor=receipt.anchor,
+            client=receipt.client,
+            record=receipt.record,
+            status=receipt.status,
+        )
+
+    def to_proto(self) -> proto.RecordReceipt:
+        return proto.RecordReceipt(
+            anchor=self.anchor,
+            client=self.client,
+            record=self.record,
+            status=self.status,
+        )

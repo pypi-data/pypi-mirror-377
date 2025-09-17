ca5f202fc1cf9b5d823b371289e8d63b57d7e088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 07:05:16 +0000

    Add package timc-vector-toolkit/20250916/timc_vector_toolkit-20250916.tar.gz

---
+++ packages/timc-vector-toolkit/timc_vector_toolkit-20250916.tar.gz/timc_vector_toolkit-20250916/config.mk
@@ -0,0 +1,8 @@
+SHELL = /bin/bash
+VERSION = 20250916
+OUTPUT = out
+PLATFORM = $(shell uname -s)-$(shell uname -m)
+SIZE_HEADER = 8192
+PYTHON_VERSION = 3.13
+IMAGE_BASE = ubuntu:24.04
+TAG = tutteinstitute/$(1)$(and $(2),:$(2))

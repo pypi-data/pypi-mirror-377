991a4563bc80ace99397f29f91f466cfe86f7fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:05:13 +0000

    Add package cognee/0.3.4.dev0/cognee-0.3.4.dev0.tar.gz

---
+++ packages/cognee/cognee-0.3.4.dev0.tar.gz/cognee-0.3.4.dev0/cognee/infrastructure/databases/relational/ModelBase.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """
+    Represents a base class for declarative models using SQLAlchemy.
+
+    The Base class provides the foundation for creating ORM-mapped classes and manages the
+    mapping of classes to database tables.
+    """
+
+    pass

97aa94246a6433369b5d108ee54099002107fa75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:05:22 +0000

    Add package OBPAnalyser/0.0.5/obpanalyser-0.0.5.tar.gz

---
+++ packages/OBPAnalyser/obpanalyser-0.0.5.tar.gz/obpanalyser-0.0.5/src/obanalyser/config.py
@@ -0,0 +1,9 @@
+import math
+
+class Config:
+    def __init__(self):
+        # Parameters for build simulation
+        self.transition_time = 7.844617526 #time [s] for recoter and transition between layers
+        
+
+config = Config()

b9675f189010ec0313771a3aca7814848fc37b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:05:23 +0000

    Add package zipline-tej/2.2.1/zipline_tej-2.2.1-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/zipline-tej/zipline_tej-2.2.1-cp39-cp39-macosx_11_0_arm64.whl/zipline/data/bundles/__init__.py
@@ -0,0 +1,45 @@
+# These imports are necessary to force module-scope register calls to happen.
+from . import quandl  # noqa
+from . import csvdir  # noqa
+from . import tquant  #20230303 
+from . import fundamentals # 20230823
+from . import morning_txf #20230921
+from . import tquant_future # 20250212
+from .core import (
+    UnknownBundle,
+    bundles,
+    clean,
+    from_bundle_ingest_dirname,
+    ingest,
+    ingestions_for_bundle,
+    load,
+    register,
+    to_bundle_ingest_dirname,
+    unregister,
+    update,
+    switch,
+    bundle_info,
+    add,
+    download_bundle_info,
+
+)
+
+
+__all__ = [
+    "UnknownBundle",
+    "bundles",
+    "clean",
+    "from_bundle_ingest_dirname",
+    "ingest",
+    "ingestions_for_bundle",
+    "load",
+    "register",
+    "to_bundle_ingest_dirname",
+    "unregister",
+    "update",
+    "switch",
+    "bundle_info",
+    "add",
+    "download_bundle_info",
+
+]

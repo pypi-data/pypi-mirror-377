ce7d3a1c341ea6ee959e9496c9abc440fdec6120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:13:44 +0000

    Add package maleo-utils/0.0.14/maleo_utils-0.0.14-py3-none-any.whl

---
+++ packages/maleo-utils/maleo_utils-0.0.14-py3-none-any.whl/maleo/utils/loaders/yaml.py
@@ -0,0 +1,17 @@
+import yaml
+from pathlib import Path
+from typing import Any, Union
+
+
+def from_path(path: Union[Path, str]) -> Any:
+    file_path = Path(path)
+
+    if not file_path.exists() or not file_path.is_file():
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    with file_path.open("r") as f:
+        return yaml.safe_load(f)
+
+
+def from_string(string: str) -> Any:
+    return yaml.safe_load(string)

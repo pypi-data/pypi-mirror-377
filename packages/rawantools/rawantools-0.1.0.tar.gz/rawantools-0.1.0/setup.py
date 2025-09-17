980812bbd7bc477975e43c6edde3f9f2fd32f697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:06:45 +0000

    Add package rawantools/0.1.0/rawantools-0.1.0.tar.gz

---
+++ packages/rawantools/rawantools-0.1.0.tar.gz/rawantools-0.1.0/setup.py
@@ -0,0 +1,21 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="rawantools",
+    version="0.1.0",
+    author="Rawan",
+    author_email="rawanradman328@gmail.com",  # بريدك الشخصي
+    description="مكتبة قوية توفر أدوات للنصوص، الرياضيات، العشوائية، البيانات والمطورين",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/rawantools",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)

9d813e5e875e97e6dd63b9f80a7f6c0d3fbfc20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:05:20 +0000

    Add package kanuni-layer-sdk/0.1.1/kanuni_layer_sdk-0.1.1-py3-none-any.whl

---
+++ packages/kanuni-layer-sdk/kanuni_layer_sdk-0.1.1-py3-none-any.whl/main.py
@@ -0,0 +1,14 @@
+from modules.redactor import redactor
+from modules.validator import validate_prompt
+
+def main(prompt: str, opted_in: bool = True) -> str:
+    validated_prompt = validate_prompt(prompt)
+    redacted_prompt =  redactor(validated_prompt, opted_in)
+    return redacted_prompt
+
+if __name__ == "__main__":
+    response = main(
+        "My name is Jimmy Cricket with email <Jiminy.Cricket@example.com>, I am blind and deaf and mute. I am also quadriplegia and I would like to get directions to the nearest shopping centre. My credit card number is 4111 1111 1111 1111 and my phone number is +1 (555) 123-4567. Also, my SSN is 123-45-6789.",
+    )
+
+    print(response)

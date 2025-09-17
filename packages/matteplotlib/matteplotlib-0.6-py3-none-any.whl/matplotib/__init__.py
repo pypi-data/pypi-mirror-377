aca7283e0caf221ede769d441c128a2e2abd9736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 05:05:04 +0000

    Add package matteplotlib/0.6/matteplotlib-0.6-py3-none-any.whl

---
+++ packages/matteplotlib/matteplotlib-0.6-py3-none-any.whl/matplotib/__init__.py
@@ -0,0 +1,25 @@
+# __init__.py
+from .main import (
+    # --- Machine Learning (ML) Practicals ---
+    ml_help,
+    ml_index,
+    ml_prac_1a, ml_prac_1b, ml_prac_1c, ml_prac_1d,
+    ml_prac_2a,
+    ml_prac_3a, ml_prac_3b, ml_prac_3c,
+    ml_prac_4a, ml_prac_4b, ml_prac_4c, ml_prac_4d, ml_prac_4e, ml_prac_4f,
+    ml_prac_5a, ml_prac_5b,
+    ml_prac_6a, ml_prac_6b,
+    ml_prac_7a, ml_prac_7b,
+    ml_prac_8a,
+    ml_prac_9a,
+
+    # --- Advanced AI (AAI) Practicals ---
+    aai_setup,
+    aai_help,
+    aai_index,
+    aai_prac_1a, aai_prac_1b, aai_prac_1c,
+    aai_prac_2a, aai_prac_2b,
+    aai_prac_3a,
+    aai_prac_4a,
+    aai_prac_5a,
+)

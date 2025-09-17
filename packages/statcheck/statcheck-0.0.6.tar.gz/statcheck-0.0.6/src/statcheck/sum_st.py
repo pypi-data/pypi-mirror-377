5d51caa7e2ff3e3ee6e8d2152b5fd5039c282ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:05:30 +0000

    Add package statcheck/0.0.6/statcheck-0.0.6.tar.gz

---
+++ packages/statcheck/statcheck-0.0.6.tar.gz/statcheck-0.0.6/src/statcheck/sum_st.py
@@ -0,0 +1,43 @@
+import pandas as pd
+# suppress pandas warnings
+pd.options.mode.chained_assignment = None
+def summary_statcheck(Res):
+    """
+    Summarizes the results of statcheck
+    :param Res: output of statcheck
+    :return: summary of results
+    """
+
+    # Source
+    source = list(Res['Source'].unique())
+    source.append("Total")
+
+    # groupby Source
+    p_values = Res.groupby('Source').count()
+    p_values = list(p_values['Reported_P_Value'].values)
+    p_values.append(len(Res))
+
+    # Number of errors per article and in total
+    errors = Res.groupby('Source').sum()
+    errors = list(errors['Error'].values)
+    errors.append(sum(errors))
+
+    # Number of decision errors per article and in total
+    decision_errors = Res.groupby('Source').sum()
+    decision_errors = list(decision_errors['Decision_Error'].values)
+    decision_errors.append(sum(decision_errors))
+
+    # Results in dataframe
+    res = pd.DataFrame(
+        {
+            "Source": source,
+            "pValues": p_values,
+            "Errors": errors,
+            "Decision_Errors": decision_errors
+        }
+    )
+
+    # Rename columns based on constants in constants.R file
+    res.columns = ['Source', 'pValues', 'Errors', 'Decision_Errors']
+
+    return res

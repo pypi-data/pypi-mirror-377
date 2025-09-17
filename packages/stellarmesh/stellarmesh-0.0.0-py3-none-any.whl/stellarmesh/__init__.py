e117727fdab0f3ac3479fc5ee4ec4d0a2caa1699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:44 +0000

    Add package stellarmesh/0.0.0/stellarmesh-0.0.0-py3-none-any.whl

---
+++ packages/stellarmesh/stellarmesh-0.0.0-py3-none-any.whl/stellarmesh/__init__.py
@@ -0,0 +1,34 @@
+"""Gmsh wrapper and DAGMC geometry creator."""
+
+from .geometry import Geometry
+from .mesh import (
+    GmshSurfaceAlgo,
+    GmshSurfaceOptions,
+    GmshVolumeAlgo,
+    GmshVolumeOptions,
+    Mesh,
+    OCCSurfaceAlgo,
+    OCCSurfaceOptions,
+    SurfaceMesh,
+    VolumeMesh,
+)
+from .moab import DAGMCModel, DAGMCSurface, DAGMCVolume, MOABModel
+
+__all__ = [
+    "DAGMCModel",
+    "DAGMCSurface",
+    "DAGMCVolume",
+    "Geometry",
+    "GmshSurfaceAlgo",
+    "GmshSurfaceOptions",
+    "GmshVolumeAlgo",
+    "GmshVolumeOptions",
+    "MOABModel",
+    "Mesh",
+    "OCCSurfaceAlgo",
+    "OCCSurfaceOptions",
+    "SurfaceMesh",
+    "VolumeMesh",
+]
+
+__version__ = "0.1.0"

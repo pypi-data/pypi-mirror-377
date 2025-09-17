8d0d7503ecd31eed8788ff4967e3ef11b9288578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 05:05:07 +0000

    Add package programming-game/0.0.17/programming_game-0.0.17.tar.gz

---
+++ packages/programming-game/programming_game-0.0.17.tar.gz/programming_game-0.0.17/programming_game/tasks/task_meta.py
@@ -0,0 +1,14 @@
+class TaskMeta(type):
+    def __new__(cls, name, bases, attrs):
+        _on_tick_method = []
+
+        for _attr_name, attr_value in attrs.items():
+            if getattr(attr_value, "_on_tick_method", False):
+                _on_tick_method.append(attr_value)
+                attr_value._last_run = 0
+                if interval := getattr(attr_value, "interval", None):
+                    attr_value._interval = interval
+
+        attrs["_on_tick_method"] = _on_tick_method
+
+        return super().__new__(cls, name, bases, attrs)

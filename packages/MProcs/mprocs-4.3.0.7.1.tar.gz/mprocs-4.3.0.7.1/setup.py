b42cbac5fa8e44b003b1792eaa47341aa7d0d0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:05:42 +0000

    Add package MProcs/4.3.0.7.1/mprocs-4.3.0.7.1.tar.gz

---
+++ packages/MProcs/mprocs-4.3.0.7.1.tar.gz/mprocs-4.3.0.7.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='MProcs',
+    version='4.3.0.7.1',
+    entry_points={
+        'console_scripts': [
+            'MProcs=MProcs'
+        ]
+    }
+)

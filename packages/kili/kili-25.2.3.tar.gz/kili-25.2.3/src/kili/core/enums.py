7d42a143f76ac7e4736305faea0660c1f7907555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:05:41 +0000

    Add package kili/25.2.3/kili-25.2.3.tar.gz

---
+++ packages/kili/kili-25.2.3.tar.gz/kili-25.2.3/src/kili/core/enums.py
@@ -0,0 +1,104 @@
+"""DEPRECATED.
+
+From https://docs.kili-technology.com/reference/graphql-api#enums.
+"""
+
+from typing import Literal
+
+LicenseType = Literal[
+    "DISCOVERY",
+    "FREEMIUM",
+    "PAID",
+]
+
+
+LockType = Literal[
+    "REVIEW",
+    "DEFAULT",
+]
+
+
+NotificationStatus = Literal[
+    "FAILURE",
+    "PENDING",
+    "SUCCESS",
+]
+
+
+OrganizationRole = Literal[
+    "ADMIN",
+    "USER",
+]
+
+
+ProjectRole = Literal[
+    "ADMIN",
+    "TEAM_MANAGER",
+    "REVIEWER",
+    "LABELER",
+]
+
+
+ProjectType = Literal[
+    "IMAGE_CLASSIFICATION_SINGLE",
+    "IMAGE_CLASSIFICATION_MULTI",
+    "IMAGE_OBJECT_DETECTION_RECTANGLE",
+    "IMAGE_OBJECT_DETECTION_POLYGON",
+    "IMAGE_OBJECT_DETECTION_SEMANTIC",
+    "IMAGE_POSE_ESTIMATION",
+    "OCR",
+    "PDF_CLASSIFICATION_SINGLE",
+    "PDF_CLASSIFICATION_MULTI",
+    "PDF_OBJECT_DETECTION_RECTANGLE",
+    "PDF_NAMED_ENTITY_RECOGNITION",
+    "SPEECH_TO_TEXT",
+    "TEXT_CLASSIFICATION_SINGLE",
+    "TEXT_CLASSIFICATION_MULTI",
+    "TEXT_TRANSCRIPTION",
+    "TEXT_NER",
+    "TIME_SERIES",
+    "VIDEO_CLASSIFICATION_SINGLE",
+    "VIDEO_OBJECT_DETECTION",
+    "VIDEO_FRAME_CLASSIFICATION",
+    "VIDEO_FRAME_OBJECT_TRACKING",
+]
+
+
+DemoProjectType = Literal[
+    "DEMO_COMPUTER_VISION_TUTORIAL",
+    "DEMO_TEXT_TUTORIAL",
+    "DEMO_PDF_TUTORIAL",
+    "VIDEO_FRAME_OBJECT_TRACKING",
+    "DEMO_SEGMENTATION_COCO",
+    "DEMO_NER",
+    "DEMO_ID_OCR",
+    "DEMO_REVIEWS",
+    "DEMO_OCR",
+    "DEMO_NER_TWEETS",
+    "DEMO_PLASTIC",
+    "DEMO_CHATBOT",
+    "DEMO_PDF",
+    "DEMO_ANIMALS",
+    "DEMO_LLM",
+    "DEMO_LLM_INSTR_FOLLOWING",
+    "DEMO_SEGMENTATION",
+]
+
+
+Right = Literal[
+    "CAN_ACCESS_SMART_TOOLS",
+    "CAN_LABEL",
+    "CREATE_AUDIO",
+    "CREATE_FRAME",
+    "CREATE_IMAGE",
+    "CREATE_PDF",
+    "CREATE_TEXT",
+    "CREATE_TIMESERIES",
+    "CREATE_VIDEO",
+    "MAKE_PUBLIC_PROJECT",
+    "SEE_LICENSE_BANNER",
+    "SEE_UPGRADE_BUTTON",
+    "UPLOAD_CLOUD_DATA",
+    "USE_API",
+    "USE_API_PRIORITY",
+]

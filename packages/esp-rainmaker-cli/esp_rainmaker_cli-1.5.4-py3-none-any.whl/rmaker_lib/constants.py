cc440c393e54cd2c0d63697032c895649694f407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:14:30 +0000

    Add package esp-rainmaker-cli/1.5.4/esp_rainmaker_cli-1.5.4-py3-none-any.whl

---
+++ packages/esp-rainmaker-cli/esp_rainmaker_cli-1.5.4-py3-none-any.whl/rmaker_lib/constants.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2020-2025 Espressif Systems (Shanghai) CO LTD
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Filename
+NODE = 'node'
+ENDPOINT = 'endpoint'
+NODE_INFO = 'node_info'
+
+# File Extensions
+DOT_INFO = '.info'
+DOT_KEY = '.key'
+DOT_CRT = '.crt'
+DOT_CSV = '.csv'
+
+HOST = "host"
+
+HOME_DIRECTORY = '~/'
+
+RM_CONFIG_FILE = 'rainmaker_config.json'
+
+

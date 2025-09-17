82d73c67f1f2e5698afc2ca3c1da2eda4002ff73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:13:48 +0000

    Add package shapes3d/0.1.0/shapes3d-0.1.0.tar.gz

---
+++ packages/shapes3d/shapes3d-0.1.0.tar.gz/shapes3d-0.1.0/shapes3d/__init__.py
@@ -0,0 +1,9 @@
+from .shapes3_area import (
+    area_of_cube,
+    area_of_cuboid,
+    area_of_cylinder,
+    area_of_cone,
+    area_of_sphere,
+)
+
+__all__ = ["area_of_cube", "area_of_cuboid", "area_of_cylinder", "area_of_cone", "area_of_sphere"]

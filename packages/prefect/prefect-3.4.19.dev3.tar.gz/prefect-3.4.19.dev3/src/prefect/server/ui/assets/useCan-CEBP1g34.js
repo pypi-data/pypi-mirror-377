2d2192c36adc404e10a60668cc025180bc537bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:04:49 +0000

    Add package prefect/3.4.19.dev3/prefect-3.4.19.dev3.tar.gz

---
+++ packages/prefect/prefect-3.4.19.dev3.tar.gz/prefect-3.4.19.dev3/src/prefect/server/ui/assets/useCan-CEBP1g34.js
@@ -0,0 +1,2 @@
+import{cH as n}from"./index-ChnDZ-CZ.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
+//# sourceMappingURL=useCan-CEBP1g34.js.map
---
+++ packages/prefect/prefect-3.4.19.dev3.tar.gz/prefect-3.4.19.dev3/src/prefect/server/ui/assets/usePrefectApi-vEH0yk2e.js
@@ -0,0 +1,2 @@
+import{cH as r}from"./index-ChnDZ-CZ.js";import{p as e}from"./api-BcAcaCW5.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-vEH0yk2e.js.map

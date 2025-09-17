3b993bf28cabe526939a159904d435d9ea09c522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:05:38 +0000

    Add package DBSamizdat/0.11.0/dbsamizdat-0.11.0.tar.gz

---
+++ packages/DBSamizdat/dbsamizdat-0.11.0.tar.gz/dbsamizdat-0.11.0/src/dbsamizdat/__init__.py
@@ -0,0 +1,3 @@
+from .samizdat import entitypes, SamizdatView, SamizdatMaterializedView, SamizdatFunction, SamizdatTrigger  # noqa F401
+
+default_app_config = 'dbsamizdat.apps.DBSamizdatConfig'  # For Django < 4.0

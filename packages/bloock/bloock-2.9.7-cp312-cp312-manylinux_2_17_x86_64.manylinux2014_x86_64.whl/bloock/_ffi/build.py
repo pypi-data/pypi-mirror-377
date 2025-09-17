44ae4fe9ed084722c90e7790cc20a10c59b29e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 05:05:24 +0000

    Add package bloock/2.9.7/bloock-2.9.7-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/bloock/bloock-2.9.7-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/bloock/_ffi/build.py
@@ -0,0 +1,62 @@
+import os
+import platform
+import sys
+from typing import List
+
+from cffi import FFI
+
+ffi_builder = FFI()
+
+env = os.environ.get("BLOOCK_ENV", "DEVELOPMENT")
+
+if env == "CI":
+    lib_dirs = "bloock/_ffi/native"
+else:
+    if platform.machine() == "x86_64" and sys.platform.startswith("linux"):
+        lib_dirs = "bloock/_ffi/native/x86_64-unknown-linux-musl"
+    elif sys.platform.startswith("linux"):
+        lib_dirs = "bloock/_ffi/native/aarch64-unknown-linux-musl"
+    elif sys.platform.startswith("win"):
+        lib_dirs = "bloock/_ffi/native/x86_64-pc-windows-msvc"
+    elif platform.machine() == "x86_64" and sys.platform.startswith("darwin"):
+        lib_dirs = "bloock/_ffi/native/x86_64-apple-darwin"
+    else:
+        lib_dirs = "bloock/_ffi/native/aarch64-apple-darwin"
+
+include_dir = "bloock/_ffi/native"
+
+libs: List[str] = []
+if sys.platform.startswith("win"):
+    libs.extend(
+        (
+            f"{lib_dirs}/bloock_bridge.lib",
+            "Ws2_32.lib",
+            "advapi32.lib",
+            "userenv.lib",
+            "bcrypt.lib",
+            "ntdll.lib",
+        )
+    )
+elif sys.platform.startswith("darwin"):
+    libs.extend(("-framework", "Security"))
+    libs.append(f"{lib_dirs}/libbloock_bridge.a")
+else:
+    libs.append(f"{lib_dirs}/libbloock_bridge.a")
+
+ffi_builder.set_source(
+    "_bloock_bridge_lib",
+    r"""
+    #include "bloock_bridge.h"
+    """,
+    library_dirs=[lib_dirs],
+    include_dirs=[include_dir],
+    libraries=["rt"] if sys.platform.startswith("linux") else [],
+    extra_link_args=libs,
+)
+
+with open(f"{include_dir}/bloock_bridge.h") as f:
+    header = f.read()
+    ffi_builder.cdef(header)
+
+if __name__ == "__main__":  # not when running with setuptools
+    ffi_builder.compile(verbose=True)

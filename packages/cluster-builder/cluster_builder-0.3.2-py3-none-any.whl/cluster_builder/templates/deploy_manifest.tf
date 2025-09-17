a6a636fa1b57d9f433739369dd9230edf5309036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:07:22 +0000

    Add package cluster-builder/0.3.2/cluster_builder-0.3.2-py3-none-any.whl

---
+++ packages/cluster-builder/cluster_builder-0.3.2-py3-none-any.whl/cluster_builder/templates/deploy_manifest.tf
@@ -0,0 +1,43 @@
+# main.tf
+
+variable "manifest_folder" {}
+variable "ssh_private_key_path" {}
+variable "master_ip" {}
+variable "ssh_user" {}
+
+resource "null_resource" "copy_manifests" {
+  connection {
+    type        = "ssh"
+    user        = var.ssh_user
+    private_key = file(var.ssh_private_key_path)
+    host        = var.master_ip
+  }
+
+  # Ensure the manifests folder exists on the remote host
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/${var.ssh_user}/manifests",
+      "sudo chmod 755 /home/${var.ssh_user}/manifests"
+    ]
+  }
+
+  # Copy the manifests
+  provisioner "file" {
+    source      = var.manifest_folder
+    destination = "/home/${var.ssh_user}"
+  }
+
+  # Apply namespace.yaml first
+  provisioner "remote-exec" {
+    inline = [
+      "sudo -E KUBECONFIG=/etc/rancher/k3s/k3s.yaml kubectl apply -f /home/${var.ssh_user}/manifests/namespace.yaml"
+    ]
+  }
+
+  # Apply the rest of the manifests
+  provisioner "remote-exec" {
+    inline = [
+      "sudo -E KUBECONFIG=/etc/rancher/k3s/k3s.yaml kubectl apply -R -f /home/${var.ssh_user}/manifests/ --selector='!namespace.yaml'"
+    ]
+  }
+}

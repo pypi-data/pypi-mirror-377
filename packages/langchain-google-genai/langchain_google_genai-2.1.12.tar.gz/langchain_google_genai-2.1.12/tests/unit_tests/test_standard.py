92d25ebe22826981d9c475372e75082d90d5b0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 14:05:51 +0000

    Add package langchain-google-genai/2.1.12/langchain_google_genai-2.1.12.tar.gz

---
+++ packages/langchain-google-genai/langchain_google_genai-2.1.12.tar.gz/langchain_google_genai-2.1.12/tests/unit_tests/test_standard.py
@@ -0,0 +1,40 @@
+from langchain_core.language_models import BaseChatModel
+from langchain_tests.unit_tests import ChatModelUnitTests
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+class TestGeminiAIStandard(ChatModelUnitTests):
+    @property
+    def chat_model_class(self) -> type[BaseChatModel]:
+        return ChatGoogleGenerativeAI
+
+    @property
+    def chat_model_params(self) -> dict:
+        return {"model": "models/gemini-1.0-pro-001"}
+
+    @property
+    def init_from_env_params(self) -> tuple[dict, dict, dict]:
+        return (
+            {"GOOGLE_API_KEY": "api_key"},
+            self.chat_model_params,
+            {"google_api_key": "api_key"},
+        )
+
+
+class TestGemini15AIStandard(ChatModelUnitTests):
+    @property
+    def chat_model_class(self) -> type[BaseChatModel]:
+        return ChatGoogleGenerativeAI
+
+    @property
+    def chat_model_params(self) -> dict:
+        return {"model": "models/gemini-1.5-pro-001"}
+
+    @property
+    def init_from_env_params(self) -> tuple[dict, dict, dict]:
+        return (
+            {"GOOGLE_API_KEY": "api_key"},
+            self.chat_model_params,
+            {"google_api_key": "api_key"},
+        )

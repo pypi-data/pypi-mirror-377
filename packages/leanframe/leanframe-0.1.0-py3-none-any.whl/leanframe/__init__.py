f524ca1cfc676e8f8676f453df0a0a3af9aacf10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:07:31 +0000

    Add package leanframe/0.1.0/leanframe-0.1.0-py3-none-any.whl

---
+++ packages/leanframe/leanframe-0.1.0-py3-none-any.whl/leanframe/__init__.py
@@ -0,0 +1,23 @@
+# Copyright 2025 Google LLC, LeanFrame Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""LeanFrame provides a DataFrame API for BigQuery."""
+
+from leanframe.core.session import Session
+from leanframe.version import __version__
+
+__all__ = [
+    "__version__",
+    "Session",
+]

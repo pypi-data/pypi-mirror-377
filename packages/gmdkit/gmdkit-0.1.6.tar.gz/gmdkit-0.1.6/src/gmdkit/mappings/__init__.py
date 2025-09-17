f64665bf4a2b1534fa67285982c96978f4643451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:07:30 +0000

    Add package gmdkit/0.1.6/gmdkit-0.1.6.tar.gz

---
+++ packages/gmdkit/gmdkit-0.1.6.tar.gz/gmdkit-0.1.6/src/gmdkit/mappings/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["color_id","color_prop","obj_id","lvl_id","prop_id","obj_groups"]
+
+from gmdkit.mappings.color_ids import color_id
+from gmdkit.mappings.object_ids import obj_id
+from gmdkit.mappings.level_props import lvl_id
+from gmdkit.mappings.object_props import prop_id
+from gmdkit.mappings.color_props import  color_prop

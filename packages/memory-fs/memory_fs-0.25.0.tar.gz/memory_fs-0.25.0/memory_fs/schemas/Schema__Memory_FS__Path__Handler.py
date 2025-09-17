17d77d28e7f8af6fc1436622156dd83974aa27bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 14:05:42 +0000

    Add package memory-fs/0.25.0/memory_fs-0.25.0.tar.gz

---
+++ packages/memory-fs/memory_fs-0.25.0.tar.gz/memory_fs-0.25.0/memory_fs/schemas/Schema__Memory_FS__Path__Handler.py
@@ -0,0 +1,6 @@
+from osbot_utils.type_safe.primitives.domains.identifiers.Safe_Id     import Safe_Id
+from osbot_utils.type_safe.Type_Safe import Type_Safe
+
+class Schema__Memory_FS__Path__Handler(Type_Safe):
+    name            : Safe_Id
+    enabled         : bool = True

e31d60858d4fa5d7effc48cc2e56bd25176981a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:14:17 +0000

    Add package opik/1.8.47/opik-1.8.47.tar.gz

---
+++ packages/opik/opik-1.8.47.tar.gz/opik-1.8.47/src/opik/evaluation/evaluation_result.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+
+import dataclasses
+
+from . import test_result
+
+
+@dataclasses.dataclass
+class EvaluationResult:
+    experiment_id: str
+    dataset_id: str
+    experiment_name: Optional[str]
+    test_results: List[test_result.TestResult]
+    experiment_url: Optional[str]

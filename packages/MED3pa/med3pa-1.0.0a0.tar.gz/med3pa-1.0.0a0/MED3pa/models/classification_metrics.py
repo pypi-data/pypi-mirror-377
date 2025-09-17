4a0f85ac609b7e3cb0a76256555bd81780ecb999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:12 +0000

    Add package MED3pa/1.0.0a0/med3pa-1.0.0a0.tar.gz

---
+++ packages/MED3pa/med3pa-1.0.0a0.tar.gz/med3pa-1.0.0a0/MED3pa/models/classification_metrics.py
@@ -0,0 +1,314 @@
+"""
+The ``classification_metrics.py`` module defines the ``ClassificationEvaluationMetrics`` class, 
+that contains various classification metrics that can be used to assess the model's performance. 
+"""
+
+import numpy as np
+import warnings
+from collections.abc import Callable
+from sklearn.metrics import (accuracy_score, average_precision_score, f1_score, log_loss, matthews_corrcoef,
+                             precision_score, recall_score, roc_auc_score)
+from typing import List, Optional
+
+from .abstract_metrics import EvaluationMetric
+
+
+class ClassificationEvaluationMetrics(EvaluationMetric):
+    """
+    A class to compute various classification evaluation metrics.
+    """
+
+    @staticmethod
+    def accuracy(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None, **kwargs) -> Optional[float]:
+        """
+        Calculate the accuracy score.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Accuracy score.
+        """
+        if y_true.size == 0 or y_pred.size == 0:
+            return None
+        return accuracy_score(y_true, y_pred, sample_weight=sample_weight)
+
+    @staticmethod
+    def recall(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None, **kwargs) -> Optional[float]:
+        """
+        Calculate the recall score.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Recall score.
+        """
+        if y_true.size == 0 or y_pred.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return recall_score(y_true, y_pred, sample_weight=sample_weight, zero_division=0)
+
+    @staticmethod
+    def roc_auc(y_true: np.ndarray, y_prob: np.ndarray, sample_weight: np.ndarray = None, **kwargs) -> Optional[float]:
+        """
+        Calculate the ROC AUC score.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_prob (np.ndarray): Predicted probabilities.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: ROC AUC score.
+        """
+        if y_true.size == 0 or y_prob.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return roc_auc_score(y_true, y_prob, sample_weight=sample_weight)
+
+    @staticmethod
+    def average_precision(y_true: np.ndarray, y_prob: np.ndarray, sample_weight: np.ndarray = None, **kwargs
+                          ) -> Optional[float]:
+        """
+        Calculate the average precision score.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_prob (np.ndarray): Predicted probabilities.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Average precision score.
+        """
+        if y_true.size == 0 or y_prob.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return average_precision_score(y_true, y_prob, sample_weight=sample_weight)
+
+    @staticmethod
+    def matthews_corrcoef(y_true: np.ndarray, y_pred: np.ndarray, **kwargs
+                          ) -> Optional[float]:
+        """
+        Calculate the Matthews correlation coefficient.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+
+        Returns:
+            float: Matthews correlation coefficient.
+        """
+        if y_true.size == 0 or y_pred.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return matthews_corrcoef(y_true, y_pred)
+
+    @staticmethod
+    def precision(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None, **kwargs
+                  ) -> Optional[float]:
+        """
+        Calculate the precision score.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Precision score.
+        """
+        if y_true.size == 0 or y_pred.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return precision_score(y_true, y_pred, sample_weight=sample_weight, zero_division=0)
+
+    @staticmethod
+    def f1_score(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None, **kwargs) -> Optional[float]:
+        """
+        Calculate the F1 score.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: F1 score.
+        """
+        if y_true.size == 0 or y_pred.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return f1_score(y_true, y_pred, sample_weight=sample_weight, zero_division=0)
+
+    @staticmethod
+    def sensitivity(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None, **kwargs
+                    ) -> Optional[float]:
+        """
+        Calculate the sensitivity (recall for the positive class).
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Sensitivity score.
+        """
+        if y_true.size == 0 or y_pred.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return recall_score(y_true, y_pred, pos_label=1, sample_weight=sample_weight, zero_division=0)
+
+    @staticmethod
+    def specificity(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None, **kwargs
+                    ) -> Optional[float]:
+        """
+        Calculate the specificity (recall for the negative class).
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Specificity score.
+        """
+        if y_true.size == 0 or y_pred.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return recall_score(y_true, y_pred, pos_label=0, sample_weight=sample_weight, zero_division=0)
+
+    @staticmethod
+    def ppv(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None, **kwargs) -> Optional[float]:
+        """
+        Calculate the positive predictive value (PPV).
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Positive predictive value.
+        """
+        if y_true.size == 0 or y_pred.size == 0:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return precision_score(y_true, y_pred, pos_label=1, sample_weight=sample_weight, zero_division=0)
+
+    @staticmethod
+    def npv(y_true: np.ndarray, y_pred: np.ndarray, sample_weight: np.ndarray = None, **kwargs) -> Optional[float]:
+        """
+        Calculate the negative predictive value (NPV).
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Negative predictive value.
+        """
+        if y_true.size == 0 or y_pred.size == 0:
+            return None
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return precision_score(y_true, y_pred, pos_label=0, sample_weight=sample_weight, zero_division=0)
+
+    @staticmethod
+    def balanced_accuracy(y_true: np.ndarray, y_pred: np.ndarray, **kwargs) -> Optional[float]:
+        """
+        Calculate the balanced accuracy score.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_pred (np.ndarray): Predicted labels.
+
+        Returns:
+            float: Balanced accuracy score.
+        """
+        if y_true.size == 0 or y_pred.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        sens = ClassificationEvaluationMetrics.sensitivity(y_true, y_pred)
+        spec = ClassificationEvaluationMetrics.specificity(y_true, y_pred)
+        if sens is not None and spec is not None:
+            return (sens + spec) / 2
+        else:
+            return None
+
+    @staticmethod
+    def log_loss(y_true: np.ndarray, y_prob: np.ndarray, sample_weight: np.ndarray = None, **kwargs) -> Optional[float]:
+        """
+        Calculate the log loss score.
+
+        Args:
+            y_true (np.ndarray): True labels.
+            y_prob (np.ndarray): Predicted probabilities.
+            sample_weight (np.ndarray, optional): Sample weights.
+
+        Returns:
+            float: Log loss score.
+        """
+        # return np.random.rand()
+        if y_true.size == 0 or y_prob.size == 0 or len(np.unique(y_true)) == 1:
+            return None
+        y_pred = np.clip(y_prob, 1e-15, 1 - 1e-15)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return log_loss(y_true, y_pred, sample_weight=sample_weight)
+
+    @classmethod
+    def get_metric(cls, metric_name: str = '') -> Callable | List[str]:
+        """
+        Get the metric function based on the metric name.
+
+        Args:
+            metric_name (str): The name of the metric.
+
+        Returns: function: The function corresponding to the metric, if metric_name is not ''. Otherwise returns the
+        list of supported classification metrics.
+        """
+        metrics_mappings = {
+            'Accuracy': cls.accuracy,
+            'BalancedAccuracy': cls.balanced_accuracy,
+            'Precision': cls.precision,
+            'F1Score': cls.f1_score,
+            'Specificity': cls.specificity,
+            'Sensitivity': cls.sensitivity,
+            'Auc': cls.roc_auc,
+            'LogLoss': cls.log_loss,
+            'Auprc': cls.average_precision,
+            'MCC': cls.matthews_corrcoef
+        }
+        if metric_name == '':
+            return list(metrics_mappings)
+        else:
+            metric_function = metrics_mappings.get(metric_name)
+            if metric_function is None:
+                raise ValueError(
+                    f"Metric '{metric_name}' is not recognized. Please choose from: {list(metrics_mappings)}")
+            return metric_function
+
+    @classmethod
+    def supported_metrics(cls) -> List[str]:
+        """
+        Get a list of supported classification metrics.
+
+        Returns:
+            list: A list of supported classification metrics.
+        """
+        return cls.get_metric()
---
+++ packages/MED3pa/med3pa-1.0.0a0.tar.gz/med3pa-1.0.0a0/MED3pa/models/factories.py
@@ -0,0 +1,260 @@
+"""
+This module utilizes the **Factory design pattern** to abstract the creation process of machine learning models. 
+It defines a general factory class and specialized factories for different model types, such as XGBoost. 
+This setup allows for dynamic model instantiation based on provided specifications or configurations. 
+By decoupling model creation from usage"""
+
+import json
+import pickle
+import re
+import warnings
+import xgboost as xgb
+from typing import Union
+
+from .abstract_models import Model
+from .concrete_classifiers import XGBoostModel
+
+
+class ModelFactory:
+    """
+    A factory class for creating models with different types, using the factory design pattern.
+    It supports creating models based on hyperparameters or loading them from pickled files.
+    """
+    
+    model_mapping = {
+        'XGBoostModel': [xgb.Booster, xgb.XGBClassifier],
+    }
+
+    factories = {
+        'XGBoostModel': lambda: XGBoostFactory(),
+    }
+
+    @staticmethod
+    def get_factory(model_type: str) -> 'ModelFactory':
+        """
+        Retrieves the factory object for the given model type.
+
+        Args:
+            model_type (str): The type of model for which the factory is to be retrieved.
+
+        Returns:
+            ModelFactory: An instance of the factory associated with the given model type.
+
+        Raises:
+            ValueError: If no factory is available for the given model type.
+        """
+        factory_initializer = ModelFactory.factories.get(model_type)
+        if factory_initializer:
+            return factory_initializer()
+        else:
+            raise ValueError(f"No factory available for model type: {model_type}")
+        
+    @staticmethod
+    def get_supported_models() -> list:
+        """
+        Retrieves a list of all supported model types.
+
+        Returns:
+            list: A list containing the keys from model_mapping which represent the supported model types.
+        """
+        return list(ModelFactory.model_mapping)
+        
+    @staticmethod
+    def create_model_with_hyperparams(model_type: str, hyperparams: dict) -> Model:
+        """
+        Creates a model of the specified type with the given hyperparameters.
+
+        Args:
+            model_type (str): The type of model to create.
+            hyperparams (dict): A dictionary of hyperparameters for the model.
+
+        Returns:
+            Model: A model instance of the specified type, initialized with the given hyperparameters.
+        """
+        factory = ModelFactory.get_factory(model_type)
+        return factory.create_model_with_hyperparams(hyperparams)
+
+    @staticmethod
+    def create_model_from_pickled(pickled_file_path: str) -> Model:
+        """
+        Creates a model by loading it from a pickled file.
+
+        Args:
+            pickled_file_path (str): The file path to the pickled model file.
+
+        Returns:
+            Model: A model instance loaded from the pickled file.
+
+        Raises:
+            IOError: If there is an error loading the model from the file.
+            TypeError: If the loaded model is not of a supported type.
+        """
+        warnings.filterwarnings("ignore", message=r".*WARNING.*", category=UserWarning, module="xgboost.core")
+        try:
+            with open(pickled_file_path, 'rb') as file:
+                loaded_model = pickle.load(file)
+        except Exception as e:
+            raise IOError(f"Failed to load the model from {pickled_file_path}: {e}")
+        
+        for model_type, model_classes in ModelFactory.model_mapping.items():
+            if any(isinstance(loaded_model, model_class) for model_class in model_classes):
+                factory = ModelFactory.get_factory(model_type)
+                return factory.create_model_from_pickled(pickled_file_path)
+
+        raise TypeError("The loaded model is not of a supported type")
+
+
+class XGBoostFactory(ModelFactory):
+    """
+    A factory for creating XGBoost model objects, either from hyperparameters or by loading from pickled files.
+    Inherits from ModelFactory and specifies creation methods for XGBoost models.
+    """
+    
+    def create_model_with_hyperparams(self, hyperparams: dict) -> XGBoostModel:
+        """
+        Creates an XGBoostModel with the given hyperparameters.
+
+        Args:
+            hyperparams (dict): A dictionary of hyperparameters for the XGBoost model.
+
+        Returns:
+            XGBoostModel: An instance of XGBoostModel initialized with the given hyperparameters.
+        """
+        return XGBoostModel(params=hyperparams)
+
+    def create_model_from_pickled(self, pickled_file_path: str) -> XGBoostModel:
+        """
+        Recreates an XGBoostModel from a loaded pickled model.
+
+        Args:
+            pickled_file_path (str): The file path to the pickled model file.
+
+        Returns:
+            XGBoostModel: An instance of XGBoostModel created from the loaded model.
+
+        Raises:
+            IOError: If there is an error loading the model from the file.
+            TypeError: If the loaded model is not a supported implementation of the XGBoost model.
+            ValueError: If the XGBoost model version is not supported.
+        """
+        warnings.filterwarnings("ignore", message=r".*WARNING.*", category=UserWarning, module="xgboost.core")
+        try:
+            with open(pickled_file_path, 'rb') as file:
+                loaded_model = pickle.load(file)
+        except Exception as e:
+            raise IOError(f"Failed to load the model from {pickled_file_path}: {e}")
+        
+        if isinstance(loaded_model, (xgb.Booster, xgb.XGBClassifier)):
+            if self.check_version(loaded_model):
+                extracted_params = self.extract_params(loaded_model)
+                xgb_model =  XGBoostModel(params=extracted_params, model=loaded_model)
+                xgb_model.set_file_path(file_path=pickled_file_path)
+                return xgb_model
+            else:
+                raise ValueError("XGBoost model version is not supported. Please use version 2.0.0 or later.")
+        else:
+            raise TypeError("Loaded model is not an XGBoost model")
+
+    def check_version(self, loaded_model: Union[xgb.Booster, xgb.XGBClassifier]) -> bool:
+        """
+        Checks the version of the loaded XGBoost model to ensure it is supported.
+
+        Args:
+            loaded_model (xgb.Booster | xgb.XGBClassifier): The loaded model object.
+
+        Returns:
+            bool: True if the model version is supported, False otherwise.
+        """
+        config_json = loaded_model.save_config()
+        config = json.loads(config_json)
+        version_list = config.get('version', 'Not available')
+        if isinstance(version_list, list):
+            version_str = '.'.join(map(str, version_list))
+        else:
+            version_str = version_list
+
+        version_match = re.match(r'(\d+)\.(\d+)\.(\d+)', version_str)
+        if version_match:
+            major, minor, patch = map(int, version_match.groups())
+            return (major, minor, patch) >= (2, 0, 0)
+        else:
+            return False
+
+    def extract_params(self, loaded_model: Union[xgb.Booster, xgb.XGBClassifier]) -> dict:
+        """
+        Extracts the parameters from a loaded XGBoost model.
+
+        Args:
+            loaded_model (xgb.Booster | xgb.XGBClassifier): The loaded model object.
+
+        Returns:
+            dict: A dictionary of extracted parameters.
+        """
+        try:
+            boosted_rounds = loaded_model.num_boosted_rounds()
+            config_json = loaded_model.save_config()
+            config = json.loads(config_json)
+        except AttributeError as e:
+            print(f"Error extracting configuration from model: {e}")
+            return {}
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON configuration: {e}")
+            return {}
+
+        try:
+            learner = config['learner']
+            gradient_booster = learner['gradient_booster']
+            
+            general_params = learner['generic_param']
+            booster_params = gradient_booster.get('gbtree_train_param', {})
+            tree_params = gradient_booster.get('tree_train_param', {})
+            
+            updater_params = {}
+            if 'updater' in gradient_booster and isinstance(gradient_booster['updater'], list):
+                for updater in gradient_booster['updater']:
+                    if 'hist_train_param' in updater:
+                        updater_params.update(updater['hist_train_param'])
+            
+            learning_task_params = learner['learner_train_param']
+            objective_params = learner['objective'].get('reg_loss_param', {})
+            learner_model_params = learner['learner_model_param']
+
+            params = {}
+            params.update(general_params)
+            params.update(booster_params)
+            params.update(tree_params)
+            params.update(updater_params)
+            params.update(learning_task_params)
+            params.update(objective_params)
+            params.update(learner_model_params)
+
+            if 'metrics' in learner:
+                metrics = [metric['name'] for metric in learner['metrics']]
+                if metrics:
+                    params['eval_metric'] = metrics
+            params['num_boost_rounds'] = boosted_rounds
+
+            for key, value in params.items():
+                try:
+                    if isinstance(value, str):
+                        if '.' in value or 'E' in value or 'e' in value:
+                            params[key] = float(value)
+                        elif value.isdigit():
+                            params[key] = int(value)
+                        else:
+                            # Skip conversion for non-numeric strings
+                            continue
+                    else:
+                        params[key] = int(value)
+                except (ValueError, TypeError) as e:
+                    continue
+
+            removable_keys = ['num_trees']
+            for key in removable_keys:
+                params.pop(key, None)
+
+        except KeyError as e:
+            print(f"Key error while extracting parameters: {e}")
+            return {}
+
+        return params

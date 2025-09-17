885816b033dacffff33fc06e7f2abd890be4a5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:04:55 +0000

    Add package sift-stack-py/0.9.0/sift_stack_py-0.9.0-py3-none-any.whl

---
+++ packages/sift-stack-py/sift_stack_py-0.9.0-py3-none-any.whl/sift_client/_internal/low_level_wrappers/base.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from abc import ABC
+from typing import Any, Callable
+
+
+class LowLevelClientBase(ABC):
+    @staticmethod
+    async def _handle_pagination(
+        func: Callable,
+        kwargs: dict[str, Any] | None = None,
+        page_size: int | None = None,
+        page_token: str | None = None,
+        order_by: str | None = None,
+        max_results: int | None = None,
+    ) -> list[Any]:
+        """Handle pagination for a given function by calling the function until all results are retrieved or the max_results is reached.
+
+        Args:
+            func: The function to call.
+            kwargs: Keyword arguments to pass to the function.
+            page_size: The number of results to return per page.
+            page_token: The token to use for the next page.
+            order_by: How to order the retrieved results.
+            max_results: Maximum number of results to return.  NOTE: Will be in increments of page_size or default page size defined by the call if no page_size is provided.
+
+        Returns:
+            A list of all matching results.
+        """
+        if kwargs is None:
+            kwargs = {}
+
+        results: list[Any] = []
+        if page_token is None:
+            page_token = ""
+        while True:
+            if max_results is not None and len(results) >= max_results:
+                break
+            response, page_token = await func(
+                page_size=page_size,
+                page_token=page_token,
+                order_by=order_by,
+                **kwargs,
+            )
+            results.extend(response)
+            if page_token == "":
+                break
+        return results
---
+++ packages/sift-stack-py/sift_stack_py-0.9.0-py3-none-any.whl/sift_client/sift_types/__init__.py
@@ -0,0 +1,41 @@
+from sift_client.sift_types.asset import Asset, AssetUpdate
+from sift_client.sift_types.calculated_channel import (
+    CalculatedChannel,
+    CalculatedChannelUpdate,
+)
+from sift_client.sift_types.channel import (
+    Channel,
+    ChannelBitFieldElement,
+    ChannelDataType,
+    ChannelReference,
+)
+from sift_client.sift_types.ingestion import IngestionConfig
+from sift_client.sift_types.rule import (
+    Rule,
+    RuleAction,
+    RuleActionType,
+    RuleAnnotationType,
+    RuleUpdate,
+    RuleVersion,
+)
+from sift_client.sift_types.run import Run, RunUpdate
+
+__all__ = [
+    "Asset",
+    "AssetUpdate",
+    "CalculatedChannel",
+    "CalculatedChannelUpdate",
+    "Channel",
+    "ChannelBitFieldElement",
+    "ChannelDataType",
+    "ChannelReference",
+    "IngestionConfig",
+    "Rule",
+    "RuleAction",
+    "RuleActionType",
+    "RuleAnnotationType",
+    "RuleUpdate",
+    "RuleVersion",
+    "Run",
+    "RunUpdate",
+]

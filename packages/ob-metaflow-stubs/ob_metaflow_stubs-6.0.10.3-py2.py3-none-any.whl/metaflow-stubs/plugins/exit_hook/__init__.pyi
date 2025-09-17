44317625c6f4140a1b0215be42c7471d40437dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:23 +0000

    Add package ob-metaflow-stubs/6.0.10.3/ob_metaflow_stubs-6.0.10.3-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.10.3-py2.py3-none-any.whl/metaflow-stubs/plugins/exit_hook/__init__.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.5.1+obcheckpoint(0.2.5);ob(v1)                                                    #
+# Generated on 2025-09-16T18:01:26.362410                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import exit_hook_decorator as exit_hook_decorator
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.10.3-py2.py3-none-any.whl/metaflow-stubs/profilers/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.5.1+obcheckpoint(0.2.5);ob(v1)                                                    #
+# Generated on 2025-09-16T18:01:26.306696                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ..mf_extensions.outerbounds.profilers import gpu as gpu
+from ..mf_extensions.outerbounds.profilers.gpu import gpu_profile as gpu_profile
+

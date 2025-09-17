f6d092f84d49e2a2e6a1e5004e4d7553b5ab8eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:05:22 +0000

    Add package hestia-earth-models/0.74.9/hestia_earth_models-0.74.9.tar.gz

---
+++ packages/hestia-earth-models/hestia_earth_models-0.74.9.tar.gz/hestia_earth_models-0.74.9/tests/models/utils/test_indicator.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+from tests.utils import TERM
+
+from hestia_earth.models.utils.indicator import _new_indicator
+
+class_path = 'hestia_earth.models.utils.indicator'
+
+
+@patch(f"{class_path}.include_methodModel", side_effect=lambda n, x: n)
+@patch(f"{class_path}.download_term", return_value=TERM)
+def test_new_indicator(*args):
+    # with a Term as string
+    indicator = _new_indicator('term')
+    assert indicator == {
+        '@type': 'Indicator',
+        'term': TERM
+    }
+
+    # with a Term as dict
+    indicator = _new_indicator(TERM)
+    assert indicator == {
+        '@type': 'Indicator',
+        'term': TERM
+    }

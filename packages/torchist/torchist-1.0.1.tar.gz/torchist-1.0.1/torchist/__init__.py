281b64ebc4bf9cd3f538c6e2bc332c719a963d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:06:39 +0000

    Add package torchist/1.0.1/torchist-1.0.1.tar.gz

---
+++ packages/torchist/torchist-1.0.1.tar.gz/torchist-1.0.1/torchist/__init__.py
@@ -0,0 +1,11 @@
+r"""NumPy-style histograms in PyTorch"""
+
+__version__ = "1.0.1"
+
+from .histograms import (  # noqa: F401
+    histogram,
+    histogram_edges,
+    histogramdd,
+    ravel_multi_index,
+    unravel_index,
+)

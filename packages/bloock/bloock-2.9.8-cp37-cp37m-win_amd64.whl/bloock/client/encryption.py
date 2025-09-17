45c72eb8c262d379a50a40e2c66c68d21037aa95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:07 +0000

    Add package bloock/2.9.8/bloock-2.9.8-cp37-cp37m-win_amd64.whl

---
+++ packages/bloock/bloock-2.9.8-cp37-cp37m-win_amd64.whl/bloock/client/encryption.py
@@ -0,0 +1,107 @@
+from warnings import warn
+
+from bloock._bridge import bridge
+from bloock._bridge.proto.bloock_encryption_pb2 import (
+    EncryptRequest,
+    DecryptRequest,
+    EncryptionAlgRequest,
+)
+from bloock._bridge.proto.bloock_keys_pb2 import GenerateLocalKeyRequest
+from bloock._bridge.proto.bloock_shared_pb2 import Error
+from bloock._config.config import Config
+from bloock.entity.encryption.encrypter import Encrypter
+from bloock.entity.encryption.encryption_alg import EncryptionAlg
+from bloock.entity.key.key_pair import KeyPair
+from bloock.entity.key.key_type import KeyType
+from bloock.entity.key.rsa_key_pair import RsaKeyPair
+from bloock.entity.record.record import Record
+
+
+class EncryptionClient:
+    """
+    Represents a client for interacting with the [Bloock Encryption service](https://dashboard.bloock.com/login).
+    """
+    def __init__(self, config_data=None) -> None:
+        """
+        Creates a new instance of the EncryptionClient with the provided configuration.
+        :type config_data: object
+        :rtype: object
+        """
+        self.bridge_client = bridge.BloockBridge()
+        if config_data is None:
+            config_data = Config.default()
+        self.config_data = config_data
+
+    def generate_rsa_keypair(self) -> KeyPair:
+        """
+        GenerateRsaKeyPair generates an RSA key pair for encryption.
+
+        Will be deleted in future versions.
+        ...deprecated:: 2.8.0
+            Use KeyClient.newLocalKey function instead.
+        :rtype: object
+        """
+        warn('Will be deleted in future versions. Use KeyClient.newLocalKey function instead.',
+             DeprecationWarning, stacklevel=2
+             )
+        res = self.bridge_client.key().GenerateLocalKey(
+            GenerateLocalKeyRequest(config_data=self.config_data, key_type=KeyType.Rsa2048.to_proto())
+        )
+
+        if res.error != Error():
+            raise Exception(res.error.message)
+
+        return RsaKeyPair.from_proto(res)
+
+    def encrypt(self, record: Record, encrypter: Encrypter) -> Record:
+        """
+        Encrypts a Bloock record using the specified encrypter.
+        :type encrypter: object
+        :type record: object
+        :rtype: object
+        """
+        req = EncryptRequest(
+            config_data=self.config_data,
+            record=record.to_proto(),
+            encrypter=encrypter.to_proto(),
+        )
+        res = self.bridge_client.encryption().Encrypt(req)
+
+        if res.error != Error():
+            raise Exception(res.error.message)
+
+        return Record.from_proto(res.record, self.config_data)
+
+    def decrypt(self, record: Record, decrypter: Encrypter) -> Record:
+        """
+        Decrypts a Bloock record using the specified decrypter.
+        :type decrypter: object
+        :type record: object
+        :rtype: object
+        """
+        req = DecryptRequest(
+            config_data=self.config_data,
+            record=record.to_proto(),
+            decrypter=decrypter.to_proto(),
+        )
+        res = self.bridge_client.encryption().Decrypt(req)
+
+        if res.error != Error():
+            raise Exception(res.error.message)
+
+        return Record.from_proto(res.record, self.config_data)
+
+    def get_encryption_alg(self, record: Record) -> EncryptionAlg:
+        """
+        Gets the encryption algorithm used for a Bloock record.
+        :type record: object
+        :rtype: object
+        """
+        client = bridge.BloockBridge()
+        req = EncryptionAlgRequest(
+            config_data=self.config_data, record=record.to_proto()
+        )
+        res = client.encryption().GetEncryptionAlg(req)
+        if res.error != Error():
+            raise Exception(res.error.message)
+        return EncryptionAlg.from_proto(res.alg)
---
+++ packages/bloock/bloock-2.9.8-cp37-cp37m-win_amd64.whl/bloock/entity/key/managed_certificate_params.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import bloock._bridge.proto.bloock_keys_entities_pb2 as proto
+from bloock.entity.key.key_type import KeyType
+from bloock.entity.key.subject_certificate_params import SubjectCertificateParams
+
+
+class ManagedCertificateParams:
+    """
+    Represents parameters for creating a managed certificate.
+    """
+    def __init__(
+            self,
+            key_type: KeyType,
+            subject: SubjectCertificateParams,
+            expiration: int = 0,
+    ) -> None:
+        """
+        Constructs a ManagedCertificateParams object with the specified parameters.
+        :type expiration: object
+        :type subject: object
+        :type key_type: object
+        :rtype: object
+        """
+        self.key_type = key_type
+        """
+        Is the type of the key.
+        """
+        self.subject = subject
+        """
+        Represents the subject details of the certificate.
+        """
+        self.expiration = expiration
+        """
+        Is the number of months until the certificate expiration.
+        """
+
+    @staticmethod
+    def from_proto(key: proto.ManagedCertificateParams) -> ManagedCertificateParams:
+        return ManagedCertificateParams(
+            key_type=KeyType.from_proto(key.key_type),
+            subject=SubjectCertificateParams.fromProto(
+                key.cn, key.c, key.ou, key.o),
+            expiration=key.expiration
+        )
+
+    def to_proto(self) -> proto.ManagedCertificateParams:
+        return proto.ManagedCertificateParams(
+            key_type=self.key_type.to_proto(),
+            subject=self.subject.to_proto(),
+            expiration=self.expiration
+        )

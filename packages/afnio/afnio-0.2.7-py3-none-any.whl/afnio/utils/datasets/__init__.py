32fc27ab584f2f2c3917ce993ddc74f127a2fafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 04:05:17 +0000

    Add package afnio/0.2.7/afnio-0.2.7-py3-none-any.whl

---
+++ packages/afnio/afnio-0.2.7-py3-none-any.whl/afnio/utils/datasets/__init__.py
@@ -0,0 +1,10 @@
+# afnio/utils/datasets/__init__.py
+
+from afnio.utils.datasets.trec import (
+    TREC,
+)
+
+__all__ = ["TREC"]
+
+# Please keep this list sorted
+assert __all__ == sorted(__all__)

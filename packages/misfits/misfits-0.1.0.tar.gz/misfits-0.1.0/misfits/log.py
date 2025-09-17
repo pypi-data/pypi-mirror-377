e800b849189eda3c6b2b30eba4202c912fd52b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:13:33 +0000

    Add package misfits/0.1.0/misfits-0.1.0.tar.gz

---
+++ packages/misfits/misfits-0.1.0.tar.gz/misfits-0.1.0/misfits/log.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+
+from astropy.io.fits.verify import VerifyWarning
+
+from misfits.mtypes import ColumnType
+from misfits.mtypes import LogLevel
+
+
+class Logger:
+    def __init__(self):
+        self.logstack = []
+
+    def push(self, message: str, level: LogLevel | None = LogLevel.INFO):
+        now_str = "[dim cyan]" + datetime.now().strftime("(%H:%M:%S)") + "[/]"
+        match level:
+            case LogLevel.INFO:
+                prefix = f"{now_str} [dim green][INFO][/]: "
+            case LogLevel.WARNING:
+                prefix = f"{now_str} [dim yellow][WARNING][/]: "
+            case LogLevel.ERROR:
+                prefix = f"{now_str} [bold red][ERROR][/]: "
+            case _:
+                prefix = ""
+        self.logstack.append(prefix + message)
+
+    def pop(self) -> str | None:
+        return self.logstack.pop(0) if self.logstack else None
+
+    def push_hdu_info(self, content):
+        # fmt: off
+        self.push(f"Found HDU {repr(content['name'])} of type {repr(content['type'])}")
+        if content["is_table"]:
+            ncols = len(content["data"].columns)
+            self.push(f"HDU contains a table with {len(content['data'])} rows and {ncols} columns")
+        # fmt: on
+
+    def push_data_info(self, data):
+        # fmt: off
+        columns = {coltype: [] for coltype in ColumnType}
+        for colname, coltype in data.columns.items():
+            columns[coltype].append(colname)
+
+        if scalar_columns := columns[ColumnType.SCALAR]:
+            self.push(f"Data table contains {len(scalar_columns)} scalar columns: {scalar_columns}")
+        if vector_columns := columns[ColumnType.VECTOR]:
+            self.push(f"Data table contains {len(vector_columns)} vector columns: {vector_columns}")
+        if varlen_columns := columns[ColumnType.VARLEN]:
+            self.push(f"Data table contains {len(varlen_columns)} variable len columns: {varlen_columns}", level = LogLevel.WARNING)
+        # fmt: on
+
+    def push_verification_warning(self, warnings: list[VerifyWarning]):
+        for w in warnings:
+            self.push(str(w.message).rstrip(), level=LogLevel.WARNING)
+
+
+log = Logger()
---
+++ packages/misfits/misfits-0.1.0.tar.gz/misfits-0.1.0/misfits/screens.py
@@ -0,0 +1,115 @@
+import asyncio
+from pathlib import Path
+from typing import Iterable
+
+from rich.text import Text
+from textual import work
+from textual.app import ComposeResult
+from textual.containers import Container
+from textual.screen import ModalScreen
+from textual.widgets import DirectoryTree
+from textual.widgets import Footer
+from textual.widgets import Label
+from textual.widgets import RichLog
+from textual.widgets import Static
+from textual.widgets import TextArea
+
+from misfits.data import _validate_fits
+from misfits.headers import Header
+from misfits.log import log
+from misfits.logo import LOGO
+
+
+class LogScreen(ModalScreen):
+    """An alternative screen showing a log"""
+
+    BINDINGS = [("escape", "app.pop_screen", "Return to dashboard")]
+
+    def compose(self) -> ComposeResult:
+        yield Header(mid_label="Log")
+        yield RichLog(highlight=True, markup=True)
+        yield Footer()
+
+    def on_screen_resume(self):
+        """When screen is shown, pushes message on the stack to the screen."""
+        while line := log.pop():
+            self.query_one(RichLog).write(line)
+
+
+class InfoScreen(ModalScreen):
+    """Shows an information screen."""
+
+    BINDINGS = [("escape", "app.pop_screen", "Return to dashboard")]
+
+    @staticmethod
+    def get_text():
+        return Text.from_markup(
+            f"   A FITS table viewer. [italic]~p24[/].\n"
+            f"   [dim]https://github.com/peppedilillo - https://gdilillo.com\n",
+        )
+
+    def compose(self) -> ComposeResult:
+        with Container():
+            yield Label(Text(LOGO, style="green bold"))
+            yield Static(self.get_text())
+        yield Footer()
+
+
+class FileExplorerScreen(ModalScreen):
+    """A pop-up screen showing a file explorer so that the user may choose an
+    input navigating the file system.
+    To be used at main app's start-up, if no input file is provided.
+    For this reason the screen is not escapable without quitting."""
+
+    BINDINGS = [("ctrl+q", "app.quit", "Quit")]
+
+    def __init__(self, rootdir: Path = Path.cwd()):
+        super().__init__()
+        self.rootdir = rootdir
+
+    def compose(self) -> ComposeResult:
+        with Container():
+            yield Header(mid_label="Open File")
+            yield FilteredDirectoryTree(self.rootdir)
+        yield Footer()
+
+    # threaded because validating requires IO which may cause laggish behaviour
+    @work(exclusive=True, group="file_check")
+    async def on_directory_tree_file_selected(self, event: DirectoryTree.FileSelected):
+        isvalid = await asyncio.to_thread(_validate_fits, event.path)
+        if not isvalid:
+            self.query_one(DirectoryTree).add_class("error")
+            return
+        self.query_one(DirectoryTree).remove_class("error")
+        # noinspection PyAsyncCall
+        self.dismiss(event.path)
+
+
+class EscapableFileExplorerScreen(FileExplorerScreen):
+    """Like `FileExplorer` but with bindings to leave the screen.
+    To be used when a file input has already been provided."""
+
+    BINDINGS = [("escape", "app.pop_screen", "Return to dashboard")]
+
+
+class FilteredDirectoryTree(DirectoryTree):
+    """A directory tree widget filtering hidden files."""
+
+    def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
+        return [path for path in paths if not path.name.startswith(".")]
+
+
+class HeaderEntry(ModalScreen):
+    """Displays header's entries in a pop-up screen. Useful with long entries."""
+
+    BINDINGS = [("escape", "app.pop_screen", "Return to dashboard")]
+
+    def __init__(self, text: str):
+        super().__init__()
+        self.text = text
+
+    def compose(self) -> ComposeResult:
+        with Container():
+            yield Header(mid_label="Header entry")
+            yield TextArea.code_editor(self.text, read_only=True, soft_wrap=True, show_line_numbers=False)
+        yield Footer()

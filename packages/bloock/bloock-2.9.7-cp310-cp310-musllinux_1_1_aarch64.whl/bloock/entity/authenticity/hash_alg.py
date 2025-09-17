0a0c499f125ec0b1931643e17d9604f2a041e3f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 05:05:22 +0000

    Add package bloock/2.9.7/bloock-2.9.7-cp310-cp310-musllinux_1_1_aarch64.whl

---
+++ packages/bloock/bloock-2.9.7-cp310-cp310-musllinux_1_1_aarch64.whl/bloock/entity/authenticity/hash_alg.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from enum import Enum
+import bloock._bridge.proto.bloock_authenticity_entities_pb2 as proto
+
+class HashAlg(Enum):
+    """
+    Represents different hash algorithms.
+    """
+    SHA_256 = 0
+    """
+    Represents the SHA-256 hash algorithm.
+    """
+    KECCAK_256 = 1
+    """
+    Represents the Keccak-256 hash algorithm.
+    """
+    POSEIDON = 2
+    """
+    Represents the Poseidon hash algorithm.
+    """
+    NONE = 3
+    """
+    Represents no hash algorithm.
+    """
+    UNRECOGNIZED = -1
+    """
+    Represents an unrecognized hash algorithm.
+    """
+
+    def __int__(self):
+        return self.value
+
+    @staticmethod
+    def from_proto(alg: proto.HashAlg.ValueType | None) -> HashAlg:
+        if alg == proto.HashAlg.SHA_256:
+            return HashAlg.SHA_256
+        elif alg == proto.HashAlg.KECCAK_256:
+            return HashAlg.KECCAK_256
+        elif alg == proto.HashAlg.POSEIDON:
+            return HashAlg.POSEIDON
+        elif alg == proto.HashAlg.NONE:
+            return HashAlg.NONE
+        else:
+            return HashAlg.UNRECOGNIZED
+
+    def to_proto(self) -> proto.HashAlg:
+        if self == HashAlg.SHA_256:
+            return proto.HashAlg.SHA_256
+        elif self == HashAlg.KECCAK_256:
+            return proto.HashAlg.KECCAK_256
+        elif self == HashAlg.POSEIDON:
+            return proto.HashAlg.POSEIDON
+        elif self == HashAlg.NONE:
+            return proto.HashAlg.NONE
+        else:
+            return proto.HashAlg.SHA_256
---
+++ packages/bloock/bloock-2.9.7-cp310-cp310-musllinux_1_1_aarch64.whl/bloock/entity/key/managed_key.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import bloock._bridge.proto.bloock_keys_entities_pb2 as proto
+from bloock.entity.key.key_protection_level import KeyProtectionLevel
+from bloock.entity.key.key_type import KeyType
+from bloock.entity.key.access_control_type import AccessControlType
+
+
+class ManagedKey:
+    """
+    Represents a managed key.
+    """
+    def __init__(
+            self,
+            id: str,
+            name: str,
+            protection: KeyProtectionLevel,
+            key_type: KeyType,
+            expiration: int,
+            key: str,
+            access_control_type: AccessControlType,
+    ) -> None:
+        """
+        Constructs a ManagedKey object with the specified parameters.
+        :type key: object
+        :type key_type: object
+        :type expiration: object
+        :type protection: object
+        :type access_control_type: object
+        :type name: object
+        :type id: object
+        """
+        self.id = id
+        self.name = name
+        self.protection = protection
+        self.key_type = key_type
+        self.expiration = expiration
+        self.key = key
+        self.access_control_type = access_control_type
+
+    @staticmethod
+    def from_proto(key: proto.ManagedKey) -> ManagedKey:
+        return ManagedKey(
+            id=key.id,
+            name=key.name,
+            protection=KeyProtectionLevel.from_proto(key.protection),
+            key_type=KeyType.from_proto(key.key_type),
+            expiration=key.expiration,
+            key=key.key,
+            access_control_type=AccessControlType.from_proto(key.access_control_type)
+        )
+
+    def to_proto(self) -> proto.ManagedKey:
+        return proto.ManagedKey(
+            id=self.id,
+            name=self.name,
+            protection=self.protection.to_proto(),
+            key_type=self.key_type.to_proto(),
+            expiration=self.expiration,
+            key=self.key,
+            access_control_type=self.access_control_type.to_proto(),
+        )

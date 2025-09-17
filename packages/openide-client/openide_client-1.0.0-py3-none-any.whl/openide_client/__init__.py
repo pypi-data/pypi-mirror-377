365d1d3b3d33cdab4c027b36cf9b2bccc877c916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:07:02 +0000

    Add package openide-client/1.0.0/openide_client-1.0.0-py3-none-any.whl

---
+++ packages/openide-client/openide_client-1.0.0-py3-none-any.whl/openide_client/__init__.py
@@ -0,0 +1,29 @@
+"""
+OpenIDE Client Library
+Python библиотека для взаимодействия с OpenIDE API
+"""
+
+from .client import (
+    OpenIDEClient,
+    OpenIDEAPIClient,
+    OpenIDEConfig,
+    create_client,
+    create_api_client
+)
+
+__version__ = "1.0.0"
+__author__ = "ArtemJS"
+__email__ = "artemjs@example.com"
+__description__ = "Python client library for OpenIDE container system"
+
+__all__ = [
+    "OpenIDEClient",
+    "OpenIDEAPIClient", 
+    "OpenIDEConfig",
+    "create_client",
+    "create_api_client",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__description__"
+]

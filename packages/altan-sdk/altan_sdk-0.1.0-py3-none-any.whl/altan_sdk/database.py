ed8621dd4c18e1a5186ed976eb03926a35177f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 04:05:01 +0000

    Add package altan-sdk/0.1.0/altan_sdk-0.1.0-py3-none-any.whl

---
+++ packages/altan-sdk/altan_sdk-0.1.0-py3-none-any.whl/altan_sdk/database.py
@@ -0,0 +1,20 @@
+"""
+Database class for the Altan SDK (placeholder)
+"""
+
+from .exceptions import AltanSDKError
+
+
+class Database:
+    """Database class for Altan SDK - to be implemented"""
+    
+    def __init__(self, altan_api_key: str):
+        """
+        Initialize the Database client
+        
+        Args:
+            altan_api_key: Your Altan API key
+        """
+        self.api_key = altan_api_key
+        # TODO: Implement database functionality
+        pass

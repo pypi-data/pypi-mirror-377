39cc9d67392dc5a077fb55ad69adc966155f4e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 05:05:25 +0000

    Add package bloock/2.9.7/bloock-2.9.7-cp312-cp312-win_amd64.whl

---
+++ packages/bloock/bloock-2.9.7-cp312-cp312-win_amd64.whl/bloock/_config/config.py
@@ -0,0 +1,20 @@
+import bloock
+from bloock._bridge.proto.bloock_config_pb2 import ConfigData, Configuration
+
+
+class Config:
+    @staticmethod
+    def default() -> ConfigData:
+        return ConfigData(
+            config=Configuration(
+                library_name="Python",
+                api_key=bloock.api_key,
+                identityApiHost=bloock.identity_api_host,
+                host=bloock.api_host,
+            ),
+            networks_config=bloock.network_config,
+        )
+
+    @staticmethod
+    def new(config_data: ConfigData) -> ConfigData:
+        return config_data

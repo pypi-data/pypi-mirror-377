bcf67ef1320fc4f71b9cc7fe5e5106d9f46e90f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:04:55 +0000

    Add package sift-stack-py/0.9.0/sift_stack_py-0.9.0.tar.gz

---
+++ packages/sift-stack-py/sift_stack_py-0.9.0.tar.gz/sift_stack_py-0.9.0/lib/sift_client/resources/sync_stubs/__init__.py
@@ -0,0 +1,22 @@
+"""Synchronous API wrappers generated from async classes.
+This package contains synchronous versions of all async API classes.
+"""
+
+from sift_client._internal.sync_wrapper import generate_sync_api
+from sift_client.resources import (
+    AssetsAPIAsync,
+    CalculatedChannelsAPIAsync,
+    ChannelsAPIAsync,
+    PingAPIAsync,
+    RulesAPIAsync,
+    RunsAPIAsync,
+)
+
+PingAPI = generate_sync_api(PingAPIAsync, "PingAPI")
+AssetsAPI = generate_sync_api(AssetsAPIAsync, "AssetsAPI")
+CalculatedChannelsAPI = generate_sync_api(CalculatedChannelsAPIAsync, "CalculatedChannelsAPI")
+ChannelsAPI = generate_sync_api(ChannelsAPIAsync, "ChannelsAPI")
+RulesAPI = generate_sync_api(RulesAPIAsync, "RulesAPI")
+RunsAPI = generate_sync_api(RunsAPIAsync, "RunsAPI")
+
+__all__ = ["AssetsAPI", "CalculatedChannelsAPI", "PingAPI", "RunsAPI"]

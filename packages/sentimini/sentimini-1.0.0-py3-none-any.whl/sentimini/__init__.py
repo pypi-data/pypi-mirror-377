8f5759c5151e74cbc723252ffa31c0bcca548f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 05:05:00 +0000

    Add package sentimini/1.0.0/sentimini-1.0.0-py3-none-any.whl

---
+++ packages/sentimini/sentimini-1.0.0-py3-none-any.whl/sentimini/__init__.py
@@ -0,0 +1,4 @@
+from .core import polarity, score, add_words
+
+__all__ = ["polarity", "score", "add_words"]
+__version__ = "1.0.0"
---
+++ packages/sentimini/sentimini-1.0.0-py3-none-any.whl/sentimini/lexicon.py
@@ -0,0 +1,12 @@
+POSITIVE_WORDS = {
+    "love", "good", "great", "awesome", "amazing", "happy", "excellent",
+    "fantastic", "wonderful", "perfect", "nice", "like"
+}
+
+NEGATIVE_WORDS = {
+    "bad", "terrible", "awful", "worst", "sad", "angry", "horrible",
+    "poor", "hate", "disappointing", "ugly"
+}
+
+NEGATIONS = {"not", "never", "no", "none", "hardly", "scarcely", "rarely"}
+INTENSIFIERS = {"very", "extremely", "super", "really", "too"}

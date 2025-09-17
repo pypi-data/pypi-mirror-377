132a20559d2f4b2d2539bc0f64058a4772f9f2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 14:05:39 +0000

    Add package UW-RestClients-IASystem/1.1.5/uw_restclients_iasystem-1.1.5-py3-none-any.whl

---
+++ packages/UW-RestClients-IASystem/uw_restclients_iasystem-1.1.5-py3-none-any.whl/uw_iasystem/exceptions.py
@@ -0,0 +1,10 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from restclients_core.exceptions import DataFailureException
+
+
+class TermEvalNotCreated(DataFailureException):
+
+    def __init__(self, url, status, msg):
+        super(TermEvalNotCreated, self).__init__(url, status, msg)
---
+++ packages/UW-RestClients-IASystem/uw_restclients_iasystem-1.1.5-py3-none-any.whl/uw_iasystem/util/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from restclients_core.util.decorators import use_mock
+from uw_iasystem.dao import (
+    IASystem_UW_DAO, IASystem_UWB_DAO,
+    IASystem_UWT_DAO, IASystem_UWEO_AP_DAO, IASystem_UWEO_IELP_DAO)
+
+
+fdao_ias_override = use_mock(IASystem_UW_DAO(), IASystem_UWB_DAO(),
+                             IASystem_UWT_DAO(), IASystem_UWEO_AP_DAO(),
+                             IASystem_UWEO_IELP_DAO())

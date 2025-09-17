3a896609c6c8b0e79f083c83a96cf37bc07ce8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 04:04:56 +0000

    Add package glpi-provider/0.2.5/glpi_provider-0.2.5-py3-none-any.whl

---
+++ packages/glpi-provider/glpi_provider-0.2.5-py3-none-any.whl/glpi_provider/__init__.py
@@ -0,0 +1,20 @@
+from .decorators.session import with_session
+from .models.entity import Entity
+from .models.ticket import Ticket
+from .models.user import User
+from .providers.glpi_provider import GlpiProvider
+from .services.glpi_service import GlpiService
+from .utils.url import url_transform
+from .settings import BASE_URL, USER_TOKEN
+
+
+__all__ = [
+    'Entity',
+    'Ticket',
+    'User',
+    'GlpiProvider',
+    'url_transform',
+    'with_session',
+    'BASE_URL',
+    'USER_TOKEN'
+]

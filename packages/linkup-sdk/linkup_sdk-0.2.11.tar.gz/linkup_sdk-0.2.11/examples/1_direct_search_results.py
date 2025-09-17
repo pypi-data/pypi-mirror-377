eb1dac8e4ca18c4827da7c342171afee988f1533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:05:41 +0000

    Add package linkup-sdk/0.2.11/linkup_sdk-0.2.11.tar.gz

---
+++ packages/linkup-sdk/linkup_sdk-0.2.11.tar.gz/linkup_sdk-0.2.11/examples/1_direct_search_results.py
@@ -0,0 +1,19 @@
+"""
+The Linkup search can output raw search results which can then be re-used in different use-cases,
+for instance in a RAG system, with the output_type parameter set to "searchResults".
+"""
+
+from dotenv import load_dotenv
+from rich import print
+
+from linkup import LinkupClient
+
+load_dotenv()
+client = LinkupClient()
+
+response = client.search(
+    query="What are the 3 major events in the life of Abraham Lincoln?",
+    depth="standard",  # or "deep"
+    output_type="searchResults",
+)
+print(response)
---
+++ packages/linkup-sdk/linkup_sdk-0.2.11.tar.gz/linkup_sdk-0.2.11/tests/unit/conftest.py
@@ -0,0 +1,13 @@
+import os
+
+import pytest
+
+from linkup import LinkupClient
+
+
+@pytest.fixture(scope="session")
+def client() -> LinkupClient:
+    if os.getenv("LINKUP_API_KEY") is None:
+        os.environ["LINKUP_API_KEY"] = "fake-api"
+
+    return LinkupClient()

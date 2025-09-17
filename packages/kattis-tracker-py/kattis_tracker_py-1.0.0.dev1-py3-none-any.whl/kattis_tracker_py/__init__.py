45bbbb00b7c790d4f2437c3f33d1f1782b910770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 14:06:04 +0000

    Add package kattis-tracker-py/1.0.0.dev1/kattis_tracker_py-1.0.0.dev1-py3-none-any.whl

---
+++ packages/kattis-tracker-py/kattis_tracker_py-1.0.0.dev1-py3-none-any.whl/kattis_tracker_py/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025-present Christian <chrille_0313@hotmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from importlib.metadata import version as get_version
+
+__version__ = get_version(__package__)

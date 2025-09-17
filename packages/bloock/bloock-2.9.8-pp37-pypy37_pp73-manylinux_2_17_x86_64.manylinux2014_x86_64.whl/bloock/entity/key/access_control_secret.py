0d8984d5288af0e844ff60fac684c4a15626395e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:10 +0000

    Add package bloock/2.9.8/bloock-2.9.8-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/bloock/bloock-2.9.8-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/bloock/entity/key/access_control_secret.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import bloock._bridge.proto.bloock_keys_entities_pb2 as proto
+
+
+class AccessControlSecret:
+    """
+    Represents a secret used for secret-based access control.
+    """
+    secret = None
+
+    def __init__(self, secret) -> None:
+        """
+        Creates a new AccessControlSecret instance with the provided secret.
+        :type secret: object
+        :rtype: object
+        """
+        self.secret = secret
+
+    def to_proto(self) -> proto.AccessControlSecret:
+        return proto.AccessControlSecret(
+            secret=self.secret
+        )

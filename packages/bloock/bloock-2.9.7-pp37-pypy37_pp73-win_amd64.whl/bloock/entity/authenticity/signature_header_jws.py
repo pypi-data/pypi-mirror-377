0875079db24076865934bf04538e382567615c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 05:05:28 +0000

    Add package bloock/2.9.7/bloock-2.9.7-pp37-pypy37_pp73-win_amd64.whl

---
+++ packages/bloock/bloock-2.9.7-pp37-pypy37_pp73-win_amd64.whl/bloock/entity/authenticity/signature_header_jws.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import bloock._bridge.proto.bloock_identity_entities_pb2 as proto
+
+
+class SignatureHeaderJws:
+    """
+    Represents the header of a JSON Web Signature (JWS). [RFC 7515](https://datatracker.ietf.org/doc/html/rfc7515).
+    """
+    def __init__(self, alg: str, kid: str) -> None:
+        """
+        Constructs a SignatureHeaderJws object with the specified algorithm and key identifier.
+        :type kid: object
+        :type alg: object
+        :rtype: object
+        """
+        self.alg = alg
+        self.kid = kid
+
+    @staticmethod
+    def from_proto(header: proto.SignatureHeaderJWS) -> SignatureHeaderJws:
+        return SignatureHeaderJws(alg=header.alg, kid=header.kid)
+
+    def to_proto(self) -> proto.SignatureHeaderJWS:
+        return proto.SignatureHeaderJWS(alg=self.alg, kid=self.kid)
---
+++ packages/bloock/bloock-2.9.7-pp37-pypy37_pp73-win_amd64.whl/bloock/entity/availability/ipfs_loader.py
@@ -0,0 +1,19 @@
+import bloock._bridge.proto.bloock_availability_entities_pb2 as proto
+from bloock.entity.availability.loader import Loader
+from bloock.entity.availability.loader_args import LoaderArgs
+
+
+class IpfsLoader(Loader):
+    """
+    Represents a loader for IPFS data availability.
+    """
+    def __init__(self, hash: str) -> None:
+        """
+        Constructs a IpfsLoader object with the specified parameters.
+        :type hash: object
+        :rtype: object
+        """
+        super().__init__(type=proto.IPFS, args=LoaderArgs(hash))
+
+    def to_proto(self) -> proto.Loader:
+        return proto.Loader(type=self.type, args=self.args.to_proto())

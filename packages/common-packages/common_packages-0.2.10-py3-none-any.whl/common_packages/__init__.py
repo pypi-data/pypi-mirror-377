1dfda222679d22f307e6e14dc540398a6b2c8dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:05:19 +0000

    Add package common-packages/0.2.10/common_packages-0.2.10-py3-none-any.whl

---
+++ packages/common-packages/common_packages-0.2.10-py3-none-any.whl/common_packages/__init__.py
@@ -0,0 +1,17 @@
+import importlib
+
+# TODO: common_packages --> common_utils
+
+
+class CommonPackageManager:
+    """公共包管理器"""
+
+    def _check_dependencies(self, module_names: list[str]):
+        """检查依赖项是否已安装"""
+        for module_name in module_names:
+            try:
+                setattr(self, module_name, importlib.import_module(module_name))
+            except ImportError:
+                print(
+                    f"Missing required dependencies. use pip install {module_name} to install it."
+                )

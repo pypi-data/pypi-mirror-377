76bf59c6b5842230c289ba41262f8fe504e02f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:04:42 +0000

    Add package osdu-perf/1.0.0/osdu_perf-1.0.0.tar.gz

---
+++ packages/osdu-perf/osdu_perf-1.0.0.tar.gz/osdu_perf-1.0.0/osdu_perf/__init__.py
@@ -0,0 +1,24 @@
+# osdu_perf/__init__.py
+"""
+OSDU Performance Testing Framework - Core Library
+"""
+
+from .core.base_service import BaseService
+from .core.service_orchestrator import ServiceOrchestrator
+from .core.input_handler import InputHandler
+from .core.auth import AzureTokenManager
+from .locust.user_base import PerformanceUser
+from .utils.environment import detect_environment
+
+__version__ = "1.0.5"
+__author__ = "Janraj CJ"
+__email__ = "janrajcj@microsoft.com"
+
+__all__ = [
+    "BaseService",
+    "ServiceOrchestrator", 
+    "InputHandler",
+    "AzureTokenManager",
+    "PerformanceUser",
+    "detect_environment"
+]

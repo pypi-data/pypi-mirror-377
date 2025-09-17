480d150ee8b06edc99534f800e88e5c023aaf411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 08:05:16 +0000

    Add package UW-RestClients-Libraries/1.3.3/uw_restclients_libraries-1.3.3-py3-none-any.whl

---
+++ packages/UW-RestClients-Libraries/uw_restclients_libraries-1.3.3-py3-none-any.whl/uw_libraries/dao.py
@@ -0,0 +1,25 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Contains UW Library DAO implementations.
+"""
+from restclients_core.dao import DAO
+from os.path import abspath, dirname
+import os
+
+
+class MyLib_DAO(DAO):
+    def service_name(self):
+        return "libraries"
+
+    def service_mock_paths(self):
+        return [abspath(os.path.join(dirname(__file__), "resources"))]
+
+
+class SubjectGuide_DAO(DAO):
+    def service_name(self):
+        return "libcurrics"
+
+    def service_mock_paths(self):
+        return [abspath(os.path.join(dirname(__file__), "resources"))]
---
+++ packages/UW-RestClients-Libraries/uw_restclients_libraries-1.3.3-py3-none-any.whl/uw_libraries/tests/test_daos.py
@@ -0,0 +1,14 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from unittest import TestCase
+from uw_libraries.dao import MyLib_DAO, SubjectGuide_DAO
+
+
+class TestDao(TestCase):
+
+    def test_mylib_dao(self):
+        self.assertEqual(MyLib_DAO().service_name(), "libraries")
+
+    def test_subjectguide_dao(self):
+        self.assertEqual(SubjectGuide_DAO().service_name(), "libcurrics")

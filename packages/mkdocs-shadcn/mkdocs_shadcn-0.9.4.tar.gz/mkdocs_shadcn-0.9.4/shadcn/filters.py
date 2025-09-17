c45f37b59ab0b02571dab869b87e80d36eeca594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:14:02 +0000

    Add package mkdocs-shadcn/0.9.4/mkdocs_shadcn-0.9.4.tar.gz

---
+++ packages/mkdocs-shadcn/mkdocs_shadcn-0.9.4.tar.gz/mkdocs_shadcn-0.9.4/shadcn/filters.py
@@ -0,0 +1,69 @@
+import urllib.parse
+import urllib.request
+from functools import lru_cache
+from typing import Any, Union
+
+from mkdocs.structure.nav import Navigation, Section
+from mkdocs.structure.pages import Page
+
+
+@lru_cache()
+def iconify(key: str, height: str = "20px", **kwargs) -> str:
+    base_url = "https://api.iconify.design"
+    icon = key.split(":")
+    if len(icon) != 2:
+        raise ValueError(
+            f"Invalid icon format: {key}. Expected format 'provider:name'."
+        )
+    provider, name = icon
+    url = f"{base_url}/{provider}/{name}.svg?{urllib.parse.urlencode({'height': height, **kwargs})}"
+    with urllib.request.urlopen(url) as response:
+        content = response.read().decode(
+            "utf-8"
+        )  # Convert to string if needed
+    return content
+
+
+def parse_author(site_author: str) -> Union[str, None]:
+    """Returns the email address of the site author."""
+    # parse thinks like "Alban Siffer <31479857+asiffer@users.noreply.github.com>"
+    if "<" in site_author and ">" in site_author:
+        chunks = site_author.split("<")
+        email = chunks[-1].split(">")[0]
+        name = chunks[0].strip()
+    else:
+        email = None
+        name = site_author.strip()
+
+    if email:
+        return f'<a href="mailto:{email}">{name}</a>'
+    return f"<span>{name}</span>"
+
+
+def setattribute(value: Union[dict, object], k: str, v: Any):
+    if hasattr(value, "__setattr__"):
+        value.__setattr__(k, v)
+    return value
+
+
+def active_section(nav: Navigation) -> Union[Section, None]:
+    """Return the top-level active section"""
+    for item in nav:
+        if isinstance(item, Section) and item.is_section and item.active:
+            return item
+    return None
+
+
+def first_page(section: Section) -> Union[Page, None]:
+    """Return the first page in a section"""
+    for item in section.children:
+        if isinstance(item, Page) and item.is_page:
+            return item
+
+    for item in section.children:
+        if isinstance(item, Section):
+            fp = first_page(item)
+            if fp:
+                return fp
+
+    return None

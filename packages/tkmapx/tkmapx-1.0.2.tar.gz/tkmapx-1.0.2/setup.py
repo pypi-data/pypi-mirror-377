9f4724401781821fe8d77f10025a0a8e8c49600d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 17:05:43 +0000

    Add package tkmapx/1.0.2/tkmapx-1.0.2.tar.gz

---
+++ packages/tkmapx/tkmapx-1.0.2.tar.gz/tkmapx-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tkmapx",
+    version="1.0.2",
+    author="Nebula Company",
+    description="A Tkinter-based Python package for creating and visualizing simple maps with nodes and paths.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Krishna-Developer5000/tkmap",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    python_requires=">=3.8",
+)

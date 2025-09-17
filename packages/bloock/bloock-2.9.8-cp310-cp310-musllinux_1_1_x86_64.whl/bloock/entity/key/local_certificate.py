901bccc54bfc17e0b9f486c0d73993b66e971451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 06:05:09 +0000

    Add package bloock/2.9.8/bloock-2.9.8-cp310-cp310-musllinux_1_1_x86_64.whl

---
+++ packages/bloock/bloock-2.9.8-cp310-cp310-musllinux_1_1_x86_64.whl/bloock/entity/key/local_certificate.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import bloock._bridge.proto.bloock_keys_entities_pb2 as proto
+
+
+class LocalCertificate:
+    """
+    Represents a local certificate along with its password.
+    """
+    def __init__(
+            self,
+            pkcs12: bytes,
+            password: str,
+    ) -> None:
+        """
+        Constructs a LocalCertificate object with the specified parameters.
+        :type password: object
+        :type pkcs12: object
+        :rtype: object
+        """
+        self.pkcs12 = pkcs12
+        self.password = password
+
+    @staticmethod
+    def from_proto(certificate: proto.LocalCertificate) -> LocalCertificate:
+        return LocalCertificate(
+            pkcs12=certificate.pkcs12,
+            password=certificate.password,
+        )
+
+    def to_proto(self) -> proto.LocalCertificate:
+        return proto.LocalCertificate(
+            pkcs12=self.pkcs12,
+            password=self.password,
+        )

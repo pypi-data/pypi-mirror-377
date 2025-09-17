40f3d364d0d42e942d1ba494a3db8e661340c9ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 03:13:53 +0000

    Add package shapes2d/0.1.0/shapes2d-0.1.0-py3-none-any.whl

---
+++ packages/shapes2d/shapes2d-0.1.0-py3-none-any.whl/shapes2d/__init__.py
@@ -0,0 +1,3 @@
+from .shapes_area import area_of_circle,area_of_parallelogram,area_of_rectangle,area_of_square,area_of_trapezoid,area_of_triangle
+
+__all__ = ["area_of_circle","area_of_parallelogram","area_of_rectangle","area_of_square","area_of_trapezoid","area_of_triangle"]
---
+++ packages/shapes2d/shapes2d-0.1.0-py3-none-any.whl/shapes2d/shapes_area.py
@@ -0,0 +1,18 @@
+def area_of_triangle(base,height):
+    total = (1/2)*base*height
+    return total
+def area_of_rectangle(length,breath):
+    total=length*breath
+    return total
+def area_of_square(side):
+    total= side *side 
+    return total
+def area_of_circle(radius):
+    total= 3.14159265359 * (radius* radius)
+    return total
+def area_of_trapezoid(length,breath,height):
+    total = (1/2)*(length + breath) *height
+    return total
+def area_of_parallelogram(breath,height):
+    total= breath*height
+    return total

1696098175575143a2fcddbd43e7872aea5fc41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 07:05:25 +0000

    Add package UW-RestClients-CoDa/1.0.4/uw_restclients_coda-1.0.4-py3-none-any.whl

---
+++ packages/UW-RestClients-CoDa/uw_restclients_coda-1.0.4-py3-none-any.whl/uw_coda/dao.py
@@ -0,0 +1,23 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from os.path import abspath, dirname
+from restclients_core.dao import DAO
+import os
+
+
+class CoDa_DAO(DAO):
+    def service_name(self):
+        return 'coda'
+
+    def service_mock_paths(self):
+        return [abspath(os.path.join(dirname(__file__), "resources"))]
+
+    def _custom_headers(self, method, url, headers, body):
+        custom_headers = {}
+
+        token = self.get_service_setting('AUTH_TOKEN')
+        if token is not None:
+            custom_headers["Authorization"] = "Token {}".format(token)
+
+        return custom_headers
---
+++ packages/UW-RestClients-CoDa/uw_restclients_coda-1.0.4-py3-none-any.whl/uw_coda/tests/test_endpoints.py
@@ -0,0 +1,29 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from unittest import TestCase
+
+import uw_coda
+
+POL_S_LABEL = "2014-winter-POL%20S-201-A"
+POL_S_AA_LABEL = "2014-winter-POL%20S-201-AA"
+
+
+class TestEndpoints(TestCase):
+
+    def test_fail_rate(self):
+        fail_rate = uw_coda.get_fail_rate(POL_S_LABEL)
+        self.assertEqual(fail_rate['failure_rate'], 0.03790613718411552)
+
+    def test_majors(self):
+        majors = uw_coda.get_majors(POL_S_LABEL, 2)['current_student_majors']
+        self.assertEqual(len(majors), 2)
+        self.assertEqual(majors[0]['major'], 'PRE MAJOR (A&S)')
+
+    def test_cgpa(self):
+        fail_rate = uw_coda.get_course_cgpa(POL_S_LABEL)
+        self.assertEqual(fail_rate['current_median'], 3.4)
+
+    def test_secondary(self):
+        fail_rate = uw_coda.get_course_cgpa(POL_S_AA_LABEL)
+        self.assertEqual(fail_rate, {})

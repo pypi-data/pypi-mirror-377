55836c5d032a037ec8b62b2cd348e4b00a1bc6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 07:05:45 +0000

    Add package profinder/0.2.1/profinder-0.2.1-py3-none-any.whl

---
+++ packages/profinder/profinder-0.2.1-py3-none-any.whl/profinder/__init__.py
@@ -0,0 +1,9 @@
+from .example_data import get_example_data, synthetic_glider_pressure
+from .profinder import find_profiles, find_yo
+
+__all__ = [
+    "find_yo",
+    "find_profiles",
+    "get_example_data",
+    "synthetic_glider_pressure",
+]

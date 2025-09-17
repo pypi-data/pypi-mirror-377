69c41835f1352e541ccce9e0d872e99f4d872f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 18:05:30 +0000

    Add package red-tidegear/1.1.2/red_tidegear-1.1.2-py3-none-any.whl

---
+++ packages/red-tidegear/red_tidegear-1.1.2-py3-none-any.whl/tidegear/constants.py
@@ -0,0 +1,6 @@
+"""This module contains constants related to the Discord API or general cog development."""
+
+MAX_MESSAGE_CHARACTERS = 2000
+"""The maximum amount of characters Discord will allow you to post in a single message when using the `content` field."""
+MAX_EMBED_CHARACTERS = 4096
+"""The maximum amount of characters Discord will allow you to post in a single message when using embeds or components."""

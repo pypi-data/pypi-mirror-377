1e0eff418c85845085d8996dff158f66b439a8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-17 14:05:29 +0000

    Add package runkmc/0.1.0a2/runkmc-0.1.0a2.tar.gz

---
+++ packages/runkmc/runkmc-0.1.0a2.tar.gz/runkmc-0.1.0a2/runkmc/models/__init__.py
@@ -0,0 +1,39 @@
+from typing import Dict, Any
+from pathlib import Path
+
+
+TEMPLATE_DIR = Path(__file__).parent / "templates"
+
+
+def create_input_file(
+    model_name: str, kmc_inputs: Dict[str, Any], filepath: Path | str
+) -> str:
+
+    base_dir = Path(__file__).parent.parent.parent
+
+    if model_name == "FRP2":
+        model_filepath = TEMPLATE_DIR / "FRP2_Template.txt"
+    elif model_name == "CRP3":
+        model_filepath = TEMPLATE_DIR / "CRP3_Template.txt"
+    elif model_name == "AMS_MMA":
+        model_filepath = TEMPLATE_DIR / "AMS_MMA_Template.txt"
+    else:
+        raise ValueError(f"Model {model_name} not supported")
+
+    # Read model template file
+    with open(model_filepath, "r") as file:
+        template_content = file.read()
+
+    # Replace placeholders in template with input values
+    for key, value in kmc_inputs.items():
+        placeholder = "{" + key + "}"
+
+        try:
+            value = str(value)
+        except ValueError:
+            raise ValueError(f"Value for {key} cannot be converted to string.")
+
+        template_content = template_content.replace(placeholder, value)
+
+    with open(filepath, "w") as file:
+        file.write(template_content)

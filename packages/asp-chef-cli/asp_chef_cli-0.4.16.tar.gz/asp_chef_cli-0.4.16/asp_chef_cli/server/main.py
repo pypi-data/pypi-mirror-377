fd172e968afb85b1ba3280a75be62af0266562ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 04:11:14 +0000

    Add package asp-chef-cli/0.4.16/asp_chef_cli-0.4.16.tar.gz

---
+++ packages/asp-chef-cli/asp_chef_cli-0.4.16.tar.gz/asp_chef_cli-0.4.16/asp_chef_cli/server/main.py
@@ -0,0 +1,21 @@
+from fastapi import Depends
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .routers import clingo, dumbo
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5188",
+        "https://asp-chef.alviano.net",
+        "https://aspchef.alviano.net",
+        "https://asp-chef.netlify.app",
+    ],
+    allow_credentials=False,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
+app.include_router(dumbo.router, prefix="/dumbo", tags=["dumbo"])
+app.include_router(clingo.router, prefix="/clingo", tags=["clingo"])

b3d95a10bc4782d1a6b206af4168bd41fbaf444f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 07:06:43 +0000

    Add package olympipe/1.5.1/olympipe-1.5.1-py3-none-any.whl

---
+++ packages/olympipe/olympipe-1.5.1-py3-none-any.whl/olympipe/pipes/task.py
@@ -0,0 +1,26 @@
+from multiprocessing.managers import DictProxy
+from typing import Callable, List
+
+from olympipe.shuttable_queue import ShuttableQueue
+from olympipe.types import InPacket, OutPacket
+
+from .generic import GenericPipe
+
+
+class TaskPipe(GenericPipe[InPacket, OutPacket]):
+    def __init__(
+        self,
+        father_process_dag: "DictProxy[str, List[str]]",
+        source: "ShuttableQueue[InPacket]",
+        task: Callable[[InPacket], OutPacket],
+        target: "ShuttableQueue[OutPacket]",
+    ):
+        self._task = task
+        super().__init__(father_process_dag, source, target)
+
+    @property
+    def shortname(self) -> str:
+        return self._task.__name__
+
+    def _perform_task(self, data: InPacket) -> OutPacket:
+        return self._task(data)

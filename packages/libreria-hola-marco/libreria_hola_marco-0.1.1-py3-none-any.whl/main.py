e381690b69788db37d6d30e1ad163541f25d498f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:05:42 +0000

    Add package libreria-hola-marco/0.1.1/libreria_hola_marco-0.1.1-py3-none-any.whl

---
+++ packages/libreria-hola-marco/libreria_hola_marco-0.1.1-py3-none-any.whl/main.py
@@ -0,0 +1,13 @@
+def hello_marco():
+    """
+    This function prints a greeting message from Marco.
+    """
+    print("Hello from Marco!")
+
+
+def main():
+    hello_marco()
+
+
+if __name__ == "__main__":
+    main()

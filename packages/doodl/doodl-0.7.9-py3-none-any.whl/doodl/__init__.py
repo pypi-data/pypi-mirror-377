6104a1e8761a3b16e28ce2ea3509bb96b888c00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:18:20 +0000

    Add package doodl/0.7.9/doodl-0.7.9-py3-none-any.whl

---
+++ packages/doodl/doodl-0.7.9-py3-none-any.whl/doodl/__init__.py
@@ -0,0 +1,4 @@
+from doodl.doodl import\
+    linechart, piechart, skey, barchart, tree, venn, gantt, treemap,\
+    heatmap, dotplot, scatterplot, boxplot, force, chord, disjoint,\
+    bollinger, dendrogram, contour, areachart, bubblechart, voronoi

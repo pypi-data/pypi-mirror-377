35ef28541d64139871d70866fb826c80309e485b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:08:28 +0000

    Add package len-sentence/0.1.0/len_sentence-0.1.0.tar.gz

---
+++ packages/len-sentence/len_sentence-0.1.0.tar.gz/len_sentence-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="len-sentence",
+    version="0.1.0",
+    author="JackyHe398",
+    author_email="hekinghung@gmail.com",
+    description="Length of sentence utilities for counting the number of words/characters in a sentence",
+    install_requires=[],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
+
+# python -m build
+# twine upload dist/*
---
+++ packages/len-sentence/len_sentence-0.1.0.tar.gz/len_sentence-0.1.0/test/test.py
@@ -0,0 +1,8 @@
+from len_sentence import count_sentence
+
+print(count_sentence("Hello world!", "Latn"))
+print(count_sentence("你好世界", "Hans"))
+print(count_sentence("こんにちは", "Jpan"))
+print(count_sentence("བཀྲ་ཤིས་བདེ་ལེགས", "Tibt"))
+print(count_sentence("مرحبا بالعالم", "Arab"))
+print(count_sentence("Привет мир", "Cyrl"))

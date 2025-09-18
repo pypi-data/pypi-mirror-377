8028f1d84bb923aa80953cdc59f19eb34e63f08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 07:05:49 +0000

    Add package mtcli-atr/1.0.0/mtcli_atr-1.0.0.tar.gz

---
+++ packages/mtcli-atr/mtcli_atr-1.0.0.tar.gz/mtcli_atr-1.0.0/mtcli_atr/atr.py
@@ -0,0 +1,24 @@
+"""Lógica do ATR."""
+
+from mtcli.logger import setup_logger
+
+
+log = setup_logger()
+
+
+def calcular_atr(rates, periodo):
+    """Calcula o ATR (Average True Range)."""
+    trs = []
+    for i in range(1, len(rates)):
+        high = rates[i]["high"]
+        low = rates[i]["low"]
+        prev_close = rates[i - 1]["close"]
+        tr = max(high - low, abs(high - prev_close), abs(low - prev_close))
+        trs.append(tr)
+
+    if len(trs) < periodo:
+        msg = f"Quantidade insuficiente de dados para período {periodo}"
+        log.error(msg)
+        raise ValueError(msg)
+
+    return sum(trs[-periodo:]) / periodo

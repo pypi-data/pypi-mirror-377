5857de889f980417b2fb92bb5e67cdea6568309c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 06:05:58 +0000

    Add package SigProfilerAssignment/0.2.6/sigprofilerassignment-0.2.6-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-0.2.6-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.2.6'
+version = '0.2.6'
+Update = 'v0.2.6:  Support for assigning signatures for rn7 and mm39 genomes.'
+
+    

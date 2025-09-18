936f932acfab76f96551c9afd354544c2f1ef43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:08:26 +0000

    Add package fileorganizer-farhan/0.1.0/fileorganizer_farhan-0.1.0-py3-none-any.whl

---
+++ packages/fileorganizer-farhan/fileorganizer_farhan-0.1.0-py3-none-any.whl/fileorganizer/__init__.py
@@ -0,0 +1,12 @@
+"""
+FileOrganizer - A Python package to organize messy files into categorized folders.
+"""
+
+from .organizer import FileOrganizer
+from .cli import main
+
+__version__ = '0.1.0'
+__author__ = 'Farhan Ahmad'
+__email__ = 'farhanbangash091@gmail.com'
+
+__all__ = ['FileOrganizer', 'main']

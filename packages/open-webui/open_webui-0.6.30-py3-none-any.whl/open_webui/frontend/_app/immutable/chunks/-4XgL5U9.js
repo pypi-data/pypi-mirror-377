659cf44fb470faf2e7bfe9cad37fafc167ebfdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 06:05:36 +0000

    Add package open-webui/0.6.30/open_webui-0.6.30-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.6.30-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/-4XgL5U9.js
@@ -0,0 +1,110 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./CJqyLrOH.js","./C1FmrZbK.js","./G5mHU2ZO.js","./IHki7fMi.js","./Vcq4gwWv.js","./DmTNmOsd.js","./BnUnmcxH.js","./Bf22bvAJ.js","./Bk82nkIB.js","./BLy842NA.js","../assets/Toaster.DQwrSZtH.css"])))=>i.map(i=>d[i]);
+import{_ as ft}from"./C1FmrZbK.js";import{S as dt,i as mt,s as _t,H as Ce,I as je,C as ee,l as A,e as m,t as W,D as te,p as N,c as _,a as h,d,b as j,f,E as oe,g as ae,h as l,G as ge,J as ht,K as Ge,j as Q,y as pt,r as $,u as gt,q as F,F as se,O as vt,k as lt,v as bt,n as V,L as ve,M as Ye}from"./G5mHU2ZO.js";import{h as wt,u as kt}from"./jYArkn2m.js";import"./IHki7fMi.js";import{g as yt}from"./Bk82nkIB.js";import{C as Et}from"./2iWpB1LN.js";import{C as It}from"./BzjHydOn.js";import{T as Me}from"./BOU1-HoI.js";import{L as Tt}from"./U3rq_aIM.js";import{A as Dt}from"./DBG97xPh.js";import{u as Ct}from"./Bf22bvAJ.js";function rt(t){t[35]=t[36].default}function Pt(t){let e,o,n,a,s;return o=new It({props:{strokeWidth:"2.5"}}),{c(){e=m("button"),ee(o.$$.fragment),this.h()},l(r){e=_(r,"BUTTON",{class:!0,type:!0});var p=h(e);te(o.$$.fragment,p),p.forEach(d),this.h()},h(){f(e,"class","w-full text-left text-sm py-1.5 px-1 rounded-lg dark:text-gray-300 dark:hover:text-white hover:bg-black/5 dark:hover:bg-gray-850"),f(e,"type","button")},m(r,p){ae(r,e,p),oe(o,e,null),n=!0,a||(s=ge(e,"click",t[20]),a=!0)},p:V,i(r){n||(F(o.$$.fragment,r),n=!0)},o(r){$(o.$$.fragment,r),n=!1},d(r){r&&d(e),se(o),a=!1,s()}}}function Vt(t){let e,o,n,a;return{c(){e=m("input"),this.h()},l(s){e=_(s,"INPUT",{class:!0,type:!0,placeholder:!0}),this.h()},h(){f(e,"class","w-full text-2xl font-medium bg-transparent outline-hidden font-primary"),f(e,"type","text"),f(e,"placeholder",o=t[12].t("Tool Name")),e.required=!0},m(s,r){ae(s,e,r),ve(e,t[0]),n||(a=ge(e,"input",t[21]),n=!0)},p(s,r){r[0]&4096&&o!==(o=s[12].t("Tool Name"))&&f(e,"placeholder",o),r[0]&1&&e.value!==s[0]&&ve(e,s[0])},d(s){s&&d(e),n=!1,a()}}}function qt(t){let e,o;return e=new Me({props:{className:"w-full",content:t[12].t("e.g. my_tools"),placement:"top-start",$$slots:{default:[Nt]},$$scope:{ctx:t}}}),{c(){ee(e.$$.fragment)},l(n){te(e.$$.fragment,n)},m(n,a){oe(e,n,a),o=!0},p(n,a){const s={};a[0]&4096&&(s.content=n[12].t("e.g. my_tools")),a[0]&4132|a[1]&64&&(s.$$scope={dirty:a,ctx:n}),e.$set(s)},i(n){o||(F(e.$$.fragment,n),o=!0)},o(n){$(e.$$.fragment,n),o=!1},d(n){se(e,n)}}}function At(t){let e,o;return{c(){e=m("div"),o=W(t[2]),this.h()},l(n){e=_(n,"DIV",{class:!0});var a=h(e);o=j(a,t[2]),a.forEach(d),this.h()},h(){f(e,"class","text-sm text-gray-500 shrink-0")},m(n,a){ae(n,e,a),l(e,o)},p(n,a){a[0]&4&&Q(o,n[2])},i:V,o:V,d(n){n&&d(e)}}}function Nt(t){let e,o,n,a;return{c(){e=m("input"),this.h()},l(s){e=_(s,"INPUT",{class:!0,type:!0,placeholder:!0}),this.h()},h(){f(e,"class","w-full text-sm disabled:text-gray-500 bg-transparent outline-hidden"),f(e,"type","text"),f(e,"placeholder",o=t[12].t("Tool ID")),e.required=!0,e.disabled=t[5]},m(s,r){ae(s,e,r),ve(e,t[2]),n||(a=ge(e,"input",t[23]),n=!0)},p(s,r){r[0]&4096&&o!==(o=s[12].t("Tool ID"))&&f(e,"placeholder",o),r[0]&32&&(e.disabled=s[5]),r[0]&4&&e.value!==s[2]&&ve(e,s[2])},d(s){s&&d(e),n=!1,a()}}}function St(t){let e,o,n,a;return{c(){e=m("input"),this.h()},l(s){e=_(s,"INPUT",{class:!0,type:!0,placeholder:!0}),this.h()},h(){f(e,"class","w-full text-sm bg-transparent outline-hidden"),f(e,"type","text"),f(e,"placeholder",o=t[12].t("Tool Description")),e.required=!0},m(s,r){ae(s,e,r),ve(e,t[3].description),n||(a=ge(e,"input",t[24]),n=!0)},p(s,r){r[0]&4096&&o!==(o=s[12].t("Tool Description"))&&f(e,"placeholder",o),r[0]&8&&e.value!==s[3].description&&ve(e,s[3].description)},d(s){s&&d(e),n=!1,a()}}}function Ut(t){return{c:V,l:V,m:V,p:V,i:V,o:V,d:V}}function Mt(t){rt(t);let e,o,n={value:t[1],lang:"python",boilerplate:t[14],onChange:t[25],onSave:t[26]};return e=new t[35]({props:n}),t[27](e),{c(){ee(e.$$.fragment)},l(a){te(e.$$.fragment,a)},m(a,s){oe(e,a,s),o=!0},p(a,s){rt(a);const r={};s[0]&2&&(r.value=a[1]),s[0]&512&&(r.onChange=a[25]),s[0]&64&&(r.onSave=a[26]),e.$set(r)},i(a){o||(F(e.$$.fragment,a),o=!0)},o(a){$(e.$$.fragment,a),o=!1},d(a){t[27](null),se(e,a)}}}function Lt(t){return{c:V,l:V,m:V,p:V,i:V,o:V,d:V}}function Ot(t){let e,o,n,a=t[12].t("Please carefully review the following warnings:")+"",s,r,p,v,w=t[12].t("Tools have a function calling system that allows arbitrary code execution.")+"",k,S,b,E=t[12].t("Do not install tools from sources you do not fully trust.")+"",T,y,q,I=t[12].t("I acknowledge that I have read and I understand the implications of my action. I am aware of the risks associated with executing arbitrary code and I have verified the trustworthiness of the source.")+"",D;return{c(){e=m("div"),o=m("div"),n=m("div"),s=W(a),r=A(),p=m("ul"),v=m("li"),k=W(w),S=A(),b=m("li"),T=W(E),y=A(),q=m("div"),D=W(I),this.h()},l(g){e=_(g,"DIV",{class:!0});var C=h(e);o=_(C,"DIV",{class:!0});var O=h(o);n=_(O,"DIV",{});var R=h(n);s=j(R,a),R.forEach(d),r=N(O),p=_(O,"UL",{class:!0});var H=h(p);v=_(H,"LI",{});var ne=h(v);k=j(ne,w),ne.forEach(d),S=N(H),b=_(H,"LI",{});var U=h(b);T=j(U,E),U.forEach(d),H.forEach(d),O.forEach(d),y=N(C),q=_(C,"DIV",{class:!0});var P=h(q);D=j(P,I),P.forEach(d),C.forEach(d),this.h()},h(){f(p,"class","mt-1 list-disc pl-4 text-xs"),f(o,"class","bg-yellow-500/20 text-yellow-700 dark:text-yellow-200 rounded-lg px-4 py-3"),f(q,"class","my-3"),f(e,"class","text-sm text-gray-500")},m(g,C){ae(g,e,C),l(e,o),l(o,n),l(n,s),l(o,r),l(o,p),l(p,v),l(v,k),l(p,S),l(p,b),l(b,T),l(e,y),l(e,q),l(q,D)},p(g,C){C[0]&4096&&a!==(a=g[12].t("Please carefully review the following warnings:")+"")&&Q(s,a),C[0]&4096&&w!==(w=g[12].t("Tools have a function calling system that allows arbitrary code execution.")+"")&&Q(k,w),C[0]&4096&&E!==(E=g[12].t("Do not install tools from sources you do not fully trust.")+"")&&Q(T,E),C[0]&4096&&I!==(I=g[12].t("I acknowledge that I have read and I understand the implications of my action. I am aware of the risks associated with executing arbitrary code and I have verified the trustworthiness of the source.")+"")&&Q(D,I)},d(g){g&&d(e)}}}function Bt(t){var Xe,Ze,xe,et;let e,o,n,a,s,r,p,v,w,k,S,b,E,T,y,q,I,D,g,C,O,R=t[12].t("Access")+"",H,ne,U,P,M,me,G,be,X,we,Y,le,L,re,i=t[12].t("Warning:")+"",Pe,Le,ke=t[12].t("Tools are a function calling system with arbitrary code execution")+"",Ve,Oe,Be,Re,_e,ye=t[12].t("don't install random tools from sources you don't trust.")+"",qe,$e,fe,Ee=t[12].t("Save")+"",Ae,Ne,K,Fe,Z,He,Ke;function it(u){t[18](u)}function ut(u){t[19](u)}let We={accessRoles:["read","write"],allowPublic:((xe=(Ze=(Xe=t[11])==null?void 0:Xe.permissions)==null?void 0:Ze.sharing)==null?void 0:xe.public_tools)||((et=t[11])==null?void 0:et.role)==="admin"};t[8]!==void 0&&(We.show=t[8]),t[4]!==void 0&&(We.accessControl=t[4]),e=new Dt({props:We}),Ce.push(()=>je(e,"show",it)),Ce.push(()=>je(e,"accessControl",ut)),b=new Me({props:{content:t[12].t("Back"),$$slots:{default:[Pt]},$$scope:{ctx:t}}}),y=new Me({props:{content:t[12].t("e.g. My Tools"),placement:"top-start",$$slots:{default:[Vt]},$$scope:{ctx:t}}}),g=new Tt({props:{strokeWidth:"2.5",className:"size-3.5"}});const ze=[At,qt],ie=[];function Je(u,c){return u[5]?0:1}P=Je(t),M=ie[P]=ze[P](t),G=new Me({props:{className:"w-full self-center items-center flex",content:t[12].t("e.g. Tools for performing various operations"),placement:"top-start",$$slots:{default:[St]},$$scope:{ctx:t}}});let B={ctx:t,current:null,token:null,hasCatch:!1,pending:Lt,then:Mt,catch:Ut,value:36,blocks:[,,,]};wt(ft(()=>import("./CJqyLrOH.js").then(u=>u.Q),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url),B);function ct(u){t[30](u)}let Qe={$$slots:{default:[Ot]},$$scope:{ctx:t}};return t[7]!==void 0&&(Qe.show=t[7]),K=new Et({props:Qe}),Ce.push(()=>je(K,"show",ct)),K.$on("confirm",t[31]),{c(){ee(e.$$.fragment),a=A(),s=m("div"),r=m("div"),p=m("form"),v=m("div"),w=m("div"),k=m("div"),S=m("div"),ee(b.$$.fragment),E=A(),T=m("div"),ee(y.$$.fragment),q=A(),I=m("div"),D=m("button"),ee(g.$$.fragment),C=A(),O=m("div"),H=W(R),ne=A(),U=m("div"),M.c(),me=A(),ee(G.$$.fragment),be=A(),X=m("div"),B.block.c(),we=A(),Y=m("div"),le=m("div"),L=m("div"),re=m("span"),Pe=W(i),Le=A(),Ve=W(ke),Oe=A(),Be=m("br"),Re=W(`—
+							`),_e=m("span"),qe=W(ye),$e=A(),fe=m("button"),Ae=W(Ee),Ne=A(),ee(K.$$.fragment),this.h()},l(u){te(e.$$.fragment,u),a=N(u),s=_(u,"DIV",{class:!0});var c=h(s);r=_(c,"DIV",{class:!0});var x=h(r);p=_(x,"FORM",{class:!0});var he=h(p);v=_(he,"DIV",{class:!0});var z=h(v);w=_(z,"DIV",{class:!0});var ue=h(w);k=_(ue,"DIV",{class:!0});var J=h(k);S=_(J,"DIV",{class:!0});var pe=h(S);te(b.$$.fragment,pe),pe.forEach(d),E=N(J),T=_(J,"DIV",{class:!0});var Ie=h(T);te(y.$$.fragment,Ie),Ie.forEach(d),q=N(J),I=_(J,"DIV",{class:!0});var Te=h(I);D=_(Te,"BUTTON",{class:!0,type:!0});var de=h(D);te(g.$$.fragment,de),C=N(de),O=_(de,"DIV",{class:!0});var De=h(O);H=j(De,R),De.forEach(d),de.forEach(d),Te.forEach(d),J.forEach(d),ne=N(ue),U=_(ue,"DIV",{class:!0});var Se=h(U);M.l(Se),me=N(Se),te(G.$$.fragment,Se),Se.forEach(d),ue.forEach(d),be=N(z),X=_(z,"DIV",{class:!0});var tt=h(X);B.block.l(tt),tt.forEach(d),we=N(z),Y=_(z,"DIV",{class:!0});var Ue=h(Y);le=_(Ue,"DIV",{class:!0});var ot=h(le);L=_(ot,"DIV",{class:!0});var ce=h(L);re=_(ce,"SPAN",{class:!0});var st=h(re);Pe=j(st,i),st.forEach(d),Le=N(ce),Ve=j(ce,ke),Oe=N(ce),Be=_(ce,"BR",{}),Re=j(ce,`—
+							`),_e=_(ce,"SPAN",{class:!0});var at=h(_e);qe=j(at,ye),at.forEach(d),ce.forEach(d),ot.forEach(d),$e=N(Ue),fe=_(Ue,"BUTTON",{class:!0,type:!0});var nt=h(fe);Ae=j(nt,Ee),nt.forEach(d),Ue.forEach(d),z.forEach(d),he.forEach(d),x.forEach(d),c.forEach(d),Ne=N(u),te(K.$$.fragment,u),this.h()},h(){f(S,"class","shrink-0 mr-2"),f(T,"class","flex-1"),f(O,"class","text-sm font-medium shrink-0"),f(D,"class","bg-gray-50 hover:bg-gray-100 text-black dark:bg-gray-850 dark:hover:bg-gray-800 dark:text-white transition px-2 py-1 rounded-full flex gap-1 items-center"),f(D,"type","button"),f(I,"class","self-center shrink-0"),f(k,"class","flex w-full items-center"),f(U,"class","flex gap-2 px-1 items-center"),f(w,"class","w-full mb-2 flex flex-col gap-0.5"),f(X,"class","mb-2 flex-1 overflow-auto h-0 rounded-lg"),f(re,"class","font-semibold dark:text-gray-200"),f(_e,"class","font-medium dark:text-gray-400"),f(L,"class","text-xs text-gray-500 line-clamp-2"),f(le,"class","flex-1 pr-3"),f(fe,"class","px-3.5 py-1.5 text-sm font-medium bg-black hover:bg-gray-900 text-white dark:bg-white dark:text-black dark:hover:bg-gray-100 transition rounded-full"),f(fe,"type","submit"),f(Y,"class","pb-3 flex justify-between"),f(v,"class","flex flex-col flex-1 overflow-auto h-0 rounded-lg"),f(p,"class","flex flex-col max-h-[100dvh] h-full"),f(r,"class","mx-auto w-full md:px-0 h-full"),f(s,"class","flex flex-col justify-between w-full overflow-y-auto h-full")},m(u,c){oe(e,u,c),ae(u,a,c),ae(u,s,c),l(s,r),l(r,p),l(p,v),l(v,w),l(w,k),l(k,S),oe(b,S,null),l(k,E),l(k,T),oe(y,T,null),l(k,q),l(k,I),l(I,D),oe(g,D,null),l(D,C),l(D,O),l(O,H),l(w,ne),l(w,U),ie[P].m(U,null),l(U,me),oe(G,U,null),l(v,be),l(v,X),B.block.m(X,B.anchor=null),B.mount=()=>X,B.anchor=null,l(v,we),l(v,Y),l(Y,le),l(le,L),l(L,re),l(re,Pe),l(L,Le),l(L,Ve),l(L,Oe),l(L,Be),l(L,Re),l(L,_e),l(_e,qe),l(Y,$e),l(Y,fe),l(fe,Ae),t[28](p),ae(u,Ne,c),oe(K,u,c),Z=!0,He||(Ke=[ge(D,"click",t[22]),ge(p,"submit",ht(t[29]))],He=!0)},p(u,c){var Ie,Te,de,De;t=u;const x={};c[0]&2048&&(x.allowPublic=((de=(Te=(Ie=t[11])==null?void 0:Ie.permissions)==null?void 0:Te.sharing)==null?void 0:de.public_tools)||((De=t[11])==null?void 0:De.role)==="admin"),!o&&c[0]&256&&(o=!0,x.show=t[8],Ge(()=>o=!1)),!n&&c[0]&16&&(n=!0,x.accessControl=t[4],Ge(()=>n=!1)),e.$set(x);const he={};c[0]&4096&&(he.content=t[12].t("Back")),c[1]&64&&(he.$$scope={dirty:c,ctx:t}),b.$set(he);const z={};c[0]&4096&&(z.content=t[12].t("e.g. My Tools")),c[0]&4097|c[1]&64&&(z.$$scope={dirty:c,ctx:t}),y.$set(z),(!Z||c[0]&4096)&&R!==(R=t[12].t("Access")+"")&&Q(H,R);let ue=P;P=Je(t),P===ue?ie[P].p(t,c):(pt(),$(ie[ue],1,1,()=>{ie[ue]=null}),gt(),M=ie[P],M?M.p(t,c):(M=ie[P]=ze[P](t),M.c()),F(M,1),M.m(U,me));const J={};c[0]&4096&&(J.content=t[12].t("e.g. Tools for performing various operations")),c[0]&4104|c[1]&64&&(J.$$scope={dirty:c,ctx:t}),G.$set(J),kt(B,t,c),(!Z||c[0]&4096)&&i!==(i=t[12].t("Warning:")+"")&&Q(Pe,i),(!Z||c[0]&4096)&&ke!==(ke=t[12].t("Tools are a function calling system with arbitrary code execution")+"")&&Q(Ve,ke),(!Z||c[0]&4096)&&ye!==(ye=t[12].t("don't install random tools from sources you don't trust.")+"")&&Q(qe,ye),(!Z||c[0]&4096)&&Ee!==(Ee=t[12].t("Save")+"")&&Q(Ae,Ee);const pe={};c[0]&4096|c[1]&64&&(pe.$$scope={dirty:c,ctx:t}),!Fe&&c[0]&128&&(Fe=!0,pe.show=t[7],Ge(()=>Fe=!1)),K.$set(pe)},i(u){Z||(F(e.$$.fragment,u),F(b.$$.fragment,u),F(y.$$.fragment,u),F(g.$$.fragment,u),F(M),F(G.$$.fragment,u),F(B.block),F(K.$$.fragment,u),Z=!0)},o(u){$(e.$$.fragment,u),$(b.$$.fragment,u),$(y.$$.fragment,u),$(g.$$.fragment,u),$(M),$(G.$$.fragment,u);for(let c=0;c<3;c+=1){const x=B.blocks[c];$(x)}$(K.$$.fragment,u),Z=!1},d(u){u&&(d(a),d(s),d(Ne)),se(e,u),se(b),se(y),se(g),ie[P].d(),se(G),B.block.d(),B.token=null,B=null,t[28](null),se(K,u),He=!1,vt(Ke)}}}function Rt(t,e,o){let n,a;lt(t,Ct,i=>o(11,n=i));const s=bt("i18n");lt(t,s,i=>o(12,a=i));let r=null,p=!1,v=!1,{edit:w=!1}=e,{clone:k=!1}=e,{onSave:S=()=>{}}=e,{id:b=""}=e,{name:E=""}=e,{meta:T={description:""}}=e,{content:y=""}=e,{accessControl:q={}}=e,I="";const D=()=>{o(9,I=y)};let g,C=`import os
+import requests
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+class Tools:
+    def __init__(self):
+        pass
+
+    # Add your custom tools using pure Python code here, make sure to add type hints and descriptions
+	
+    def get_user_name_and_email_and_id(self, __user__: dict = {}) -> str:
+        """
+        Get the user name, Email and ID from the user object.
+        """
+
+        # Do not include a descrption for __user__ as it should not be shown in the tool's specification
+        # The session user object will be passed as a parameter when the function is called
+
+        print(__user__)
+        result = ""
+
+        if "name" in __user__:
+            result += f"User: {__user__['name']}"
+        if "id" in __user__:
+            result += f" (ID: {__user__['id']})"
+        if "email" in __user__:
+            result += f" (Email: {__user__['email']})"
+
+        if result == "":
+            result = "User: Unknown"
+
+        return result
+
+    def get_current_time(self) -> str:
+        """
+        Get the current time in a more human-readable format.
+        """
+
+        now = datetime.now()
+        current_time = now.strftime("%I:%M:%S %p")  # Using 12-hour format with AM/PM
+        current_date = now.strftime(
+            "%A, %B %d, %Y"
+        )  # Full weekday, month name, day, and year
+
+        return f"Current Date and Time = {current_date}, {current_time}"
+
+    def calculator(
+        self,
+        equation: str = Field(
+            ..., description="The mathematical equation to calculate."
+        ),
+    ) -> str:
+        """
+        Calculate the result of an equation.
+        """
+
+        # Avoid using eval in production code
+        # https://nedbatchelder.com/blog/201206/eval_really_is_dangerous.html
+        try:
+            result = eval(equation)
+            return f"{equation} = {result}"
+        except Exception as e:
+            print(e)
+            return "Invalid equation"
+
+    def get_current_weather(
+        self,
+        city: str = Field(
+            "New York, NY", description="Get the current weather for a given city."
+        ),
+    ) -> str:
+        """
+        Get the current weather for a given city.
+        """
+
+        api_key = os.getenv("OPENWEATHER_API_KEY")
+        if not api_key:
+            return (
+                "API key is not set in the environment variable 'OPENWEATHER_API_KEY'."
+            )
+
+        base_url = "http://api.openweathermap.org/data/2.5/weather"
+        params = {
+            "q": city,
+            "appid": api_key,
+            "units": "metric",  # Optional: Use 'imperial' for Fahrenheit
+        }
+
+        try:
+            response = requests.get(base_url, params=params)
+            response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
+            data = response.json()
+
+            if data.get("cod") != 200:
+                return f"Error fetching weather data: {data.get('message')}"
+
+            weather_description = data["weather"][0]["description"]
+            temperature = data["main"]["temp"]
+            humidity = data["main"]["humidity"]
+            wind_speed = data["wind"]["speed"]
+
+            return f"Weather in {city}: {temperature}°C"
+        except requests.RequestException as e:
+            return f"Error fetching weather data: {str(e)}"
+`;const O=async()=>{S({id:b,name:E,meta:T,content:y,access_control:q})},R=async()=>{if(g){o(1,y=I),await Ye();const i=await g.formatPythonCodeHandler();await Ye(),o(1,y=I),await Ye(),i&&O()}};function H(i){v=i,o(8,v)}function ne(i){q=i,o(4,q)}const U=()=>{yt("/workspace/tools")};function P(){E=this.value,o(0,E)}const M=()=>{o(8,v=!0)};function me(){b=this.value,o(2,b),o(0,E),o(5,w),o(16,k)}function G(){T.description=this.value,o(3,T)}const be=i=>{o(9,I=i)},X=async()=>{r&&r.requestSubmit()};function we(i){Ce[i?"unshift":"push"](()=>{g=i,o(10,g)})}function Y(i){Ce[i?"unshift":"push"](()=>{r=i,o(6,r)})}const le=()=>{w?R():o(7,p=!0)};function L(i){p=i,o(7,p)}const re=()=>{R()};return t.$$set=i=>{"edit"in i&&o(5,w=i.edit),"clone"in i&&o(16,k=i.clone),"onSave"in i&&o(17,S=i.onSave),"id"in i&&o(2,b=i.id),"name"in i&&o(0,E=i.name),"meta"in i&&o(3,T=i.meta),"content"in i&&o(1,y=i.content),"accessControl"in i&&o(4,q=i.accessControl)},t.$$.update=()=>{t.$$.dirty[0]&2&&y&&D(),t.$$.dirty[0]&65569&&E&&!w&&!k&&o(2,b=E.replace(/\s+/g,"_").toLowerCase())},[E,y,b,T,q,w,r,p,v,I,g,n,a,s,C,R,k,S,H,ne,U,P,M,me,G,be,X,we,Y,le,L,re]}class Xt extends dt{constructor(e){super(),mt(this,e,Rt,Bt,_t,{edit:5,clone:16,onSave:17,id:2,name:0,meta:3,content:1,accessControl:4},null,[-1,-1])}}export{Xt as T};
+//# sourceMappingURL=-4XgL5U9.js.map
---
+++ packages/open-webui/open_webui-0.6.30-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.BIgETXcy.js
@@ -0,0 +1,2 @@
+import{l as o,c as r}from"../chunks/Bk82nkIB.js";export{o as load_css,r as start};
+//# sourceMappingURL=start.BIgETXcy.js.map

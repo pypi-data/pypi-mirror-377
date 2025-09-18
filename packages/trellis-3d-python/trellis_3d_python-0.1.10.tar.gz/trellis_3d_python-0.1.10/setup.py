66ad675f3598ca1ad5b67834a7acbb03a5512cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 04:11:29 +0000

    Add package trellis-3d-python/0.1.10/trellis_3d_python-0.1.10.tar.gz

---
+++ packages/trellis-3d-python/trellis_3d_python-0.1.10.tar.gz/trellis_3d_python-0.1.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="trellis-3d-python",
+    version="0.1.10",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "numpy",
+        "pillow",
+    ],
+    author="Microsoft Corporation",
+    author_email="t-jxiang@microsoft.com",
+    description="TRELLIS is a large 3D asset generation model.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/microsoft/trellis",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+) 

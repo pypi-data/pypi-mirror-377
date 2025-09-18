51a55a30cbfb01906abc8bf6f8768af0b5fb2374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:18:18 +0000

    Add package ai-import-plugin/0.3.0/ai_import_plugin-0.3.0-py3-none-any.whl

---
+++ packages/ai-import-plugin/ai_import_plugin-0.3.0-py3-none-any.whl/ai_import_plugin/cms_app.py
@@ -0,0 +1,30 @@
+from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
+from openedx.core.djangoapps.plugins.plugin_settings import PluginSettings
+
+class AIImportCMSAppConfig(PluginSettings):
+    """
+    Plugin configuration for CMS.
+    """
+    plugin_app = {
+        "settings_config": {
+            "cms.djangoapp": {
+                "common": {"relative_path": "settings.common"},
+            },
+        },
+        "urls": {
+            "cms.djangoapp": {
+                "namespace": "ai_import_plugin",
+                "regex": r"^ai-import/",
+                "relative_path": "urls",
+            }
+        },
+        "template_extensions": {
+            "cms.djangoapp": {
+                "index.html": {   # inject into cms/templates/index.html
+                    "before": ["new_course_button"],  # hook point
+                    "template": "ai_import_plugin/import_button.html",
+                }
+            }
+        },
+    }
+

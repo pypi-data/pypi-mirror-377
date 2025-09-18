07b1c32af0f25609220ae18b3f0eb33b3f12f2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 14:06:56 +0000

    Add package MAVez/2.1.0/mavez-2.1.0-py3-none-any.whl

---
+++ packages/MAVez/mavez-2.1.0-py3-none-any.whl/MAVez/__init__.py
@@ -0,0 +1,13 @@
+from . import coordinate
+from . import flight_controller
+from . import controller
+from . import mission_item
+from . import mission
+
+__all__ = [
+    "coordinate",
+    "flight_controller",
+    "controller",
+    "mission_item",
+    "mission"
+]

301796716a5279b508ffc207715b459c620c2c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 07:05:50 +0000

    Add package dshellInterpreter/0.2.16.0/dshellinterpreter-0.2.16.0-py3-none-any.whl

---
+++ packages/dshellInterpreter/dshellinterpreter-0.2.16.0-py3-none-any.whl/Dshell/DISCORD_COMMANDS/utils/utils_global.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "utils_len",
+]
+
+async def utils_len(ctx, value):
+    """
+    Return the length of a list, or a string
+    :param value:
+    :return:
+    """
+    from ..._DshellParser.ast_nodes import ListNode
+    if not isinstance(value, (str, ListNode)):
+        raise TypeError(f"value must be a list or a string in len command, not {type(value)}")
+
+    return len(value)

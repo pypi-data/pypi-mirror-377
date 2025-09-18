9ada70ad3de1c0144c18b8842e234af0e45c2249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 07:06:22 +0000

    Add package microsoft-agents-hosting-core/0.3.2/microsoft_agents_hosting_core-0.3.2-py3-none-any.whl

---
+++ packages/microsoft-agents-hosting-core/microsoft_agents_hosting_core-0.3.2-py3-none-any.whl/microsoft_agents/hosting/core/storage/__init__.py
@@ -0,0 +1,5 @@
+from .store_item import StoreItem
+from .storage import Storage, AsyncStorageBase
+from .memory_storage import MemoryStorage
+
+__all__ = ["StoreItem", "Storage", "AsyncStorageBase", "MemoryStorage"]

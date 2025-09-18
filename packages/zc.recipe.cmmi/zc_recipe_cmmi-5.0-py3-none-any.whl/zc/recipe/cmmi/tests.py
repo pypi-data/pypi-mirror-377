ad1d99aa72a32fa5b81eeeaf8b4f694c7a57b8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:07:12 +0000

    Add package zc.recipe.cmmi/5.0/zc_recipe_cmmi-5.0-py3-none-any.whl

---
+++ packages/zc.recipe.cmmi/zc_recipe_cmmi-5.0-py3-none-any.whl/zc/recipe/cmmi/tests.py
@@ -0,0 +1,149 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import doctest
+import os
+import re
+import sys
+import tarfile
+import unittest
+from io import BytesIO as _BytesIO
+
+import zc.buildout.testing
+import zc.buildout.tests
+from zope.testing import renormalizing
+
+
+def _as_bytes(s):
+    return s.encode('utf-8') if not isinstance(s, bytes) else s
+
+
+def BytesIO(s):
+    return _BytesIO(_as_bytes(s))
+
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install_develop('zc.recipe.cmmi', test)
+    distros = test.globs['distros'] = test.globs['tmpdir']('distros')
+
+    tarpath = os.path.join(distros, 'foo.tgz')
+    with tarfile.open(tarpath, 'w:gz') as tar:
+        configure = configure_template % sys.executable
+        info = tarfile.TarInfo('configure')
+        info.size = len(configure)
+        info.mode = 0o755
+        tar.addfile(info, BytesIO(configure))
+
+    tarpath = os.path.join(distros, 'bar.tgz')
+    with tarfile.open(tarpath, 'w:gz') as tar:
+        configure = configure_template % sys.executable
+        info = tarfile.TarInfo('configure.in')
+        info.size = len(configure)
+        info.mode = 0o755
+        tar.addfile(info, BytesIO(configure))
+
+        autogen = autogen_template
+        info = tarfile.TarInfo('autogen.sh')
+        info.size = len(autogen)
+        info.mode = 0o755
+        tar.addfile(info, BytesIO(autogen))
+
+    tarpath = os.path.join(distros, 'baz.tgz')
+    with tarfile.open(tarpath, 'w:gz') as tar:
+        configure = configure_template % sys.executable
+        info = tarfile.TarInfo('configure.py')
+        info.size = len(configure)
+        info.mode = 0o755
+        tar.addfile(info, BytesIO(configure))
+
+
+def tearDown(test):
+    zc.buildout.testing.buildoutTearDown(test)
+
+
+configure_template = """#!%s
+import sys
+print("configuring foo " + ' '.join(sys.argv[1:]))
+
+Makefile_template = '''
+all:
+\techo building foo
+
+install:
+\techo installing foo
+'''
+
+with open('Makefile', 'w') as f: f.write(Makefile_template)
+
+"""
+
+autogen_template = """#!/bin/sh
+mv configure.in configure
+"""
+
+checker = renormalizing.RENormalizing([
+    zc.buildout.testing.normalize_path,
+    zc.buildout.testing.normalize_script,
+    zc.buildout.testing.normalize_egg_py,
+    zc.buildout.tests.normalize_bang,
+    (re.compile('http://localhost:[0-9]{4,5}/'), 'http://localhost/'),
+    (re.compile('occured'), 'occurred'),
+    (re.compile('extdemo[.]pyd'), 'extdemo.so'),
+    (re.compile('[0-9a-f]{40}'), '<BUILDID>'),
+    # Buildout or setuptools has a bug not closing .egg-link files,
+    # leading to issues being reported by PyPy, which naturally
+    # mess up doctests.
+    (re.compile('Exception IOError: IOError.*finalizer of <closed file.*'),
+     ''),
+    # Cope with different versions of the patch command:
+    (re.compile(r'Hunk #1 succeeded at 1 with fuzz 1\.'), ''),
+    (re.compile('patch unexpectedly ends in middle of line'), ''),
+    (re.compile(
+        r'patch: \*\*\*\* Only garbage was found in the patch input.'), ''),
+    (re.compile('  I can\'t seem to find a patch in there anywhere.'), ''),
+])
+
+optionflags = (
+    doctest.ELLIPSIS
+    | doctest.NORMALIZE_WHITESPACE
+    | doctest.REPORT_NDIFF
+)
+
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite(
+            'README.rst',
+            setUp=setUp, tearDown=tearDown,
+            checker=checker,
+            optionflags=optionflags
+        ),
+        doctest.DocFileSuite(
+            'downloadcache.rst',
+            'patching.rst',
+            'shared.rst',
+            setUp=setUp,
+            tearDown=tearDown,
+            checker=checker,
+            optionflags=optionflags
+        ),
+        doctest.DocFileSuite(
+            'misc.rst',
+            setUp=setUp,
+            tearDown=tearDown,
+            checker=checker,
+            optionflags=optionflags
+        ),
+    ))

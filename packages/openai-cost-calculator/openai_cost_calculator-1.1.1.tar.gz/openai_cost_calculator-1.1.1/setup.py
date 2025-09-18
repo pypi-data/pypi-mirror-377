60533f3875cf1bbe159a928edc7c989a42e79209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:07:02 +0000

    Add package openai-cost-calculator/1.1.1/openai_cost_calculator-1.1.1.tar.gz

---
+++ packages/openai-cost-calculator/openai_cost_calculator-1.1.1.tar.gz/openai_cost_calculator-1.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="openai_cost_calculator",
+    version="1.1.1",
+    description="A library to estimate OpenAI API costs based on token usage.",
+    author="Orkun Kınay, Murat Barkın Kınay",
+    author_email="orkunkinay@sabanciuniv.edu",
+    url="https://github.com/orkunkinay/openai_cost_calculator", 
+    packages=find_packages(),
+    include_package_data=True, 
+    package_data={
+        "openai_cost_calculator": ["data/gpt_pricing_data.csv"],
+    },
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)

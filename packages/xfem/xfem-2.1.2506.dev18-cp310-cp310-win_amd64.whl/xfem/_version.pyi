c264c802d623a888c37b16dd675b8262dd55085d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:07:45 +0000

    Add package xfem/2.1.2506.dev18/xfem-2.1.2506.dev18-cp310-cp310-win_amd64.whl

---
+++ packages/xfem/xfem-2.1.2506.dev18-cp310-cp310-win_amd64.whl/xfem/_version.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from builtins import object as VERSION_TUPLE
+from builtins import object as COMMIT_ID
+__all__: list = ['__version__', '__version_tuple__', 'version', 'version_tuple', '__commit_id__', 'commit_id']
+__commit_id__: str = 'gb265c146d'
+__version__: str = '2.1.2506.dev18'
+__version_tuple__: tuple = (2, 1, 2506, 'dev18')
+commit_id: str = 'gb265c146d'
+version: str = '2.1.2506.dev18'
+version_tuple: tuple = (2, 1, 2506, 'dev18')

b247ff624b5761692fb371f531224e2a39e04be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:05:40 +0000

    Add package tess-ida-tools/1.1.2/tess_ida_tools-1.1.2.tar.gz

---
+++ packages/tess-ida-tools/tess_ida_tools-1.1.2.tar.gz/tess_ida_tools-1.1.2/get-tess-ida.py
@@ -0,0 +1,272 @@
+#!/bin/env python3
+# ----------------------------------------------------------------------
+# Copyright (c) 2024 Rafael Gonzalez.
+#
+# See the LICENSE file for details
+# ----------------------------------------------------------------------
+
+# -----------------------
+# Standard Python imports
+# ----------------------
+
+import os
+import sys
+import logging
+import logging.handlers
+from datetime import datetime
+import argparse
+from argparse import Namespace
+from typing import Union
+
+# -------------------
+# Third party imports
+# -------------------
+
+import decouple
+import requests
+from dateutil.relativedelta import relativedelta
+
+# ----------------
+# Module constants
+# ----------------
+
+DESCRIPTION = "Get TESS-W IDA monthly files from NextCloud server"
+__version__ = "1.0.1"
+
+OptStr = Union[str, None]
+
+# -----------------------
+# Module global variables
+# -----------------------
+
+# get the root logger
+log = logging.getLogger(__name__.split('.')[-1])
+
+# -------------------
+# Auxiliary functions
+# -------------------
+
+
+def vmonth(datestr: str) -> datetime:
+    return datetime.strptime(datestr, '%Y-%m')
+
+
+def vyear(datestr: str) -> datetime:
+    return datetime.strptime(datestr, '%Y')
+
+
+def cur_month() -> datetime:
+    return datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+
+def prev_month() -> datetime:
+    month = datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+    return month - relativedelta(months=1)
+
+
+def to_phot_dir(base_dir: str | None, name: str) -> str:
+    cwd = os.getcwd()
+    base_dir = cwd if base_dir is None else base_dir
+    base_dir = os.path.join(cwd, base_dir) if not os.path.isabs(
+        base_dir) else base_dir
+    full_dir_path = os.path.join(base_dir, name)
+    return full_dir_path
+
+
+def makedirs(base_dir: OptStr, name: str) -> str:
+    full_dir_path = to_phot_dir(base_dir, name)
+    if not os.path.isdir(full_dir_path):
+        os.makedirs(full_dir_path)
+    return full_dir_path
+
+
+def name_month(ida_file_path: str) -> tuple:
+    name = os.path.basename(os.path.dirname(ida_file_path))
+    month = os.path.splitext(os.path.basename(ida_file_path))[0].split('_')[1]
+    return name, month
+
+# --------------
+# Work functions
+# --------------
+
+
+def do_ida_single_month(base_url: str, ida_base_dir: str, name: str, month: OptStr, exact: OptStr, timeout: int) -> None:
+    url = base_url + '/download'
+    target_file = name + '_' + month + '.dat' if not exact else exact
+    _, month1 = name_month(target_file)
+    params = {'path': '/' + name, 'files': target_file}
+    resp = requests.get(url, params=params, timeout=timeout, verify=False)
+    if resp.status_code == 404:
+        log.warning("[%s] [%s] No monthly file exits: %s",
+                    name, month1, target_file)
+        return
+    resp.raise_for_status()  # catch other unexpected return code
+    log.info("[%s] [%s] GET %s [%d OK]", name,
+             month1, resp.url, resp.status_code)
+    contents = resp.text
+    full_dir_path = makedirs(ida_base_dir, name)
+    file_path = os.path.join(full_dir_path, target_file)
+    with open(file_path, mode='w') as f:
+        log.info("[%s] [%s] Writing %s", name, month1, file_path)
+        f.write(contents)
+
+# ================================
+# COMMAND LINE INTERFACE FUNCTIONS
+# ================================
+
+
+def cli_ida_single(base_url: str, args: Namespace) -> None:
+    month = args.month.strftime('%Y-%m') if not args.exact else None
+    do_ida_single_month(
+        base_url=base_url,
+        ida_base_dir=args.out_dir,
+        name=args.name,
+        month=month,
+        exact=args.exact,
+        timeout=4
+    )
+
+
+def cli_ida_range(base_url: str, args: Namespace) -> None:
+    month = args.since
+    while month <= args.until:
+        do_ida_single_month(
+            base_url=base_url,
+            ida_base_dir=args.out_dir,
+            name=args.name,
+            month=month.strftime('%Y-%m'),
+            exact=None,
+            timeout=4
+        )
+        month += relativedelta(months=1)
+
+
+def cli_ida_photometers(base_url: str, args: Namespace) -> None:
+    rang = sorted(args.range) if args.range is not None else None
+    seq = sorted(args.list) if args.list is not None else None
+    if rang:
+        for i in range(rang[0],  rang[1]+1):
+            args.name = 'stars' + str(i)
+            cli_ida_range(base_url, args)
+    else:
+        for i in seq:
+            args.name = 'stars' + str(i)
+            cli_ida_range(base_url, args)
+
+# =================
+# LOGGER AND PARSER
+# =================
+
+
+def configure_log(args: Namespace):
+    '''Configure the root logger'''
+    if args.verbose:
+        level = logging.DEBUG
+    elif args.quiet:
+        level = logging.WARNING
+    else:
+        level = logging.INFO
+    # set the root logger level
+    log = logging.getLogger()
+    log.setLevel(level)
+    # Log formatter
+    fmt = logging.Formatter(
+        '%(asctime)s [%(levelname)s] [%(name)-4s] %(message)s')
+    # create console handler and set level to debug
+    if args.console:
+        ch = logging.StreamHandler()
+        ch.setFormatter(fmt)
+        ch.setLevel(logging.DEBUG)
+        log.addHandler(ch)
+    # Create a file handler suitable for logrotate usage
+    if args.log_file:
+        fh = logging.handlers.WatchedFileHandler(args.log_file)
+        #fh = logging.handlers.TimedRotatingFileHandler(args.log_file, when='midnight', interval=1, backupCount=365)
+        fh.setFormatter(fmt)
+        fh.setLevel(logging.DEBUG)
+        log.addHandler(fh)
+
+
+def args_parser(name: str, version: str, description: str) -> None:
+    # create the top-level parser with generic options
+    parser = argparse.ArgumentParser(prog=name, description=description)
+    parser.add_argument('--version', action='version',
+                        version='{0} {1}'.format(name, version))
+    parser.add_argument('--console', action='store_true',
+                        help='Log to vanilla console.')
+    parser.add_argument('--log-file', type=str,
+                        metavar="<FILE>", default=None, help='Log to file.')
+    group1 = parser.add_mutually_exclusive_group()
+    group1.add_argument('--verbose', action='store_true',
+                        help='Verbose output.')
+    group1.add_argument('--quiet',   action='store_true', help='Quiet output.')
+    # Now parse the application specific parts
+    subparser = parser.add_subparsers(dest='command')
+    parser_single = subparser.add_parser(
+        'single', help='Download single monthly file from a photometer')
+    parser_single.add_argument(
+        '-n', '--name', type=str, required=True, help='Photometer name')
+    parser_single.add_argument(
+        '-o', '--out-dir', type=str, default=None, help='Output base directory')
+    group1 = parser_single.add_mutually_exclusive_group(required=True)
+    group1.add_argument('-e', '--exact', type=str,
+                        default=None, help='Specific monthly file name')
+    group1.add_argument('-m', '--month',  type=vmonth,
+                        default=None, metavar='<YYYY-MM>', help='Year and Month')
+    parser_range = subparser.add_parser(
+        'range', help='Download a month range from a photometer')
+    parser_range.add_argument('-n', '--name', type=str,
+                              required=True, help='Photometer name')
+    parser_range.add_argument('-s', '--since',  type=vmonth, default=prev_month(
+    ), metavar='<YYYY-MM>', help='Year and Month (defaults to %(default)s')
+    parser_range.add_argument('-u', '--until',  type=vmonth, default=cur_month(
+    ), metavar='<YYYY-MM>', help='Year and Month (defaults to %(default)s')
+    parser_range.add_argument(
+        '-o', '--out-dir', type=str, default=None, help='Output IDA base directory')
+    parser_phot = subparser.add_parser(
+        'photometers', help='Download a month range from selected photometers')
+    group2 = parser_phot.add_mutually_exclusive_group(required=True)
+    group2.add_argument('-l', '--list', type=int, default=None,
+                        nargs='+', metavar='<N>', help='Photometer number list')
+    group2.add_argument('-r', '--range', type=int, default=None,
+                        metavar='<N>', nargs=2, help='Photometer number range')
+    parser_phot.add_argument('-s', '--since',  type=vmonth, default=prev_month(
+    ), metavar='<YYYY-MM>', help='Year and Month (defaults to %(default)s')
+    parser_phot.add_argument('-u', '--until',  type=vmonth, default=cur_month(),
+                             metavar='<YYYY-MM>', help='Year and Month (defaults to %(default)s')
+    parser_phot.add_argument('-o', '--out-dir', type=str,
+                             default=None, help='Output IDA base directory')
+    return parser
+
+# =============
+# MAIN FUNCTION
+# =============
+
+
+CMD_TABLE = {
+    'single': cli_ida_single,
+    'range': cli_ida_range,
+    'photometers': cli_ida_photometers,
+}
+
+
+def main() -> None:
+    '''The main entry point specified by pyproject.toml'''
+    parser = args_parser(
+        name=__name__,
+        version=__version__,
+        description=DESCRIPTION
+    )
+    args = parser.parse_args(sys.argv[1:])
+    configure_log(args)
+    try:
+        base_url = decouple.config('IDA_URL')
+        func = CMD_TABLE[args.command]
+        func(base_url, args)
+        log.info("done!")
+    except KeyboardInterrupt:
+        log.warn("Application quits by user request")
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/tess-ida-tools/tess_ida_tools-1.1.2.tar.gz/tess_ida_tools-1.1.2/src/tess/ida/pipeline.py
@@ -0,0 +1,317 @@
+# ----------------------------------------------------------------------
+# Copyright (c) 2024 Rafael Gonzalez.
+#
+# See the LICENSE file for details
+# ----------------------------------------------------------------------
+
+# -----------------------
+# Standard Python imports
+# ----------------------
+
+import asyncio
+import logging
+
+
+from datetime import datetime
+from argparse import Namespace, ArgumentParser
+from typing import Sequence
+
+# -------------------
+# Third party imports
+# -------------------
+
+import decouple
+
+from lica.asyncio.cli import execute
+from lica.typing import OptStr
+
+# --------------
+# local imports
+# -------------
+
+from . import __version__
+from .utils import parser as prs
+from .dbase import aux_dbase_load, aux_dbase_save
+from .download import (
+    download_ida_single, 
+    download_ida_range, 
+    ida_names_by_seq_or_range, 
+    ida_names_by_location
+)
+from .timeseries import (
+    to_ecsv_single,
+    to_ecsv_range,
+    to_ecsv_combine,
+    NoCoordinatesError,
+)
+
+# ----------------
+# Module constants
+# ----------------
+
+DESCRIPTION = "Full Download, Tranform and Combine IDA data from NextCloud Server"
+
+# -----------------------
+# Module global variables
+# -----------------------
+
+# get the root logger
+log = logging.getLogger(__name__.split(".")[-1])
+
+# -------------------
+# Auxiliary functions
+# -------------------
+
+
+# ===========
+# Generic API
+# ===========
+
+
+async def pipe_single(
+    base_url: str,
+    ida_base_dir: OptStr,
+    ecsv_base_dir: OptStr,
+    name: str,
+    month: OptStr,
+    exact: OptStr,
+    fix: bool,
+    timeout: int,
+) -> None:
+    await download_ida_single(base_url, ida_base_dir, name, month, exact, timeout)
+    await asyncio.to_thread(
+        to_ecsv_single, ida_base_dir, name, month, exact, ecsv_base_dir, fix
+    )
+
+
+async def pipe_range(
+    base_url: str,
+    ida_base_dir: OptStr,
+    ecsv_base_dir: OptStr,
+    name: str,
+    since: datetime,
+    until: datetime,
+    skip_download: bool,
+    oname: str,
+    fix: bool,
+    concurrent: int,
+    timeout: int,
+) -> None:
+    if not skip_download:
+        await download_ida_range(
+            base_url, ida_base_dir, name, since, until, concurrent, timeout
+        )
+    await asyncio.to_thread(
+        to_ecsv_range, ida_base_dir, name, ecsv_base_dir, since, until, fix
+    )
+    await asyncio.to_thread(to_ecsv_combine, ecsv_base_dir, name, since, until, oname)
+
+
+async def pipe_photometers(
+    base_url: str,
+    ida_base_dir: OptStr,
+    ecsv_base_dir: OptStr,
+    rang: Sequence[int],
+    seq: Sequence[int],
+    since: datetime,
+    until: datetime,
+    skip_download: bool,
+    oname: str,
+    fix: bool,
+    concurrent: int,
+    timeout: int,
+) -> None:
+    names = ida_names_by_seq_or_range(seq, rang)
+    if not skip_download:
+        for name in names:
+            await download_ida_range(base_url, ida_base_dir, name, since, until, concurrent, timeout)
+    for name in names:
+        await asyncio.to_thread(
+            to_ecsv_range, ida_base_dir, name, ecsv_base_dir, since, until, fix
+        )
+        await asyncio.to_thread(
+            to_ecsv_combine, ecsv_base_dir, name, since, until, oname
+        )
+
+async def pipe_location(
+    base_url: str,
+    ida_base_dir: OptStr,
+    ecsv_base_dir: OptStr,
+    lon: float,
+    lat: float,
+    radius: float,
+    since: datetime,
+    until: datetime,
+    skip_download: bool,
+    oname: str,
+    fix: bool,
+    concurrent: int,
+    timeout: int,
+) -> None:
+    names = await ida_names_by_location(
+        base_url, ida_base_dir, lon, lat, radius, timeout
+    )
+    if not skip_download:
+        for name in names:
+            await download_ida_range(base_url, ida_base_dir, name, since, until, concurrent, timeout)
+    for name in names:
+        await asyncio.to_thread(
+            to_ecsv_range, ida_base_dir, name, ecsv_base_dir, since, until, fix
+        )
+        await asyncio.to_thread(
+            to_ecsv_combine, ecsv_base_dir, name, since, until, oname
+        )
+
+# ================================
+# COMMAND LINE INTERFACE FUNCTIONS
+# ================================
+
+async def cli_pipe_single(args: Namespace) -> None:
+    await pipe_single(
+        base_url=args.base_url,
+        ida_base_dir=args.in_dir,
+        ecsv_base_dir=args.out_dir,
+        name=args.name,
+        month=args.month,
+        exact=args.exact,
+        fix=True if args.fix else False,
+        timeout=args.timeout,
+    )
+
+
+async def cli_pipe_range(args: Namespace) -> None:
+    await pipe_range(
+        base_url=args.base_url,
+        ida_base_dir=args.in_dir,
+        ecsv_base_dir=args.out_dir,
+        name=args.name,
+        since=args.since,
+        until=args.until,
+        skip_download=args.skip_download,
+        oname=args.out_filename,
+        fix=True if args.fix else False,
+        concurrent=args.concurrent,
+        timeout=args.timeout,
+    )
+
+
+async def cli_pipe_photometers(args: Namespace) -> None:
+    await pipe_photometers(
+        base_url=args.base_url,
+        ida_base_dir=args.in_dir,
+        ecsv_base_dir=args.out_dir,
+        seq=args.list,
+        rang=args.range,
+        since=args.since,
+        until=args.until,
+        skip_download=args.skip_download,
+        oname=args.out_filename,
+        fix=True if args.fix else False,
+        concurrent=args.concurrent,
+        timeout=args.timeout,
+    )
+
+
+async def cli_pipe_location(args: Namespace) -> None:
+    await pipe_location(
+        base_url=args.base_url,
+        ida_base_dir=args.in_dir,
+        ecsv_base_dir=args.out_dir,
+        lon=args.longitude,
+        lat=args.latitude,
+        radius=args.radius,
+        since=args.since,
+        until=args.until,
+        skip_download=args.skip_download,
+        oname=args.out_filename,
+        fix=True if args.fix else False,
+        concurrent=args.concurrent,
+        timeout=args.timeout,
+    )
+
+
+def add_args(parser: ArgumentParser) -> ArgumentParser:
+    # Now parse the application specific parts
+    subparser = parser.add_subparsers(dest="command")
+    parser_single = subparser.add_parser(
+        "single",
+        parents=[
+            prs.name(),
+            prs.out_dir("ECSV"),
+            prs.inout_file("IDA", "combined ECSV", in_dir_exists=False),
+            prs.mon_single(),
+            prs.timeout(),
+            prs.fix(),
+        ],
+        help="Process single monthly file from a photometer",
+    )
+    parser_single.set_defaults(func=cli_pipe_single)
+    parser_range = subparser.add_parser(
+        "range",
+        parents=[
+            prs.name(),
+            prs.out_dir("ECSV"),
+            prs.inout_file("IDA", "combined ECSV", in_dir_exists=False),
+            prs.mon_range(),
+            prs.concurrent(),
+            prs.fix(),
+            prs.skip(),
+        ],
+        help="Process a month range from a photometer",
+    )
+    parser_range.set_defaults(func=cli_pipe_range)
+    parser_phots = subparser.add_parser(
+        "photometers",
+        parents=[
+            prs.phot_range(),
+            prs.out_dir("ECSV"),
+            prs.inout_file("IDA", "combined ECSV", in_dir_exists=False),
+            prs.mon_range(),
+            prs.concurrent(),
+            prs.fix(),
+            prs.skip(),
+        ],
+        help="Download a month range for selected photometers",
+    )
+    parser_phots.set_defaults(func=cli_pipe_photometers)
+    parser_location = subparser.add_parser(
+        "near",
+        parents=[
+            prs.location(),
+            prs.out_dir("ECSV"),
+            prs.inout_file("IDA", "combined ECSV", in_dir_exists=False),
+            prs.mon_range(),
+            prs.concurrent(),
+            prs.fix(),
+            prs.skip(),
+        ],
+        help="Process a month range from photometers near a given location",
+    )
+    parser_location.set_defaults(func=cli_pipe_location)
+    return parser
+
+
+async def cli_pipeline(args: Namespace) -> None:
+    """The main entry point specified by pyproject.toml"""
+    args.base_url = decouple.config("IDA_URL")
+    aux_dbase_load()
+    try:
+        await args.func(args)
+    except NoCoordinatesError:
+        pass
+    aux_dbase_save()
+    log.info("done!")
+
+
+def main() -> None:
+    execute(
+        main_func=cli_pipeline,
+        add_args_func=add_args,
+        name="tess-ida-pipe",
+        version=__version__,
+        description=DESCRIPTION,
+    )
+
+
+if __name__ == "__main__":
+    main()

7b02f91fadfb4241f97348a2d16256e71e9dafce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:06:57 +0000

    Add package iptv9/0.1.0/iptv9-0.1.0.tar.gz

---
+++ packages/iptv9/iptv9-0.1.0.tar.gz/iptv9-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='iptv9',  # Lowercase, unique on PyPI
+    version='0.1.0',
+    py_modules=['iptv9'],  # Matches your filename
+    entry_points={
+        'console_scripts': [
+            'iptv9 = iptv9:main',  # CLI command: maps to iptv9.py:main()
+        ],
+    },
+    author='ssskingsss12',
+    author_email='smalls3000i@gmail.com',
+    description='Web Enumeration Tool',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.12',
+)
+

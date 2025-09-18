4da163cdb1054cfed7603424a3203fb55873fe16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:06:46 +0000

    Add package aws-message-client/1.6.1/aws_message_client-1.6.1.tar.gz

---
+++ packages/aws-message-client/aws_message_client-1.6.1.tar.gz/aws_message_client-1.6.1/README.md
@@ -0,0 +1,35 @@
+# AWS SNS/SQS Message App
+
+[![Build Status](https://github.com/uw-it-aca/aws-message-client/workflows/tests/badge.svg)](https://github.com/uw-it-aca/aws-message-client/actions)
+[![Coverage Status](https://coveralls.io/repos/github/uw-it-aca/aws-message-client/badge.svg?branch=main)](https://coveralls.io/github/uw-it-aca/aws-message-client?branch=main)
+[![PyPi Version](https://img.shields.io/pypi/v/aws-message-client.svg)](https://pypi.python.org/pypi/aws-message-client)
+![Python versions](https://img.shields.io/badge/python-3.12-blue.svg)
+
+Application on which to build AWS SNS endpoints and SQS gatherers
+
+Installation
+------------
+
+    $ pip install aws-message-client
+
+Project settings.py
+------------------
+
+**AWS App settings**
+
+     AWS_SQS = {
+         '<settings_name>': {
+             'QUEUE_ARN': 'arn:aws:sqs:...',
+             'KEY_ID': '<longrandomlookingstring>',
+             'KEY': '<longerrandomlookingstring>',
+             'WAIT_TIME': 10,
+             'VISIBILITY_TIMEOUT': 10,
+             'MESSAGE_GATHER_SIZE': 10,
+             'VALIDATE_SNS_SIGNATURE': True,
+             'VALIDATE_BODY_SIGNATURE': False,
+             'BODY_DECRYPT_KEYS': {
+                '<key>': '<secret>',
+             },
+         },
+         ...
+     }
---
+++ packages/aws-message-client/aws_message_client-1.6.1.tar.gz/aws_message_client-1.6.1/setup.py
@@ -0,0 +1,45 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from setuptools import setup
+
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/aws-message-client>`_.
+"""
+
+version_path = 'aws_message/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='aws-message-client',
+    version=VERSION,
+    packages=['aws_message'],
+    include_package_data=True,
+    install_requires = [
+        'boto3',
+        'cryptography',
+        'urllib3',
+        'commonconf',
+        'uw-memcached-clients',
+    ],
+    license='Apache License, Version 2.0',
+    description=('An application on which to build AWS SQS endpoints and '
+                 'SQS gatherers'),
+    long_description=README,
+    url='https://github.com/uw-it-aca/aws-message-client',
+    author = "UW-IT SETS",
+    author_email = "aca-it@uw.edu",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)

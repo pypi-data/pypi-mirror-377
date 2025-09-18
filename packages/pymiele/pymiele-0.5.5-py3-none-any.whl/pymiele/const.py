c986344f7f4e17db19d7c26df5881f4125ccaed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:06:02 +0000

    Add package pymiele/0.5.5/pymiele-0.5.5-py3-none-any.whl

---
+++ packages/pymiele/pymiele-0.5.5-py3-none-any.whl/pymiele/const.py
@@ -0,0 +1,9 @@
+"""Constants for pymiele."""
+
+VERSION = "0.5.5"
+
+MIELE_API = "https://api.mcs3.miele.com/v1"
+OAUTH2_AUTHORIZE = "https://api.mcs3.miele.com/thirdparty/login"
+OAUTH2_TOKEN = "https://api.mcs3.miele.com/thirdparty/token"
+
+AIO_TIMEOUT = 15

4b2601cea9f0f5daefb0cd2634b44c8bd38baef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:08:11 +0000

    Add package lacrei-models/0.1.6/lacrei_models-0.1.6.tar.gz

---
+++ packages/lacrei-models/lacrei_models-0.1.6.tar.gz/lacrei_models-0.1.6/src/lacrei_models/address/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class LacreiModelsAddressConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "lacrei_models.address"
+    label = "lacrei_models_address"

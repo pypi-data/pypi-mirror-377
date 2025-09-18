4f1d6fbf83f88c7d01630f1bdd7e0033796aa874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:16:51 +0000

    Add package xfem/2.1.2506.dev16/xfem-2.1.2506.dev16-cp311-cp311-macosx_10_15_universal2.whl

---
+++ packages/xfem/xfem-2.1.2506.dev16-cp311-cp311-macosx_10_15_universal2.whl/xfem/_version.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from builtins import object as VERSION_TUPLE
+from builtins import object as COMMIT_ID
+__all__: list = ['__version__', '__version_tuple__', 'version', 'version_tuple', '__commit_id__', 'commit_id']
+__commit_id__: str = 'g1cd7651c2'
+__version__: str = '2.1.2506.dev16'
+__version_tuple__: tuple = (2, 1, 2506, 'dev16')
+commit_id: str = 'g1cd7651c2'
+version: str = '2.1.2506.dev16'
+version_tuple: tuple = (2, 1, 2506, 'dev16')

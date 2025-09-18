66d217416cc891a9d2976c70bcda7a2741668e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:08:14 +0000

    Add package django-nativemojo/0.1.26/django_nativemojo-0.1.26.tar.gz

---
+++ packages/django-nativemojo/django_nativemojo-0.1.26.tar.gz/django_nativemojo-0.1.26/mojo/apps/docit/rest/page.py
@@ -0,0 +1,22 @@
+import mojo.decorators as md
+from ..models import Page
+
+
+@md.URL('page')
+@md.URL('page/<int:pk>')
+def on_page(request, pk=None):
+    """
+    Standard CRUD endpoints for Page model
+
+    GET /api/docit/page - List pages
+    POST /api/docit/page - Create new page
+    GET /api/docit/page/<id> - Get single page
+    PUT /api/docit/page/<id> - Update page
+    DELETE /api/docit/page/<id> - Delete page
+    """
+    return Page.on_rest_request(request, pk)
+
+
+@md.URL('page/slug/<str:slug>')
+def on_page_by_slug(request, slug=None):
+    return Page.objects.get(slug=slug).on_rest_get(request)

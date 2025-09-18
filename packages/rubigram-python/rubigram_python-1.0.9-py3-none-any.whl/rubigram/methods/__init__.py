7cc6c98c25ce25d31838f95841be786559efcef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:07:10 +0000

    Add package rubigram-python/1.0.9/rubigram_python-1.0.9-py3-none-any.whl

---
+++ packages/rubigram-python/rubigram_python-1.0.9-py3-none-any.whl/rubigram/methods/__init__.py
@@ -0,0 +1,40 @@
+
+from .advanced import Advanced
+from .utilities import Utilities
+from .users import Users
+from .auth import Auth
+from .messages import Messages
+from .chats import Chats
+from .groups import Groups
+from .decorators import Decorators
+from .contacts import Contacts
+from .settings import Settings
+from .stickers import Stickers
+from .channels import Channels
+from .extras import Exctras
+from .gif import Gifs
+from .voice_chat import VoiceCall
+from .links import Links
+from .live import Lives
+
+
+class Methods(
+    Advanced,
+    Utilities,
+    Users,
+    Auth,
+    Messages,
+    Chats,
+    Groups,
+    Decorators,
+    Contacts,
+    Settings,
+    Stickers,
+    Channels,
+    Exctras,
+    Gifs,
+    VoiceCall,
+    Links,
+    Lives
+):
+    pass

4a87420c54e5887de23bacf62767a479628f1a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:07:12 +0000

    Add package zc.lockfile/4.0/zc_lockfile-4.0.tar.gz

---
+++ packages/zc.lockfile/zc_lockfile-4.0.tar.gz/zc_lockfile-4.0/setup.py
@@ -0,0 +1,74 @@
+##############################################################################
+#
+# Copyright (c) Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os
+
+from setuptools import setup
+
+
+version = '4.0'
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('src', 'zc', 'lockfile', 'README.txt')
+    + '\n' +
+    read('CHANGES.rst')
+)
+
+setup(
+    name='zc.lockfile',
+    version=version,
+    author="Zope Foundation",
+    author_email="zope-dev@zope.dev",
+    description="Basic inter-process locks",
+    long_description=long_description,
+    license="ZPL-2.1",
+    keywords="lock",
+    url='https://github.com/zopefoundation/zc.lockfile',
+    python_requires='>=3.9',
+    install_requires='setuptools',
+    extras_require=dict(
+        test=[
+            'zope.testing',
+        ]),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)

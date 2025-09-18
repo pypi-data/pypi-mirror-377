af2d2cb6278fb09840725b6a6538a74b95583bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 07:06:15 +0000

    Add package juliacall/0.9.28/juliacall-0.9.28.tar.gz

---
+++ packages/juliacall/juliacall-0.9.28.tar.gz/juliacall-0.9.28/src/C/context.jl
@@ -0,0 +1,262 @@
+"""
+    mutable struct Context
+
+A handle to a loaded instance of libpython, its interpreter, function pointers, etc.
+"""
+@kwdef mutable struct Context
+    is_embedded::Bool = false
+    is_initialized::Bool = false
+    is_preinitialized::Bool = false
+    lib_ptr::Ptr{Cvoid} = C_NULL
+    exe_path::Union{String,Missing} = missing
+    lib_path::Union{String,Missing} = missing
+    dlopen_flags::UInt32 = RTLD_LAZY | RTLD_DEEPBIND | RTLD_GLOBAL
+    pyprogname::Union{String,Missing} = missing
+    pyprogname_w::Any = missing
+    pyhome::Union{String,Missing} = missing
+    pyhome_w::Any = missing
+    which::Symbol = :unknown # :CondaPkg, :PyCall, :embedded or :unknown
+    version::Union{VersionNumber,Missing} = missing
+end
+
+const CTX = Context()
+
+function _atpyexit()
+    if CTX.is_initialized && !CTX.is_preinitialized
+        @warn "Python exited unexpectedly"
+    end
+    CTX.is_initialized = false
+    return
+end
+
+function init_context()
+
+    CTX.is_embedded = hasproperty(Base.Main, :__PythonCall_libptr)
+
+    if CTX.is_embedded
+        # In this case, getting a handle to libpython is easy
+        CTX.lib_ptr = Base.Main.__PythonCall_libptr::Ptr{Cvoid}
+        init_pointers()
+        # Check Python is initialized
+        Py_IsInitialized() == 0 && error("Python is not already initialized.")
+        CTX.is_initialized = true
+        CTX.which = :embedded
+        exe_path = get(ENV, "JULIA_PYTHONCALL_EXE", "")
+        if exe_path != ""
+            CTX.exe_path = exe_path
+            # this ensures PyCall uses the same Python interpreter
+            get!(ENV, "PYTHON", exe_path)
+        end
+    else
+        # Find Python executable
+        exe_path = get(ENV, "JULIA_PYTHONCALL_EXE", "")
+        if exe_path == "" || exe_path == "@CondaPkg"
+            if CondaPkg.backend() == :Null
+                exe_path = Sys.which("python")
+                if exe_path === nothing
+                    error("CondaPkg is using the Null backend but Python is not installed")
+                end
+                exe_path::String
+            else
+                # By default, we use Python installed by CondaPkg.
+                exe_path =
+                    Sys.iswindows() ? joinpath(CondaPkg.envdir(), "python.exe") :
+                    joinpath(CondaPkg.envdir(), "bin", "python")
+                # It's not sufficient to only activate the env while Python is initialising,
+                # it must also be active when loading extension modules (e.g. numpy). So we
+                # activate the environment globally.
+                # TODO: is this really necessary?
+                CondaPkg.activate!(ENV)
+            end
+            CTX.which = :CondaPkg
+        elseif exe_path == "@PyCall"
+            # PyCall compatibility mode
+            PyCall = Base.require(PYCALL_PKGID)::Module
+            exe_path = PyCall.python::String
+            CTX.lib_path = PyCall.libpython::String
+            CTX.which = :PyCall
+        elseif exe_path == "@venv"
+            # load from a .venv in the active project
+            exe_path = abspath(dirname(Base.active_project()), ".venv")
+            if Sys.iswindows()
+                exe_path = abspath(exe_path, "Scripts", "python.exe")::String
+            else
+                exe_path = abspath(exe_path, "bin", "python")::String
+            end
+        elseif startswith(exe_path, "@")
+            error("invalid JULIA_PYTHONCALL_EXE=$exe_path")
+        else
+            # Otherwise we use the Python specified
+            CTX.which = :unknown
+            if isabspath(exe_path)
+                # nothing to do
+            elseif '/' in exe_path || '\\' in exe_path
+                # it's a relative path, interpret it as relative to the current project
+                exe_path = abspath(dirname(Base.active_project()), exe_path)::String
+            else
+                # it's a command, find it in the PATH
+                given_exe_path = exe_path
+                exe_path = Sys.which(exe_path)
+                exe_path === nothing &&
+                    error("Python executable $(repr(given_exe_path)) not found.")
+                exe_path::String
+            end
+        end
+
+        # Ensure Python is runnable
+        if !ispath(exe_path)
+            error("Python executable $(repr(exe_path)) does not exist.")
+        end
+        try
+            run(pipeline(`$exe_path --version`, stdout = devnull, stderr = devnull))
+        catch
+            error("Python executable $(repr(exe_path)) is not executable.")
+        end
+        CTX.exe_path = exe_path
+
+        # For calling Python with UTF-8 IO
+        function python_cmd(args)
+            env = copy(ENV)
+            env["PYTHONIOENCODING"] = "UTF-8"
+            delete!(env, "PYTHONHOME")
+            setenv(`$(CTX.exe_path) $args`, env)
+        end
+
+        # Find and open Python library
+        lib_path = something(
+            CTX.lib_path === missing ? nothing : CTX.lib_path,
+            get(ENV, "JULIA_PYTHONCALL_LIB", nothing),
+            Some(nothing),
+        )
+        if lib_path !== nothing
+            lib_ptr = dlopen_e(lib_path, CTX.dlopen_flags)
+            if lib_ptr == C_NULL
+                error("Python library $(repr(lib_path)) could not be opened.")
+            else
+                CTX.lib_path = lib_path
+                CTX.lib_ptr = lib_ptr
+            end
+        else
+            for lib_path in readlines(
+                python_cmd([joinpath(@__DIR__, "find_libpython.py"), "--list-all"]),
+            )
+                lib_ptr = dlopen_e(lib_path, CTX.dlopen_flags)
+                if lib_ptr == C_NULL
+                    @warn "Python library $(repr(lib_path)) could not be opened."
+                else
+                    CTX.lib_path = lib_path
+                    CTX.lib_ptr = lib_ptr
+                    break
+                end
+            end
+            CTX.lib_path === missing && error("""
+                Could not find Python library for Python executable $(repr(CTX.exe_path)).
+
+                If you know where the library is, set environment variable 'JULIA_PYTHONCALL_LIB' to its path.
+                """)
+        end
+
+        # Close the library when Julia exits
+        atexit() do
+            dlclose(CTX.lib_ptr)
+        end
+
+        # Get function pointers from the library
+        init_pointers()
+
+        # Initialize the interpreter
+        CTX.is_preinitialized = Py_IsInitialized() != 0
+        if !CTX.is_preinitialized
+            # Find ProgramName and PythonHome
+            script = if Sys.iswindows()
+                """
+                import sys
+                print(sys.executable)
+                if hasattr(sys, "base_exec_prefix"):
+                    sys.stdout.write(sys.base_exec_prefix)
+                else:
+                    sys.stdout.write(sys.exec_prefix)
+                """
+            else
+                """
+                import sys
+                print(sys.executable)
+                if hasattr(sys, "base_exec_prefix"):
+                    sys.stdout.write(sys.base_prefix)
+                    sys.stdout.write(":")
+                    sys.stdout.write(sys.base_exec_prefix)
+                else:
+                    sys.stdout.write(sys.prefix)
+                    sys.stdout.write(":")
+                    sys.stdout.write(sys.exec_prefix)
+                """
+            end
+            CTX.pyprogname, CTX.pyhome = readlines(python_cmd(["-c", script]))
+
+            # Set PythonHome
+            CTX.pyhome_w = Base.cconvert(Cwstring, CTX.pyhome)
+            Py_SetPythonHome(pointer(CTX.pyhome_w))
+
+            # Set ProgramName
+            CTX.pyprogname_w = Base.cconvert(Cwstring, CTX.pyprogname)
+            Py_SetProgramName(pointer(CTX.pyprogname_w))
+
+            # Start the interpreter and register exit hooks
+            Py_InitializeEx(0)
+            atexit() do
+                CTX.is_initialized = false
+                if Py_FinalizeEx() == -1
+                    @warn "Py_FinalizeEx() error"
+                end
+            end
+        end
+        CTX.is_initialized = true
+        if Py_AtExit(@cfunction(_atpyexit, Cvoid, ())) == -1
+            @warn "Py_AtExit() error"
+        end
+    end
+
+    # HACK: If we are using CondaPkg, prevent child processes from using it by explicitly
+    # setting the executable. Our tests sometimes fail on Windows because Aqua launches a
+    # child process which causes CondaPkg to re-resolve (should work out why, I assume it's
+    # a filesystem timestamp thing) which causes some stdlibs to disappear for a bit.
+    #
+    # A better solution may be to use some environment variable to "freeze" CondaPkg in
+    # child processes.
+    # 
+    # Only done when CI=true since it's a hack.
+    if (get(ENV, "CI", "false") == "true") && (CTX.which === :CondaPkg)
+        ENV["JULIA_PYTHONCALL_EXE"] = CTX.exe_path::String
+    end
+
+    # Get the python version
+    verstr = Base.unsafe_string(Py_GetVersion())
+    vermatch = match(r"^[0-9.]+", verstr)
+    if vermatch === nothing
+        error("Cannot parse version from version string: $(repr(verstr))")
+    end
+    CTX.version = VersionNumber(vermatch.match)
+    v"3.9" ≤ CTX.version < v"4" || error(
+        "Only Python 3.9+ is supported, this is Python $(CTX.version) at $(CTX.exe_path===missing ? "unknown location" : CTX.exe_path).",
+    )
+
+    @debug "Initialized PythonCall.jl" CTX.is_embedded CTX.is_initialized CTX.exe_path CTX.lib_path CTX.lib_ptr CTX.pyprogname CTX.pyhome CTX.version
+
+    return
+end
+
+function Base.show(io::IO, ::MIME"text/plain", ctx::Context)
+    show(io, typeof(io))
+    print(io, ":")
+    for k in fieldnames(Context)
+        println(io)
+        print(io, "  ", k, " = ")
+        show(io, getfield(ctx, k))
+    end
+end
+
+const PYTHONCALL_UUID = Base.UUID("6099a3de-0909-46bc-b1f4-468b9a2dfc0d")
+const PYTHONCALL_PKGID = Base.PkgId(PYTHONCALL_UUID, "PythonCall")
+
+const PYCALL_UUID = Base.UUID("438e738f-606a-5dbb-bf0a-cddfbfd45ab0")
+const PYCALL_PKGID = Base.PkgId(PYCALL_UUID, "PyCall")
---
+++ packages/juliacall/juliacall-0.9.28.tar.gz/juliacall-0.9.28/src/Wrap/PyIO.jl
@@ -0,0 +1,216 @@
+
+pyio_finalize!(io::PyIO) = begin
+    C.CTX.is_initialized || return
+    io.own ? close(io) : flush(io)
+    return
+end
+
+ispy(io::PyIO) = true
+Py(io::PyIO) = io.py
+
+pyconvert_rule_io(::Type{PyIO}, x::Py) = pyconvert_return(PyIO(x))
+
+"""
+    PyIO(f, x; ...)
+
+Equivalent to `f(PyIO(x; ...))` except the stream is automatically flushed or closed according to `own`.
+
+For use in a `do` block, as in
+```
+PyIO(x, ...) do io
+    ...
+end
+```
+"""
+function PyIO(f::Function, o; opts...)
+    io = PyIO(o; opts...)
+    try
+        return f(io)
+    finally
+        pydel!(io.py)
+    end
+end
+
+# If obuf is non-empty, write it to the underlying stream.
+function putobuf(io::PyIO)
+    if !isempty(io.obuf)
+        data = io.text ? pystr_fromUTF8(io.obuf) : pybytes(io.obuf)
+        pydel!(@py io.write(data))
+        pydel!(data)
+        empty!(io.obuf)
+    end
+    return
+end
+
+# If ibuf is empty, read some more from the underlying stream.
+# After this call, if ibuf is empty then we are at EOF.
+# TODO: in binary mode, `io.readinto()` to avoid copying data
+function getibuf(io::PyIO)
+    if isempty(io.ibuf)
+        data = @py io.read(@jl io.ibuflen)
+        if io.text
+            append!(io.ibuf, pystr_asUTF8vector(data))
+        else
+            append!(io.ibuf, pybytes_asvector(data))
+        end
+        pydel!(data)
+    end
+    return
+end
+
+function Base.flush(io::PyIO)
+    putobuf(io)
+    pydel!(@py io.flush())
+    return
+end
+
+function Base.close(io::PyIO)
+    flush(io)
+    pydel!(@py io.close())
+    return
+end
+
+function Base.eof(io::PyIO)
+    if io.eof
+        true
+    elseif !isempty(io.ibuf)
+        false
+    else
+        getibuf(io)
+        io.eof = isempty(io.ibuf)
+    end
+end
+
+Base.fd(io::PyIO) = pyconvert(Int, @py io.fileno())
+
+Base.isreadable(io::PyIO) = pyconvert(Bool, @py io.readable())
+
+Base.iswritable(io::PyIO) = pyconvert(Bool, @py io.writable())
+
+Base.isopen(io::PyIO) = !pyconvert(Bool, @py io.closed)
+
+function Base.unsafe_write(io::PyIO, ptr::Ptr{UInt8}, n::UInt)
+    ntodo = n
+    while ntodo > 0
+        nroom = max(0, io.obuflen - length(io.obuf))
+        if ntodo < nroom
+            buf = unsafe_wrap(Array, ptr, ntodo)
+            if io.line_buffering
+                i = findlast(∈((0x0A, 0x0D)), buf)
+                if i === nothing
+                    append!(io.obuf, buf)
+                else
+                    append!(io.obuf, unsafe_wrap(Array, ptr, i))
+                    putobuf(io)
+                    append!(io.obuf, unsafe_wrap(Array, ptr + i, ntodo - i))
+                end
+            else
+                append!(io.obuf, buf)
+            end
+            break
+        else
+            buf = unsafe_wrap(Array, ptr, nroom)
+            append!(io.obuf, buf)
+            putobuf(io)
+            ptr += nroom
+            ntodo -= nroom
+        end
+    end
+    return n
+end
+
+function Base.write(io::PyIO, c::UInt8)
+    push!(io.obuf, c)
+    if (length(io.obuf) ≥ io.obuflen) || (io.line_buffering && (c == 0x0A || c == 0x0D))
+        putobuf(io)
+    end
+    return
+end
+
+function Base.unsafe_read(io::PyIO, ptr::Ptr{UInt8}, n::UInt)
+    ntodo = n
+    while true
+        navail = length(io.ibuf)
+        if ntodo ≤ navail
+            unsafe_wrap(Array, ptr, ntodo) .= splice!(io.ibuf, 1:ntodo)
+            return
+        else
+            unsafe_wrap(Array, ptr, navail) .= io.ibuf
+            ptr += navail
+            ntodo -= navail
+            empty!(io.ibuf)
+            eof(io) && throw(EOFError())
+        end
+    end
+    return
+end
+
+function Base.read(io::PyIO, ::Type{UInt8})
+    eof(io) && throw(EOFError())
+    popfirst!(io.ibuf)
+end
+
+function Base.seek(io::PyIO, pos::Integer)
+    putobuf(io)
+    empty!(io.ibuf)
+    io.eof = false
+    pydel!(@py io.seek(pos))
+    return io
+end
+
+function Base.truncate(io::PyIO, pos::Integer)
+    seek(io, position(io))
+    pydel!(@py io.truncate(pos))
+    return io
+end
+
+function Base.seekstart(io::PyIO)
+    putobuf(io)
+    empty!(io.ibuf)
+    io.eof = false
+    pydel!(@py io.seek(0))
+    return io
+end
+
+function Base.seekend(io::PyIO)
+    putobuf(io)
+    empty!(io.ibuf)
+    io.eof = false
+    pydel!(@py io.seek(0, 2))
+    return io
+end
+
+function Base.skip(io::PyIO, n::Integer)
+    putobuf(io)
+    if io.text
+        if n ≥ 0
+            read(io, n)
+        else
+            error("`skip(io, n)` for text PyIO streams only implemented for positive `n`")
+        end
+    else
+        if 0 ≤ n ≤ io.ibuflen
+            read(io, n)
+        else
+            pydel!(@py io.seek(@jl(n - length(io.ibuf)), 1))
+            empty!(io.ibuf)
+            io.eof = false
+        end
+    end
+    return io
+end
+
+function Base.position(io::PyIO)
+    putobuf(io)
+    if io.text
+        if isempty(io.ibuf)
+            return pyconvert(Int, @py io.tell())
+        else
+            error(
+                "`position(io)` text PyIO streams only implemented for empty input buffer (e.g. do `read(io, length(io.ibuf))` first)",
+            )
+        end
+    else
+        return pyconvert(Int, @py io.tell()) - length(io.ibuf)
+    end
+end

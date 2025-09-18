bfaa0d69e6c18aaebc0ac35d13955e54a7e0eef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:07:46 +0000

    Add package tombi/0.6.9/tombi-0.6.9.tar.gz

---
+++ packages/tombi/tombi-0.6.9.tar.gz/tombi-0.6.9/crates/tombi-lsp/src/semantic_tokens/array.rs
@@ -0,0 +1,32 @@
+use tombi_ast::AstNode;
+
+use super::{AppendSemanticTokens, SemanticTokensBuilder};
+
+impl AppendSemanticTokens for tombi_ast::Array {
+    fn append_semantic_tokens(&self, builder: &mut SemanticTokensBuilder) {
+        for comments in self.inner_begin_dangling_comments() {
+            for comment in comments {
+                comment.append_semantic_tokens(builder);
+            }
+        }
+
+        for (value, comma) in self.values_with_comma() {
+            value.append_semantic_tokens(builder);
+            if let Some(comma) = comma {
+                for comment in comma.leading_comments() {
+                    comment.append_semantic_tokens(builder);
+                }
+
+                if let Some(comment) = comma.trailing_comment() {
+                    comment.append_semantic_tokens(builder);
+                }
+            }
+        }
+
+        for comments in self.inner_end_dangling_comments() {
+            for comment in comments {
+                comment.append_semantic_tokens(builder);
+            }
+        }
+    }
+}

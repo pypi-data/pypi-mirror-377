62375ae5466f60cf96a271b8511d8aec5fea37f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:06:19 +0000

    Add package enthusiast-common/1.3.0/enthusiast_common-1.3.0-py3-none-any.whl

---
+++ packages/enthusiast-common/enthusiast_common-1.3.0-py3-none-any.whl/enthusiast_common/__init__.py
@@ -0,0 +1,12 @@
+from .interfaces import DocumentSourcePlugin, EmbeddingProvider, LanguageModelProvider, ProductSourcePlugin
+from .structures import DocumentDetails, ProductDetails, RepositoriesInstances
+
+__all__ = [
+    "DocumentDetails",
+    "DocumentSourcePlugin",
+    "EmbeddingProvider",
+    "LanguageModelProvider",
+    "ProductSourcePlugin",
+    "RepositoriesInstances",
+    "ProductDetails",
+]

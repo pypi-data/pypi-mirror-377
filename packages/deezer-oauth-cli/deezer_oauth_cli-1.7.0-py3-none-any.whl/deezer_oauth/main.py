9a0d7e4bdd004bfb500ca4670d722c639ec2a184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:05:59 +0000

    Add package deezer-oauth-cli/1.7.0/deezer_oauth_cli-1.7.0-py3-none-any.whl

---
+++ packages/deezer-oauth-cli/deezer_oauth_cli-1.7.0-py3-none-any.whl/deezer_oauth/main.py
@@ -0,0 +1,36 @@
+"""Simple script to obtain an API token via OAuth."""
+
+from __future__ import annotations
+
+import webbrowser
+
+import typer
+from rich import print
+
+from deezer_oauth.client import OAuthDancer
+from deezer_oauth.server import run_server
+
+app = typer.Typer()
+
+
+@app.command()
+def main(app_id: str, app_secret: str) -> None:
+    """
+    Get an API token from Deezer.
+
+    This script spins up a basic HTTP server, performs the OAuth flow to generate
+    an API token and write it to an .env file in the local directory.
+
+    You should log in/signup at https://developers.deezer.com/ and create an app
+    with a redirect URL of 'http://localhost:8080/oauth/return'.
+
+    This will give you an application ID a secret key to pass as arguments.
+    """
+    # Begin OAuth flow
+    oauth_dancer = OAuthDancer(app_id=app_id, app_secret=app_secret)
+    start_url = oauth_dancer.get_auth_page()
+    print(f"Opening {start_url} in web browser...")
+    webbrowser.open(start_url)
+
+    # Run server until flow complete
+    run_server(oauth_dancer)

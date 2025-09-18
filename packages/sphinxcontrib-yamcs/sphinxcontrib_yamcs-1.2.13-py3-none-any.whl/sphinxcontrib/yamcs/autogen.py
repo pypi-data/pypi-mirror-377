a5e22a61e47d8fd8ca553c7b105b0a558d27bbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:05:42 +0000

    Add package sphinxcontrib-yamcs/1.2.13/sphinxcontrib_yamcs-1.2.13-py3-none-any.whl

---
+++ packages/sphinxcontrib-yamcs/sphinxcontrib_yamcs-1.2.13-py3-none-any.whl/sphinxcontrib/yamcs/autogen.py
@@ -0,0 +1,188 @@
+import os
+import re
+from pathlib import Path
+
+from sphinx.util.osutil import FileAvoidWrite
+from sphinx.util.template import ReSTRenderer
+from yamcs.api import annotations_pb2
+
+from sphinxcontrib.yamcs import templates
+
+
+def camel_to_slug(name, sep="-", lower=True):
+    name = re.sub("(.)([A-Z][a-z]+)", r"\1" + sep + r"\2", name)
+    name = re.sub("([a-z0-9])([A-Z])", r"\1" + sep + r"\2", name)
+    return name.lower() if lower else name
+
+
+def titlecase(text):
+    return camel_to_slug(text, sep=" ", lower=False)
+
+
+def replace(a, b, c):
+    return a.replace(b, c)
+
+
+class YamcsReSTRenderer(ReSTRenderer):
+    def __init__(self):
+        super().__init__()
+        self.env.filters["slug"] = camel_to_slug
+        self.env.filters["replace"] = replace
+        self.env.filters["titlecase"] = titlecase
+
+
+def create_service_file(symbol, service, filename):
+    methods = []
+    for method in service.method:
+        route_options = method.options.Extensions[annotations_pb2.route]
+        if route_options.deprecated:
+            continue
+
+        ws_options = method.options.Extensions[annotations_pb2.websocket]
+        if ws_options.deprecated:
+            continue
+
+        methods.append(method)
+
+    service_name = titlecase(service.name.replace("Api", ""))
+    label = service.options.Extensions[annotations_pb2.label]
+    if label:
+        service_name = label
+
+    context = {
+        "symbol": symbol,
+        "service": service,
+        "service_name": service_name,
+        "methods": methods,
+    }
+
+    text = YamcsReSTRenderer().render_string(templates.service, context)
+    with FileAvoidWrite(filename) as f:
+        f.write(text)
+        f.write("\n")
+
+
+def create_route_file(symbol, method, filename, has_related):
+    method_name = titlecase(method.name)
+    label = method.options.Extensions[annotations_pb2.route].label
+    if label:
+        method_name = label
+
+    context = {
+        "symbol": symbol,
+        "method": method,
+        "method_name": method_name,
+        "has_related": has_related,
+        "route_options": method.options.Extensions[annotations_pb2.route],
+    }
+    text = YamcsReSTRenderer().render_string(templates.route, context)
+    with FileAvoidWrite(filename) as f:
+        f.write(text)
+        f.write("\n")
+
+
+def create_websocket_file(symbol, method, filename, has_related):
+    method_name = titlecase(method.name)
+    label = method.options.Extensions[annotations_pb2.websocket].label
+    if label:
+        method_name = label
+
+    context = {
+        "symbol": symbol,
+        "method": method,
+        "method_name": method_name,
+        "has_related": has_related,
+        "websocket_options": method.options.Extensions[annotations_pb2.websocket],
+    }
+    text = YamcsReSTRenderer().render_string(templates.websocket, context)
+    with FileAvoidWrite(filename) as f:
+        f.write(text)
+        f.write("\n")
+
+
+def generate(parser, destdir, title, additional_docs):
+    service_count = 0
+    for file in parser.proto.file:
+        service_count += len(file.service)
+
+    service_links = []
+    method_links = []
+    generated_files = []
+    for file in parser.proto.file:
+        for service in file.service:
+            if service_count > 1:
+                servicedirname = camel_to_slug(service.name).replace("-api", "")
+                servicedir = Path(destdir, servicedirname)
+                servicedir.mkdir(exist_ok=True)
+                generated_files.append(servicedirname)
+
+                servicefile = os.path.join(servicedir, "index.rst")
+                symbol = "." + file.package + "." + service.name
+                create_service_file(symbol, service, servicefile)
+                service_links.append(servicedir.name + "/index")
+
+                for method in service.method:
+                    route_options = method.options.Extensions[annotations_pb2.route]
+                    if route_options.deprecated:
+                        continue
+
+                    ws_options = method.options.Extensions[annotations_pb2.websocket]
+                    if ws_options.deprecated:
+                        continue
+
+                    filename = camel_to_slug(method.name) + ".rst"
+                    methodfile = os.path.join(servicedir, filename)
+                    symbol = "." + file.package + "." + service.name + "." + method.name
+                    related_types = parser.find_types_related_to_method(symbol)
+                    related_enums = parser.find_enums_related_to_method(symbol)
+                    has_related = len(related_types) > 0 or len(related_enums) > 0
+
+                    if method.options.HasExtension(annotations_pb2.route):
+                        create_route_file(symbol, method, methodfile, has_related)
+                    elif method.options.HasExtension(annotations_pb2.websocket):
+                        create_websocket_file(symbol, method, methodfile, has_related)
+            else:
+                for method in service.method:
+                    route_options = method.options.Extensions[annotations_pb2.route]
+                    if route_options.deprecated:
+                        continue
+
+                    ws_options = method.options.Extensions[annotations_pb2.websocket]
+                    if ws_options.deprecated:
+                        continue
+
+                    filename = camel_to_slug(method.name) + ".rst"
+                    methodfile = Path(destdir, filename)
+                    symbol = "." + file.package + "." + service.name + "." + method.name
+                    related_types = parser.find_types_related_to_method(symbol)
+                    related_enums = parser.find_enums_related_to_method(symbol)
+                    has_related = len(related_types) > 0 or len(related_enums) > 0
+                    method_links.append(camel_to_slug(method.name))
+
+                    if method.options.HasExtension(annotations_pb2.route):
+                        create_route_file(symbol, method, methodfile, has_related)
+                        generated_files.append(filename)
+                    elif method.options.HasExtension(annotations_pb2.websocket):
+                        create_websocket_file(symbol, method, methodfile, has_related)
+                        generated_files.append(filename)
+
+    service_links.sort()
+    text = YamcsReSTRenderer().render_string(
+        templates.index,
+        {
+            "title": title,
+            "additional_docs": additional_docs,
+            "service_links": service_links,
+            "method_links": method_links,
+        },
+    )
+    indexfile = os.path.join(destdir, "index.rst")
+    with FileAvoidWrite(indexfile) as f:
+        f.write(text)
+        f.write("\n")
+    generated_files.append("index.rst")
+
+    with Path(destdir, ".autogen").open("w") as f:
+        for file in generated_files:
+            f.write(file)
+            f.write("\n")
---
+++ packages/sphinxcontrib-yamcs/sphinxcontrib_yamcs-1.2.13-py3-none-any.whl/sphinxcontrib/yamcs/options.py
@@ -0,0 +1,154 @@
+import yaml
+from docutils import nodes
+from docutils.statemachine import ViewList
+from sphinx.util.docutils import SphinxDirective
+from sphinx.util.nodes import nested_parse_with_titles
+
+
+def produce_nodes(state, rst_text):
+    # Deindent small indents to not trigger unwanted rst
+    # blockquotes. This uses a simple algorithm that only
+    # keeps indents in multiples of 4.
+    deindented = []
+    for line in rst_text.splitlines():
+        indent_size = len(line) - len(line.lstrip())
+        allowed_indent = int(indent_size / 4) * "    "
+        deindented.append(allowed_indent + line.lstrip())
+
+    unprocessed = ViewList()
+    for line in deindented:
+        unprocessed.append(line, "fakefile.rst", 1)
+
+    temp_node = nodes.section()
+    temp_node.document = state.document
+    nested_parse_with_titles(state, unprocessed, temp_node)
+    return [node for node in temp_node.children]
+
+
+class OptionsDirective(SphinxDirective):
+    required_arguments = 1
+    option_spec = {"scope": str}
+
+    def run(self):
+        conf_key = "options"
+        if "scope" in self.options:
+            if self.options["scope"] == "global":
+                conf_key = "options"
+            if self.options["scope"] == "instance":
+                conf_key = "instanceOptions"
+            else:
+                raise Exception(f"Unexpected scope {conf_key}")
+
+        result = []
+        yaml_file = self.arguments[0]
+        with open(yaml_file) as f:
+            descriptor = yaml.load(f, Loader=yaml.FullLoader)
+            options = descriptor[conf_key].items()
+            head = []
+            tail = []
+            self.generate_nodes(options, head=head, tail=tail)
+            result += head + tail
+        return result
+
+    def generate_nodes(self, options, head, tail):
+        head_items = []
+        for option_name, option in options:
+            type_string = option["type"]
+            if option["type"] == "LIST":
+                type_string += " of " + option["elementType"] + "s"
+            elif option["type"] == "LIST_OR_ELEMENT":
+                type_string = (
+                    option["elementType"] + " or list of " + option["elementType"] + "s"
+                )
+
+            deprecated = "deprecationMessage" in option
+
+            term_nodes = [
+                nodes.literal("", option_name),
+                nodes.Text(" ("),
+                nodes.Text(type_string.lower()),
+                nodes.Text(")"),
+            ]
+
+            definition_nodes = []
+
+            if option.get("hidden", False):
+                continue
+
+            if deprecated:
+                para_nodes = [nodes.Text("Deprecated.")]
+                para_nodes += produce_nodes(self.state, option["deprecationMessage"])
+                # node = nodes.Text("Deprecated: " + option["deprecationMessage"])
+                definition_nodes.append(
+                    nodes.warning("", nodes.paragraph("", "", *para_nodes))
+                )
+
+            if "description" in option:
+                for idx, para in enumerate(option["description"]):
+                    para_nodes = []
+                    if idx == 0 and option.get("required", False):
+                        para = "**Required.** " + para
+                        para_nodes += produce_nodes(self.state, para)
+                    else:
+                        para_nodes += produce_nodes(self.state, para)
+
+                    definition_nodes.append(nodes.paragraph("", "", *para_nodes))
+
+            if "choices" in option:
+                choices = option["choices"]
+                para_nodes = [nodes.Text("One of ")]
+                for idx, choice in enumerate(choices):
+                    if idx == 0:
+                        para_nodes += [nodes.literal("", str(choice))]
+                    elif idx < len(choices) - 1:
+                        para_nodes += [nodes.Text(", "), nodes.literal("", str(choice))]
+                    else:
+                        para_nodes += [
+                            nodes.Text(" or "),
+                            nodes.literal("", str(choice)),
+                        ]
+                definition_nodes.append(nodes.paragraph("", "", *para_nodes))
+
+            if "default" in option:
+                default_value = option["default"]
+                if option["type"] == "BOOLEAN":  # True, False ==> true, false
+                    default_value = str(default_value).lower()
+                default_nodes = [
+                    nodes.Text("Default: "),
+                    nodes.literal("", default_value),
+                ]
+                definition_nodes.append(nodes.paragraph("", "", *default_nodes))
+
+            if option["type"] == "MAP":
+                title = option.get("title", option_name) + " sub-configuration"
+                definition_nodes.append(
+                    nodes.paragraph(text='See "' + title + '" section below.')
+                )
+                tail += [nodes.rubric("", title)]
+                new_tail = []
+                self.generate_nodes(
+                    option["suboptions"].items(), head=tail, tail=new_tail
+                )
+                tail += new_tail
+
+            if option["type"] == "LIST" or option["type"] == "LIST_OR_ELEMENT":
+                if option["elementType"] == "MAP":
+                    title = option.get("title", option_name) + " sub-configuration"
+                    definition_nodes.append(
+                        nodes.paragraph(text='See "' + title + '" section below.')
+                    )
+                    tail += [nodes.rubric("", title)]
+                    new_tail = []
+                    self.generate_nodes(
+                        option["suboptions"].items(), head=tail, tail=new_tail
+                    )
+                    tail += new_tail
+
+            head_items.append(
+                nodes.definition_list_item(
+                    "",
+                    nodes.term("", "", *term_nodes),
+                    nodes.definition("", *definition_nodes),
+                )
+            )
+        head += [nodes.definition_list("", *head_items)]

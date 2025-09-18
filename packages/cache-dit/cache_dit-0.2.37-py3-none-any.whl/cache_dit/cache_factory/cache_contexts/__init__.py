e50d9e767b23478cd5f8a45b44d46a8731d6ff7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:17:49 +0000

    Add package cache-dit/0.2.37/cache_dit-0.2.37-py3-none-any.whl

---
+++ packages/cache-dit/cache_dit-0.2.37-py3-none-any.whl/cache_dit/cache_factory/cache_contexts/__init__.py
@@ -0,0 +1,5 @@
+# namespace alias: for _CachedContext and many others' cache context funcs.
+from cache_dit.cache_factory.cache_contexts.cache_context import CachedContext
+from cache_dit.cache_factory.cache_contexts.cache_manager import (
+    CachedContextManager,
+)

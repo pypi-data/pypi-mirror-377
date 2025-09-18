d0df6392eb4d08fa47c70ff32d3109af1e1ab938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 07:06:42 +0000

    Add package minha-biblioteca-xpto/0.1.0/minha_biblioteca_xpto-0.1.0.tar.gz

---
+++ packages/minha-biblioteca-xpto/minha_biblioteca_xpto-0.1.0.tar.gz/minha_biblioteca_xpto-0.1.0/src/minha_biblioteca/__init__.py
@@ -0,0 +1,4 @@
+#from .exemplo import cumprimentar
+
+def cumprimentar(nome):
+    return f"Olá, {nome}! Bem-vindo à UFCD 10794."
---
+++ packages/minha-biblioteca-xpto/minha_biblioteca_xpto-0.1.0.tar.gz/minha_biblioteca_xpto-0.1.0/tests/test_exemplo.py
@@ -0,0 +1,5 @@
+from minha_biblioteca import cumprimentar
+
+def test_cumprimentar():
+    assert cumprimentar("Susana") == "Olá, Susana! Bem-vindo à UFCD 10794."
+

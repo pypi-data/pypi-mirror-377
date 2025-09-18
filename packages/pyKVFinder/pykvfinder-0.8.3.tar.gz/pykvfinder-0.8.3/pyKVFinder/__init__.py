da8d741cabc1bc4b804bde9a33e8822dea6b61e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:08:25 +0000

    Add package pyKVFinder/0.8.3/pykvfinder-0.8.3.tar.gz

---
+++ packages/pyKVFinder/pykvfinder-0.8.3.tar.gz/pykvfinder-0.8.3/pyKVFinder/__init__.py
@@ -0,0 +1,39 @@
+"""Python-C parallel KVFinder.
+
+pyKVFinder detects and characterizes cavities in biomolecular structures.
+
+The characterization includes shape, volume, area, depth, hydropathy and
+interface residues and their frequencies.
+
+In addition to the set of functions that can be imported into Python scripts,
+it contains a command line interface (CLI).
+
+Python package
+--------------
+>>> import pyKVFinder
+
+Command Line Interface
+----------------------
+  Usage: pyKVFinder [-h] [-v] [--version] [-b <str>] [-O <str>]\
+                    [-m <int>] [--nthreads <int>] [-d <str>] [-s <float>]\
+                    [-i <float>] [-o <float>] [-V <float>] [-R <float>]\
+                    [-S <str>] [--ignore_backbone] [-D] [--plot_frequencies]\
+                    [--hydropathy [{EisenbergWeiss, HessaHeijne, KyteDoolittle,\
+                    MoonFleming, RadzickaWolfenden, WimleyWhite, ZhaoLondon, <.toml>}]]\
+                    [-B <.toml>] [-L (<.pdb> | <.xyz>)] [--ligand_cutoff <float>]\
+                    (<.pdb> | <.xyz>)
+
+See also
+--------
+* GitHub repository: https://github.com/LBC-LNBio/pyKVFinder
+
+* Documentation: https://lbc-lnbio.github.io/pyKVFinder
+"""
+
+__name__ = "pyKVFinder"
+__version__ = "0.8.3"
+license = "GNU GPL-3.0 License"
+
+from .utils import *
+from .grid import *
+from .main import *

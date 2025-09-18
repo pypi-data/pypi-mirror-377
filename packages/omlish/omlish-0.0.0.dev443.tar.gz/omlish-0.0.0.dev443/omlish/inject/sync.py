d3fb7ae8aa9c1baa241422611c5bee114c6d4ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:08:37 +0000

    Add package omlish/0.0.0.dev443/omlish-0.0.0.dev443.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev443.tar.gz/omlish-0.0.0.dev443/omlish/inject/sync.py
@@ -0,0 +1,46 @@
+import abc
+import typing as ta
+
+from .. import lang
+from .elements import Elemental
+from .elements import as_elements
+from .inspect import KwargsTarget
+from .keys import Key
+
+
+with lang.auto_proxy_import(globals()):
+    from .impl import sync as _sync
+
+
+T = ta.TypeVar('T')
+
+
+##
+
+
+class Injector(lang.Abstract):
+    @abc.abstractmethod
+    def try_provide(self, key: ta.Any) -> lang.Maybe[ta.Any]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def provide(self, key: ta.Any) -> ta.Any:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def provide_kwargs(self, kt: KwargsTarget) -> ta.Mapping[str, ta.Any]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def inject(self, obj: ta.Any) -> ta.Any:
+        raise NotImplementedError
+
+    def __getitem__(
+            self,
+            target: Key[T] | type[T],
+    ) -> T:
+        return self.provide(target)
+
+
+def create_injector(*args: Elemental) -> Injector:
+    return _sync.create_injector(as_elements(*args))

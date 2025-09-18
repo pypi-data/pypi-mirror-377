98d09d6688d0135f01ae1aec6bc9128756bc385e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:05:46 +0000

    Add package oarepo-ui/6.0.0.dev5/oarepo_ui-6.0.0.dev5-py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-6.0.0.dev5-py3-none-any.whl/oarepo_ui/ui/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025 CESNET
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+"""OARepo UI package."""
+
+from __future__ import annotations

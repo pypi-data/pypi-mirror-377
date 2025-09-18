3dda8f4daa4def337e022c5e54125da234cdb135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:06:45 +0000

    Add package niwrap-helper/0.5.0/niwrap_helper-0.5.0.tar.gz

---
+++ packages/niwrap-helper/niwrap_helper-0.5.0.tar.gz/niwrap_helper-0.5.0/src/niwrap_helper/types.py
@@ -0,0 +1,20 @@
+"""Custom types."""
+
+from typing import Literal, TypeAlias
+
+from bids2table._pathlib import Path, PathT
+from styxdefs import LocalRunner
+from styxdocker import DockerRunner
+from styxsingularity import SingularityRunner
+
+StrPath = str | Path
+StrPathT = str | PathT
+BaseRunner = DockerRunner | LocalRunner | SingularityRunner
+
+DockerType: TypeAlias = Literal[
+    "docker", "Docker", "DOCKER", "podman", "Podman", "PODMAN"
+]
+SingularityType: TypeAlias = Literal[
+    "singularity", "Singularity", "SINGULARITY", "apptainer", "Apptainer", "APPTAINER"
+]
+LocalType: TypeAlias = Literal["local", "Local", "LOCAL"]

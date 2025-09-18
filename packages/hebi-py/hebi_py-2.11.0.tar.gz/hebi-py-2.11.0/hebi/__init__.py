582e83ac8020b27ac4ae7fc56c049ab3b08a2380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 06:05:48 +0000

    Add package hebi-py/2.11.0/hebi_py-2.11.0.tar.gz

---
+++ packages/hebi-py/hebi_py-2.11.0.tar.gz/hebi-py-2.11.0/hebi/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#  HEBI Core python API - Copyright 2022 HEBI Robotics
+#  See https://hebi.us/softwarelicense for license details
+#
+# ------------------------------------------------------------------------------
+
+# NOTE: This line _must_ be the first import!
+from ._internal.ffi import loader as __loader
+from . import arm, robot_model, trajectory, util, config, version
+from ._internal.graphics import Color
+from ._internal.ffi._message_types import GroupCommand, GroupFeedback, GroupInfo
+
+from . import version
+__version__ = str(version.py_version())
+
+
+################################################################################
+# Lookup API
+################################################################################
+
+from ._internal.lookup import Lookup
+
+################################################################################
+# Message Types
+################################################################################
+
+
+__all__ = ['Lookup', 'GroupCommand', 'GroupFeedback', 'GroupInfo', 'Color',
+           'robot_model', 'trajectory', 'util', 'arm', 'config', 'version']

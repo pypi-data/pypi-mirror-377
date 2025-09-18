20dcf32409a1e833d24dbfda91b428d749478a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:07:01 +0000

    Add package metaprinter/0.2.7/metaprinter-0.2.7.tar.gz

---
+++ packages/metaprinter/metaprinter-0.2.7.tar.gz/metaprinter-0.2.7/src/metaprinter/__init__.py
@@ -0,0 +1,15 @@
+from .core import (
+    print_metadata,
+    export_metadata,
+    extract_metadict,
+    print_and_export_metadata,
+    make_labels,
+)
+
+__all__ = [
+    "print_metadata",
+    "export_metadata",
+    "extract_metadict",
+    "print_and_export_metadata",
+    "make_labels",
+]

a938a1cb36319699a99f0cac1d8cbbfebc592161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 04:10:43 +0000

    Add package xpress/9.3.7/xpress-9.3.7-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/xpress/xpress-9.3.7-cp39-cp39-macosx_11_0_arm64.whl/xpress/examples/example_resolve.py
@@ -0,0 +1,19 @@
+# Reads a problem, solves it, then adds a constraint and re-solves it
+#
+# (C) Fair Isaac Corp., 1983-2025
+
+from __future__ import print_function
+
+import xpress
+
+p = xpress.problem()
+
+p.read("example.lp")
+p.optimize()
+print("solution of the original problem: ", p.getVariable(), "-->",
+      p.getSolution())
+
+x = p.getVariable()
+p.addConstraint(xpress.Sum(x) <= 1.1)
+p.optimize()
+print("New solution: ", p.getSolution())

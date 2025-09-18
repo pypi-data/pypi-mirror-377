b021bdf5c5aa4d73c190a37ea27fcc737e718d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:17:43 +0000

    Add package tellimer/0.2.0/tellimer-0.2.0.tar.gz

---
+++ packages/tellimer/tellimer-0.2.0.tar.gz/tellimer-0.2.0/src/tellimer/_models.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+import pandas as pd
+
+DATASETS: dict[str, str] = {
+    "macro_data": "macro-data-v2",
+    "parallel_fx": "parallel-fx-data",
+    # "imf_arrangements": "imf-arrangements",
+    # "debt_composition": "debt-comp",
+    # "event_calendar": "calendar",
+}
+
+
+@dataclass
+class Result:
+    data: pd.DataFrame
+    metadata: list[dict]

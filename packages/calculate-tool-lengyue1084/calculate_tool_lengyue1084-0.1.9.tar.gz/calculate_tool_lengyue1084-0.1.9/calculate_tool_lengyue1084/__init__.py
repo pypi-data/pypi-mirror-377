a8a5a366c89060bc4925216ad2bc669867423806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:07:13 +0000

    Add package calculate-tool-lengyue1084/0.1.9/calculate_tool_lengyue1084-0.1.9.tar.gz

---
+++ packages/calculate-tool-lengyue1084/calculate_tool_lengyue1084-0.1.9.tar.gz/calculate_tool_lengyue1084-0.1.9/calculate_tool_lengyue1084/__init__.py
@@ -0,0 +1,12 @@
+"""
+calculate_tool_lengyue1084 Package
+
+一个简单的计算工具包，提供基本的数学运算和问候功能。
+"""
+
+__version__ = "0.1.9"
+__author__ = "lengyue1084"
+__email__ = "95695864@qq.com"
+
+# 从main.py导入主要功能
+from .main import lucky_star, num_add, greeting

4f44b2b53a82fbb37d681a8df29ab70cfec39a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:07:10 +0000

    Add package prompt4py/0.0.3/prompt4py-0.0.3.tar.gz

---
+++ packages/prompt4py/prompt4py-0.0.3.tar.gz/prompt4py-0.0.3/prompt4py/template/base_template.py
@@ -0,0 +1,109 @@
+from abc import abstractmethod
+from typing_extensions import Any
+
+
+class BaseTemplate:
+    def __init__(self):
+        super().__init__()
+        self._input: Any = ''
+        self._context: Any = ''
+        self._role: Any = ''
+        self._instruction: Any = ''
+        self._objective: Any = ''
+        self._capability: Any = ''
+        self._output_dtype: Any = ''
+        self._output_format: Any = ''
+        self._output_example: Any = ''
+        self._output_language: Any = ''
+        self._constraint: Any = ''
+
+    @abstractmethod
+    def render(self, parse_markdown: bool) -> str: ...
+
+    @property
+    def input(self):
+        return self._input
+
+    @input.setter
+    def input(self, value: Any):
+        self._input = value
+
+    @property
+    def context(self):
+        return self._context
+
+    @context.setter
+    def context(self, value: Any):
+        self._context = value
+
+    @property
+    def role(self):
+        return self._role
+
+    @role.setter
+    def role(self, value: Any):
+        self._role = value
+
+    @property
+    def instruction(self):
+        return self._instruction
+
+    @instruction.setter
+    def instruction(self, value: Any):
+        self._instruction = value
+
+    @property
+    def objective(self):
+        return self._objective
+
+    @objective.setter
+    def objective(self, value: Any):
+        self._objective = value
+
+    @property
+    def capability(self):
+        return self._capability
+
+    @capability.setter
+    def capability(self, value: Any):
+        self._capability = value
+
+    @property
+    def constraint(self):
+        return self._constraint
+
+    @constraint.setter
+    def constraint(self, value: Any):
+        self._constraint = value
+
+    @property
+    def output_dtype(self):
+        return self._output_dtype
+
+    @output_dtype.setter
+    def output_dtype(self, value: Any):
+        self._output_dtype = value
+
+    @property
+    def output_format(self):
+        return self._output_format
+
+    @output_format.setter
+    def output_format(self, value: Any):
+        self._output_format = value
+
+    @property
+    def output_example(self):
+        return self._output_example
+
+    @output_example.setter
+    def output_example(self, value: Any):
+        self._output_example = value
+
+    @property
+    def output_language(self):
+        return self._output_language
+
+    @output_language.setter
+    def output_language(self, value: Any):
+        self._output_language = value
---
+++ packages/prompt4py/prompt4py-0.0.3.tar.gz/prompt4py-0.0.3/prompt4py/template/general_template.py
@@ -0,0 +1,50 @@
+import json
+import re
+import time
+
+from prompt4py.template.base_template import BaseTemplate
+from prompt4py.util.json2markdown import json_to_markdown, json_replace
+
+_VAR_LEFT = '{{'
+_VAR_RIGHT = '}}'
+_VAR_PATTERN = re.compile(r'{{(\w+)}}')
+
+
+class GeneralTemplate(BaseTemplate):
+    def __init__(self):
+        super().__init__()
+
+    def render(self, markdown: bool = True, **kwargs) -> str:
+        _prompt_object = {
+            '_timestamp'.upper(): f'[{time.perf_counter()}]',
+            'role'.upper(): self.role,
+            'objective'.upper(): self.objective,
+            'instruction'.upper(): self.instruction,
+            'capability'.upper(): self.capability,
+            'context'.upper(): self.context,
+            'input'.upper(): self.input,
+            'output_language'.upper(): self.output_language,
+            'output_datatype'.upper(): self.output_dtype,
+            'output_format'.upper(): self.output_format,
+            'output_example'.upper(): json.dumps(self.output_example, ensure_ascii=False),
+            'constraint'.upper(): self.constraint
+            if (isinstance(self.output_example, list) or isinstance(self.output_example, dict))
+            else str(self.output_example)
+        }
+        for k in list(_prompt_object.keys()):
+            if len(str(_prompt_object[k])) < 1:
+                del _prompt_object[k]
+        for k, v in kwargs.items():
+            if not isinstance(k, str):
+                k = str(k)
+            if not isinstance(v, str):
+                v = str(v)
+            _prompt_object = json_replace(_prompt_object, f'{_VAR_LEFT}{k}{_VAR_RIGHT}', v)
+        json_str = json.dumps(_prompt_object, ensure_ascii=False)
+        _vars_left_to_fill = _VAR_PATTERN.findall(json_str)
+        if len(_vars_left_to_fill) > 0:
+            _vars_str = ', '.join([f'"{_}"' for _ in _vars_left_to_fill])
+            raise TypeError(f'render() missing {len(_vars_left_to_fill)} required arguments: {_vars_str}')
+        if markdown:
+            return json_to_markdown(json_data=_prompt_object)
+        return json_str

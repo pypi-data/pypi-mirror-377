2a0a79d7df65555b6e8d868c068ea824e8a9ca8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:06:34 +0000

    Add package django-auto-prefetch/1.14.0/django_auto_prefetch-1.14.0.tar.gz

---
+++ packages/django-auto-prefetch/django_auto_prefetch-1.14.0.tar.gz/django_auto_prefetch-1.14.0/CHANGELOG.rst
@@ -0,0 +1,133 @@
+=========
+Changelog
+=========
+
+1.14.0 (2025-09-18)
+-------------------
+
+* Support Django 6.0.
+
+1.13.0 (2025-09-08)
+-------------------
+
+* Support Python 3.14.
+
+1.12.0 (2025-02-06)
+-------------------
+
+* Support Django 5.2.
+
+1.11.0 (2024-10-29)
+-------------------
+
+* Drop Django 3.2 to 4.1 support.
+
+1.10.0 (2024-10-08)
+-------------------
+
+* Drop Python 3.8 support.
+
+* Support Python 3.13.
+
+1.9.0 (2024-06-19)
+------------------
+
+* Support Django 5.1.
+
+1.8.0 (2023-10-11)
+------------------
+
+* Support Django 5.0.
+
+1.7.0 (2023-07-10)
+------------------
+
+* Drop Python 3.7 support.
+
+1.6.0 (2023-06-14)
+------------------
+
+* Support Python 3.12.
+
+1.5.1 (2023-03-29)
+------------------
+
+* Fix the system check for ``Meta.base_manager_name`` to work with multiple inheritance of ``Meta`` classes.
+
+  Thanks to Julius Seporaitis for the report in `Issue #228 <https://github.com/tolomea/django-auto-prefetch/issues/228>`__.
+
+1.5.0 (2023-02-25)
+------------------
+
+* Support Django 4.2.
+
+1.4.0 (2022-11-09)
+------------------
+
+* Reduce memory footprint when fetching single objects.
+
+1.3.0 (2022-06-05)
+------------------
+
+* Support Python 3.11.
+
+* Support Django 4.1.
+
+1.2.0 (2022-05-10)
+------------------
+
+* Drop support for Django 2.2, 3.0, and 3.1.
+
+1.1.0 (2022-01-10)
+------------------
+
+* Drop Python 3.6 support.
+
+1.0.0 (2021-10-05)
+------------------
+
+* Support Python 3.10.
+
+0.1.1 (2021-09-28)
+------------------
+
+* Fix versions advertised in README.
+
+0.1.0 (2021-09-28)
+------------------
+
+* Support Django 3.2.
+
+* Add system check that the ``Meta`` class is inherited in all subclasses.
+
+0.0.6 (2020-07-29)
+------------------
+
+* Fix model subclasses’ default manager back to ``objects`` rather than
+  ``prefetch_manager``.
+
+0.0.5 (2020-04-05)
+------------------
+
+* Remove tests from built package.
+
+0.0.4 (2020-03-26)
+------------------
+
+* Fix pickled model instances. Instances loaded from pickle remove their peer
+  tracking so will be subject to 1+N queries for any foreign key access.
+
+0.0.3 (2020-03-26)
+------------------
+
+* Declare Python 3.5 support.
+
+0.0.2 (2020-03-25)
+------------------
+
+* Fix packaging issues.
+
+0.0.1 (2020-03-24)
+------------------
+
+* Initial release.
---
+++ packages/django-auto-prefetch/django_auto_prefetch-1.14.0.tar.gz/django_auto_prefetch-1.14.0/src/auto_prefetch/__init__.py
@@ -0,0 +1,155 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+from weakref import WeakValueDictionary
+
+from django.core import checks
+from django.db import models
+from django.db.models.fields import related_descriptors
+
+if TYPE_CHECKING:  # pragma: no cover
+
+    class DescriptorBase:
+        field: models.Field
+
+        def is_cached(self, instance: models.Model) -> bool: ...
+
+        def __get__(
+            self,
+            instance: models.Model | None,
+            instance_type: type[models.Model] | None = None,
+        ) -> Any: ...
+
+else:
+    DescriptorBase = object
+
+
+class DescriptorMixin(DescriptorBase):
+    def _field_name(self) -> str:
+        return self.field.name
+
+    def _is_cached(self, instance: models.Model) -> bool:
+        return self.is_cached(instance)
+
+    def _should_prefetch(self, instance: models.Model | None) -> bool:
+        return (
+            instance is not None  # getattr on the class passes None to the descriptor
+            and not self._is_cached(instance)  # already loaded
+            and len(getattr(instance, "_peers", [])) >= 2  # no peers no prefetch
+        )
+
+    def __get__(
+        self,
+        instance: models.Model | None,
+        instance_type: type[models.Model] | None = None,
+    ) -> Any:
+        if instance is not None and self._should_prefetch(instance):
+            prefetch = models.query.Prefetch(self._field_name())
+            peers = [p for p in instance._peers.values() if not self._is_cached(p)]
+            models.query.prefetch_related_objects(peers, prefetch)
+        return super().__get__(instance, instance_type)
+
+
+class ForwardDescriptorMixin(DescriptorMixin):
+    def _should_prefetch(self, instance: models.Model | None) -> bool:
+        return super()._should_prefetch(
+            instance
+        ) and None not in self.field.get_local_related_value(instance)  # field is null
+
+
+class ForwardManyToOneDescriptor(
+    ForwardDescriptorMixin, related_descriptors.ForwardManyToOneDescriptor
+):
+    pass
+
+
+class ForwardOneToOneDescriptor(
+    ForwardDescriptorMixin, related_descriptors.ForwardOneToOneDescriptor
+):
+    pass
+
+
+class ReverseOneToOneDescriptor(
+    DescriptorMixin, related_descriptors.ReverseOneToOneDescriptor
+):
+    def _is_cached(self, instance: models.Model) -> bool:
+        return self.related.is_cached(instance)
+
+    def _field_name(self) -> str:
+        return self.related.get_accessor_name()
+
+
+class ForeignKey(models.ForeignKey):
+    forward_related_accessor_class = ForwardManyToOneDescriptor
+
+
+class OneToOneField(models.OneToOneField):
+    forward_related_accessor_class = ForwardOneToOneDescriptor
+    related_accessor_class = ReverseOneToOneDescriptor
+
+
+class QuerySet(models.QuerySet):
+    def _fetch_all(self) -> None:
+        set_peers = self._result_cache is None
+        super()._fetch_all()
+        # ModelIterable tests for query sets returning model instances vs
+        # values or value lists etc
+        if (
+            set_peers
+            and issubclass(self._iterable_class, models.query.ModelIterable)
+            and len(self._result_cache) >= 2
+        ):
+            peers = WeakValueDictionary((id(o), o) for o in self._result_cache)
+            for peer in peers.values():
+                peer._peers = peers
+
+
+Manager = models.Manager.from_queryset(QuerySet)
+
+
+class Model(models.Model):
+    class Meta:
+        abstract = True
+        base_manager_name = "prefetch_manager"
+
+    objects = Manager()
+    prefetch_manager = Manager()
+
+    def __getstate__(self) -> dict[str, Any]:
+        # drop the peers info when pickling etc
+        res = super().__getstate__()
+        if "_peers" not in res:  # pragma: no cover
+            return res
+
+        res = dict(res)
+        del res["_peers"]
+        return res
+
+    @classmethod
+    def check(cls, **kwargs: Any) -> list[checks.Error]:
+        errors: list[checks.Error] = super().check(**kwargs)
+        errors.extend(cls._check_meta_inheritance())
+        return errors
+
+    @classmethod
+    def _check_meta_inheritance(cls) -> list[checks.Error]:
+        errors = []
+        base_manager_name = cls._meta.base_manager_name
+        if base_manager_name != "prefetch_manager":
+            errors.append(
+                checks.Error(
+                    id="auto_prefetch.E001",
+                    obj=cls,
+                    msg=(
+                        f"{cls.__name__} inherits from auto_prefetch.Model"
+                        + " but its base_manager_name is not"
+                        + " 'prefetch_manager'"
+                    ),
+                    hint=(
+                        f"The base_manager_name is instead {base_manager_name!r}."
+                        + " Check the Meta class inherits from"
+                        + " auto_prefetch.Model.Meta."
+                    ),
+                )
+            )
+        return errors

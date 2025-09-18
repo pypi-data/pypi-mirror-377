6d7a016f328ffc1ab9cf4ae090048c1fcfe15331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:07:39 +0000

    Add package libknot/3.5.0/libknot-3.5.0.tar.gz

---
+++ packages/libknot/libknot-3.5.0.tar.gz/libknot-3.5.0/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+import setuptools
+
+p = pathlib.Path("README.md")
+if p.exists():
+    long_description = p.read_text()
+
+setuptools.setup(
+    name='libknot',
+    version='3.5.0',
+    description='Python bindings for libknot',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='CZ.NIC, z.s.p.o.',
+    author_email='knot-dns@labs.nic.cz',
+    url='https://gitlab.nic.cz/knot/knot-dns/-/tree/master/python/libknot',
+    license='GPL-2.0-or-later',
+    packages=['libknot'],
+    classifiers=[ # See https://pypi.org/classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Systems Administration',
+    ],
+    python_requires='>=3.5',
+)

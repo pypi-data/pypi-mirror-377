3739b01b44f11e1451518b761619c01e9d07642c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 15:08:15 +0000

    Add package llmakits/0.1.2/llmakits-0.1.2-py3-none-any.whl

---
+++ packages/llmakits/llmakits-0.1.2-py3-none-any.whl/llmakits/message/__init__.py
@@ -0,0 +1,16 @@
+"""
+Message处理模块
+提供消息格式化、准备和验证功能
+"""
+
+from .builder import prepare_messages, rebuild_messages_single_image
+from .formatter import convert_to_json, extract_field
+from .validator import validate_message_format
+
+__all__ = [
+    'prepare_messages',
+    'rebuild_messages_single_image',
+    'convert_to_json',
+    'extract_field',
+    'validate_message_format',
+]

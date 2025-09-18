33f4e56685bb587b7c3503c9ddce9d9ee9e8c0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:17:25 +0000

    Add package pyairios/1.0.7/pyairios-1.0.7-py3-none-any.whl

---
+++ packages/pyairios/pyairios-1.0.7-py3-none-any.whl/pyairios/__init__.py
@@ -0,0 +1,104 @@
+"""The Airios RF bridge API entrypoint."""
+
+from pyairios.models.brdg_02r13 import BRDG02R13
+from pyairios.models.brdg_02r13 import DEFAULT_SLAVE_ID as BRDG02R13_DEFAULT_SLAVE_ID
+from pyairios.models.vmd_02rps78 import VMD02RPS78
+from pyairios.models.vmn_05lm02 import VMN05LM02
+
+from .client import (
+    AiriosBaseTransport,
+    AiriosRtuTransport,
+    AiriosTcpTransport,
+    AsyncAiriosModbusClient,
+    AsyncAiriosModbusRtuClient,
+    AsyncAiriosModbusTcpClient,
+)
+from .constants import BindingStatus, ProductId
+from .data_model import AiriosBoundNodeInfo, AiriosData, AiriosNodeData
+from .exceptions import AiriosException
+from .node import AiriosNode
+
+
+class Airios:
+    """The Airios RF bridge API."""
+
+    _client: AsyncAiriosModbusClient
+    bridge: BRDG02R13
+
+    def __init__(
+        self, transport: AiriosBaseTransport, slave_id: int = BRDG02R13_DEFAULT_SLAVE_ID
+    ) -> None:
+        """Initialize the API instance."""
+        if isinstance(transport, AiriosTcpTransport):
+            transport.__class__ = AiriosTcpTransport
+            self._client = AsyncAiriosModbusTcpClient(transport)
+        elif isinstance(transport, AiriosRtuTransport):
+            transport.__class__ = AiriosRtuTransport
+            self._client = AsyncAiriosModbusRtuClient(transport)
+        else:
+            raise AiriosException(f"Unknown transport {transport}")
+        self.bridge = BRDG02R13(slave_id, self._client)
+
+    async def nodes(self) -> list[AiriosBoundNodeInfo]:
+        """Get the list of bound nodes."""
+        return await self.bridge.nodes()
+
+    async def node(self, slave_id: int) -> AiriosNode:
+        """Get a node instance by its Modbus slave ID."""
+        return await self.bridge.node(slave_id)
+
+    async def bind_status(self) -> BindingStatus:
+        """Get the bind status."""
+        return await self.bridge.bind_status()
+
+    async def bind_controller(
+        self,
+        slave_id: int,
+        product_id: ProductId,
+        product_serial: int | None = None,
+    ) -> bool:
+        """Bind a new controller to the bridge."""
+        return await self.bridge.bind_controller(slave_id, product_id, product_serial)
+
+    async def bind_accessory(
+        self,
+        controller_slave_id: int,
+        slave_id: int,
+        product_id: ProductId,
+    ) -> bool:
+        """Bind a new accessory to the bridge."""
+        return await self.bridge.bind_accessory(controller_slave_id, slave_id, product_id)
+
+    async def unbind(self, slave_id: int) -> bool:
+        """Remove a bound node from the bridge by its Modbus slave ID."""
+        return await self.bridge.unbind(slave_id)
+
+    async def fetch(self) -> AiriosData:
+        """Get the data from all nodes at once."""
+        data: dict[int, AiriosNodeData] = {}
+
+        brdg_data = await self.bridge.fetch_bridge()
+        if brdg_data["rf_address"] is None:
+            raise AiriosException("Failed to fetch node RF address")
+        bridge_rf_address = brdg_data["rf_address"].value
+        data[self.bridge.slave_id] = brdg_data
+
+        for node in await self.bridge.nodes():
+            if node.product_id == ProductId.VMD_02RPS78:
+                vmd = VMD02RPS78(node.slave_id, self.bridge.client)
+                vmd_data = await vmd.fetch_vmd_data()
+                data[node.slave_id] = vmd_data
+            if node.product_id == ProductId.VMN_05LM02:
+                vmn = VMN05LM02(node.slave_id, self.bridge.client)
+                vmn_data = await vmn.fetch_vmn_data()
+                data[node.slave_id] = vmn_data
+
+        return AiriosData(bridge_rf_address=bridge_rf_address, nodes=data)
+
+    async def connect(self) -> bool:
+        """Establish underlying Modbus connection."""
+        return await self._client.connect()
+
+    def close(self) -> None:
+        """Close underlying Modbus connection."""
+        return self._client.close()
---
+++ packages/pyairios/pyairios-1.0.7-py3-none-any.whl/pyairios/registers.py
@@ -0,0 +1,259 @@
+"""Register definitions."""
+
+import datetime
+import struct
+import typing as t
+from dataclasses import dataclass
+from enum import Flag, IntEnum, auto
+
+from pymodbus.client.mixin import ModbusClientMixin
+
+from .constants import ValueStatusFlags, ValueStatusSource
+from .exceptions import AiriosDecodeError, AiriosInvalidArgumentException
+
+T = t.TypeVar("T")
+
+
+class RegisterAddress(IntEnum):
+    """The register address base class."""
+
+
+class RegisterAccess(Flag):
+    """Register access flags."""
+
+    READ = auto()
+    WRITE = auto()
+    STATUS = auto()
+
+
+@dataclass(frozen=True)
+class RegisterDescription:
+    """Register description."""
+
+    address: RegisterAddress
+    length: int
+    access: RegisterAccess
+
+
+class RegisterBase(t.Generic[T]):
+    """Base class for register definitions."""
+
+    description: RegisterDescription
+    datatype: ModbusClientMixin.DATATYPE
+
+    def __init__(self, description: RegisterDescription) -> None:
+        """Initialize the register instance."""
+        self.description = description
+
+    def decode(self, registers: list[int]) -> T:
+        """Decode register bytes to value."""
+        return ModbusClientMixin.convert_from_registers(
+            registers, self.datatype, word_order="little"
+        )  # type: ignore
+
+    def encode(self, value: T) -> list[int]:
+        """Encode value to register bytes."""
+        return ModbusClientMixin.convert_to_registers(
+            value,  # type: ignore
+            self.datatype,
+            word_order="little",
+        )
+
+
+class StringRegister(RegisterBase[str]):
+    """String register."""
+
+    datatype = ModbusClientMixin.DATATYPE.STRING
+
+    def __init__(self, address: RegisterAddress, length: int, access: RegisterAccess) -> None:
+        """Initialize the StringRegister instance."""
+        description = RegisterDescription(address, length, access)
+        super().__init__(description)
+
+    def decode(self, registers: list[int]) -> str:
+        """Decode register bytes to value."""
+
+        def registers_to_bytearray(registers: list[int]) -> bytearray:
+            """Convert registers to bytes."""
+            b = bytearray()
+            for x in registers:
+                b.extend(x.to_bytes(2, "big"))
+            return b
+
+        b = registers_to_bytearray(registers)
+
+        # remove trailing null bytes
+        trailing_nulls_begin = len(b)
+        while trailing_nulls_begin > 0 and b[trailing_nulls_begin - 1] == 0:
+            trailing_nulls_begin -= 1
+
+        b = b[:trailing_nulls_begin]
+
+        try:
+            result = b.decode("utf-8")
+        except UnicodeDecodeError as err:
+            raise AiriosDecodeError from err
+        return result
+
+    def encode(self, value: str) -> list[int]:
+        return ModbusClientMixin.convert_to_registers(value, self.datatype, word_order="little")
+
+
+class NumberRegister(RegisterBase[T]):
+    """Base class for number registers."""
+
+    def decode(self, registers: list[int]) -> T:
+        """Decode register bytes to value."""
+        result: T = t.cast(
+            T,
+            ModbusClientMixin.convert_from_registers(registers, self.datatype, word_order="little"),
+        )
+        return result
+
+    def encode(self, value: T) -> list[int]:
+        """Encode value to register bytes."""
+        if isinstance(value, int):
+            return ModbusClientMixin.convert_to_registers(value, self.datatype, word_order="little")
+        if isinstance(value, (bool, float)):
+            return ModbusClientMixin.convert_to_registers(
+                int(value), self.datatype, word_order="little"
+            )
+        raise AiriosInvalidArgumentException(f"Unsupported type {type(value)}")
+
+
+class U16Register(NumberRegister[int]):
+    """Unsigned 16-bit register."""
+
+    datatype = ModbusClientMixin.DATATYPE.UINT16
+
+    def __init__(self, address: RegisterAddress, access: RegisterAccess) -> None:
+        """Initialize the U16Register instance."""
+        description = RegisterDescription(address, 1, access)
+        super().__init__(description)
+
+
+class I16Register(NumberRegister[int]):
+    """Signed 16-bit register."""
+
+    datatype = ModbusClientMixin.DATATYPE.INT16
+
+    def __init__(self, address: RegisterAddress, access: RegisterAccess) -> None:
+        """Initialize the I16Register instance."""
+        description = RegisterDescription(address, 1, access)
+        super().__init__(description)
+
+
+class U32Register(NumberRegister[int]):
+    """Unsigned 32-bit register."""
+
+    datatype = ModbusClientMixin.DATATYPE.UINT32
+
+    def __init__(self, address: RegisterAddress, access: RegisterAccess) -> None:
+        """Initialize the U32Register instance."""
+        description = RegisterDescription(address, 2, access)
+        super().__init__(description)
+
+
+class FloatRegister(NumberRegister[float]):
+    """Float register."""
+
+    datatype = ModbusClientMixin.DATATYPE.FLOAT32
+
+    def __init__(self, address: RegisterAddress, access: RegisterAccess) -> None:
+        """Initialize the FloatRegister instance."""
+        description = RegisterDescription(address, 2, access)
+        super().__init__(description)
+
+
+class DateRegister(RegisterBase[datetime.date]):
+    """Date register."""
+
+    datatype = ModbusClientMixin.DATATYPE.UINT32
+
+    def __init__(self, address: RegisterAddress, access: RegisterAccess) -> None:
+        """Initialize the DateRegister instance."""
+        description = RegisterDescription(address, 2, access)
+        super().__init__(description)
+
+    def decode(self, registers: list[int]) -> datetime.date:
+        """Decode register bytes to value."""
+        value: int = t.cast(int, super().decode(registers))
+
+        if value == 0xFFFFFFFF:
+            return datetime.date.min
+
+        buf = value.to_bytes(4, "big")
+        (day, month, year) = struct.unpack(">BBH", buf)
+        return datetime.date(year, month, day)
+
+    def encode(self, value: datetime.date) -> list[int]:
+        """Encode value to register bytes."""
+
+        buf = struct.pack(">BBH", value.day, value.month, value.year)
+        ival = int.from_bytes(buf, byteorder="big")
+        return ModbusClientMixin.convert_to_registers(ival, self.datatype, word_order="little")
+
+
+class DateTimeRegister(RegisterBase[datetime.datetime]):
+    """DateTime register."""
+
+    datatype = ModbusClientMixin.DATATYPE.UINT32
+
+    def __init__(self, address: RegisterAddress, access: RegisterAccess) -> None:
+        """Initialize the DateTimeRegister instance."""
+        description = RegisterDescription(address, 2, access)
+        super().__init__(description)
+
+    def decode(self, registers: list[int]) -> datetime.datetime:
+        """Decode register bytes to value."""
+        value: int = t.cast(int, super().decode(registers))
+
+        if value == 0xFFFFFFFF:
+            return datetime.datetime.min
+
+        return datetime.datetime.fromtimestamp(value, tz=datetime.timezone.utc)
+
+    def encode(self, value: datetime.datetime) -> list[int]:
+        """Encode value to register bytes."""
+
+        ts: float = value.replace(tzinfo=datetime.timezone.utc).timestamp()
+        ival: int = int(ts)
+        return ModbusClientMixin.convert_to_registers(ival, self.datatype, word_order="little")
+
+
+@dataclass
+class ResultStatus:
+    """Metadata associated to a register value."""
+
+    age: datetime.timedelta
+    source: ValueStatusSource
+    flags: ValueStatusFlags
+
+    def __str__(self) -> str:
+        return f"value is {self.age} old, last seen from {self.source}, flags: {self.flags}"
+
+
+@dataclass
+class Result(t.Generic[T]):
+    """Register read result."""
+
+    value: T
+    status: ResultStatus | None
+
+    def __init__(self, value: T, status: ResultStatus | None = None) -> None:
+        super().__init__()
+        self.value = value
+        self.status = status
+
+    def __str__(self) -> str:
+        if isinstance(self.value, str):
+            return f"{self.value}"
+        if isinstance(self.value, datetime.date):
+            return f"{self.value.isoformat()}"
+        if isinstance(self.value, datetime.datetime):
+            return f"{self.value.isoformat()}"
+        if isinstance(self.value, float):
+            return f"{self.value:.04f}"
+        if isinstance(self.value, int):
+            return f"{self.value}"
+        return f"{self.value}"

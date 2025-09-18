cca47d7a78ca40d53894aca0998a1347b5378820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 14:06:56 +0000

    Add package FreeDynamics-simaf/1.5.8/freedynamics_simaf-1.5.8.tar.gz

---
+++ packages/FreeDynamics-simaf/freedynamics_simaf-1.5.8.tar.gz/freedynamics_simaf-1.5.8/vacancy_predictor/__version__.py
@@ -0,0 +1,6 @@
+"""Version information for vacancy-predictor"""
+
+__version__ = "1.5.8"
+__author__ = "Bergamin Santiago"
+__email__ = "santiagobergamin@com.com"
+__description__ = "A comprehensive ML tool for vacancy prediction with GUI"

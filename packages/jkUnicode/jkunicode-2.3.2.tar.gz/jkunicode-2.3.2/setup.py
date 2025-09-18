1e5f7e87e7d45395dbae38d1aceec99ecd870ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:17:49 +0000

    Add package jkUnicode/2.3.2/jkunicode-2.3.2.tar.gz

---
+++ packages/jkUnicode/jkunicode-2.3.2.tar.gz/jkunicode-2.3.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+# from mypyc.build import mypycify
+
+
+if __name__ == "__main__":
+    setup(
+        # ext_modules=mypycify(
+        #     [
+        #         "lib/jkUnicode/tools/helpers.py",
+        #     ]
+        # ),
+    )

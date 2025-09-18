9fe36c8c2906fdb3dbe54689c39a663206cdca9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:07:11 +0000

    Add package zc.catalog/5.0/zc_catalog-5.0.tar.gz

---
+++ packages/zc.catalog/zc_catalog-5.0.tar.gz/zc_catalog-5.0/README.rst
@@ -0,0 +1,25 @@
+==========
+zc.catalog
+==========
+
+.. image:: https://img.shields.io/pypi/v/zc.catalog.svg
+   :target: https://pypi.org/project/zc.catalog/
+   :alt: Latest Version
+
+.. image:: https://img.shields.io/pypi/pyversions/zc.catalog.svg
+   :target: https://pypi.org/project/zc.catalog/
+   :alt: Supported Python versions
+
+.. image:: https://github.com/zopefoundation/zc.catalog/actions/workflows/tests.yml/badge.svg
+   :target: https://github.com/zopefoundation/zc.catalog/actions/workflows/tests.yml
+   :alt: Build Status
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zc.catalog/badge.svg
+   :target: https://coveralls.io/github/zopefoundation/zc.catalog
+   :alt: Code Coverage
+
+
+zc.catalog is an extension to the Zope 3 catalog, Zope 3's indexing
+and search facility. zc.catalog contains a number of extensions to the
+Zope 3 catalog, such as some new indexes, improved globbing and
+stemming support, and an alternative catalog implementation.
---
+++ packages/zc.catalog/zc_catalog-5.0.tar.gz/zc_catalog-5.0/setup.py
@@ -0,0 +1,138 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zc.catalog package
+
+"""
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname('.'), *rnames)) as f:
+        return f.read()
+
+
+stemmer_requires = [
+    'zopyx.txng3.ext >= 2.0.0',
+]
+
+tests_require = [
+    'ZODB',
+    'beautifulsoup4',
+    'zope.annotation',
+    'zope.app.appsetup',
+    'zope.app.basicskin',
+    'zope.app.catalog',
+    'zope.app.catalog',
+    'zope.app.component',
+    'zope.app.container',
+    'zope.app.form',
+    'zope.app.publisher >= 4.0',
+    'zope.app.rotterdam',
+    'zope.app.schema >= 4.0',
+    'zope.app.wsgi',
+    'zope.browsermenu',
+    'zope.browserpage',
+    'zope.browserresource',
+    'zope.dottedname',
+    'zope.keyreference',
+    'zope.login',
+    'zope.password',
+    'zope.principalannotation',
+    'zope.principalregistry',
+    'zope.securitypolicy',
+    'zope.testbrowser >= 5.2',
+    'zope.testing',
+    'zope.testrunner >= 6.4',
+] + stemmer_requires
+
+setup(name='zc.catalog',
+      version='5.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.dev',
+      description="Extensions to the Zope 3 Catalog",
+      long_description=(
+          read('README.rst')
+          + '\n\n.. contents::\n\n' +
+          read('CHANGES.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'valueindex.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'setindex.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'normalizedindex.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'extentcatalog.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'stemmer.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'legacy.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'globber.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'callablewrapper.rst')
+          + '\n\n' +
+          read('src', 'zc', 'catalog', 'browser', 'README.rst')
+      ),
+      keywords="zope3 i18n date time duration catalog index",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope :: 3',
+      ],
+      url='https://github.com/zopefoundation/zc.catalog',
+      license='ZPL-2.1',
+      python_requires='>=3.9',
+      extras_require={
+          'test': tests_require,
+          'browser': [
+              'zope.app.form',
+              'zope.browsermenu',
+          ],
+          'stemmer': stemmer_requires,
+      },
+      tests_require=tests_require,
+      install_requires=[
+          'BTrees >= 4.4.1',
+          'persistent',
+          'pytz',
+          'setuptools',
+          'zope.catalog >= 4.2.1',
+          'zope.component >= 4.3.0',
+          'zope.container >= 4.1.0',
+          'zope.i18nmessageid >= 4.1.0',
+          'zope.index >= 4.3.0',
+          'zope.interface >= 4.4.0',
+          'zope.intid >= 4.2.0',
+          'zope.publisher >= 3.12',
+          'zope.schema >= 4.4.2',
+          'zope.security >= 4.1.0',
+      ],
+      include_package_data=True,
+      zip_safe=False)

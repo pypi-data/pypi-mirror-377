a109e84ebe6d55cd8b49d50ad519efa5ebbc06a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 04:10:55 +0000

    Add package cabaret/0.3.0/cabaret-0.3.0.tar.gz

---
+++ packages/cabaret/cabaret-0.3.0.tar.gz/cabaret-0.3.0/src/cabaret/__init__.py
@@ -0,0 +1,17 @@
+from cabaret.camera import Camera
+from cabaret.focuser import Focuser
+from cabaret.image import generate_image
+from cabaret.observatory import Observatory
+from cabaret.site import Site
+from cabaret.sources import Sources
+from cabaret.telescope import Telescope
+
+__all__ = [
+    "Camera",
+    "Focuser",
+    "generate_image",
+    "Observatory",
+    "Site",
+    "Sources",
+    "Telescope",
+]

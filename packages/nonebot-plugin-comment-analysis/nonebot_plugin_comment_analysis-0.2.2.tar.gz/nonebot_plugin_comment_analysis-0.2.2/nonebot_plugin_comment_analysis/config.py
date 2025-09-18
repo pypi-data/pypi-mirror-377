ef51bc3d90bc936d8210e0543e1f87c158b6b2a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:18:09 +0000

    Add package nonebot-plugin-comment-analysis/0.2.2/nonebot_plugin_comment_analysis-0.2.2.tar.gz

---
+++ packages/nonebot-plugin-comment-analysis/nonebot_plugin_comment_analysis-0.2.2.tar.gz/nonebot_plugin_comment_analysis-0.2.2/nonebot_plugin_comment_analysis/config.py
@@ -0,0 +1,23 @@
+﻿from typing import Optional
+
+from pydantic import BaseModel, Extra
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    xhs_ck: Optional[str] = ""
+    douyin_ck: Optional[str] = ""
+    is_oversea: Optional[bool] = False
+    bili_sessdata: Optional[str] = ""
+    r_global_nickname: Optional[str] = ""
+    video_duration_maximum: Optional[int] = 480
+    global_resolve_controller: Optional[str] = ""
+
+    # AI Summary configs
+    gemini_key: Optional[str] = None  # Gemini 接口密钥
+    openai_base_url: Optional[str] = None  # OpenAI 接口地址
+    openai_api_key: Optional[str] = None  # OpenAI 接口密钥
+    summary_model: str = "gemini-1.5-flash"  # 默认模型名称
+    proxy: Optional[str] = None  # 外部 API 代理
+    summary_max_length: int = 1000  # 总结最大长度
+    summary_min_length: int = 50  # 总结最小长度
+    time_out: int = 120  # 模型 API 请求超时时间（秒）

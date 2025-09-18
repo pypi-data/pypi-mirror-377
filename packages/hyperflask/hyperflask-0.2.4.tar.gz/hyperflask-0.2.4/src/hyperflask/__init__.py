c02e0ba162be21c06215b41679d8935ffb295dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 07:06:35 +0000

    Add package hyperflask/0.2.4/hyperflask-0.2.4.tar.gz

---
+++ packages/hyperflask/hyperflask-0.2.4.tar.gz/hyperflask-0.2.4/src/hyperflask/__init__.py
@@ -0,0 +1,16 @@
+from flask import *
+from .app import Hyperflask
+from .utils.htmx import htmx_redirect, htmx_oob
+
+# useful exports from hyperflask managed extensions
+from flask_babel import gettext, _, pgettext, _p, ngettext, _n, npgettext, _np, lazy_gettext, _lazy
+from flask_file_routes import page
+from flask_files import save_file, validate_file
+from flask_mailman_templates import send_mail
+from flask_mercure_sse import publish_signal, mercure_publish
+
+# useful exports from 3rd party libs
+from htmx_flask import request, make_response
+from jinja2_fragments.flask import render_block
+from slugify import slugify
+from blinker import signal

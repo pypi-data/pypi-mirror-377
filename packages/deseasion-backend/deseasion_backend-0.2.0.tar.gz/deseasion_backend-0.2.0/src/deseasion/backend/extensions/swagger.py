6e68298599bc9ce7c96ede327540eb84c8c807b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:07:22 +0000

    Add package deseasion-backend/0.2.0/deseasion_backend-0.2.0.tar.gz

---
+++ packages/deseasion-backend/deseasion_backend-0.2.0.tar.gz/deseasion_backend-0.2.0/src/deseasion/backend/extensions/swagger.py
@@ -0,0 +1,13 @@
+from flasgger import Swagger
+from flask import Flask
+
+swagger = Swagger()
+
+
+def init_app(app: Flask):
+    """Extract OpenAPI specification and create swagger"""
+    from ..openapi.extract_specification import extract_openapi
+
+    spec = extract_openapi()
+    swagger.template = spec.to_dict()
+    swagger.init_app(app)

a363477c2a453d7fe62b22f1f356a58278f141ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:05:57 +0000

    Add package hoprd-config-tool/0.1.19/hoprd_config_tool-0.1.19-py3-none-any.whl

---
+++ packages/hoprd-config-tool/hoprd_config_tool-0.1.19-py3-none-any.whl/hoprd-config-tool/yaml/yaml_objects.py
@@ -0,0 +1,25 @@
+from .parser import YAMLParser
+
+class IPv4(YAMLParser):
+    scalar = True
+    ipv4: str
+
+class Token(YAMLParser):
+    scalar = True
+    token: str
+
+class Aggregating(YAMLParser):
+    aggregation_threshold: int
+    unrealized_balance_ratio: float
+    aggregate_on_channel_close: bool
+
+class AutoFunding(YAMLParser):
+    funding_amount: str
+    min_stake_threshold: str
+
+class AutoRedeeming(YAMLParser):
+    redeem_only_aggregated: bool
+    minimum_redeem_ticket_value: str
+
+class ClosureFinalizer(YAMLParser):
+    max_closure_overdue: int

e414068e2bf87ac8f2602cd5c5633268008cdbc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:07:15 +0000

    Add package pixel-sdk/1.0.24/pixel_sdk-1.0.24.tar.gz

---
+++ packages/pixel-sdk/pixel_sdk-1.0.24.tar.gz/pixel_sdk-1.0.24/pixel/sdk/nodes/input_node.py
@@ -0,0 +1,28 @@
+from pixel.core import Metadata, Node
+from pixel.sdk import StorageClient
+
+
+class InputNode(Node):
+    node_type = "input"
+
+    metadata = {
+        "inputs": {
+            "input": { "type": "FILEPATH_ARRAY", "required": True, "widget": "FILE_PICKER", "default": set() }
+        },
+        "outputs": {
+            "output": { "type": "FILEPATH_ARRAY", "required": True, "widget": "LABEL" }
+        },
+        "display": {
+            "name": "Input",
+            "category": "IO",
+            "description": "Input files",
+            "color": "#AED581",
+            "icon": "InputIcon"
+        }
+    }
+
+    def exec(self, input, meta: Metadata):
+        output_files = []
+        for file in input:
+            output_files.append(StorageClient.store_from_workspace_to_task(meta.task_id, meta.node_id, file))
+        return {"output": output_files}

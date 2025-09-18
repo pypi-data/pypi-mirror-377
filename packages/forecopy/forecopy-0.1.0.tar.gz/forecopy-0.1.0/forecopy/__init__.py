1090c555d81dec3ed9ccdcdf5c90dd144312e594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:05:54 +0000

    Add package forecopy/0.1.0/forecopy-0.1.0.tar.gz

---
+++ packages/forecopy/forecopy-0.1.0.tar.gz/forecopy-0.1.0/forecopy/__init__.py
@@ -0,0 +1,15 @@
+__name__ = 'forecopy'
+__version__ = '0.1.0'
+
+from forecopy.cov import cscov, tecov
+from forecopy.tools import cstools, tetools
+from forecopy.lsrec import csrec, terec
+
+__all__ = [
+    'cscov', 
+    'tecov', 
+    'cstools', 
+    'tetools', 
+    'csrec', 
+    'terec'
+]

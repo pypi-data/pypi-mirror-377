fef9a033c4a1c6680a73163bd9998f74b7f56cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 16:07:26 +0000

    Add package check-msdefender/1.1.6/check_msdefender-1.1.6.tar.gz

---
+++ packages/check-msdefender/check_msdefender-1.1.6.tar.gz/check_msdefender-1.1.6/check_msdefender/__init__.py
@@ -0,0 +1,5 @@
+"""Check Microsoft Defender API endpoints and check values - Nagios plugin."""
+
+__version__ = "1.0.0"
+__author__ = "ldvchosal"
+__email__ = "ldvchosa@github.com"
---
+++ packages/check-msdefender/check_msdefender-1.1.6.tar.gz/check_msdefender-1.1.6/check_msdefender/cli/commands/__init__.py
@@ -0,0 +1,19 @@
+"""Commands package for CLI."""
+
+from typing import Any
+from .lastseen import register_lastseen_commands
+from .vulnerabilities import register_vulnerability_commands
+from .onboarding import register_onboarding_commands
+from .machines import register_machines_commands
+from .detail import register_detail_commands
+from .alerts import register_alerts_commands
+
+
+def register_all_commands(main_group: Any) -> None:
+    """Register all commands with the main CLI group."""
+    register_lastseen_commands(main_group)
+    register_vulnerability_commands(main_group)
+    register_onboarding_commands(main_group)
+    register_machines_commands(main_group)
+    register_detail_commands(main_group)
+    register_alerts_commands(main_group)

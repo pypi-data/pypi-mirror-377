08963a7e7b5c215396ee8f25734c5be6f15ab45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 03:16:52 +0000

    Add package gov-uk-dashboards/26.6.0/gov_uk_dashboards-26.6.0-py3-none-any.whl

---
+++ packages/gov-uk-dashboards/gov_uk_dashboards-26.6.0-py3-none-any.whl/gov_uk_dashboards/constants.py
@@ -0,0 +1,26 @@
+"""Constants that are common across the dashboard."""
+
+CUSTOM_DATA = "custom_data"
+HOVER_TEXT_HEADERS = "hover_text_headers"
+DATE_VALID = "Date valid"
+CHART_LABEL_FONT_SIZE = 19
+FINANCIAL_YEAR_ENDING = "Financial year ending"
+VALUE = "Value"
+MAIN_TITLE = "main_title"
+SUBTITLE = "subtitle"
+MEASURE = "measure"
+FILL_TO_PREVIOUS_TRACE = "Fill to previous trace"
+REMOVE_INITIAL_MARKER = "Remove initial marker"
+YEAR = "Year"
+
+
+UNIT_SIZE = "Unit size"
+DEFAULT_COLOURSCALE = "tealgrn"
+
+LEGEND_SPACING = "\u00a0" * 5
+
+DOWNLOAD_BUTTON_CLASSES = (
+    "govuk-button govuk-button--primary "
+    "govuk-!-margin-bottom-0 govuk-!-margin-top-4 "
+    "flex w-auto items-center gap-2 print:hidden"
+)

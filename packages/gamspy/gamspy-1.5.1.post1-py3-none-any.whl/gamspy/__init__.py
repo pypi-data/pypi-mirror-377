fe144f83dfbf2397f65e76b8f05fe738a7d81a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:07:12 +0000

    Add package gamspy/1.5.1.post1/gamspy-1.5.1.post1-py3-none-any.whl

---
+++ packages/gamspy/gamspy-1.5.1.post1-py3-none-any.whl/gamspy/__init__.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+from gams.transfer import SpecialValues
+
+import gamspy.formulations as formulations
+import gamspy.math as math
+import gamspy.utils as utils
+from gamspy._algebra import (
+    Card,
+    Domain,
+    Number,
+    Ord,
+    Product,
+    Sand,
+    Smax,
+    Smin,
+    Sor,
+    Sum,
+)
+from gamspy._algebra.expression import Expression
+from gamspy._backend.engine import EngineClient
+from gamspy._backend.neos import NeosClient
+from gamspy._container import Container
+from gamspy._model import Model, ModelStatus, Problem, Sense, SolveStatus
+from gamspy._options import ModelInstanceOptions, Options
+from gamspy._symbols import (
+    Alias,
+    Equation,
+    EquationType,
+    Parameter,
+    Set,
+    UniverseAlias,
+    Variable,
+    VariableType,
+)
+
+from .version import __version__
+
+_ctx_managers: dict[tuple[int, int], Container] = dict()
+
+__all__ = [
+    "Container",
+    "Set",
+    "Alias",
+    "UniverseAlias",
+    "Parameter",
+    "Variable",
+    "Equation",
+    "Model",
+    "Problem",
+    "Sense",
+    "VariableType",
+    "EquationType",
+    "ModelStatus",
+    "SolveStatus",
+    "Sum",
+    "Product",
+    "Smax",
+    "Smin",
+    "Sand",
+    "Sor",
+    "Domain",
+    "Number",
+    "Ord",
+    "Card",
+    "Options",
+    "ModelInstanceOptions",
+    "Expression",
+    "EngineClient",
+    "NeosClient",
+    "math",
+    "formulations",
+    "utils",
+    "SpecialValues",
+    "__version__",
+]

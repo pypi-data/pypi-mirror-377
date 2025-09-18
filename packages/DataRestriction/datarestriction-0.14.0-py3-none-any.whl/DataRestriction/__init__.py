913a70831ea873e321f21a3cbda3347e6ea9301c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 14:06:16 +0000

    Add package DataRestriction/0.14.0/datarestriction-0.14.0-py3-none-any.whl

---
+++ packages/DataRestriction/datarestriction-0.14.0-py3-none-any.whl/DataRestriction/__init__.py
@@ -0,0 +1,32 @@
+"""
+Author: Big Panda
+Time: 26.04.2025   17:06
+Description:
+    
+"""
+from DataRestriction.BaseProperties import *
+from DataRestriction.DerivedProperties import *
+
+__all__ = [
+    # ================== IntProperties ==================
+    "IntProperty",
+    "PositiveIntProperty",
+    "NonNegativeIntProperty",
+    # ================== FloatProperties ==================
+    "FloatProperty",
+    "PositiveFloatProperty",
+    "NonNegativeFloatProperty",
+    # ================== NumberProperties ==================
+    "NumberProperty",
+    "PositiveNumberProperty",
+    "NonNegativeNumberProperty",
+    # ================== OtherProperties ==================
+    "StringProperty",
+    "BoolProperty",
+    "DictProperty",
+    "LockedProperty",
+    "AngleProperty",
+    "FractionProperty",
+    # ================== CoordinatesProperties ==================
+    "Coord2DProperty",
+]

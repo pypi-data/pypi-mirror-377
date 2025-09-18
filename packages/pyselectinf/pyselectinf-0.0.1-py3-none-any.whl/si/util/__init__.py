79201aff7a9850594ce093227970b33392bec0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:07:21 +0000

    Add package pyselectinf/0.0.1/pyselectinf-0.0.1-py3-none-any.whl

---
+++ packages/pyselectinf/pyselectinf-0.0.1-py3-none-any.whl/si/util/__init__.py
@@ -0,0 +1,15 @@
+from si.util.util import (
+    FormatArray,
+    solve_linear_inequalities,
+    solve_quadratic_inequality,
+    intersect,
+    compute_p_value,
+)
+
+__all__ = [
+    "FormatArray",
+    "solve_linear_inequalities",
+    "solve_quadratic_inequality",
+    "intersect",
+    "compute_p_value",
+]

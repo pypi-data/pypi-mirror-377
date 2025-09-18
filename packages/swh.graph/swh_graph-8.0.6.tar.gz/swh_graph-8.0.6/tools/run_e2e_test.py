7d3b3ceb4e3b6e75aebe478798de47ff3bd995d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 05:07:15 +0000

    Add package swh.graph/8.0.6/swh_graph-8.0.6.tar.gz

---
+++ packages/swh.graph/swh_graph-8.0.6.tar.gz/swh_graph-8.0.6/tools/run_e2e_test.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2019-2025  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+"""
+Script to run the end to end compression tests without having to
+compress an entire graph.
+"""
+
+from swh.graph.e2e_tests import run_e2e_test
+
+run_e2e_test(
+    graph_name="example",
+    in_dir="swh/graph/example_dataset/orc/",
+    out_dir="swh/graph/example_dataset/compressed/",
+    sensitive_in_dir="swh/graph/example_dataset-sensitive/orc/",
+    sensitive_out_dir="swh/graph/example_dataset-sensitive/compressed/",
+    test_flavor="example",
+    profile="debug",
+)

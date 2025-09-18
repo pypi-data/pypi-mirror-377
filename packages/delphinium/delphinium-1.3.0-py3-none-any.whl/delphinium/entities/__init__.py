fe0a1ad6f2ca6909b5ea8d576a24e9e7a514b94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 14:06:32 +0000

    Add package delphinium/1.3.0/delphinium-1.3.0-py3-none-any.whl

---
+++ packages/delphinium/delphinium-1.3.0-py3-none-any.whl/delphinium/entities/__init__.py
@@ -0,0 +1,22 @@
+from delphinium.entities.artist import Artist
+from delphinium.entities.character import Character
+from delphinium.entities.file import File, ResolvedFile
+from delphinium.entities.galleryinfo import Galleryinfo
+from delphinium.entities.group import Group
+from delphinium.entities.info import Info
+from delphinium.entities.language import Language
+from delphinium.entities.parody import Parody
+from delphinium.entities.tag import Tag
+
+__all__ = [
+    "Artist",
+    "Character",
+    "File",
+    "ResolvedFile",
+    "Galleryinfo",
+    "Group",
+    "Info",
+    "Language",
+    "Parody",
+    "Tag",
+]

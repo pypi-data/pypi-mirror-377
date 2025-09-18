70683ee110a3561bd12d7d5096369663ba6cb3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 14:06:27 +0000

    Add package superb-ai-onprem/0.7.0/superb_ai_onprem-0.7.0.tar.gz

---
+++ packages/superb-ai-onprem/superb_ai_onprem-0.7.0.tar.gz/superb_ai_onprem-0.7.0/spb_onprem/entities.py
@@ -0,0 +1,60 @@
+from .data.entities import (
+    Data,
+    Scene,
+    Annotation,
+    AnnotationVersion,
+    Prediction,
+    DataMeta,
+    DataSlice,
+    Frame,
+)
+from .datasets.entities import Dataset
+from .slices.entities import Slice
+from .data.enums import (
+    DataType,
+    SceneType,
+    DataMetaTypes,
+    DataMetaValue,
+    DataStatus,
+)
+from .activities.entities import (
+    Activity,
+    ActivityHistory,
+    ActivityStatus,
+    ActivitySchema,
+    SchemaType,
+)
+from .exports.entities import Export
+from .contents.entities import Content
+from .predictions.entities import PredictionSet
+from .models.entities import Model
+
+__all__ = [
+    # Core Entities
+    "Data",
+    "Scene",
+    "Annotation",
+    "AnnotationVersion",
+    "Prediction",
+    "DataMeta",
+    "DataSlice",
+    "Dataset",
+    "Slice",
+    "Activity",
+    "ActivityHistory",
+    "Export",
+    "Content",
+    "Frame",
+    "PredictionSet",
+    "Model",
+
+    # Enums
+    "DataType",
+    "SceneType",
+    "DataMetaTypes",
+    "DataMetaValue",
+    "DataStatus",
+    "ActivityStatus",
+    "ActivitySchema",
+    "SchemaType",
+] 

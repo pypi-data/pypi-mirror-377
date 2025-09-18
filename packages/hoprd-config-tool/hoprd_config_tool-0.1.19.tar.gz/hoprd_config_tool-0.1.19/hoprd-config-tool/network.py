ac5a0713ada838860aca809ed2b343d8d28ae049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:05:57 +0000

    Add package hoprd-config-tool/0.1.19/hoprd_config_tool-0.1.19.tar.gz

---
+++ packages/hoprd-config-tool/hoprd_config_tool-0.1.19.tar.gz/hoprd_config_tool-0.1.19/hoprd-config-tool/network.py
@@ -0,0 +1,16 @@
+from .baseobject import BaseObject
+from .meta import Meta
+from .node import Node
+
+
+class Network(BaseObject):
+    keys = {
+        "meta": "meta",
+        "config": "config",
+        "nodes": "nodes",
+        "slug": "nodes_slug"
+    }
+
+    def post_init(self):
+        self.nodes = [Node(subdata) for subdata in self.nodes]
+        self.meta = Meta(self.meta)

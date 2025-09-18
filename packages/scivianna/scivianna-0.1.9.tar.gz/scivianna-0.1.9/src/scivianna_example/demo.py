cd54f68fbd650186a50b0f666e24f5c8c63820dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 17:06:37 +0000

    Add package scivianna/0.1.9/scivianna-0.1.9.tar.gz

---
+++ packages/scivianna/scivianna-0.1.9.tar.gz/scivianna-0.1.9/src/scivianna_example/demo.py
@@ -0,0 +1,61 @@
+from scivianna_example.europe_grid.europe_grid import (
+    make_europe_panel as europe_example,
+)
+import scivianna_example.europe_grid.europe_grid as europe_grid
+from scivianna_example.medcoupling.split_item_example import (
+    get_panel as medcoupling_example,
+)
+import scivianna_example.medcoupling.split_item_example as split_item_example
+
+from pathlib import Path
+
+import panel_material_ui as pmui
+
+
+def make_demo(servable=False):
+    europe_panel = europe_example(None)
+    with open(Path(europe_grid.__file__).parent / "description.md", "r") as f:
+        europe_with_description = pmui.Row(
+            europe_panel.main_frame, 
+            pmui.Typography(f.read(), width=300)
+        )
+
+    medcoupling_panel = medcoupling_example(None)
+    with open(Path(split_item_example.__file__).parent / "description.md", "r") as f:
+        medcoupling_with_description = pmui.Row(
+            medcoupling_panel.main_frame, 
+            pmui.Typography(f.read(), width=300)
+        )
+    sidebars = [
+            europe_panel.side_bar,
+            medcoupling_panel.side_bar,
+        ]
+    
+    tabs = pmui.Tabs(
+        ("Europe example", europe_with_description),
+        ("Medcoupling example", medcoupling_with_description),
+    )
+    
+    def change_active(e):
+        for sidebar in sidebars:
+            sidebar.visible = (tabs.active == sidebars.index(sidebar))
+
+    tabs.param.watch(change_active, "active")
+    change_active(None)
+
+    page = pmui.Page(
+        main=[tabs],
+        sidebar=sidebars,
+        sidebar_variant="temporary",
+        sidebar_open=False,
+        title="Scivianna demonstrator"
+    )
+    
+    if servable:
+        page.servable()
+    else:
+        page.show()
+
+
+if __name__ == "__main__":
+    make_demo()

768636ee8a01c36d551ecc7a5a849853e84bee7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-18 07:05:37 +0000

    Add package DesmosKiller/3.2/desmoskiller-3.2.tar.gz

---
+++ packages/DesmosKiller/desmoskiller-3.2.tar.gz/desmoskiller-3.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="DesmosKiller",
+      version="3.2",
+      packages=["dkpkg"],
+      author="YusufA442",
+      author_email="yusuf365820@gmail.com",
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('readme.txt').read())

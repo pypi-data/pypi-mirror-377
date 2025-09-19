a7b53265f0641f4e504ec80273a3eb01a1a734e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:06:01 +0000

    Add package TexTOM/0.6.5/textom-0.6.5-py3-none-any.whl

---
+++ packages/TexTOM/textom-0.6.5-py3-none-any.whl/textom/input/background_subtraction.py
@@ -0,0 +1,8 @@
+# choose a mode among the following
+mode = 'simple' # 'polynomial' # 'chebyshev' # 'none'
+# not used parameters will be ignored
+order_polynom = 4
+order_chebyshev = 10
+# choose q-range where the baseline should be drawn (can be important for 'polynomial' & 'chebyshev')
+q_min=0 
+q_max=100

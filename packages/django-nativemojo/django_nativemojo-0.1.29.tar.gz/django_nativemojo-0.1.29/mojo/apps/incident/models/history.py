8e8b8c66cb05a6bc8e50764e47b8fb7a985fa177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:07:20 +0000

    Add package django-nativemojo/0.1.29/django_nativemojo-0.1.29.tar.gz

---
+++ packages/django-nativemojo/django_nativemojo-0.1.29.tar.gz/django_nativemojo-0.1.29/mojo/apps/incident/models/history.py
@@ -0,0 +1,36 @@
+from django.db import models
+from mojo.models import MojoModel
+
+class IncidentHistory(models.Model, MojoModel):
+    class Meta:
+        ordering = ['-created']
+
+    class RestMeta:
+        GRAPHS = {
+            "default": {
+                "extra": [
+                    ("get_state_display", "state_display"),
+                    ("get_priority_display", "priority_display"),
+                ],
+                "graphs": {
+                    "by": "basic",
+                    "to": "basic",
+                    "media": "basic"
+                }
+            },
+        }
+    parent = models.ForeignKey("incident.Incident", related_name="history", on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    group = models.ForeignKey("account.Group", blank=True, null=True, default=None, related_name="+", on_delete=models.CASCADE)
+
+    kind = models.CharField(max_length=80, blank=True, null=True, default=None, db_index=True)
+
+    to = models.ForeignKey("account.User", blank=True, null=True, default=None, related_name="+", on_delete=models.CASCADE)
+    by = models.ForeignKey("account.User", blank=True, null=True, default=None, related_name="+", on_delete=models.CASCADE)
+
+    state = models.IntegerField(default=0)
+    priority = models.IntegerField(default=0)
+
+    note = models.TextField(blank=True, null=True, default=None)
+    media = models.ForeignKey("fileman.File", related_name="+", null=True, default=None, on_delete=models.CASCADE)

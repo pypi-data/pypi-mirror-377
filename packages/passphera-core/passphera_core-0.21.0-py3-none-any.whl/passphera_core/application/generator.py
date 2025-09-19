92bf9157aa07366e24fd94a4b399642984216f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:07:19 +0000

    Add package passphera-core/0.21.0/passphera_core-0.21.0-py3-none-any.whl

---
+++ packages/passphera-core/passphera_core-0.21.0-py3-none-any.whl/passphera_core/application/generator.py
@@ -0,0 +1,54 @@
+from passphera_core.entities import Generator
+from passphera_core.interfaces import GeneratorRepository
+
+
+class GetGeneratorUseCase:
+    def __init__(self, generator_repository: GeneratorRepository):
+        self.generator_repository: GeneratorRepository = generator_repository
+
+    def __call__(self) -> Generator:
+        return self.generator_repository.get()
+
+
+class SetGeneratorPropertyUseCase:
+    def __init__(self, generator_repository: GeneratorRepository):
+        self.generator_repository: GeneratorRepository = generator_repository
+
+    def __call__(self, field: str, value: str) -> Generator:
+        generator_entity: Generator = self.generator_repository.get()
+        generator_entity.set_property(field, value)
+        self.generator_repository.update(generator_entity)
+        return generator_entity
+
+
+class ResetGeneratorPropertyUseCase:
+    def __init__(self, generator_repository: GeneratorRepository):
+        self.generator_repository: GeneratorRepository = generator_repository
+
+    def __call__(self, field: str) -> Generator:
+        generator_entity: Generator = self.generator_repository.get()
+        generator_entity.reset_property(field)
+        self.generator_repository.update(generator_entity)
+        return generator_entity
+
+
+class AddCharacterReplacementUseCase:
+    def __init__(self, generator_repository: GeneratorRepository):
+        self.generator_repository: GeneratorRepository = generator_repository
+
+    def __call__(self, character: str, replacement: str) -> Generator:
+        generator_entity: Generator = self.generator_repository.get()
+        generator_entity.replace_character(character, replacement)
+        self.generator_repository.update(generator_entity)
+        return generator_entity
+
+
+class ResetCharacterReplacementUseCase:
+    def __init__(self, generator_repository: GeneratorRepository,):
+        self.generator_repository: GeneratorRepository = generator_repository
+
+    def __call__(self, character: str) -> Generator:
+        generator_entity: Generator = self.generator_repository.get()
+        generator_entity.reset_character(character)
+        self.generator_repository.update(generator_entity)
+        return generator_entity

ed9216c1beecf6a7b63503a14f8b96a89cd66bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:07:22 +0000

    Add package backboard-sdk/1.0.0/backboard_sdk-1.0.0.tar.gz

---
+++ packages/backboard-sdk/backboard_sdk-1.0.0.tar.gz/backboard_sdk-1.0.0/backboard/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from backboard.api.assistants_api import AssistantsApi
+from backboard.api.documents_api import DocumentsApi
+from backboard.api.threads_api import ThreadsApi
+

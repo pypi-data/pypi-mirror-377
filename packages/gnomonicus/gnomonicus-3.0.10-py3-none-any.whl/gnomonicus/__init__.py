c1b113664de7f57e9c54e0f82250949d9deb4c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:06:06 +0000

    Add package gnomonicus/3.0.10/gnomonicus-3.0.10-py3-none-any.whl

---
+++ packages/gnomonicus/gnomonicus-3.0.10-py3-none-any.whl/gnomonicus/__init__.py
@@ -0,0 +1,34 @@
+"""gnomonicus is a library providing functions which pull together output VCF of the Lodestone TB pipeline
+    with a reference genome and a resistance catalogue, and utilise gumpy and
+    piezo to produce variants, mutations and an antibiogram.
+
+Provides a CLI script (bin/gnomonicus) which links these functions together to produce all outputs from the inputs.
+Makes the assumption that VCF files are named `<GUID>.vcf`
+
+Classes:
+    * MissingFieldException
+    * InvalidMutationException
+
+Functions:
+    * loadGenome
+    * loadGenomeAndGenes
+    * populateVariants
+    * populateMutations
+    * populateEffects
+    * assignMutationBools
+    * countNucleotideChanges
+    * saveJSON
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("gnomonicus")
+
+from .gnomonicus_lib import (
+    InvalidMutationException,  # noqa: F401
+    populateEffects,  # noqa: F401
+    get_minority_population_type,  # noqa: F401
+    populateMutations,  # noqa: F401
+    populateVariants,  # noqa: F401
+    saveJSON,  # noqa: F401
+)

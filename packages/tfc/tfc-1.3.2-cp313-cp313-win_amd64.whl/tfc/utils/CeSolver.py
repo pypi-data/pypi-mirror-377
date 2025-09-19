8c5874b9caec1e9f732f9682c723eca75378bafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:20 +0000

    Add package tfc/1.3.2/tfc-1.3.2-cp313-cp313-win_amd64.whl

---
+++ packages/tfc/tfc-1.3.2-cp313-cp313-win_amd64.whl/tfc/utils/CeSolver.py
@@ -0,0 +1,498 @@
+import sympy as sp
+from sympy import Expr
+from sympy.core.function import AppliedUndef
+from sympy.printing.pycode import PythonCodePrinter
+from sympy.simplify.simplify import nc_simplify
+from .tfc_types import ConstraintOperators, Exprs, Any, Literal, ConstraintOperator
+from .TFCUtils import TFCPrint
+
+
+class CeSolver:
+    """
+    Constrained expression solver.
+
+    This class solves constrained expressions for you.
+
+    Parameters
+    ----------
+    C : ConstraintOperators
+        This is a tuple or list constraint operators. Each element in the
+        iterable should be a Python function that takes in a sympy function,
+        such as `g(x)`, and outputs that function evaluated in the same way
+        as the function in the constraint. For example, if the constraint was
+        u(3) = 0, then the assocaited constraint operator would be:
+        `lambda u: = u.subs(x,3)`.
+    kappa : Exprs
+        This is a tuple or list of the kappa portion of each constraint. For the
+        example u(3) = 0, the kappa portion is simply 0. Note, the standard
+        Python 0 should not be used, but rather, sympy.re(0).
+    s : Exprs
+        This is a tuple or list of the support functions. These should be given
+        in terms of sympy symbols and constants. For example, if we wanted to
+        use the constant function x = 1 as a support function, then we would
+        use sympy.re(1) in this iterable.
+    g : AppliedUndef| Any
+        This is the free function used in the constrained expression. For example,
+        `g(x)`.
+
+    References
+    ----------
+    The algorithm used here is given at 26:13 of this video: https://www.youtube.com/watch?v=uisOZVBHA2U&t=1573s
+
+    Examples
+    --------
+    Consider the constraints `u(0) = 2` and `u_x(2) = 1` where `u_x` is the derivative of `u(x)` with respect to `x`.
+    Moreover, suppose we want to use `g(x)` as the free function.
+
+    import sympy as sp
+    from tfc.utils import CeSolver
+
+    x = sp.Symbol("x")
+    y = sp.Symbol("y")
+    u = sp.Function("u")
+    g = sp.Function("g")
+
+    C = [lambda u: u.subs(x,0), lambda u: sp.diff(u,x).subs(x,2)]
+    K = [sp.re(2), sp.re(1)]
+    s = [sp.re(1), x]
+
+    cs = CeSolver(C,K,s, g(x))
+    ce = cs.ce
+
+    In the above code example, `ce` is the constrained expression that satisfies these constraints.
+    """
+
+    def __init__(self, C: ConstraintOperators, kappa: Exprs, s: Exprs, g: AppliedUndef | Any):
+        self._C = C
+        self._K = kappa
+        self._s = s
+        self._g = g
+        self._ce_stale: bool = True
+        self._S_stale: bool = True
+        self._alpha_stale: bool = True
+        self._phi_stale: bool = True
+        self._rho_stale: bool = True
+
+    @property
+    def print_type(self) -> Literal["tfc", "pretty", "latex", "str"]:
+        return self._print_type
+
+    @print_type.setter
+    def print_type(self, print_type: Literal["tfc", "pretty", "latex", "str"]) -> None:
+        from sympy import init_printing
+
+        self._print_type = print_type
+        if self._print_type == "tfc":
+            tfc_printer = TfcPrinter()
+            init_printing(pretty_print=True, pretty_printer=tfc_printer.doprint)
+        elif self._print_type == "str":
+            init_printing(pretty_print=False)
+        elif self._print_type == "pretty":
+            init_printing()
+        elif self._print_type == "latex":
+            from sympy import latex
+
+            init_printing(pretty_print=True, pretty_printer=latex)
+        else:
+            TFCPrint.Error(
+                f'print_type was specified as {print_type} but only "tfc", "pretty", "latex", and "str" are accepted.'
+            )
+
+    @property
+    def ce(self) -> Any:
+        """
+        Constrained expression.
+
+        Returns
+        -------
+        Any
+            Constrained expression.
+        """
+        if self._ce_stale:
+            self._solveCe()
+            self._ce_stale = False
+        return self._ce
+
+    @ce.setter
+    def ce(self, ce: Any) -> None:
+        """
+        Sets the constrained expression to the user-supplied value.
+
+        Parameters
+        ----------
+        ce: Any
+            Sympy representation of the constrained expression.
+        """
+        self._ce_stale = False
+        self._ce = ce
+
+    @property
+    def phi(self) -> Any:
+        """
+        Switching functions.
+
+        Returns
+        -------
+        Any
+            Switching functions.
+        """
+        if self._phi_stale:
+            s_vec = sp.Matrix([s for s in self._s])
+            self._phi = s_vec.transpose() * self.alpha
+            self._phi_stale = False
+        return self._phi
+
+    @phi.setter
+    def phi(self, phi: Any):
+        """
+        Set the switching functions.
+
+        Parameters
+        ----------
+        phi : Any
+            The switching functions.
+        """
+        self._phi = phi
+        self._phi_stale = False
+
+    @property
+    def alpha(self) -> sp.Matrix:
+        """
+        Alpha matrix (inverse of the support matrix)
+
+        Returns
+        sp.Matrix
+            alpha matrix. The elements are on the field over which
+            the constrained expression is defined.
+        """
+        if self._alpha_stale:
+            self._alpha = self.S.inv()
+            self._alpha_stale = False
+        return self._alpha
+
+    @property
+    def S(self) -> sp.Matrix:
+        """
+        Support matrix.
+
+        Returns
+        sp.Matrix
+            Support matrix. The elements are on the field over which
+            the constrained expression is defined.
+        """
+
+        def _applyC(c: ConstraintOperator, s) -> Any:
+            """
+            Apply the constraint operator to the switching function.
+
+            Parameters
+            ----------
+            c : ConstraintOperator
+                Constraint operator.
+            s : Expr
+                Switching function.
+
+            Returns
+            -------
+            Any
+                c(s), which is a number on the field over which the
+                constrained expression is defined.
+            """
+
+            dark = c(s)
+            if isinstance(dark, sp.Matrix) or isinstance(dark, sp.MatMul):
+                dark = nc_simplify(dark)[0]
+            return dark
+
+        if self._S_stale:
+            self._S = sp.Matrix([[_applyC(c, s) for s in self._s] for c in self._C])
+            self._S_stale = False
+        return self._S
+
+    @property
+    def rho(self) -> Any:
+        """
+        Projection functionals.
+
+        Returns
+        -------
+        Any
+            Projection functionals.
+        """
+        if self._rho_stale:
+            self._rho = sp.Matrix(
+                [sp.Add(kappa, -self._C[k](self._g)) for k, kappa in enumerate(self._K)]
+            )
+        return self._rho
+
+    @property
+    def s(self) -> Exprs:
+        """
+        Switching functions.
+
+        Returns
+        -------
+        Exprs
+            Support functions.
+        """
+        return self._s
+
+    @s.setter
+    def s(self, s: Exprs) -> None:
+        """
+        Set the support functions.
+
+        Parameters
+        ----------
+        s : Exprs
+            This is a tuple or list of the support functions. These should be given
+            in terms of sympy symbols and constants. For example, if we wanted to
+            use the constant function x = 1 as a support function, then we would
+            use sympy.re(1) in this iterable.
+        """
+        self._s = s
+        self._S_stale = True
+        self._alpha_stale = True
+        self._phi_stale = True
+        self._ce_stale = True
+
+    @property
+    def kappa(self):
+        """
+        Kappa values.
+
+        Returns
+        -------
+        Exprs
+            Kappa values.
+        """
+        return self._K
+
+    @kappa.setter
+    def kappa(self, kappa: Exprs) -> None:
+        """
+        Set the kappa values.
+
+        Parameters
+        -------
+        kappa : Exprs
+            This is a tuple or list of the kappa portion of each constraint. For the
+            example u(3) = 0, the kappa portion is simply 0. Note, the standard
+            Python 0 should not be used, but rather, sympy.re(0).
+        """
+        self._K = kappa
+        self._rho_stale = True
+        self._ce_stale = True
+
+    @property
+    def C(self) -> ConstraintOperators:
+        """
+        Constraint operators.
+
+        Returns
+        -------
+        ConstraintOperators
+            Constraint operators.
+        """
+        return self._C
+
+    @C.setter
+    def C(self, C: ConstraintOperators) -> None:
+        """
+        Parameters
+        ----------
+        C : ConstraintOperators
+            This is a tuple or list constraint operators. Each element in the
+            iterable should be a Python function that takes in a sympy function,
+            such as `g(x)`, and outputs that function evaluated in the same way
+            as the function in the constraint. For example, if the constraint was
+            u(3) = 0, then the assocaited constraint operator would be:
+            `lambda u: = u.subs(x,3)`.
+        """
+        self._C = C
+        self._S_stale = True
+        self._alpha_stale = True
+        self._phi_stale = True
+        self._rho_stale = True
+        self._ce_stale = True
+
+    @property
+    def g(self) -> AppliedUndef | Any:
+        """
+        Free function.
+
+        Returns
+        -------
+        AppliedUndef | Any
+            Free function.
+        """
+        return self._g
+
+    @g.setter
+    def g(self, g: AppliedUndef | Any) -> None:
+        """
+        Set the free function.
+
+        Parameters
+        ----------
+        g : AppliedUndef | Any
+            This is the free function used in the constrained expression. For example,
+            `g(x)`.
+        """
+        self._g = g
+        self._ce_stale = True
+        self._rho_stale = True
+
+    def _solveCe(self) -> None:
+        """
+        Solves the constrained expression and stores it in self.ce
+        """
+        self._ce = sp.Add(self.g, (self.phi * self.rho)[0])
+
+    def checkCe(self) -> bool:
+        """
+        Checks the constrained expression stored in the class against the stored constraints.
+
+        Return
+        ------
+        bool:
+            Returns True if the constraint expression satisfies the constraints and false otherwise.
+        """
+        checks = [sp.simplify(c(self.ce)) == sp.simplify(k) for c, k in zip(self._C, self._K)]
+        ret = True
+        for k, check in enumerate(checks):
+            if not check:
+                TFCPrint.Warning(
+                    f"Expected result of constraint {k+1} to be {self._K[k]}, but got {self._C[k](self.ce)}."
+                )
+                ret = False
+        return ret
+
+
+class TfcPrinter(PythonCodePrinter):
+    def __init__(self, settings=None):
+        # Switch math to numpy
+        for k, v in self._kf.items():
+            if "math" in v:
+                self._kf[k] = v.replace("math", "np")
+        for k, v in self._kc.items():
+            if "math" in v:
+                self._kc[k] = v.replace("math", "np")
+
+        super().__init__(settings=settings)
+
+    def _hprint_Pow(self, expr: Expr, rational: bool = False, sqrt: str = "np.sqrt"):
+        """
+        Override _hprint_Pow to use np.sqrt rather than math.sqrt
+
+        Parameters
+        ----------
+        expr : Expr
+            Expression to print.
+        rational : bool
+            Whether the expression is rational.
+        sqrt : str
+            String to print for the sqrt.
+
+        Returns
+        -------
+        str
+            String to print.
+        """
+
+        return super()._hprint_Pow(expr, rational=rational, sqrt=sqrt)
+
+    def _print_Symbol(self, expr: Expr) -> str:
+        """
+        Add in Symbol printing function.
+
+        Parameters
+        ----------
+        expr : Expr
+            Symbol to print.
+
+        Returns
+        -------
+        str
+            String to print.
+        """
+        return self._print(str(expr))
+
+    def _print_Function(self, expr: Expr) -> str:
+        """
+        Add in Function printing function.
+
+        Parameters
+        ----------
+        expr : Expr
+            Function to print.
+
+        Returns
+        -------
+        str
+            String to print.
+        """
+        return self._print(str(expr))
+
+    def _print_Subs(self, subs: Expr) -> str:
+        """
+        Substitute values.
+
+        Parameters
+        ----------
+        subs : Expr
+            Substitution(s).
+
+        Returns
+        -------
+        str
+            String to print.
+        """
+        expr, old, new = subs.args
+        expr = self._print(expr)
+        for k in range(len(old)):
+            expr = expr.replace(self._print(old[k]), self._print(new[k]))
+        return expr
+
+    def _print_Derivative(self, expr: Expr) -> str:
+        """
+        Add in derivative printing function that uses egrad.
+
+        Parameters
+        ----------
+        expr : Expr
+            Expression to print.
+
+        Returns
+        -------
+        str
+            String to print.
+        """
+        # Function will be the full function, e.g., g(x,y)
+        # vars will be the derivative symbol and order.
+        # For example, dg/dx will have vars [(x,1)]
+        function, *vars = expr.args
+
+        # Find position for each derivative
+        function_vars = function.args
+        position_vars = []
+        for var in vars:
+            ind = function_vars.index(var[0])
+            position_vars.append((ind, var[1]))
+
+        # If you want the printer to work correctly for nested
+        # expressions then use self._print() instead of str() or latex().
+        # See the example of nested modulo below in the custom printing
+        # method section.
+        name = function.func.__name__
+        parenthesis_counter = 0
+        ret = ""
+        for pv in position_vars:
+            for _ in range(pv[1]):
+                ret += self._print("egrad(" + name + "," + str(pv[0]))
+                parenthesis_counter += 1
+        for _ in range(parenthesis_counter):
+            ret += self._print(")")
+        ret += "(" + "".join(self._print(i[0]) for i in vars) + ")"
+
+        return ret
---
+++ packages/tfc/tfc-1.3.2-cp313-cp313-win_amd64.whl/tfc/utils/MayaviMakePlot.py
@@ -0,0 +1,415 @@
+import numpy as np
+import numpy.typing as npt
+import mayavi
+from mayavi import mlab
+from matplotlib import colors as mcolors
+from .tfc_types import Path, Ge, Le, Annotated, Literal
+from typing import Optional, Any, Generator, Callable
+from .TFCUtils import TFCPrint
+
+Color = str | tuple[float | float | float | float] | npt.NDArray[np.float64]
+TFCPrint()
+
+
+class MakePlot:
+    """MakePlot class for Mayavi."""
+
+    @staticmethod
+    def _str_to_rgb(color: str) -> tuple[float, float, float]:
+        """Call matplotlib's colorConverter.to_rgb on input string.
+
+        Parameters
+        ----------
+        color : str
+            Color string
+
+        Returns
+        -------
+        color_rgb : tuple[float, float, float]
+            3-tuple of the RGB for the color
+        """
+        return mcolors.colorConverter.to_rgb(color)
+
+    @staticmethod
+    def _str_to_rgba(color, alpha: Optional[float] = None) -> tuple[float, float, float, float]:
+        """Call matplotlib's colorConverter.to_rgba on input string.
+
+        Parameters
+        ----------
+        color : str
+            Color string
+
+        alpha : float
+            Alpha value to use in the return RGBA. If None, then the returned alpha = 1. (Default value = None)
+
+        Returns
+        -------
+        color_rgba : tuple[float, float, float, float]
+            4-tuple of the RGB for the color
+        """
+        return mcolors.colorConverter.to_rgba(color, alpha=alpha)
+
+    @staticmethod
+    def _ProcessKwargs(**kwargs: Any) -> dict[str, Any]:
+        """This function effectively extends common mlab keywords.
+
+        Parameters
+        ----------
+        **kwargs : Any
+            keyword arguments
+
+        Returns
+        -------
+        kwargs : dict[str, any]
+            Same as input keyword arguments but color has been transformed to an RGB if it was a string.
+        """
+        # Process color argument if it exists
+        if "color" in kwargs:
+            if isinstance(kwargs["color"], str):
+                kwargs["color"] = MakePlot._str_to_rgb(kwargs["color"])
+
+        return kwargs
+
+    @staticmethod
+    def ColormapGradient(c1: Color, c2: Color) -> npt.NDArray[np.uint8]:
+        """Returns a Mayavi LUT for a gradient that linearly transforms from c1 to c2.
+
+        Parameters
+        ----------
+        c1 : Color
+            Input color as a string or 4-element RGBA list.
+
+        c2 : Color
+            Input color as a string or 4-element RGBA list.
+
+        Returns
+        -------
+        lut : npt.NDArray[np.uint8]
+            Mayavi LUT for a gradient that linearly transforms from c1 to c2.
+        """
+        if isinstance(c1, str):
+            c1 = np.array(MakePlot._str_to_rgba(c1))
+        if isinstance(c2, str):
+            c2 = np.array(MakePlot._str_to_rgba(c2))
+        if not isinstance(c1, np.ndarray):
+            c1 = np.array(c1)
+        if not isinstance(c2, np.ndarray):
+            c2 = np.array(c2)
+        c1 = c1 * 255
+        c2 = c2 * 255
+        return np.linspace(c1, c2, 256, dtype=np.uint8)
+
+    @staticmethod
+    def SetColormapGradient(obj: Any, c1: Color, c2: Color):
+        """
+        Applies a linear, gradient colormap to the object. The colors in the gradient are c1 and c2.
+
+        Parameters
+        ----------
+        obj : Any
+            mlab object to apply the colormap gradient to.
+        c1 : Color
+            Input color as a string or 4-element RGBA list.
+        c2 : Color
+            Input color as a string or 4-element RGBA list.
+        """
+        obj.module_manager.scalar_lut_manager.lut.table = MakePlot.ColormapGradient(c1, c2)
+
+    def __init__(self):
+        """
+        This function initializes the plot.
+        """
+
+        self.fig = mlab.figure(bgcolor=(1.0, 1.0, 1.0))
+        self.scene = self.fig.scene
+
+    def FullScreen(self):
+        """
+        This function makes the plot fullscreen.
+        """
+
+        # Get screensize
+        import tkinter as tk
+
+        root = tk.Tk()
+        width = root.winfo_screenwidth()
+        height = root.winfo_screenheight()
+
+        # Get dpi and set new figsize
+        dpi = float(self.fig.get_dpi())
+        mlab.figure(self.fig, size=(width / dpi, height / dpi))
+
+    def PartScreen(self, width: float, height: float):
+        """This function makes the plot width x height inches.
+
+        Parameters
+        ----------
+        width : float
+            Width of the plot in inches.
+
+        height : float
+            Height of the plot in inches.
+        """
+        mlab.figure(self.fig, size=(width, height))
+
+    IntRange = Annotated[int, Ge(10), Le(100000)]
+
+    def animate(
+        self,
+        animFunc: Callable[[], Generator[None, None, None]],
+        outDir: Path = "MyMovie",
+        fileName: str = "images",
+        save: bool = True,
+        delay: IntRange = 10,
+    ) -> Optional[mayavi.tools.animator.Animator]:
+        """
+
+        Parameters
+        ----------
+        animFunc : Callable[[], Generator[None, None, None]]
+            Function that modifies what is displayed on the plot
+        outDir : Path, optional
+            Directory to save frames in: only used if save = True. (Default value = "MyMovie")
+        fileName : str, optional
+            Filename for the frames: only used if save = True. (Default value = "images")
+        save : bool, optional
+            Controls whether the function saves the frames of the animation or not. (Default value = True)
+        delay: IntRange in [10,100000], optional
+            Controls the delay used by mlab.animate. (Default value = 10)
+
+        Returns
+        -------
+        animator : Optional[mayavi.tools.animator.Animator]
+            Returns an animator object only if save = False. Otherwise, there is no return value.
+        """
+        if save:
+            import os
+
+            if not os.path.exists(outDir):
+                os.mkdir(outDir)
+            k = 0
+
+            iterable = animFunc()
+            while next(iterable, -1) != -1:
+                fileNameFull = "{}{:0>6d}".format(fileName, k)
+                self.save(os.path.join(outDir, fileNameFull), fileType="png")
+                k += 1
+
+            print(
+                "ffmpeg -r 60 -i ./{0}/{1}%06d.png -c:v libx264 -profile:v high -pix_fmt yuv420p ./{0}/MyMovie.mp4".format(
+                    outDir, fileName
+                )
+            )
+        else:
+            return mlab.animate(func=animFunc, delay=delay)()
+
+    def save(
+        self,
+        fileName: Path,
+        fileType: Literal[
+            "png",
+            "jpg",
+            "bmp",
+            "tiff",
+            "ps",
+            "eps",
+            "pdf",
+            "rib",
+            "oogl",
+            "iv",
+            "wrl",
+            "vrml",
+            "obj",
+            "x3d",
+            "pov",
+            None,
+        ] = None,
+    ):
+        """
+        This function saves the figure.
+
+        Parameters
+        ----------
+        fileName : Path
+            Filename where the figure should be saved.
+        fileType : Literal["png", "jpg", "bmp", "tiff", "ps", "eps", "pdf", "rib", "oogl", "iv", "wrl", "vrml", "obj", "x3d", "pov", None], optional
+            File suffix to use. If None, then the suffix will be inferred from the file name. (Default value = None)
+        """
+        if not fileType:
+            from pathlib import Path
+
+            suffix = Path(fileName).suffix[1:]
+            if suffix in [
+                "png",
+                "jpg",
+                "bmp",
+                "tiff",
+                "ps",
+                "eps",
+                "pdf",
+                "rib",
+                "oogl",
+                "iv",
+                "wrl",
+                "vrml",
+                "obj",
+                "x3d",
+                "pov",
+            ]:
+                fileType = suffix
+            else:
+                fileType = "pdf"
+                TFCPrint.Warning(
+                    f"Warning, file type could not be inferred from {fileName}. The file type has been set to pdf."
+                )
+                fileName += "." + fileType
+        else:
+            fileName += "." + fileType
+        mlab.savefig(fileName, figure=self.fig)
+
+    def show(self):
+        """Re-draw the class's figure."""
+        return mlab.draw(figure=self.fig)
+
+    @property
+    def show_axes(self):
+        """
+        Adds axes indicator.
+        """
+        return self.scene.show_axes
+
+    @show_axes.setter
+    def show_axes(self, val: bool):
+        """
+        Set axes indicator.
+
+        Parameters
+        ----------
+        val : bool
+            True turns on the axes indicator and False turns off the axes indicator.
+        """
+        self.scene.show_axes = val
+
+    def view(self, *args: Any, **kwargs: Any):
+        """
+        Call mlab's view on the class's figure.
+
+        Parameters
+        ----------
+        *args : Any
+            args passed on to view
+        **kwargs : Any
+            kwargs passed on to view
+        """
+        return mlab.view(*args, figure=self.fig, **kwargs)
+
+    def points3d(self, *args: Any, **kwargs: Any) -> mayavi.modules.glyph.Glyph:
+        """
+        Call mlab's points3d on the class's figure.
+
+        Parameters
+        ----------
+        *args : Any
+            args passed on to points3d
+        **kwargs : Any
+            kwargs passed on to points3d after being processed by _ProcessKwargs.
+
+        Returns:
+        --------
+        points3d : mayavi.modules.glyph.Glyph
+            Glyphs corresponding to the points.
+        """
+        kwargs = MakePlot._ProcessKwargs(**kwargs)
+        return mlab.points3d(*args, figure=self.fig, **kwargs)
+
+    def plot3d(self, *args: Any, **kwargs: Any) -> mayavi.modules.surface.Surface:
+        """Call mlab's plot3d on the class's figure.
+
+        Parameters
+        ----------
+        *args : Any
+            args passed on to plot3d
+        **kwargs : Any
+            kwargs passed on to plot3d after being processed by _ProcessKwargs.
+
+        Returns:
+        --------
+        surf : mayavi.modules.surface.Surface
+            Line as a Mayavi surface.
+        """
+        kwargs = MakePlot._ProcessKwargs(**kwargs)
+        return mlab.plot3d(*args, figure=self.fig, **kwargs)
+
+    def surf(self, *args: Any, **kwargs: Any) -> mayavi.modules.surface.Surface:
+        """Call mlab's surf on the class's figure.
+
+        Parameters
+        ----------
+        *args : Any
+            args passed on to surf
+        **kwargs : Any
+            kwargs passed on to surf after being processed by _ProcessKwargs.
+
+        Returns:
+        --------
+        surf : mayavi.modules.surface.Surface
+            Surface.
+        """
+        kwargs = MakePlot._ProcessKwargs(**kwargs)
+        return mlab.surf(*args, figure=self.fig, **kwargs)
+
+    def quiver3d(self, *args: Any, **kwargs: Any) -> mayavi.modules.vectors.Vectors:
+        """
+        Call mlab's quiver3d on the class's figure.
+
+        Parameters
+        ----------
+        *args : Any
+            args passed on to quiver3d
+        **kwargs : Any
+            kwargs passed on to quiver3d after being processed by _ProcessKwargs.
+
+        Returns:
+        --------
+        vectors : mayavi.modules.vectors.Vectors
+            Vectors associated with the quiver3d plot.
+        """
+        kwargs = MakePlot._ProcessKwargs(**kwargs)
+        return mlab.quiver3d(*args, figure=self.fig, **kwargs)
+
+    def mesh(self, *args: Any, **kwargs: Any) -> mayavi.modules.surface.Surface:
+        """Call mlab's mesh on the class's figure.
+
+        Parameters
+        ----------
+        *args : Any
+            args passed on to mesh
+        **kwargs : Any
+            kwargs passed on to mesh after being processed by _ProcessKwargs.
+
+        Returns:
+        --------
+        mesh : mayavi.modules.surface.Surface
+            Mesh as a mayavi surface.
+        """
+        kwargs = MakePlot._ProcessKwargs(**kwargs)
+        return mlab.mesh(*args, figure=self.fig, **kwargs)
+
+    def contour3d(self, *args: Any, **kwargs: Any) -> mayavi.modules.iso_surface.IsoSurface:
+        """
+        Call mlab's contour3d on the class's figure.
+
+        Parameters
+        ----------
+        *args : Any
+            args passed on to contour3d
+        **kwargs : Any
+            kwargs passed on to mesh after being processed by _ProcessKwargs.
+
+        Returns:
+        --------
+        contour : mayavi.modules.iso_surface.IsoSurface
+            Coutour.
+        """
+        kwargs = MakePlot._ProcessKwargs(**kwargs)
+        return mlab.contour3d(*args, figure=self.fig, **kwargs)

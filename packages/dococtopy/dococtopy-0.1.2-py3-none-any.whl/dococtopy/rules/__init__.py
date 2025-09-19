062b7e39edf9dc3c47391f383fb3adb698e8f867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:18:21 +0000

    Add package dococtopy/0.1.2/dococtopy-0.1.2-py3-none-any.whl

---
+++ packages/dococtopy/dococtopy-0.1.2-py3-none-any.whl/dococtopy/rules/__init__.py
@@ -0,0 +1,21 @@
+"""
+Rules package for compliance checking.
+
+This package contains all compliance rules for DocOctopy.
+Rules are organized by type and severity level.
+
+Available rule types:
+- Basic compliance rules
+- Google style validation rules
+- Advanced Google style rules
+
+Planned rule types:
+- Coverage threshold rules
+- Custom rule framework
+- Language-specific rules
+"""
+
+# TODO: Add coverage threshold rules
+# TODO: Add custom rule framework
+# TODO: Add language-specific rules
+# TODO: Add rule testing framework

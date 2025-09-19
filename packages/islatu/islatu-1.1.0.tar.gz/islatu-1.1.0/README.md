4d77fcb630d1ef74a2cf82bf32a1863debbb760e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:07:23 +0000

    Add package islatu/1.1.0/islatu-1.1.0.tar.gz

---
+++ packages/islatu/islatu-1.1.0.tar.gz/islatu-1.1.0/README.md
@@ -0,0 +1,45 @@
+# islatu
+
+X-ray reflectometry reduction from Diamond Light Source
+
+[![codecov](https://codecov.io/gh/RBrearton/islatu/branch/master/graph/badge.svg?token=FGIV0MVHS8)](https://codecov.io/gh/RBrearton/islatu)
+[![Actions Status](https://github.com/RBrearton/islatu/workflows/pytest/badge.svg)](https://github.com/pytest/islatu/actions)
+[![Documentation Status](https://readthedocs.org/projects/islatu/badge/?version=latest)](https://islatu.readthedocs.io/en/latest/?badge=latest)
+
+### Install
+
+This package can be easily installed using `pip install islatu`.
+
+### Installation from source
+
+To install in a fresh environment, first ensure that pip is available. For
+example, using conda:
+
+```
+conda create --name islatu
+conda activate islatu
+conda install pip
+```
+
+Assuming that pip is available, installation of the library from source can be
+done by cloning this repository. Navigate to its directory and use pip to
+install this package and its dependencies as follows:
+
+```
+python -m pip install . -r requirements.txt
+```
+
+Make sure that your installation is functioning by running `pytest`.
+
+### Documentation
+
+The documentation for this package can be found at https://islatu.readthedocs.io/en/latest/
+
+An example of a typical data reduction workflow can be found at https://islatu.readthedocs.io/en/latest/i07_reflectivity.html
+
+To build the documentation, make sure you have sphinx installed on your system.
+Go to the docs directory and run
+
+```
+make html
+```
---
+++ packages/islatu/islatu-1.1.0.tar.gz/islatu-1.1.0/setup.py
@@ -0,0 +1,64 @@
+import io
+from glob import glob
+from os.path import basename, dirname, join, splitext, abspath
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+THIS_DIRECTORY = abspath(dirname(__file__))
+with io.open(join(THIS_DIRECTORY, 'README.md')) as f:
+    LONG_DESCRIPTION = f.read()
+
+REQUIREMENTS = [
+    "wheel",
+    "numpy",
+    "scipy",
+    "coverage",
+    "pandas",
+    "pyyaml",
+    "nexusformat",
+    "pytest",
+    "pytest-lazy-fixture",
+    "nbsphinx",
+    "jupyter-sphinx",
+    "jupyterlab",
+    "ipywidgets",
+    "pytest-cov",
+]
+
+setup(
+    name='islatu',
+    version='1.1.0',
+    license='MIT',
+    description='A package for the reduction of reflectometry data.',
+    author='Philip Mousley',
+    author_email='philip.mousley@diamond.ac.uk',
+    long_description=LONG_DESCRIPTION,
+    long_decription_content_type='text/markdown',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    setup_requires=REQUIREMENTS,
+    install_requires=REQUIREMENTS
+)

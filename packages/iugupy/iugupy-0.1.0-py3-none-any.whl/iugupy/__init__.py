3998fd822ff95236077b80d565d3b799c4c49125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:05:59 +0000

    Add package iugupy/0.1.0/iugupy-0.1.0-py3-none-any.whl

---
+++ packages/iugupy/iugupy-0.1.0-py3-none-any.whl/iugupy/__init__.py
@@ -0,0 +1,5 @@
+from .config import IuguConfig
+from .client import IuguClient
+from .errors import IuguAPIError, IuguValidationError
+
+__all__ = ["IuguConfig", "IuguClient", "IuguAPIError", "IuguValidationError"]

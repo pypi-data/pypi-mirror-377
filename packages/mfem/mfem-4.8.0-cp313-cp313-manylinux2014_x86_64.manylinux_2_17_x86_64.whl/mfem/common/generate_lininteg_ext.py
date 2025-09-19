2d7e85694056644c3e3bbb1eda71a27c846e6b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:49 +0000

    Add package mfem/4.8.0/mfem-4.8.0-cp313-cp313-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/mfem/mfem-4.8.0-cp313-cp313-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/mfem/common/generate_lininteg_ext.py
@@ -0,0 +1,49 @@
+file = "../_ser/lininteg.py"
+
+out = ["namespace mfem {"]
+fid = open(file, 'r')
+for line in fid.readlines():
+    if line.startswith("class"):
+        cname = (line.split(' ')[1]).split('(')[0]
+
+    if line.startswith("    def __init__"):
+        pp = ""
+        if line.find("*args") != -1:
+            pp = "    self._coeff = args"
+        elif line.find("self, seed_=0") != -1:
+            pp = "    self._coeff = QG"
+        elif line.find("self, vqfc, ir") != -1:
+            pp = "    self._coeff = (vqfc, ir)"
+        elif line.find("self, qfc, ir") != -1:
+            pp = "    self._coeff = (qfc, ir)"
+        elif line.find(", QG") != -1:
+            pp = "    self._coeff = QG"
+        elif line.find(", QF)") != -1:
+            pp = "    self._coeff = QF"
+        elif line.find(", F)") != -1:
+            pp = "    self._coeff = F"
+        elif line.find(", f)") != -1:
+            pp = "    self._coeff = f"
+        elif line.find(", f, s=1.0, ir=None)") != -1:
+            pp = "    self._coeff = (f, ir)"
+        elif line.find(", uD_, lambda_, mu_, alpha_, kappa_)") != -1:
+            pp = "    self._coeff = uD_"
+        elif line.find("(self)") != -1:
+            pass
+        elif line.find("(self, ir=None)") != -1:
+            pass
+        else:
+            print(cname)
+            print(line)
+            assert False, "No recipt for this pattern "
+        if pp != "":
+            out.append("%pythonappend " + cname + "::" + cname + " %{")
+            out.append(pp)
+            out.append("%}")
+fid.close()
+out.append("}")
+
+fid = open("lininteg_ext.i", "w")
+fid.write("\n".join(out))
+fid.close()
+

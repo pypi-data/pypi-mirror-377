05494432f6031e86fe6683b69468a59a7b63a7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:05:50 +0000

    Add package uipath-langchain/0.0.131/uipath_langchain-0.0.131-py3-none-any.whl

---
+++ packages/uipath-langchain/uipath_langchain-0.0.131-py3-none-any.whl/uipath_langchain/tracers/__init__.py
@@ -0,0 +1,7 @@
+from ._instrument_traceable import _instrument_traceable_attributes
+from .AsyncUiPathTracer import AsyncUiPathTracer
+
+__all__ = [
+    "AsyncUiPathTracer",
+    "_instrument_traceable_attributes",
+]

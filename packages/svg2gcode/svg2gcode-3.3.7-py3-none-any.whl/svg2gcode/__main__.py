67c9ba2dcba087077bc1f052e501c6c1040de23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:07:49 +0000

    Add package svg2gcode/3.3.7/svg2gcode-3.3.7-py3-none-any.whl

---
+++ packages/svg2gcode/svg2gcode-3.3.7-py3-none-any.whl/svg2gcode/__main__.py
@@ -0,0 +1,210 @@
+"""
+svg2gcode: convert an image to gcode.
+"""
+
+import os
+import sys
+import re
+try:
+    import tomllib
+except ImportError:
+    print("Import error: cannot import 'tomllib', python version must be 3.11 or higher!")
+    sys.exit(1)
+
+import argparse
+
+from svg2gcode.svg_to_gcode.svg_parser import parse_file
+from svg2gcode.svg_to_gcode.compiler import Compiler, interfaces
+
+from svg2gcode import __version__
+from svg2gcode.svg_to_gcode import css_color
+
+config_file = os.path.expanduser('~/.config/svg2gcode.toml')
+
+# Notes:
+# - drawing objects (when using Inkscape for example) must be converted to a 'path' to be translated in a gcode sequence
+# - images objects should NOT be converted a 'path'
+
+def svg2gcode(args) -> int:
+    """
+    svg2gcode: convert svg to gcode
+    """
+
+    def init_compiler(args):
+        # Instantiate a compiler, specifying the interface type and the speed at which the tool should move (for both image drawings and laser cutting).
+        # For line drawings 'pass_depth' controls how far down the tool moves after every pass. Set it to 0 if your machine does not support Z axis movement.
+        return Compiler(interfaces.Gcode, params={'laser_power':args.cuttingpower,'movement_speed':args.cuttingspeed, 'pixel_size':args.pixelsize,
+               'maximum_image_laser_power':args.imagepower, 'image_movement_speed':args.imagespeed, 'fan':args.fan,'rapid_move':args.rapidmove,
+               'showimage':args.showimage, 'x_axis_maximum_travel':args.xmaxtravel,'y_axis_maximum_travel':args.ymaxtravel, 'image_noise':args.noise,
+               'pass_depth':args.pass_depth, 'laser_mode':"constant" if args.constantburn else "dynamic", 'splitfile':args.splitfile, 'pathcut':args.pathcut,
+               'nofill':args.nofill, 'image_poweroffset':args.poweroffset, 'image_overscan':args.overscan, 'image_showoverscan':args.showoverscan,
+               'color_coded': args.color_coded,})
+
+    compiler = init_compiler(args)
+
+    if args.color_coded != "":
+        # check color_coded argument validity (2)
+        pathignore, pathcut, pathengrave = compiler.color_coded_paths(True)
+
+        # a path color should be in one set only
+        for i in pathignore:
+            if i in pathcut or i in pathengrave:
+                print(f"argument error: '--color_coded \"{args.color_coded}\"' has a path color '{i}' set in multiple categories (cut|engrave|ignore) .")
+                return 1
+        for i in pathcut:
+            if i in pathengrave:
+                print(f"argument error: '--color_coded \"{args.color_coded}\"' has a path color '{i}' set in multiple categories (cut|engrave|ignore) .")
+                return 1
+
+    # emit gcode for svg
+    if args.selfcenter:
+        print("pass 1")
+    compiler.compile_to_file(args.gcode, args.svg, parse_file(args.svg, delta_origin=args.origin, scale_factor=args.scale, rotate_deg=args.rotate), passes=args.passes)
+
+    if args.selfcenter:
+        # remove output files(s)
+        filename = args.gcode
+
+        if os.path.isfile(filename):
+            os.remove(filename)
+        image_filename = filename.rsplit('.',1)[0] + "_images." + filename.rsplit('.',1)[1]
+        if os.path.isfile(image_filename):
+            os.remove(image_filename)
+
+        center = compiler.boundingbox.center()
+        center = (round(center[0], compiler.precision), round(center[1], compiler.precision))
+        print(f"center: {center}")
+        center = (-center[0], -center[1])
+
+        # init compiler again
+        compiler = init_compiler(args)
+
+        # now run with origin set to center
+        # emit gcode for svg
+        print("pass 2")
+        compiler.compile_to_file(args.gcode, args.svg, parse_file(args.svg, delta_origin=center, scale_factor=args.scale, rotate_deg=args.rotate), passes=args.passes)
+        center = compiler.boundingbox.center()
+        center = (round(center[0], compiler.precision), round(center[1], compiler.precision))
+        print(f"new center: {center}")
+
+    return 0
+
+def main() -> int:
+    """
+    main
+    """
+    # defaults
+    cfg = {
+        "pixelsize_default": 0.1,
+        "imagespeed_default": 800,
+        "cuttingspeed_default": 1000,
+        "imagepower_default": 300,
+        "poweroffset_default": 0,
+        "cuttingpower_default": 850,
+        "xmaxtravel_default": 300,
+        "ymaxtravel_default": 400,
+        "rapidmove_default": 10,
+        "noise_default": 0,
+        "overscan_default": 0,
+        "pass_depth_default": 0,
+        "passes_default": 1,
+        "rotate_default": 0,
+        "colorcoded_default": "",
+        "constantburn_default": True,
+    }
+
+    if os.path.exists(config_file):
+        with open(config_file, 'rb') as f:
+            cfg.update({k + '_default': v for k,v in tomllib.load(f).items()})
+
+    # Define command line argument interface
+    parser = argparse.ArgumentParser(description='Convert svg to gcode for GRBL v1.1 compatible diode laser engravers.')
+    parser.add_argument('svg', type=str, help='svg file to be converted to gcode')
+    parser.add_argument('gcode', type=str, help='gcode output file')
+    parser.add_argument('--showimage', action='store_true', default=False, help='show b&w converted image' )
+    parser.add_argument('--selfcenter', action='store_true', default=False, help='self center the gcode (--origin cannot be used at the same time)' )
+    parser.add_argument('--pixelsize', default=cfg["pixelsize_default"], metavar="<default:" + str(cfg["pixelsize_default"])+">",
+        type=float, help="pixel size in mm (XY-axis): each image pixel is drawn this size")
+    parser.add_argument('--imagespeed', default=cfg["imagespeed_default"], metavar="<default:" + str(cfg["imagespeed_default"])+">",
+        type=int, help='image draw speed in mm/min')
+    parser.add_argument('--cuttingspeed', default=cfg["cuttingspeed_default"], metavar="<default:" + str(cfg["cuttingspeed_default"])+">",
+        type=int, help='cutting speed in mm/min')
+    parser.add_argument('--imagepower', default=cfg["imagepower_default"], metavar="<default:" +str(cfg["imagepower_default"])+ ">",
+        type=int, help="maximum laser power while drawing an image (as a rule of thumb set to 1/3 of the machine maximum for a 5W laser)")
+    parser.add_argument('--poweroffset', default=cfg["poweroffset_default"], metavar="<default:" +str(cfg["poweroffset_default"])+ ">",
+        type=int, help="pixel intensity to laser power: shift power range [0-imagepower]")
+    parser.add_argument('--cuttingpower', default=cfg["cuttingpower_default"], metavar="<default:" +str(cfg["cuttingpower_default"])+ ">",
+        type=int, help="sets laser power of line (path) cutting")
+    parser.add_argument('--passes', default=cfg["passes_default"], metavar="<default:" +str(cfg["passes_default"])+ ">",
+        type=int, help="Number of passes (iterations) for line drawings, only active when pass_depth is set")
+    parser.add_argument('--pass_depth', default=cfg["pass_depth_default"], metavar="<default:" + str(cfg["pass_depth_default"])+">",
+        type=float, help="cutting depth in mm for one pass, only active for passes > 1")
+    parser.add_argument('--rapidmove', default=cfg["rapidmove_default"], metavar="<default:" + str(cfg["rapidmove_default"])+ ">",
+        type=int, help='generate G0 moves between shapes, for images: G0 moves when skipping more than 10mm (default), 0 is no G0 moves' )
+    parser.add_argument('--noise', default=cfg["noise_default"], metavar="<default:" +str(cfg["noise_default"])+ ">",
+        type=int, help='reduces image noise by not emitting pixels with power lower or equal than this setting')
+    parser.add_argument('--overscan', default=cfg["overscan_default"], metavar="<default:" +str(cfg["overscan_default"])+ ">",
+        type=int, help="overscan image lines to avoid incorrect power levels for pixels at left and right borders, number in pixels, default off")
+    parser.add_argument('--showoverscan', action='store_true', default=False, help='show overscan pixels (note that this is visible and part of the gcode emitted!)' )
+    parser.add_argument('--constantburn', action=argparse.BooleanOptionalAction, default=cfg["constantburn_default"], help='default constant burn mode (M3)')
+    parser.add_argument('--origin', default=None, nargs=2, metavar=('delta-x', 'delta-y'),
+        type=float, help="translate origin by vector (delta-x,delta-y) in mm (default not set, option --selfcenter cannot be used at the same time)")
+    parser.add_argument('--scale', default=None, nargs=2, metavar=('factor-x', 'factor-y'),
+        type=float, help="scale svg with (factor-x,factor-y) (default not set)")
+    parser.add_argument('--rotate', default=cfg["rotate_default"], metavar="<default:" +str(cfg["rotate_default"])+ ">",
+        type=int, help="number of degrees to rotate")
+    parser.add_argument('--splitfile', action='store_true', default=False, help='split gcode output of SVG path and image objects' )
+    parser.add_argument('--pathcut', action='store_true', default=False, help='alway cut SVG path objects! (use laser power set with option --cuttingpower)' )
+    parser.add_argument('--nofill', action='store_true', default=False, help='ignore SVG fill attribute' )
+    parser.add_argument('--xmaxtravel', default=cfg["xmaxtravel_default"], metavar="<default:" +str(cfg["xmaxtravel_default"])+ ">",
+        type=int, help="machine x-axis lengh in mm")
+    parser.add_argument('--ymaxtravel', default=cfg["ymaxtravel_default"], metavar="<default:" +str(cfg["ymaxtravel_default"])+ ">",
+        type=int, help="machine y-axis lengh in mm")
+    parser.add_argument( '--color_coded', action = 'store', default=cfg["colorcoded_default"], metavar="<default:\"" + str(cfg["colorcoded_default"])+ "\">",
+         type = str, help = 'set action for path with specific stroke color "[color = [cut|engrave|ignore] *]*"'
+                            ', example: --color_coded "black = ignore purple = cut blue = engrave"' )
+    parser.add_argument('--fan', action='store_true', default=False, help='set machine fan on' )
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + __version__, help="show version number and exit")
+
+    args = parser.parse_args()
+
+    try:
+        if args.color_coded != "":
+            if args.pathcut:
+                print("options --color_coded and --pathcut cannot be used at the same time, program abort")
+                return 1
+            # check argument validity (1)
+
+            # category names
+            category = ["cut", "engrave", "ignore"]
+            # get css color names
+            colors = str([*css_color.css_color_keywords])
+            colors = re.sub("(,|\[|\]|\'| )", '', colors.replace(",", "|"))
+
+            # make a color list
+            colors = colors.split("|")
+
+            # get all names from color_coded
+            names_regex = "[a-zA-Z]+"
+            match = re.findall(names_regex, args.color_coded)
+            names = [i for i in match]
+
+            for name in names:
+                if not (name in colors or name in category):
+                    print(f"argument error: '--color_coded {args.color_coded}' has a name '{name}' that does not correspond to a css color or category (cut|engrave|ignore).")
+                    return 1
+
+        if args.origin is not None and args.selfcenter:
+            print("options --selfcenter and --origin cannot be used at the same time, program abort")
+            return 1
+
+        return svg2gcode(args)
+    except KeyboardInterrupt:
+        print(f"svg2gcode aborted!")
+    except Exception as error:
+        print(error)
+
+    return 1
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/svg2gcode/svg2gcode-3.3.7-py3-none-any.whl/svg2gcode/svg_to_gcode/__init__.py
@@ -0,0 +1,126 @@
+from typing import Any
+
+TOLERANCES 	= {"approximation": 10 ** -2, "input": 10 ** -3, "operation": 10**-6}
+UNITS      	= {"mm", "inch"}
+LASERMODE	= {"constant", "dynamic"}
+DISTANCEMODE 	= {"absolute", "incremental"}
+SETTING 	= {
+    # Machine parameters
+    "laser_mode_enable", 	# boolean 		sets grlb 1.1 laser mode (set default on most laser cutters)
+    "minimum_laser_power",	# positive integer	sets lowest power value for laser
+    "maximum_laser_power",	# positive integer	sets highest power value for laser (make sure this is set to the machine max)
+    "x_axis_maximum_rate",	# positive integer	maximum X-axis speed (typically mm/sec or mm/min) (NOTE: currently unused)
+    "y_axis_maximum_rate",	# positive integer	maximum Y-axis speed (typically mm/sec or mm/min) (NOTE: currently unused)
+    "x_axis_maximum_travel",	# positive integer	X-axis length of machine work area (mm)
+    "y_axis_maximum_travel",	# positive integer	Y-axis length of machine work area (mm)
+    "fan",			# boolean		when true 'set_laser_power' enables the fan and 'laser_off' disables the fan.
+    # Toolparameters
+    "pass_depth",		# positive integer	sets tool cutting depth in machine units (Z-axis: the depth your laser cuts in a pass; note that
+				# 			this depends on laser power and material!)
+    "dwell_time", 		# positive integer	sets the number of ms the tool should wait before moving to another cut (useful for pen plotters)
+    "movement_speed",		# positive integer	F(eed Rate) parameter; sets the movement speed of the tool while cutting (typically mm/sec or mm/min)
+    "laser_power",		# float range [0..1] 	sets the laser intensity (Spindle Speed) of the tool. Note that machine parameters
+				#			'minimum_laser_power' and 'maximum_laser_power' must be set for this to work
+    "laser_mode",		# LASERMODE		set constant or dynamic laser power mode
+    "maximum_image_laser_power",# positive integer      sets laser power (maximum) for image drawings (this is typically 1/3) of 'maximum_laser_power')
+    "image_poweroffset",        # positive integer      pixel intensity to laser power: shift power range [0-maxpower]
+    "image_movement_speed",     # positive integer      sets movement speed when drawing an image (typically a lot less than the 'maximum_rate')
+    "image_noise",              # positive integer      reduces image noise by not emitting pixels lower or equal this setting')
+    "image_overscan",           # positive integer      overscan image lines to avoid incorrect power levels for pixels at left and right borders,
+                                #                       number in pixels, default off
+    "image_showoverscan",       # boolean               show overscan via 'coloring'
+    # Configuration
+    "unit",			# UNITS			sets machine unit (milimeters, inches), this also defines the Feed (movement) parameter (mm/min or in/min)
+    "distance_mode",		# DISTANCEMODE		sets machine distance mode ('absolute' from 0,0 or 'incremental' from current position)
+    "rapid_move",		# positive integer	when 0, no rapid moves, > 0, G0 moves to the next line chain (otherwise a slow G1 S0 move is used).
+				# 			(note that the first move - to the start of the first line chain - is always a rapid move)
+                                #                       This setting is overloaded for images: a number 'n' > 0 means, do a rapid (G0) move when skipping
+                                #                       more than 'n'mm
+    "pixel_size",               # float                 sets image pixel size (in mm)
+    "showimage",                # boolean               show image used for conversion to gcode
+    "splitfile",                # boolean               return SVG path objects to <filename>.<gcext> and SVG image objects to <filename>_images.<gcext>
+    "pathcut",                  # boolean               always cut SVG path objects! use laser_power setting
+    "nofill",                   # boolean               ignore SVG fill attribute
+    "color_coded"               # string                color of path determines whether it is a cut or engrave
+}
+
+# Set defaults 'minimum_laser_power', 'pass_depth', 'dwell_time', 'laser_power', 'laser_mode', 'unit', 'distance_mode'
+DEFAULT_SETTING 	= {
+    # Machine parameters
+    "laser_mode_enable": 	None,		# usually already on
+    "minimum_laser_power": 	0,		# default set to 0
+    "maximum_laser_power": 	None,		# MANDATORY ('enter $$' on the machine console to get this - and other machine parameters)
+    "x_axis_maximum_rate": 	None,		# currently unused
+    "y_axis_maximum_rate": 	None,		# currently unused
+    "x_axis_maximum_travel": 	None,		# set this to the length of the machine x-axis (in mm)
+    "y_axis_maximum_travel": 	None,		# set this to the length of the machine y-axis (in mm)
+    "fan":			False,		# default set to false (no fan, not on)
+    # Toolparameters
+    "pass_depth": 		0,		# default set to 0 (no depth)
+    "dwell_time": 		0,		# default set to 0 (do not linger)
+    "movement_speed": 		None,		# MANDATORY
+    "laser_power": 		1000,		# default set to 1
+    "laser_mode": 		"dynamic",	# default set to dynamic (M4 safest mode, laser is off when not moving)
+    "maximum_image_laser_power":None,           # MANDATORY should be ("maximum_laser_power" - "minimum_laser_power" / 3 for a 5W laser)
+    "image_poweroffset":        0,              # default no laser power shift
+    "image_movement_speed":     None,           # MANDATORY
+    "image_noise":              0,              # default set to not reduces images noise')
+    "image_overscan":           0,              # default no overscan
+    "image_showoverscan":       False,          # default do not show overscan coloring
+    # Configuration
+    "unit": 			"mm",		# default set to milimeters
+    "distance_mode": 		"absolute",	# default set to absolute
+    "rapid_move":		10,		# default set to true, for images do a rapid move when skipping more than 10mm
+    "pixel_size":               0.1,            # laser kerf is mostly < 0.1mm
+    "showimage":                False,          # default image is not shown
+    "splitfile":                False,          # SVG path and image objects are emitted to one file: <filename>.<gcext>
+    "pathcut":                  False,          # always cut SVG path objects! use laser_power setting
+    "nofill":                   False,          # ignore SVG fill attribute
+    "color_coded":              str             # color of path determines whether it is a cut or engrave
+}
+
+def check_setting(setting: dict[str,Any] =None) -> bool:
+
+    """
+    Check all settings on type and value range.
+    :paran setting: dictionary containg all settings.
+    :return returns True when all settings are of the right type and within range.
+
+    Note that because these settings determain real world parameters of machines that have possibly lots of (laser) power, type and range
+    mistakes can have serious consequences. Also in general, to contain programming errors, it is best to - in this case - use 'mypy' and
+    'pylint' to check typing and formatting problems.
+
+    """
+
+    if setting is None:
+        return False
+
+    for key in setting.keys():
+        if key not in SETTING:
+            raise ValueError(f"Unknown setting {key}. Please specify one of the following: {SETTING}")
+        # Machine parameters
+        if key == "laser_mode_enable" and setting[key] not in {True,False}:
+            raise ValueError(f"Unknown '{key}' value '{setting[key]}'. Please specify one of the following: {{True,False}}")
+        if key in {"minimum_laser_power","maximum_laser_power","x_axis_maximum_rate","y_axis_maximum_rate", "movement_speed",
+                   "x_axis_maximum_travel","y_axis_maximum_travel", "maximum_image_laser_power",
+                   "image_movement_speed","laser_power","rapid_move", "image_poweroffset", "image_overscan"} \
+                   and (not isinstance(setting[key],int) or setting[key] < 0):
+            raise ValueError(f"'{key}' has type {type(setting[key])} and value {setting[key]}, but should be of type {type(1)} and have a value >= 0")
+        # Toolparameters
+        if key in {"pass_depth","dwell_time"}:
+            if not isinstance(setting[key],(int,float)) or setting[key] < 0:
+                raise ValueError(f"'{key}' has type {type(setting[key])} and value {setting[key]}, but should be of type {type(1)} and have a value >= 0")
+            setting[key] = float(setting[key])
+        if key == "laser_mode" and setting[key] not in LASERMODE:
+            raise ValueError(f"Unknown '{key}' '{setting[key]}'. Please specify one of the following: {LASERMODE}")
+        # Configuration
+        if key == "unit" and setting[key] not in UNITS:
+            raise ValueError(f"Unknown '{key}' value '{setting[key]}'. Please specify one of the following: {UNITS}")
+        if key == "distance_mode" and setting[key] not in DISTANCEMODE:
+            raise ValueError(f"Unknown '{key}' value '{setting[key]}'. Please specify one of the following: {DISTANCEMODE}")
+        if key in {"fan","showimage","splitfile","pathcut","nofill","image_showoverscan"} and setting[key] not in {True,False}:
+            raise ValueError(f"Unknown '{key}' value '{setting[key]}'. Please specify one of the following: {{True,False}}")
+        if key == "pixel_size" and setting[key] and (not isinstance(setting[key],(float)) or setting[key] <= 0):
+            raise TypeError(f"'{key}' is of type '{type(setting[key])}' but should be of type {type(1.0)} and have a value > 0.0")
+
+    return True

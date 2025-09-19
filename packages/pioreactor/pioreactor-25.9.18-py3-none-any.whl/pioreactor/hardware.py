e76f32f6f77ce0ed7007fd17eafe08bccdb594f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 07:06:37 +0000

    Add package pioreactor/25.9.18/pioreactor-25.9.18-py3-none-any.whl

---
+++ packages/pioreactor/pioreactor-25.9.18-py3-none-any.whl/pioreactor/hardware.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from os import environ
+
+from pioreactor import exc
+from pioreactor import types as pt
+from pioreactor.utils import adcs
+from pioreactor.version import hardware_version_info
+from pioreactor.version import rpi_version_info
+from pioreactor.whoami import is_testing_env
+
+#
+# All GPIO pins below are BCM numbered
+
+# PWMs
+# Heater PWM
+HEATER_PWM_TO_PIN: pt.PwmChannel = "5"
+
+# map between PWM channels and GPIO pins
+PWM_TO_PIN: dict[pt.PwmChannel, pt.GpioPin] = {
+    "1": 17,
+    "2": 13,  # hardware PWM1 available
+    "3": 16,
+    "4": 12,  # hardware PWM0 available
+    HEATER_PWM_TO_PIN: 18,  # dedicated to heater
+}
+
+
+# led and button GPIO pins
+PCB_LED_PIN: pt.GpioPin = 23
+PCB_BUTTON_PIN: pt.GpioPin = 24 if hardware_version_info <= (1, 0) else 4
+
+
+# hall sensor
+HALL_SENSOR_PIN: pt.GpioPin = 25 if hardware_version_info <= (1, 0) else 21
+
+
+# I2C pins
+GPIOCHIP: pt.GpioChip
+SDA: pt.I2CPin
+SCL: pt.I2CPin
+
+if rpi_version_info.startswith("Raspberry Pi 5"):
+    GPIOCHIP = 4
+    SDA = 2
+    SCL = 3
+else:
+    GPIOCHIP = 0
+    SDA = 2
+    SCL = 3
+
+## not used in app
+# if hardware_version_info >= (1,1):
+#     # SWD, used in HAT version == 1.1
+#     SWCLK: pt.GpioPin = 25
+#     SWDIO: pt.GpioPin = 24
+
+
+# I2C channels used
+TEMP_ADDRESS = 0x4F
+TEMP = TEMP_ADDRESS  # bc
+
+
+od_optics_setup = environ.get("OPTICS_MOD", "on_board")
+
+
+class ADCCurrier:
+    """
+    We don't to initiate the ADCs until we need them, so we curry them using this class, and this keeps all
+    the hardware metadata nice and neat and accessible.
+
+    from pioreactor.hardware import ADCs
+
+    adc = ADCs['pd1']()
+    reading = adc.read_from_channel()
+
+    """
+
+    def __init__(
+        self, adc_driver: type[adcs._I2C_ADC], i2c_address: pt.I2CAddress, adc_channel: pt.AdcChannel
+    ):
+        self.adc_driver = adc_driver
+        self.i2c_address = i2c_address
+        self.adc_channel = adc_channel
+
+    def __call__(self) -> adcs._I2C_ADC:
+        return self.adc_driver(SCL, SDA, self.i2c_address, self.adc_channel)
+
+    def __repr__(self) -> str:
+        return f"ADCCurrier(adc_driver={self.adc_driver.__name__}, i2c_address={hex(self.i2c_address)}, adc_channel={self.adc_channel})"
+
+
+ADCs: dict[str, ADCCurrier] = {}
+if od_optics_setup == "eye_spy":
+    ADCs["pd1"] = ADCCurrier(adcs.ADS1114_ADC, 0x48, 0)
+    ADCs["pd2"] = ADCCurrier(adcs.ADS1114_ADC, 0x49, 0)
+
+elif od_optics_setup == "on_board":
+    if hardware_version_info <= (1, 0):
+        ADCs["pd1"] = ADCCurrier(adcs.ADS1115_ADC, 0x48, 1)
+        ADCs["pd2"] = ADCCurrier(adcs.ADS1115_ADC, 0x48, 0)
+    else:
+        ADCs["pd1"] = ADCCurrier(adcs.Pico_ADC, 0x2C, 2)
+        ADCs["pd2"] = ADCCurrier(adcs.Pico_ADC, 0x2C, 3)
+else:
+    raise exc.HardwareNotFoundError()
+
+if hardware_version_info <= (1, 0):
+    ADCs["aux"] = ADCCurrier(adcs.ADS1115_ADC, 0x48, 3)
+    ADCs["version"] = ADCCurrier(adcs.ADS1115_ADC, 0x48, 2)
+else:
+    ADCs["aux"] = ADCCurrier(adcs.Pico_ADC, 0x2C, 1)
+    ADCs["version"] = ADCCurrier(adcs.Pico_ADC, 0x2C, 0)
+
+
+DAC_ADDRESS = 0x49 if (0, 0) < hardware_version_info <= (1, 0) else 0x2C
+DAC = DAC_ADDRESS  # bc
+
+
+def is_i2c_device_present(channel: int) -> bool:
+    if is_testing_env():
+        from pioreactor.utils.mock import MockI2C as I2C
+    else:
+        from busio import I2C  # type: ignore
+
+    from adafruit_bus_device.i2c_device import I2CDevice  # type: ignore
+
+    with I2C(SCL, SDA) as i2c:
+        try:
+            I2CDevice(i2c, channel, probe=True)
+            return True
+        except ValueError:
+            return False
+
+
+def is_DAC_present() -> bool:
+    return is_i2c_device_present(DAC_ADDRESS)
+
+
+def is_ADC_present(*args: int) -> bool:
+    if args:
+        to_check = set(args)
+    else:
+        to_check = set([adc.i2c_address for adc in ADCs.values()])
+    return all(is_i2c_device_present(c) for c in to_check)
+
+
+def is_heating_pcb_present() -> bool:
+    return is_i2c_device_present(TEMP_ADDRESS)
+
+
+def is_HAT_present() -> bool:
+    if is_testing_env() or (environ.get("HAT_PRESENT", "0") == "1"):
+        return True
+
+    try:
+        with open("/proc/device-tree/hat/vendor", "r") as f:
+            vendor = f.readline().strip("\x00")
+
+        with open("/proc/device-tree/hat/product_id", "r") as f:
+            product_id = f.readline().strip("\x00")
+
+        return vendor == "Pioreactor Inc." and product_id == "0x0001"
+    except FileNotFoundError:
+        return False
+
+
+def round_to_precision(x: float, p: float) -> float:
+    """
+    Ex: round_to_precision(x, 0.5) rounds to the nearest 0.5 (half-integer interval)
+    """
+    y = round(x / p) * p
+    return y
+
+
+def voltage_in_aux(precision: float = 0.1) -> float:
+    if not is_testing_env():
+        AUX_ADC = ADCs["aux"]
+    else:
+        from pioreactor.utils.mock import Mock_ADC  # type: ignore
+
+        AUX_ADC = ADCCurrier(Mock_ADC, 0x00, 0)
+
+    adc = AUX_ADC()
+    slope = 0.134  # from schematic
+
+    return round_to_precision(
+        adc.from_raw_to_voltage(adc.read_from_channel()) / slope,
+        p=precision,
+    )
---
+++ packages/pioreactor/pioreactor-25.9.18-py3-none-any.whl/pioreactor/whoami.py
@@ -0,0 +1,239 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import os
+import sys
+import time
+import warnings
+from functools import cache
+from typing import TYPE_CHECKING
+
+from pioreactor import mureq
+from pioreactor.exc import NotAssignedAnExperimentError
+from pioreactor.exc import NoWorkerFoundError
+
+if TYPE_CHECKING:
+    from pioreactor import types as pt
+    from pioreactor.structs import Model
+
+
+UNIVERSAL_IDENTIFIER = "$broadcast"
+UNIVERSAL_EXPERIMENT = "$experiment"
+NO_EXPERIMENT = "$no_experiment_present"
+
+
+def get_latest_experiment_name() -> pt.Experiment:
+    warnings.warn("Use whoami.get_assigned_experiment_name(unit) instead", DeprecationWarning, stacklevel=2)
+    return get_assigned_experiment_name(get_unit_name())
+
+
+def get_testing_experiment_name() -> pt.Experiment:
+    try:
+        exp = get_assigned_experiment_name(get_unit_name())
+        return f"_testing_{exp}"
+    except NotAssignedAnExperimentError:
+        return f"_testing_{NO_EXPERIMENT}"
+
+
+def get_assigned_experiment_name(unit_name: pt.Unit) -> pt.Experiment:
+    return _get_assigned_experiment_name(unit_name)
+
+
+def _get_assigned_experiment_name(unit_name: pt.Unit) -> pt.Experiment:
+    from pioreactor.pubsub import get_from_leader
+    from pioreactor.config import leader_address
+
+    if os.environ.get("EXPERIMENT") is not None:
+        return os.environ["EXPERIMENT"]
+    elif is_testing_env():
+        return "_testing_experiment"
+
+    retries = 6
+
+    for attempt in range(retries):
+        try:
+            result = get_from_leader(f"/api/workers/{unit_name}/experiment")
+            result.raise_for_status()
+            data = result.json()
+            return data["experiment"]
+        except mureq.HTTPErrorStatus as e:
+            if e.status_code == 401:
+                # auth error, something is wrong
+                raise mureq.HTTPException(
+                    f"Error in authentication to UI. Check http://{leader_address} and config.ini for api_key."
+                )
+            elif e.status_code == 404:
+                data = result.json()
+                raise NotAssignedAnExperimentError(data["error"])
+        except mureq.HTTPException:
+            raise mureq.HTTPException(
+                f"Not able to access experiments in UI. Check http://{leader_address} is online and check network."
+            )
+        except Exception:
+            # some other error? Keep trying
+            pass
+        time.sleep(0.5 * attempt)
+    else:
+        raise ConnectionError(
+            f"Not able to access experiments in UI. Check http://{leader_address}/api/experiments."
+        )
+
+
+def is_active(unit_name: pt.Unit) -> bool:
+    if os.environ.get("ACTIVE") == "1" or is_testing_env():
+        return True
+    elif os.environ.get("ACTIVE") == "0":
+        return False
+
+    from pioreactor.pubsub import get_from_leader
+
+    try:
+        result = get_from_leader(f"/api/workers/{unit_name}")
+        result.raise_for_status()
+        data = result.json()
+        return bool(data["is_active"])
+    except mureq.HTTPErrorStatus as e:
+        if e.status_code == 404:
+            raise NoWorkerFoundError(f"Worker {unit_name} is not present in leader's inventory")
+        else:
+            raise e
+    except mureq.HTTPException as e:
+        raise e
+
+
+@cache
+def is_testing_env() -> bool:
+    return ("pytest" in sys.modules) or (os.environ.get("TESTING", "") == "1")
+
+
+@cache
+def get_hostname() -> str:
+    import socket
+
+    if os.environ.get("HOSTNAME"):
+        return os.environ["HOSTNAME"]
+    elif is_testing_env():
+        return "testing_unit"
+    else:
+        return socket.gethostname()
+
+
+@cache
+def get_unit_name() -> pt.Unit:
+    hostname = get_hostname()
+
+    if hostname == "raspberrypi":
+        raise ValueError("Did you forget to set the hostname?")
+    else:
+        return hostname
+
+
+@cache
+def am_I_leader() -> bool:
+    if is_testing_env():
+        return True
+
+    from pioreactor.config import leader_hostname
+
+    return get_unit_name() == leader_hostname
+
+
+@cache
+def am_I_a_worker() -> bool:
+    from pioreactor.pubsub import get_from_leader
+
+    if is_testing_env():
+        return True
+
+    try:
+        result = get_from_leader(f"/api/workers/{get_unit_name()}")
+        result.raise_for_status()
+        return True
+    except mureq.HTTPErrorStatus as e:
+        if e.status_code == 404:
+            return False
+        else:
+            raise e
+
+
+@cache
+def get_pioreactor_model() -> Model:
+    from pioreactor.models import get_registered_models
+
+    name = _get_pioreactor_model_name()
+    version = _get_pioreactor_model_version()
+    try:
+        return get_registered_models()[(name, version)]
+    except KeyError:
+        raise ValueError(f"Unknown Pioreactor model {name} v{version}.")
+
+
+def _get_pioreactor_model_version() -> str:
+    # pioreactor model version
+    if os.environ.get("MODEL_VERSION"):
+        return os.environ["MODEL_VERSION"]
+
+    from pioreactor.pubsub import get_from_leader
+
+    try:
+        result = get_from_leader(f"/api/workers/{get_unit_name()}")
+        result.raise_for_status()
+        data = result.json()
+        return data["model_version"]
+    except mureq.HTTPErrorStatus as e:
+        if e.status_code == 404:
+            raise NoWorkerFoundError(f"Worker {get_unit_name()} is not present in leader's inventory")
+        else:
+            raise e
+    except mureq.HTTPException as e:
+        raise e
+
+
+def _get_pioreactor_model_name() -> str:
+    # pioreactor model name
+    if os.environ.get("MODEL_NAME"):
+        return os.environ["MODEL_NAME"]
+    elif is_testing_env():
+        return "pioreactor_40ml"
+
+    from pioreactor.pubsub import get_from_leader
+
+    try:
+        result = get_from_leader(f"/api/workers/{get_unit_name()}")
+        result.raise_for_status()
+        data = result.json()
+        return data["model_name"]
+    except mureq.HTTPErrorStatus as e:
+        if e.status_code == 404:
+            raise NoWorkerFoundError(f"Worker {get_unit_name()} is not found.")
+        else:
+            raise e
+    except mureq.HTTPException as e:
+        raise e
+
+
+@cache
+def get_image_git_hash() -> str:
+    try:
+        with open("/home/pioreactor/.pioreactor/.image_info") as f:
+            return f.read().strip().split("=")[1]
+    except OSError:  # catch FileNotFoundError, PermissionError, and other file-related exceptions
+        return "<Failed to fetch>"
+
+
+@cache
+def check_firstboot_successful() -> bool:
+    if is_testing_env():
+        return True
+    return os.path.isfile("/usr/local/bin/firstboot.sh.done")
+
+
+if is_testing_env():
+    # allow Blinka to think we are an Rpi:
+    # https://github.com/adafruit/Adafruit_Python_PlatformDetect/blob/75f69806222fbaf8535130ed2eacd07b06b1a298/adafruit_platformdetect/board.py
+    os.environ["BLINKA_FORCECHIP"] = "BCM2XXX"  # RaspberryPi
+    os.environ["BLINKA_FORCEBOARD"] = "RASPBERRY_PI_3A_PLUS"  # Raspberry Pi 3 Model A Plus Rev 1.0
+    os.environ["FIRMWARE"] = "0.5"
+    os.environ["HARDWARE"] = "1.2"
+    os.environ["MODEL_NAME"] = "pioreactor_40ml"
+    os.environ["MODEL_VERSION"] = "1.0"

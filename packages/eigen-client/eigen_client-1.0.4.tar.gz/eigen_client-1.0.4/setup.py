f24f1c9b026eb54071f43d7767f35ab84fc260ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:08:33 +0000

    Add package eigen-client/1.0.4/eigen_client-1.0.4.tar.gz

---
+++ packages/eigen-client/eigen_client-1.0.4.tar.gz/eigen_client-1.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eigen-client',
+    version='1.0.4',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        "requests==2.29.0",
+        "pytest==8.4.1",
+        "tiktoken==0.11.0",
+        "ollama==0.5.3",
+        "openai==1.101.0"
+    ],
+)

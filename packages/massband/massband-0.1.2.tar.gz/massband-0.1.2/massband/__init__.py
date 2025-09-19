126ddd30a23c41affd79e60762d89a0b53ac15f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:07:11 +0000

    Add package massband/0.1.2/massband-0.1.2.tar.gz

---
+++ packages/massband/massband-0.1.2.tar.gz/massband-0.1.2/massband/__init__.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+
+import lazy_loader as lazy
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+# attach stdout logger
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)

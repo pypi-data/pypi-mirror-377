8522be66566e9b849e4b42c136bd9829f264d1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:17:51 +0000

    Add package actinet/0.4.2/actinet-0.4.2.tar.gz

---
+++ packages/actinet/actinet-0.4.2.tar.gz/actinet-0.4.2/src/actinet/__init__.py
@@ -0,0 +1,12 @@
+name = "actinet"
+__author__ = "Aidan Acquah, Shing Chan, Aiden Doherty"
+__maintainer__ = "Shing Chan"
+__maintainer_email__ = "shing.chan@ndph.ox.ac.uk"
+__license__ = "See LICENSE file."
+
+__classifier_version__ = "ssl-ukb-c24-rw-30s-20250815"
+__classifier_md5__ = "148a5e798f2465985fe97a3f8b42ba96"
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]

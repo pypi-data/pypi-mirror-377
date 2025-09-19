0e30aac9f6428afa883f6685a69f53ed46fb4673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:07:52 +0000

    Add package mi-googlesearch-python/1.3.0.post1/mi_googlesearch_python-1.3.0.post1-py3-none-any.whl

---
+++ packages/mi-googlesearch-python/mi_googlesearch_python-1.3.0.post1-py3-none-any.whl/googlesearch/includes.py
@@ -0,0 +1,8 @@
+class SearchResult:
+    def __init__(self, url, title, description):
+        self.url = url
+        self.title = title
+        self.description = description
+
+    def __repr__(self):
+        return f"SearchResult(url={self.url}, title={self.title}, description={self.description})"
---
+++ packages/mi-googlesearch-python/mi_googlesearch_python-1.3.0.post1-py3-none-any.whl/googlesearch/user_agents.py
@@ -0,0 +1,8 @@
+import random
+
+def get_useragent():
+    """
+    Lynx was deprecated 2025.9, so we using something else.
+    """
+
+    return f"AdsBot-Google (+http://www.google.com/adsbot.html)"

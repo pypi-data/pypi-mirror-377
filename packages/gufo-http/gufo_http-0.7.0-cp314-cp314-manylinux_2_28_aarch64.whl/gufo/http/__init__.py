1548d1b10e3685e1e38bca7f7a1b6f4d7c3d9cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:39 +0000

    Add package gufo-http/0.7.0/gufo_http-0.7.0-cp314-cp314-manylinux_2_28_aarch64.whl

---
+++ packages/gufo-http/gufo_http-0.7.0-cp314-cp314-manylinux_2_28_aarch64.whl/gufo/http/__init__.py
@@ -0,0 +1,47 @@
+# ---------------------------------------------------------------------
+# Gufo HTTP: Python HTTP Client Library
+# ---------------------------------------------------------------------
+# Copyright (C) 2024-25, Gufo Labs
+# See LICENSE.md for details
+# ---------------------------------------------------------------------
+
+"""Gufo HTTP: The accelerated Python HTTP client library.
+
+Attributes:
+    __version__: Current version.
+"""
+
+# Gufo Labs modules
+from ._fast import (
+    BROTLI,
+    DEFLATE,
+    GZIP,
+    AuthBase,
+    BasicAuth,
+    BearerAuth,
+    Headers,
+    HttpError,
+    Proxy,
+    RedirectError,
+    RequestError,
+    RequestMethod,
+    Response,
+)
+
+__version__: str = "0.7.0"
+__all__ = [
+    "BROTLI",
+    "DEFLATE",
+    "GZIP",
+    "AuthBase",
+    "BasicAuth",
+    "BearerAuth",
+    "Headers",
+    "HttpError",
+    "Proxy",
+    "RedirectError",
+    "RequestError",
+    "RequestMethod",
+    "Response",
+    "__version__",
+]

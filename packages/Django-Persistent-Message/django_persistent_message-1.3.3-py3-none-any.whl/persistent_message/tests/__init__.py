18f7d2c50ec92a02fd176451a9a495a94d90cd16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:06:38 +0000

    Add package Django-Persistent-Message/1.3.3/django_persistent_message-1.3.3-py3-none-any.whl

---
+++ packages/Django-Persistent-Message/django_persistent_message-1.3.3-py3-none-any.whl/persistent_message/tests/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from django.utils import timezone
+from datetime import datetime
+
+
+def mocked_current_datetime():
+    dt = datetime(2018, 1, 1, 10, 10, 10)
+    return timezone.make_aware(dt)

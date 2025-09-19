3cd8bd5e5e3948d092058af49088604b3948ecfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 08:07:17 +0000

    Add package lhotse/1.31.1/lhotse-1.31.1.tar.gz

---
+++ packages/lhotse/lhotse-1.31.1.tar.gz/lhotse-1.31.1/lhotse/augmentation/__init__.py
@@ -0,0 +1,8 @@
+from .clipping import Clipping
+from .common import AugmentFn
+from .loudness import LoudnessNormalization
+from .rir import ReverbWithImpulseResponse
+from .torchaudio import *
+from .transform import AudioTransform
+from .utils import FastRandomRIRGenerator, convolve1d
+from .wpe import DereverbWPE, dereverb_wpe_numpy, dereverb_wpe_torch

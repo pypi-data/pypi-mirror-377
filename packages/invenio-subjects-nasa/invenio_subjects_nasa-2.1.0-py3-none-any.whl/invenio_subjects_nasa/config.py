258fa3ec42e04357d82fad102c675e1dccd5d277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:17:56 +0000

    Add package invenio-subjects-nasa/2.1.0/invenio_subjects_nasa-2.1.0-py3-none-any.whl

---
+++ packages/invenio-subjects-nasa/invenio_subjects_nasa-2.1.0-py3-none-any.whl/invenio_subjects_nasa/config.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2022-2024 KTH Royal Institute of Technology.
+#
+# invenio-subjects-nasa is free software, you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file details.
+from pathlib import Path
+
+# NASA Thesaurus CSV filename
+NASA_THESAURUS_CSV_FILENAME = "thesaurus-CSV-2025-09-17.csv"
+
+nasa_subjects_csv_input_path = (
+    Path.cwd() / "invenio_subjects_nasa" / "downloads" / NASA_THESAURUS_CSV_FILENAME
+)
+nasa_subjects_yaml_output_path = (
+    Path.cwd() / "invenio_subjects_nasa" / "vocabularies" / "nasa_thesaurus.yaml"
+)

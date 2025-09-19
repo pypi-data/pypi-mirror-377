f55876fc43dbac6c8f375e5e75db2238b2b9de2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:18:30 +0000

    Add package mhd-model/0.1.17/mhd_model-0.1.17.tar.gz

---
+++ packages/mhd-model/mhd_model-0.1.17.tar.gz/mhd_model-0.1.17/mhd_model/commands/create/create.py
@@ -0,0 +1,20 @@
+import sys
+
+import click
+
+from mhd_model.commands.create.announcement import create_announcement_file_task
+
+
+@click.group(name="create", context_settings={"help_option_names": ["-h", "--help"]})
+def create_group():
+    """utilities to create and convert files (announcement file, etc.)."""
+    pass
+
+
+create_group.add_command(create_announcement_file_task)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        create_group(["--help"])
+    else:
+        create_group()

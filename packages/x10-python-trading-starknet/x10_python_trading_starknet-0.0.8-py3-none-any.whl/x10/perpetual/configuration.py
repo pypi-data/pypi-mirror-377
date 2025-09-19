205a7c6c916350dc7f0ab68a3ef0dfbcf63a5a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:07 +0000

    Add package x10-python-trading-starknet/0.0.8/x10_python_trading_starknet-0.0.8-py3-none-any.whl

---
+++ packages/x10-python-trading-starknet/x10_python_trading_starknet-0.0.8-py3-none-any.whl/x10/perpetual/configuration.py
@@ -0,0 +1,85 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class StarknetDomain:
+    name: str
+    version: str
+    chain_id: str
+    revision: str
+
+
+@dataclass
+class EndpointConfig:
+    chain_rpc_url: str
+    api_base_url: str
+    stream_url: str
+    onboarding_url: str
+    signing_domain: str
+    collateral_asset_contract: str
+    asset_operations_contract: str
+    collateral_asset_on_chain_id: str
+    collateral_decimals: int
+    collateral_asset_id: str
+    starknet_domain: StarknetDomain
+
+
+TESTNET_CONFIG = EndpointConfig(
+    chain_rpc_url="https://rpc.sepolia.org",
+    api_base_url="https://api.starknet.sepolia.extended.exchange/api/v1",
+    stream_url="wss://starknet.sepolia.extended.exchange/stream.extended.exchange/v1",
+    onboarding_url="https://api.starknet.sepolia.extended.exchange",
+    signing_domain="starknet.sepolia.extended.exchange",
+    collateral_asset_contract="0x31857064564ed0ff978e687456963cba09c2c6985d8f9300a1de4962fafa054",
+    asset_operations_contract="",
+    collateral_asset_on_chain_id="0x1",
+    collateral_decimals=6,
+    starknet_domain=StarknetDomain(name="Perpetuals", version="v0", chain_id="SN_SEPOLIA", revision="1"),
+    collateral_asset_id="0x1",
+)
+
+MAINNET_CONFIG = EndpointConfig(
+    chain_rpc_url="https://cloudflare-eth.com",
+    api_base_url="https://api.extended.exchange/api/v1",
+    stream_url="wss://api.extended.exchange/stream.extended.exchange/v1",
+    onboarding_url="https://api.extended.exchange",
+    signing_domain="extended.exchange",
+    collateral_asset_contract="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    asset_operations_contract="0x1cE5D7f52A8aBd23551e91248151CA5A13353C65",
+    collateral_asset_on_chain_id="0x2893294412a4c8f915f75892b395ebbf6859ec246ec365c3b1f56f47c3a0a5d",
+    collateral_decimals=6,
+    collateral_asset_id="0x1",
+    starknet_domain=StarknetDomain(name="Perpetuals", version="v0", chain_id="SN_MAINNET", revision="1"),
+)
+
+"""
+Identical to the MAINNET_CONFIG, but with a different signing domain.
+Use it for accounts that were created before the signing domain was changed.
+"""
+MAINNET_CONFIG_LEGACY_SIGNING_DOMAIN = EndpointConfig(
+    chain_rpc_url="https://cloudflare-eth.com",
+    api_base_url="https://api.extended.exchange/api/v1",
+    stream_url="wss://api.extended.exchange/stream.extended.exchange/v1",
+    onboarding_url="https://api.extended.exchange",
+    signing_domain="x10.exchange",
+    collateral_asset_contract="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    asset_operations_contract="0x1cE5D7f52A8aBd23551e91248151CA5A13353C65",
+    collateral_asset_on_chain_id="0x2893294412a4c8f915f75892b395ebbf6859ec246ec365c3b1f56f47c3a0a5d",
+    collateral_decimals=6,
+    collateral_asset_id="0x1",
+    starknet_domain=StarknetDomain(name="Perpetuals", version="v0", chain_id="SN_MAINNET", revision="1"),
+)
+
+STARKNET_MAINNET_CONFIG = EndpointConfig(
+    chain_rpc_url="",
+    api_base_url="https://api.starknet.extended.exchange/api/v1",
+    stream_url="wss://api.starknet.extended.exchange/stream.extended.exchange/v1",
+    onboarding_url="https://api.starknet.extended.exchange",
+    signing_domain="extended.exchange",
+    collateral_asset_contract="",
+    asset_operations_contract="",
+    collateral_asset_on_chain_id="0x1",
+    collateral_decimals=6,
+    starknet_domain=StarknetDomain(name="Perpetuals", version="v0", chain_id="SN_MAIN", revision="1"),
+    collateral_asset_id="0x1",
+)

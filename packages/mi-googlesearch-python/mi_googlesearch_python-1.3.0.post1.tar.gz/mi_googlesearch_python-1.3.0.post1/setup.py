474121ac29f7c6d3391a1bca2b5a89b0968fcfe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:07:52 +0000

    Add package mi-googlesearch-python/1.3.0.post1/mi_googlesearch_python-1.3.0.post1.tar.gz

---
+++ packages/mi-googlesearch-python/mi_googlesearch_python-1.3.0.post1.tar.gz/mi_googlesearch_python-1.3.0.post1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding='UTF-8') as fh:
+    requirements = fh.read().split("\n")
+
+setup(
+    name="mi-googlesearch-python",
+    version="1.3.0.post1",
+    author="Nishant Vikramaditya",
+    author_email="junk4Nv7@gmail.com",
+    description="A Python library for scraping the Google search engine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nv7-GitHub/googlesearch",
+    packages=["googlesearch"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[requirements],
+    include_package_data=True,
+)

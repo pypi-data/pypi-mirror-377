4bec092de9087fe92af15ee5f28c4805ede4bc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:05:58 +0000

    Add package snakemake-executor-plugin-cannon/1.8.0.post2/snakemake_executor_plugin_cannon-1.8.0.post2.tar.gz

---
+++ packages/snakemake-executor-plugin-cannon/snakemake_executor_plugin_cannon-1.8.0.post2.tar.gz/snakemake_executor_plugin_cannon-1.8.0.post2/README.md
@@ -0,0 +1,45 @@
+# Snakemake executor plugin: cannon 
+
+This is a fork of the [SLURM executor plugin for Snakemake](https://github.com/snakemake/snakemake-executor-plugin-slurm) for the [Cannon cluster at Harvard University](https://docs.rc.fas.harvard.edu/kb/running-jobs/). It has all the same features as the SLURM plugin, but performs automatic partition selection for the Cannon cluster based on the resources specified in a given Snakemake rule. It also offers some error checking for partition selection.
+
+For full documentation, see the [Snakemake plugin catalog](https://snakemake.github.io/snakemake-plugin-catalog/plugins/executor/cannon.html).
+
+## Installation
+
+The executor can be installed [from PyPI](https://pypi.org/project/snakemake-executor-plugin-cannon/) with pip:
+
+```bash
+pip install snakemake-executor-plugin-cannon
+```
+
+Or [from bioconda](https://bioconda.github.io/recipes/snakemake-executor-plugin-cannon/README.html) with conda/mamba:
+
+```bash
+mamba install snakemake-executor-plugin-cannon
+```
+
+## Specifying the executor
+
+To use the executor with Snakemake, either specify it in the command line as:
+
+```bash
+snakemake -e cannon ...
+```
+
+Or add it to your [profile](https://github.com/harvardinformatics/snakemake-executor-plugin-cannon/blob/main/docs/profile.md):
+
+```YAML
+executor: cannon
+```
+
+## Setting up your profile
+
+While this plugin does automatic partition selection, the user is still responsible for specifying other resources for rules in their workflow. This is usually done through a cluster **profile**, but this may differ based on your workflow. 
+
+See the [profile setup page](https://github.com/harvardinformatics/snakemake-executor-plugin-cannon/blob/main/docs/profile.md) for more information. 
+
+An example profile can be found at [`tests/cannon-test-profile/config.yaml`](https://github.com/harvardinformatics/snakemake-executor-plugin-cannon/blob/main/tests/cannon-test-profile/config.yaml)
+
+## Features
+
+For full documentation, see the [Snakemake plugin catalog](https://snakemake.github.io/snakemake-plugin-catalog/plugins/executor/cannon.html).
---
+++ packages/snakemake-executor-plugin-cannon/snakemake_executor_plugin_cannon-1.8.0.post2.tar.gz/snakemake_executor_plugin_cannon-1.8.0.post2/snakemake_executor_plugin_cannon/submit_string.py
@@ -0,0 +1,100 @@
+from snakemake_executor_plugin_slurm_jobstep import get_cpu_setting
+from types import SimpleNamespace
+import shlex
+
+
+def safe_quote(value):
+    """
+    Safely quote a parameter value using shlex.quote.
+    Handles None values and converts to string if needed.
+    Returns empty quotes for empty strings.
+    """
+    str_value = str(value)
+    if str_value == "":
+        return "''"
+    return shlex.quote(str_value)
+
+
+def get_submit_command(job, params):
+    """
+    Return the submit command for the job.
+    """
+    # Convert params dict to a SimpleNamespace for attribute-style access
+    params = SimpleNamespace(**params)
+
+    call = (
+        "sbatch "
+        "--parsable "
+        f"--job-name {safe_quote(params.run_uuid)} "
+        f"--output {safe_quote(params.slurm_logfile)} "
+        "--export=ALL "
+        f"--comment {safe_quote(params.comment_str)}"
+    )
+
+    # No accout or partition checking is required, here.
+    # Checking is done in the submit function.
+
+    # here, only the string is used, as it already contains
+    # "-A '{account_name}'"
+    call += f" {params.account}"
+    # here, only the string is used, as it already contains
+    # "- p '{partition_name}'"
+    call += f" {params.partition}"
+
+    if job.resources.get("clusters"):
+        call += f" --clusters {safe_quote(job.resources.clusters)}"
+
+    if job.resources.get("runtime"):
+        call += f" -t {safe_quote(job.resources.runtime)}"
+
+    # Both, constraint and qos are optional.
+    # If not set, they will not be added to the sbatch call.
+    # If explicitly set to an empty string,
+    # `--constraint ''` or `--qos ''` will be added.
+    constraint = job.resources.get("constraint")
+    if constraint is not None:
+        call += f" -C {safe_quote(constraint)}"
+
+    qos = job.resources.get("qos")
+    if qos is not None:
+        call += f" --qos={safe_quote(qos)}"
+
+    if job.resources.get("mem_mb_per_cpu"):
+        call += f" --mem-per-cpu {job.resources.mem_mb_per_cpu}"
+    elif job.resources.get("mem_mb"):
+        call += f" --mem {job.resources.mem_mb}"
+
+    if job.resources.get("nodes", False):
+        call += f" --nodes={job.resources.get('nodes', 1)}"
+
+    gpu_job = job.resources.get("gpu") or "gpu" in job.resources.get("gres", "")
+    if gpu_job:
+        # fixes #316 - allow unsetting of tasks per gpu
+        # apparently, python's internal process manangement interfers with SLURM
+        # e.g. for pytorch
+        ntasks_per_gpu = job.resources.get("tasks_per_gpu")
+        if ntasks_per_gpu is None:
+            ntasks_per_gpu = job.resources.get("tasks")
+        if ntasks_per_gpu is None:
+            ntasks_per_gpu = 1
+
+        if ntasks_per_gpu >= 1:
+            call += f" --ntasks-per-gpu={ntasks_per_gpu}"
+    else:
+        # fixes #40 - set ntasks regardless of mpi, because
+        # SLURM v22.05 will require it for all jobs
+        call += f" --ntasks={job.resources.get('tasks') or 1}"
+
+    # we need to set cpus-per-task OR cpus-per-gpu, the function
+    # will return a string with the corresponding value
+    call += f" {get_cpu_setting(job, gpu_job)}"
+    if job.resources.get("slurm_extra"):
+        call += f" {job.resources.slurm_extra}"
+
+    # ensure that workdir is set correctly
+    # use short argument as this is the same in all slurm versions
+    # (see https://github.com/snakemake/snakemake/issues/2014)
+    if params.workdir:
+        call += f" -D {safe_quote(params.workdir)}"
+
+    return call

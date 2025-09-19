7714cb1bb60341f03b2bcb1be807307f277118e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:05:48 +0000

    Add package edgartools/4.13.0/edgartools-4.13.0-py3-none-any.whl

---
+++ packages/edgartools/edgartools-4.13.0-py3-none-any.whl/edgar/ownership/__init__.py
@@ -0,0 +1,29 @@
+from edgar.ownership.ownershipforms import (
+    Owner,
+    Issuer,
+    Address,
+    Footnotes,
+    OwnerSignature,
+    TransactionCode,
+    Ownership,
+    Form3,
+    Form4,
+    Form5,
+    DerivativeHolding,
+    DerivativeHoldings,
+    translate_ownership,
+    NonDerivativeHolding,
+    NonDerivativeHoldings,
+    DerivativeTransaction,
+    DerivativeTransactions,
+    ReportingOwners,
+    ReportingRelationship,
+    PostTransactionAmounts,
+    NonDerivativeTransaction,
+    NonDerivativeTransactions,
+    TransactionSummary
+)
+
+from edgar.ownership.html_render import (
+    ownership_to_html
+)

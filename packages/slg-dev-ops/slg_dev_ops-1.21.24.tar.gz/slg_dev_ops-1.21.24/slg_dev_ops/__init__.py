35b3c74f1bfaef5169f2a20798d5a12af8b4c835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:07:49 +0000

    Add package slg-dev-ops/1.21.24/slg_dev_ops-1.21.24.tar.gz

---
+++ packages/slg-dev-ops/slg_dev_ops-1.21.24.tar.gz/slg_dev_ops-1.21.24/slg_dev_ops/__init__.py
@@ -0,0 +1,13 @@
+"""
+This module is callable via the command line
+but importing it does nothing
+"""
+from .install_nginx import install_nginx
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()

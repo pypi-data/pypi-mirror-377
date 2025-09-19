6b324d73881252b4d628bcf5eb12dc7c4f785e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 08:07:22 +0000

    Add package bytez/1.0.10/bytez-1.0.10.tar.gz

---
+++ packages/bytez/bytez-1.0.10.tar.gz/bytez-1.0.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytez",  # Name of your package
+    version="1.0.10",
+    packages=find_packages(),
+    description="Python API client for Bytez service",
+    long_description=open("./README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Bytez",
+    url="https://github.com/bytez-com/docs",
+    install_requires=[
+        "requests>=2.32.1",  # Allow any 2.x version of requests
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)

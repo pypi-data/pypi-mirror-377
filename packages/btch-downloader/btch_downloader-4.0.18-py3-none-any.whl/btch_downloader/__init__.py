32b347bbd73ed77916662fc2d5ab18bd160d9bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:08:13 +0000

    Add package btch-downloader/4.0.18/btch_downloader-4.0.18-py3-none-any.whl

---
+++ packages/btch-downloader/btch_downloader-4.0.18-py3-none-any.whl/btch_downloader/__init__.py
@@ -0,0 +1,14 @@
+from .downloader import (
+    aio,
+    fbdown,
+    igdl,
+    ttdl,
+    twitter,
+    youtube,
+    mediafire,
+    capcut,
+    gdrive,
+    pinterest
+)
+
+__version__ = "4.0.15"

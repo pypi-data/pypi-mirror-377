00384b3e1feb0d3e5879d283a22753a8acbc1602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 07:06:53 +0000

    Add package box-ai-agents-toolkit/0.0.50/box_ai_agents_toolkit-0.0.50.tar.gz

---
+++ packages/box-ai-agents-toolkit/box_ai_agents_toolkit-0.0.50.tar.gz/box_ai_agents_toolkit-0.0.50/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## 0.0.50 - 2025-09-18
+
+* Merge pull request #4 from box-community/rb/groups (d6ee5da)
+* ruff (4474ad7)
+* ruff (5b59488)
+* implementing the group tools (abdf89e)
+* Update version to 0.0.49 and changelog (8e6d4de)
---
+++ packages/box-ai-agents-toolkit/box_ai_agents_toolkit-0.0.50.tar.gz/box_ai_agents_toolkit-0.0.50/src/box_ai_agents_toolkit/box_api_util_generic.py
@@ -0,0 +1,13 @@
+import logging
+from box_sdk_gen import (
+    BoxAPIError,
+    DataSanitizer,
+)
+
+logging.basicConfig(level=logging.INFO)
+
+
+def log_box_api_error(e: BoxAPIError) -> None:
+    """Log details of a BoxAPIError."""
+    data_sanitizer = DataSanitizer()
+    logging.error(f"Box API Error: {e.response_info.print(data_sanitizer)}")

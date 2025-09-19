93f5bc4367c94cd3987451daf037f6fb4d547bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:07:31 +0000

    Add package aioinflux1x/1.0.0/aioinflux1x-1.0.0-py3-none-any.whl

---
+++ packages/aioinflux1x/aioinflux1x-1.0.0-py3-none-any.whl/aioinflux1x/compat.py
@@ -0,0 +1,13 @@
+import warnings
+
+no_pandas_warning = "Pandas/Numpy is not available. Support for 'dataframe' mode is disabled."
+
+try:
+    import pandas as pd
+    import numpy as np
+except ModuleNotFoundError:
+    pd = None
+    np = None
+    warnings.warn(no_pandas_warning)
+
+__all__ = ['no_pandas_warning', 'pd', 'np']

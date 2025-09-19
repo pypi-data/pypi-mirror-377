ebec769494541a5fe5caf7a4d78ac4a47bddbb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:48 +0000

    Add package funny-numbers/0.2.0/funny_numbers-0.2.0-cp39-abi3-manylinux_2_34_x86_64.whl

---
+++ packages/funny-numbers/funny_numbers-0.2.0-cp39-abi3-manylinux_2_34_x86_64.whl/funny_numbers/__init__.py
@@ -0,0 +1,5 @@
+from ._core import FunnyNumber
+from ._core import DeterministicFunnyNumberFactory, RandomFunnyNumberFactory
+
+__all__ = ["FunnyNumber", "DeterministicFunnyNumberFactory", "RandomFunnyNumberFactory"]
+__version__ = "0.2.0"

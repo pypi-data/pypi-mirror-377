c28f22f29e2974034af0fb95b9cd3cb47df72acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:05:59 +0000

    Add package silence-lint-error/1.7.0/silence_lint_error-1.7.0.tar.gz

---
+++ packages/silence-lint-error/silence_lint_error-1.7.0.tar.gz/silence_lint_error-1.7.0/README.md
@@ -0,0 +1,109 @@
+# silence-lint-error
+
+Silent linting errors
+by adding `ignore` or `fixme` comments.
+
+This tool currently works with:
+
+- [`fixit`](https://github.com/Instagram/Fixit)
+- [`flake8`](https://github.com/PyCQA/flake8) (silence only)
+- [`mypy`](https://www.mypy-lang.org) (silence only)
+- [`ruff`](https://docs.astral.sh/ruff/)
+- [`semgrep`](https://semgrep.dev/docs/) (silence only)
+
+## Usage
+
+Install with pip:
+
+```shell
+python -m pip install silence-lint-error
+```
+
+You must also install the linting tool you wish to use.
+
+### silence linting errors
+
+Find linting errors
+and add the `ignore` or `fixme` comments as applicable.
+
+For example,
+to add `lint-fixme: CollapseIsinstanceChecks` comments
+to ignore the `fixit.rules:CollapseIsinstanceChecks` rule from `fixit`,
+run:
+
+```shell
+silence-lint-error fixit fixit.rules:CollapseIsinstanceChecks path/to/files/ path/to/more/files/
+```
+
+To add `noqa: F401` comments
+to ignore the `F401` rule in `flake8`,
+run:
+
+```shell
+silence-lint-error flake8 F401 path/to/files/ path/to/more/files/
+```
+
+To add `noqa: F401` comments
+to ignore the `F401` rule in `ruff`,
+run:
+
+```shell
+silence-lint-error ruff F401 path/to/files/ path/to/more/files/
+```
+
+To add `nosemgrep: python.lang.best-practice.sleep.arbitrary-sleep` comments
+to ignore the `python.lang.best-practice.sleep.arbitrary-sleep` rule in `semgrep`,
+run:
+
+```shell
+SEMGREP_RULES=r/python silence-lint-error semgrep python.lang.best-practice.sleep.arbitrary-sleep path/to/files/ path/to/more/files/
+```
+
+N.B. The rules must be configured in an environment variable.
+For more information about configuring semgrep rules,
+see the `--config` entry in the [`semgrep` documentation](https://semgrep.dev/docs/cli-reference-oss/)
+
+To add `type: ignore` comments
+to ignore the `truthy-bool` error from `mypy`,
+run:
+
+```shell
+silence-lint-error mypy truthy-bool path/to/files/ path/to/more/files/
+```
+
+### fix silenced errors
+
+If there is an auto-fix for a linting error,
+you can remove the `ignore` or `fixme` comments
+and apply the auto-fix.
+
+For example
+to remove all `lint-fixme: CollapseIsinstanceChecks` comments
+and apply the auto-fix for that rule,
+run:
+
+```shell
+fix-silenced-error fixit fixit.rules:CollapseIsinstanceChecks path/to/files/ path/to/more/files/
+```
+
+To remove `noqa: F401` comments
+and apply the auto-fix for that rule,
+run:
+
+```shell
+fix-silenced-error ruff F401 path/to/files/ path/to/more/files/
+```
+
+## Rationale
+
+When adding a new rule (or enabling more rules) for a linter
+on a large code-base,
+fixing the existing violations can be too large a task to do quickly.
+However, starting to check the rule sooner
+will prevent new violations from being introduced.
+
+Ignoring existing violations is a quick way to allow new rules to be enabled.
+You can then burn down those existing violations over time.
+
+This tool makes it easy to find and ignore all current violations of a rule
+so that it can be enabled.
---
+++ packages/silence-lint-error/silence_lint_error-1.7.0.tar.gz/silence_lint_error-1.7.0/silence_lint_error/comments.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+
+import tokenize_rt
+
+
+def add_error_silencing_comments(
+        src: str, error_lines: set[int],
+        comment_type: str, error_code: str,
+) -> str:
+    """Add comments to some code to silence linting errors.
+
+    Args:
+        src: The content of the module to add comments to.
+        error_lines: The lines on which to silence errors.
+        comment_type: The type of comment to add (e.g. `noqa` or `lint-fixme`)
+        code: The error code to silence.
+
+    Returns:
+        The content of the module with the additional comments added.
+    """
+    tokens = tokenize_rt.src_to_tokens(src)
+
+    for idx, token in tokenize_rt.reversed_enumerate(tokens):
+        if token.line not in error_lines:
+            continue
+        if not token.src.strip():
+            continue
+
+        if token.name == 'COMMENT':
+            new_comment = add_code_to_comment(token.src, comment_type, error_code)
+            tokens[idx] = tokens[idx]._replace(src=new_comment)
+        else:
+            tokens.insert(
+                idx+1, tokenize_rt.Token(
+                    'COMMENT', f'# {comment_type}: {error_code}',
+                ),
+            )
+            tokens.insert(idx+1, tokenize_rt.Token('UNIMPORTANT_WS', '  '))
+
+        error_lines.remove(token.line)
+
+    return tokenize_rt.tokens_to_src(tokens)  # type: ignore[no-any-return]
+    # tokenize-rt is a single-file distribution so cannot provide type
+    # information. See https://github.com/asottile/tokenize-rt/issues/147.
+
+
+def add_noqa_comments(src: str, lines: set[int], error_code: str) -> str:
+    """Add `noqa` comments to some code.
+
+    Args:
+        src: The content of the module to add `noqa` comments to.
+        lines: The lines on which to add `noqa` coments.
+        code: The error code to silence.
+
+    Returns:
+        The content of the module with the additional comments added.
+    """
+    return add_error_silencing_comments(src, lines, 'noqa', error_code)
+
+
+def add_code_to_comment(
+        comment: str, comment_type: str, code: str, sep: str = '',
+) -> str:
+    """Add to a comment to make it a error-silencing comment.
+
+    If the comment already includes an error silencing section of the same type, this
+    will add the code to the list of silenced errors.
+    """
+    if f'{comment_type}: ' in comment:
+        return comment.replace(
+            f'{comment_type}: ', f'{comment_type}: {code},{sep}', 1,
+        )
+    else:
+        return comment + f'  # {comment_type}: {code}'
+
+
+def remove_error_silencing_comments(
+        src: str,
+        comment_type: str, error_code: str,
+) -> str:
+    """Remove comments that silence linting errors from some code.
+
+    Args:
+        src: The content of the module to remove comments from.
+        comment_type: The type of comment to remove (e.g. `noqa` or `lint-fixme`)
+        error_code: The error code that is silenced.
+
+    Returns:
+        The content of the module without the comments that slence this error code.
+    """
+    tokens = tokenize_rt.src_to_tokens(src)
+
+    for idx, token in tokenize_rt.reversed_enumerate(tokens):
+        if (
+                token.name == 'COMMENT'
+                and comment_type in token.src and error_code in token.src
+        ):
+            new_comment = remove_code_from_comment(
+                token.src, comment_type, error_code,
+            )
+            if new_comment:
+                tokens[idx] = tokens[idx]._replace(src=new_comment)
+            else:
+                tokens.pop(idx)
+
+        # delete trailing whitespace caused by removing comments
+        elif (
+                token.name == 'UNIMPORTANT_WS'
+                and tokens[idx+1].name in {'NEWLINE', 'NL'}
+        ):
+            tokens.pop(idx)
+
+    return tokenize_rt.tokens_to_src(tokens)  # type: ignore[no-any-return]
+    # tokenize-rt is a single-file distribution so cannot provide type
+    # information. See https://github.com/asottile/tokenize-rt/issues/147.
+
+
+def remove_code_from_comment(comment: str, comment_type: str, code: str) -> str:
+    """Remove the error-silencing portion from a comment."""
+    return (
+        comment
+        .replace(f'{comment_type}: {code},', f'{comment_type}: ')
+        .replace(f',{code}', '')
+        .removeprefix(f'# {comment_type}: {code}')
+        .removesuffix(f'{comment_type}: {code}')
+        .strip()
+        .removesuffix('  #')
+    )

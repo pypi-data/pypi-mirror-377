9196d5c70164b86f98491477e8b3d3a2e15f335a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:06:47 +0000

    Add package django-client-logger/3.1.1/django_client_logger-3.1.1.tar.gz

---
+++ packages/django-client-logger/django_client_logger-3.1.1.tar.gz/django_client_logger-3.1.1/django_client_logger/urls.py
@@ -0,0 +1,10 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from django.urls import re_path
+from django_client_logger.views import LogReceiver
+
+
+urlpatterns = [
+    re_path(r'log', LogReceiver.as_view(), name='client_log_api')
+]
---
+++ packages/django-client-logger/django_client_logger-3.1.1.tar.gz/django_client_logger-3.1.1/setup.py
@@ -0,0 +1,42 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+from setuptools import setup
+
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/django_client_logger>`_.
+"""
+
+version_path = 'django_client_logger/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+url = "https://github.com/uw-it-aca/django_client_logger"
+setup(
+    name='django_client_logger',
+    version=VERSION,
+    packages=['django_client_logger'],
+    author="UW-IT SETS",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=[
+        'Django',
+        'mock',
+        'django-userservice',
+    ],
+    license='Apache License, Version 2.0',
+    description=("Client logging application for django"),
+    long_description=README,
+    url=url,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)

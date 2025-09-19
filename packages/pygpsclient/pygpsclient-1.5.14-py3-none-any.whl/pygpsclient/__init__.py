99107fd12fa9ebf73d0e7839951ae9cecb3d2299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:07:22 +0000

    Add package pygpsclient/1.5.14/pygpsclient-1.5.14-py3-none-any.whl

---
+++ packages/pygpsclient/pygpsclient-1.5.14-py3-none-any.whl/pygpsclient/__init__.py
@@ -0,0 +1,14 @@
+"""
+Created on 27 Sep 2020
+
+:author: semuadmin (Steve Smith)
+:copyright: 2020 semuadmin
+:license: BSD 3-Clause
+"""
+
+# pylint: disable=invalid-name
+
+from pygpsclient._version import __version__
+from pygpsclient.helpers import nmea2preset, ubx2preset
+
+version = __version__

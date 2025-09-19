da4c2dfb1d4a1ef36195e5e90fdec3a2fe7429e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:07:52 +0000

    Add package ry/0.0.58/ry-0.0.58-cp313-cp313-manylinux_2_28_aarch64.whl

---
+++ packages/ry/ry-0.0.58-cp313-cp313-manylinux_2_28_aarch64.whl/ry/ryo3/_sqlformat.pyi
@@ -0,0 +1,28 @@
+"""ryo3-sqlformat types"""
+
+import typing as t
+
+SqlfmtParamValue: t.TypeAlias = str | int | float | bool
+_TSqlfmtParamValue_co = t.TypeVar(
+    "_TSqlfmtParamValue_co", bound=SqlfmtParamValue, covariant=True
+)
+SqlfmtParamsLike: t.TypeAlias = (
+    dict[str, _TSqlfmtParamValue_co]
+    | t.Sequence[tuple[str, _TSqlfmtParamValue_co]]
+    | t.Sequence[_TSqlfmtParamValue_co]
+)
+
+class SqlfmtQueryParams:
+    def __init__(self, params: SqlfmtParamsLike[_TSqlfmtParamValue_co]) -> None: ...
+
+def sqlfmt_params(
+    params: SqlfmtParamsLike[_TSqlfmtParamValue_co] | SqlfmtQueryParams,
+) -> SqlfmtQueryParams: ...
+def sqlfmt(
+    sql: str,
+    params: SqlfmtParamsLike[_TSqlfmtParamValue_co] | SqlfmtQueryParams | None = None,
+    *,
+    indent: int = 2,  # -1 or any negative value will use tabs
+    uppercase: bool | None = True,
+    lines_between_statements: int = 1,
+) -> str: ...

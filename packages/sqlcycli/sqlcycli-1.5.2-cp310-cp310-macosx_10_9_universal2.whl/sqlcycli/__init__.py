b4a2ba62c252e1f27a1840938bd5e9511beaf4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:44 +0000

    Add package sqlcycli/1.5.2/sqlcycli-1.5.2-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/sqlcycli/sqlcycli-1.5.2-cp310-cp310-macosx_10_9_universal2.whl/sqlcycli/__init__.py
@@ -0,0 +1,57 @@
+from sqlcycli import constants, errors, sqlfunc, sqlintvl
+from sqlcycli._ssl import SSL
+from sqlcycli._auth import AuthPlugin
+from sqlcycli._optionfile import OptionFile
+from sqlcycli.transcode import escape, BIT, JSON
+from sqlcycli.charset import Charset, Charsets, all_charsets
+from sqlcycli.protocol import MysqlPacket, FieldDescriptorPacket
+from sqlcycli.connection import (
+    Cursor,
+    DictCursor,
+    DfCursor,
+    SSCursor,
+    SSDictCursor,
+    SSDfCursor,
+    BaseConnection,
+    Connection,
+)
+from sqlcycli import aio
+from sqlcycli.aio.pool import Pool, PoolConnection, PoolSyncConnection
+from sqlcycli._connect import connect, create_pool
+
+
+__all__ = [
+    # Module
+    "constants",
+    "errors",
+    "sqlfunc",
+    "sqlintvl",
+    # Class
+    "AuthPlugin",
+    "OptionFile",
+    "SSL",
+    "Charset",
+    "Charsets",
+    "MysqlPacket",
+    "FieldDescriptorPacket",
+    "Cursor",
+    "DictCursor",
+    "DfCursor",
+    "SSCursor",
+    "SSDictCursor",
+    "SSDfCursor",
+    "BaseConnection",
+    "Connection",
+    "aio",
+    "Pool",
+    "PoolConnection",
+    "PoolSyncConnection",
+    # Type
+    "BIT",
+    "JSON",
+    # Function
+    "all_charsets",
+    "escape",
+    "connect",
+    "create_pool",
+]

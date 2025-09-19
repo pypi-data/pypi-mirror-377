212c6a192e4b8969eb27f62dcfad02183cde3c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:17:51 +0000

    Add package sportsball/0.34.22/sportsball-0.34.22.tar.gz

---
+++ packages/sportsball/sportsball-0.34.22.tar.gz/sportsball-0.34.22/sportsball/data/nhl/position.py
@@ -0,0 +1,14 @@
+"""The enumeration of the different supported positions."""
+
+from enum import StrEnum
+
+
+class Position(StrEnum):
+    """An enumeration over the different positions."""
+
+    DEFENSEMAN = "D"
+    CENTRE = "C"
+    GOALTENDER = "G"
+    LEFT_WING = "LW"
+    RIGHT_WING = "RW"
+    FORWARD = "F"

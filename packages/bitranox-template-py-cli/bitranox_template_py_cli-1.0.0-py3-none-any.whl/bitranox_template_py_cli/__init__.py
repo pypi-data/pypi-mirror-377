c7abcfd0cd4998ad4bd0e574669c888fb3e0dabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:18:18 +0000

    Add package bitranox-template-py-cli/1.0.0/bitranox_template_py_cli-1.0.0-py3-none-any.whl

---
+++ packages/bitranox-template-py-cli/bitranox_template_py_cli-1.0.0-py3-none-any.whl/bitranox_template_py_cli/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from .bitranox_template_py_cli import hello_world
+
+__all__ = ["hello_world"]
---
+++ packages/bitranox-template-py-cli/bitranox_template_py_cli-1.0.0-py3-none-any.whl/bitranox_template_py_cli/bitranox_template_py_cli.py
@@ -0,0 +1,13 @@
+"""Temporary test stub for bitranox_template_py_cli."""
+
+from __future__ import annotations
+
+
+def hello_world() -> None:
+    """Print a friendly greeting."""
+    print("Hello World")
+
+
+def i_should_fail() -> None:
+    """Fails intentionally to test Error Behaviour"""
+    raise RuntimeError("I should fail")

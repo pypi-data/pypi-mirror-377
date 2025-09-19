f3cb00620fc97b0ac0524fb33532c00ab5b3e283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:17:53 +0000

    Add package cognee/0.3.4.dev2/cognee-0.3.4.dev2.tar.gz

---
+++ packages/cognee/cognee-0.3.4.dev2.tar.gz/cognee-0.3.4.dev2/cognee/modules/data/deletion/prune_system.py
@@ -0,0 +1,17 @@
+from cognee.infrastructure.databases.vector import get_vector_engine
+from cognee.infrastructure.databases.graph.get_graph_engine import get_graph_engine
+from cognee.infrastructure.databases.relational import get_relational_engine
+
+
+async def prune_system(graph=True, vector=True, metadata=True):
+    if graph:
+        graph_engine = await get_graph_engine()
+        await graph_engine.delete_graph()
+
+    if vector:
+        vector_engine = get_vector_engine()
+        await vector_engine.prune()
+
+    if metadata:
+        db_engine = get_relational_engine()
+        await db_engine.delete_database()

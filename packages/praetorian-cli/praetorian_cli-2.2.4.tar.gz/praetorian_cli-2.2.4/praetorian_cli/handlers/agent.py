1bd966ad2a1f2effe8d59b365fd6f812ec3b48bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 07:06:31 +0000

    Add package praetorian-cli/2.2.4/praetorian_cli-2.2.4.tar.gz

---
+++ packages/praetorian-cli/praetorian_cli-2.2.4.tar.gz/praetorian_cli-2.2.4/praetorian_cli/handlers/agent.py
@@ -0,0 +1,71 @@
+import click
+
+from praetorian_cli.handlers.chariot import chariot
+from praetorian_cli.handlers.cli_decorators import cli_handler
+
+
+@chariot.group()
+def agent():
+    """ A collection of AI features """
+    pass
+
+
+@agent.command()
+@cli_handler
+@click.argument('key')
+def affiliation(sdk, key):
+    """ Get affiliation data for risks and assets
+
+    The AI agent retrieves affiliation information for the asset or risk. This command
+    waits up to 3 minutes for the results.
+
+    \b
+    Example usages:
+        - praetorian chariot agent affiliation "#risk#www.praetorian.com#CVE-2024-1234"
+        - praetorian chariot agent affiliation "#asset#praetorian.com#www.praetorian.com"
+    """
+    click.echo("Polling for the affiliation data for up to 3 minutes.")
+    click.echo(sdk.agents.affiliation(key))
+
+@agent.group()
+def mcp():
+    """ Chariot's MCP server """
+    pass
+
+@mcp.command()
+@cli_handler
+@click.option('--allowed', '-a', type=str, multiple=True, default=['search_by_query', '*_list', '*_get'])
+def start(sdk, allowed):
+    """ Starts the Chariot MCP server
+
+    \b
+    Example usages:
+        - praetorian chariot agent mcp start
+        - praetorian chariot agent mcp start -a search_by_term -a risk_add
+        - praetorian chariot agent mcp start -a search_* -a risk_add
+
+    \b
+    Claude code configuration/usage:
+        - claude mcp add chariot -- praetorian chariot agent mcp start # read-only
+        - claude mcp add chariot -- praetorian chariot agent mcp start -a search_by_query -a risk_add -a asset_add # select write tools
+        - claude "show me my chariot assets from the example.com domain"
+        - claude "show me my chariot assets with port 22 open"
+        - claude "run a portscan on every discovered ip for example.com"
+    """
+    if len(allowed) == 0:
+        allowed = None
+    sdk.agents.start_mcp_server(allowed)
+
+@mcp.command()
+@click.option('--allowed', '-a', type=str, multiple=True, default=['search_by_query', '*_list', '*_get'])
+@cli_handler
+def tools(sdk, allowed):
+    """ Lists available mcp tools
+
+    \b
+    Example usages:
+        - praetorian chariot agent mcp tools
+        - praetorian chariot agent mcp tools -a search_* -a risk_add
+    """
+    for  tool in dict.keys(sdk.agents.list_mcp_tools(allowed)):
+        click.echo(tool)

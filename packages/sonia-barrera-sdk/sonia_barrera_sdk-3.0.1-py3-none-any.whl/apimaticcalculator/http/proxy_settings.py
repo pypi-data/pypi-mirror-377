dfb47197cf0b61ff540ab6bd32c186e6890ff939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:29 +0000

    Add package sonia-barrera-sdk/3.0.1/sonia_barrera_sdk-3.0.1-py3-none-any.whl

---
+++ packages/sonia-barrera-sdk/sonia_barrera_sdk-3.0.1-py3-none-any.whl/apimaticcalculator/http/proxy_settings.py
@@ -0,0 +1,14 @@
+
+"""
+apimaticcalculator
+
+This file was automatically generated for dgfgfdg by APIMATIC v3.0 (
+ https://www.apimatic.io ).
+"""
+
+from apimatic_core.http.configurations.proxy_settings import ProxySettings as CoreProxySettings
+
+class ProxySettings(CoreProxySettings):
+    """
+    A simple data model for configuring HTTP(S) proxy settings.
+    """
---
+++ packages/sonia-barrera-sdk/sonia_barrera_sdk-3.0.1-py3-none-any.whl/apimaticcalculator/models/operation_type.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+apimaticcalculator
+
+This file was automatically generated for dgfgfdg by APIMATIC v3.0 (
+ https://www.apimatic.io ).
+"""
+
+
+class OperationType(object):
+
+    """Implementation of the 'Operation Type' enum.
+
+    Possible operators are sum, subtract, multiply, divide
+
+    Attributes:
+        SUM: Represents the sum operator
+        SUBTRACT: Represents the subtract operator
+        MULTIPLY: Represents the multiply operator
+        DIVIDE: Represents the divide operator
+        additional_properties (Dict[str, object]): The additional properties
+            for the model.
+
+    """
+    SUM = 'SUM'
+
+    SUBTRACT = 'SUBTRACT'
+
+    MULTIPLY = 'MULTIPLY'
+
+    DIVIDE = 'DIVIDE'
+

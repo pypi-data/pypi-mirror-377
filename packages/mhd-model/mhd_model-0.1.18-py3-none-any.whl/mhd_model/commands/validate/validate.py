754a9fd1be433091cafe91b90d458c84cf7c3d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:17:49 +0000

    Add package mhd-model/0.1.18/mhd_model-0.1.18-py3-none-any.whl

---
+++ packages/mhd-model/mhd_model-0.1.18-py3-none-any.whl/mhd_model/commands/validate/validate.py
@@ -0,0 +1,20 @@
+import sys
+
+import click
+
+from mhd_model.commands.validate.announcement import validate_announcement_file_task
+
+
+@click.group(name="validate", context_settings={"help_option_names": ["-h", "--help"]})
+def validate_group():
+    """utilities to validate MetabolomicsHub files."""
+    pass
+
+
+validate_group.add_command(validate_announcement_file_task)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        validate_group(["--help"])
+    else:
+        validate_group()

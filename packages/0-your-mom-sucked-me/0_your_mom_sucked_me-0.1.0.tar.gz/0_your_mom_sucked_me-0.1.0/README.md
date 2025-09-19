e6622d033d0360d504fb67dc7cc08592c0b97748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:38 +0000

    Add package 0-your-mom-sucked-me/0.1.0/0_your_mom_sucked_me-0.1.0.tar.gz

---
+++ packages/0-your-mom-sucked-me/0_your_mom_sucked_me-0.1.0.tar.gz/0_your_mom_sucked_me-0.1.0/README.md
@@ -0,0 +1,11 @@
+# suck
+
+A sample Python package for demonstration purposes.
+
+## Usage
+
+```python
+from suck import print_hi
+print_hi('World')
+```
+
---
+++ packages/0-your-mom-sucked-me/0_your_mom_sucked_me-0.1.0.tar.gz/0_your_mom_sucked_me-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='0--___your_mom_sucked_me',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Your mom',
+    author_email='your.email@example.com',
+    description='A sample Python package.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/suck-me',
+    license='MIT',
+    python_requires='>=3.6',
+)
+

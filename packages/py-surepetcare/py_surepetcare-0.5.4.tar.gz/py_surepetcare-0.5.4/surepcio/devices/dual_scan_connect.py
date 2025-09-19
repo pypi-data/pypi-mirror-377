5543687c6e1ae7c8cac347010bac76f648c19c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:25 +0000

    Add package py-surepetcare/0.5.4/py_surepetcare-0.5.4.tar.gz

---
+++ packages/py-surepetcare/py_surepetcare-0.5.4.tar.gz/py_surepetcare-0.5.4/surepcio/devices/dual_scan_connect.py
@@ -0,0 +1,80 @@
+import logging
+from datetime import time
+from typing import Optional
+
+from pydantic import field_serializer
+
+from .device import BaseControl
+from .device import BaseStatus
+from .device import DeviceBase
+from surepcio.command import Command
+from surepcio.const import API_ENDPOINT_PRODUCTION
+from surepcio.entities.error_mixin import ImprovedErrorMixin
+from surepcio.enums import FlapLocking
+from surepcio.enums import ProductId
+
+logger = logging.getLogger(__name__)
+
+
+class Curfew(ImprovedErrorMixin):
+    enabled: bool
+    lock_time: time
+    unlock_time: time
+
+    @field_serializer("lock_time", "unlock_time")
+    def serialize_time(self, value: time, _info):
+        return value.strftime("%H:%M")
+
+
+class Locking(ImprovedErrorMixin):
+    mode: Optional[FlapLocking] = None
+
+
+class Control(BaseControl):
+    curfew: Optional[list[Curfew]] = None
+    locking: Optional[FlapLocking] = None
+    fail_safe: Optional[int] = None
+    fast_polling: Optional[bool] = None
+
+
+class Status(BaseStatus):
+    locking: Optional[Locking] = None
+
+
+class DualScanConnect(DeviceBase[Control, Status]):
+    """Representation of a Dual Scan Connect device."""
+
+    controlCls = Control
+    statusCls = Status
+
+    @property
+    def product(self) -> ProductId:
+        return ProductId.DUAL_SCAN_CONNECT
+
+    def refresh(self):
+        """Refresh the device status and control settings from the API."""
+
+        def parse(response) -> "DualScanConnect":
+            if not response:
+                return self
+            self.status = Status(**{**self.status.model_dump(), **response["data"]})
+            self.control = Control(**{**self.control.model_dump(), **response["data"]})
+            return self
+
+        return Command(
+            method="GET",
+            endpoint=f"{API_ENDPOINT_PRODUCTION}/device/{self.id}",
+            callback=parse,
+        )
+
+    def set_curfew(self, curfew: list[Curfew]) -> Command:
+        """Set the flap curfew times, using the household's timezone"""
+        return self.set_control(curfew=curfew)
+
+    def set_locking(self, locking: FlapLocking) -> Command:
+        """Set locking mode"""
+        return self.set_control(locking=locking)
+
+    def set_failsafe(self, failsafe: int) -> Command:
+        """Set failsafe mode"""
+        return self.set_control(fail_safe=failsafe)

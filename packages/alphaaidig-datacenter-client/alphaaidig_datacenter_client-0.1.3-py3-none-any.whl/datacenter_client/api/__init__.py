ecebe164affa71ee12c030112d552a2cbe27b841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:24 +0000

    Add package alphaaidig-datacenter-client/0.1.3/alphaaidig_datacenter_client-0.1.3-py3-none-any.whl

---
+++ packages/alphaaidig-datacenter-client/alphaaidig_datacenter_client-0.1.3-py3-none-any.whl/datacenter_client/api/__init__.py
@@ -0,0 +1,17 @@
+from .a_stock import AStockClient
+from .hk_stock import HKStockClient
+from .hs_industry import HSIndustryClient
+from .margin_detail import MarginDetailClient
+from .margin_summary import MarginSummaryClient
+from .sw_industry import SWIndustryClient
+from .sw_industry_company import SWIndustryCompanyClient
+
+__all__ = [
+    "AStockClient",
+    "HKStockClient",
+    "HSIndustryClient",
+    "MarginDetailClient",
+    "MarginSummaryClient",
+    "SWIndustryClient",
+    "SWIndustryCompanyClient",
+]

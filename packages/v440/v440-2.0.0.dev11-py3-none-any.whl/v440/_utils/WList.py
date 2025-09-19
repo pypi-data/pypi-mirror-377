70d4f3fd19d51b0f58a53792ff44191f77d67199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:06:58 +0000

    Add package v440/2.0.0.dev11/v440-2.0.0.dev11-py3-none-any.whl

---
+++ packages/v440/v440-2.0.0.dev11-py3-none-any.whl/v440/_utils/WList.py
@@ -0,0 +1,11 @@
+from typing import *
+
+from v440._utils import utils
+from v440._utils.VList import VList
+from v440.core.VersionError import VersionError
+
+__all__ = ["WList"]
+
+
+class WList(VList):
+    pass

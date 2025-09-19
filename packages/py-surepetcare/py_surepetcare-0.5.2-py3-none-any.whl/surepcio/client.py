17be6fbe8ef5f9c64a9671762afb00ee20bd703b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:18:23 +0000

    Add package py-surepetcare/0.5.2/py_surepetcare-0.5.2-py3-none-any.whl

---
+++ packages/py-surepetcare/py_surepetcare-0.5.2-py3-none-any.whl/surepcio/client.py
@@ -0,0 +1,114 @@
+import asyncio
+import logging
+from typing import Union
+
+from surepcio.command import Command
+from surepcio.security.auth import AuthClient
+
+logger = logging.getLogger(__name__)
+
+
+class SurePetcareClient(AuthClient):
+    """SurePetcare API client. Main object to interact with the API."""
+
+    async def get(self, endpoint: str, params: dict | None = None, headers=None) -> dict | None:
+        """Perform a GET request to the Sure Petcare API."""
+        await self.set_session()
+        async with self.session.get(endpoint, params=params, headers=headers) as response:
+            if not response.ok:
+                raise Exception(f"Error {endpoint} {response.status}: {await response.text()}")
+            if response.status == 204:
+                logger.info(f"GET {endpoint} returned 204 No Content")
+                return None
+            if response.status == 304:
+                # Not modified, keep existing data
+                logger.info(f"GET {endpoint} returned 304 Not Modified")
+                return None
+            self.populate_headers(response)
+            return await response.json()
+
+    async def post(self, endpoint: str, data: dict | None = None, headers=None, reuse=True) -> dict:
+        """Perform a POST request to the Sure Petcare API."""
+        await self.set_session()
+        async with self.session.post(endpoint, json=data, headers=headers) as response:
+            if not response.ok:
+                raise Exception(f"Error {response.status}: {await response.text()}")
+            if response.status == 204:
+                logger.info(f"POST {endpoint} returned 204 No Content")
+                return {"status": response.status}
+            self.populate_headers(response)
+            return await response.json()
+
+    async def put(self, endpoint: str, data: dict | None = None, headers=None, reuse=True) -> dict:
+        """Perform a PUT request to the Sure Petcare API."""
+        await self.set_session()
+        async with self.session.put(endpoint, json=data, headers=headers) as response:
+            if not response.ok:
+                raise Exception(f"Error {response.status}: {await response.text()}")
+            if response.status == 204:
+                logger.info("PUT {endpoint} returned 204 No Content")
+                return {"status": response.status}
+            logger.info(f"DELETE {endpoint} returned {response.status}")
+            self.populate_headers(response)
+            return await response.json()
+
+    async def delete(self, endpoint: str, data: dict | None = None, headers=None, reuse=True) -> dict:
+        """Perform a DELETE request to the Sure Petcare API."""
+        await self.set_session()
+        async with self.session.delete(endpoint, json=data, headers=headers) as response:
+            if not response.ok:
+                raise Exception(f"Error {response.status}: {await response.text()}")
+            if response.status == 204:
+                logger.info(f"DELETE {endpoint} returned 204 No Content")
+                return {"status": response.status}
+            logger.info(f"DELETE {endpoint} returned {response.status}")
+            self.populate_headers(response)
+            # Delete even if 200 sometimes has no content
+            if response.content_length == 0:
+                return {"status": response.status}
+
+            return await response.json()
+
+    async def api(self, command: Union[Command, list[Command]]):
+        """Execute a Command against the Sure Petcare API."""
+
+        if isinstance(command, list):
+            # Run all commands and return results as a list
+            return await asyncio.gather(*(self.api(cmd) for cmd in command))
+
+        headers = self._generate_headers(headers=self.headers(command.endpoint) if command.reuse else {})
+        method = command.method.lower()
+        if method == "get":
+            coro = self.get(
+                command.endpoint,
+                params=command.params,
+                headers=headers,
+            )
+        elif method == "post":
+            coro = self.post(
+                command.endpoint,
+                data=command.params,
+                headers=headers,
+            )
+        elif method == "put":
+            coro = self.put(
+                command.endpoint,
+                data=command.params,
+                headers=headers,
+            )
+
+        elif method == "delete":
+            coro = self.delete(
+                command.endpoint,
+                data=command.params,
+                headers=headers,
+            )
+        else:
+            raise NotImplementedError("HTTP method %s not supported.", command.method)
+        response = await coro
+
+        logger.debug("Response for %s refresh: %s", command.endpoint, response)
+        if command.callback:
+            return command.callback(response)
+
+        return response
---
+++ packages/py-surepetcare/py_surepetcare-0.5.2-py3-none-any.whl/surepcio/devices/hub.py
@@ -0,0 +1,90 @@
+import logging
+from typing import Optional
+
+from .device import BaseControl
+from .device import BaseStatus
+from .device import DeviceBase
+from surepcio.command import Command
+from surepcio.const import API_ENDPOINT_PRODUCTION
+from surepcio.const import API_ENDPOINT_V1
+from surepcio.enums import ProductId
+
+logger = logging.getLogger(__name__)
+
+
+class Control(BaseControl):
+    led_mode: Optional[int] = None
+    pairing_mode: Optional[int] = None
+
+
+class Status(BaseStatus):
+    led_mode: Optional[int] = None
+    pairing_mode: Optional[int] = None
+
+
+class Hub(DeviceBase[Control, Status]):
+    """Representation of a Hub device."""
+
+    controlCls = Control
+    statusCls = Status
+
+    @property
+    def product(self) -> ProductId:
+        return ProductId.HUB
+
+    @property
+    def photo(self) -> str:
+        return (
+            "https://www.surepetcare.io/assets/assets/products/hub/hub.6475b3a385180ab8fb96731c4bfd1eda.png"
+        )
+
+    def refresh(self):
+        """Refresh the device status and control settings from the API."""
+
+        def parse(response):
+            if not response:
+                return self
+
+            self.status = Status(**{**self.status.model_dump(), **response["data"]})
+            self.control = Control(**{**self.control.model_dump(), **response["data"]})
+            return self
+
+        return Command(
+            method="GET",
+            endpoint=f"{API_ENDPOINT_PRODUCTION}/device/{self.id}",
+            callback=parse,
+        )
+
+    def set_led_mode(self, led_mode: int) -> Command:
+        """Set let_mode settings"""
+
+        def parse(response):
+            if not response:
+                return self
+            # Unclear what to do with the data.. Should we refresh or is there any callback info?
+            logger.info("Parse callback from set_led_mode on device")
+            return self
+
+        return Command(
+            "PUT",
+            f"{API_ENDPOINT_V1}/device/{self.id}/control",
+            params=Control(led_mode=led_mode).model_dump(),
+            callback=parse,
+        )
+
+    def set_pairing_mode(self, pairing_mode: int) -> Command:
+        """Set pairing_mode settings"""
+
+        def parse(response):
+            if not response:
+                return self
+            # Unclear what to do with the data.. Should we refresh or is there any callback info?
+            logger.info("Parse callback from set_pairing_mode on device")
+            return self
+
+        return Command(
+            "PUT",
+            f"{API_ENDPOINT_V1}/device/{self.id}/control",
+            params=Control(pairing_mode=pairing_mode).model_dump(),
+            callback=parse,
+        )

ac56c7a91a8870d3b9cea39b760ece0cc14037c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 03:17:50 +0000

    Add package dhlab/2.42.0/dhlab-2.42.0.tar.gz

---
+++ packages/dhlab/dhlab-2.42.0.tar.gz/dhlab-2.42.0/dhlab/text/__init__.py
@@ -0,0 +1,10 @@
+"""Text analysis functionality, including building text corpora."""
+
+from .chunking import Chunks
+from .conc_coll import Collocations, Concordance, Counts
+from .corpus import Corpus
+from .dispersion import Dispersion
+from .geo_data import GeoData
+from .nbtokenizer import Tokens, tokenize
+from .parse import NER, POS, Models
+from .wildcard import WildcardWordSearch

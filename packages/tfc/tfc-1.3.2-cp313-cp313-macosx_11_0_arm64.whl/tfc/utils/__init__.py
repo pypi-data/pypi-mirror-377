48d75437b9c1dab27d3d2c1f15a579575dab65f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 04:06:20 +0000

    Add package tfc/1.3.2/tfc-1.3.2-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/tfc/tfc-1.3.2-cp313-cp313-macosx_11_0_arm64.whl/tfc/utils/__init__.py
@@ -0,0 +1,19 @@
+from .TFCUtils import TFCPrint
+
+TFCPrint()
+from .TFCUtils import egradRobust as egrad
+from .TFCUtils import egrad as egradSimple
+from .TFCUtils import (
+    TFCDict,
+    TFCDictRobust,
+    NLLS,
+    ComponentConstraintGraph,
+    NllsClass,
+    step,
+    LS,
+    LsClass,
+    pejit,
+)
+from .MakePlot import MakePlot
+from . import Latex
+from .CeSolver import CeSolver

0f682e26637518a5d14fdd3b12996a48cf31bb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 06:08:06 +0000

    Add package defineutils/0.1.1/defineutils-0.1.1-py3-none-any.whl

---
+++ packages/defineutils/defineutils-0.1.1-py3-none-any.whl/validate/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.1"
+
+from .validate import DefineSchemaValidator, DefineSchemaValidationError
+
+__all__ = ["DefineSchemaValidator", "DefineSchemaValidationError", "__version__"]

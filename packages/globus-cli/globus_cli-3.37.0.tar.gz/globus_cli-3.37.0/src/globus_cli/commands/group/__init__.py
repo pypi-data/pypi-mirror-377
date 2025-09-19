af479353e288a5bbf9d9427da79068afb41ea51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 07:06:32 +0000

    Add package globus-cli/3.37.0/globus_cli-3.37.0.tar.gz

---
+++ packages/globus-cli/globus_cli-3.37.0.tar.gz/globus_cli-3.37.0/src/globus_cli/commands/group/__init__.py
@@ -0,0 +1,25 @@
+from globus_cli.parsing import group
+
+
+@group(
+    "group",
+    lazy_subcommands={
+        "create": (".create", "group_create"),
+        "delete": (".delete", "group_delete"),
+        "invite": (".invite", "group_invite"),
+        "join": (".join", "group_join"),
+        "leave": (".leave", "group_leave"),
+        "list": (".list", "group_list"),
+        "member": (".member", "group_member"),
+        "set-policies": (".set_policies", "group_set_policies"),
+        "show": (".show", "group_show"),
+        "get-subscription-info": (
+            ".get_subscription_info",
+            "group_get_subscription_info",
+        ),
+        "get-by-subscription": (".get_by_subscription", "group_get_by_subscription"),
+        "update": (".update", "group_update"),
+    },
+)
+def group_command() -> None:
+    """Manage Globus Groups."""

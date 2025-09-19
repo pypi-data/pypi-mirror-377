707a49cc0702bb7b177c770fa7e25970da53c562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 05:07:24 +0000

    Add package planqk-quantum/2.22.0rc1/planqk_quantum-2.22.0rc1-py3-none-any.whl

---
+++ packages/planqk-quantum/planqk_quantum-2.22.0rc1-py3-none-any.whl/planqk/quantum/sdk/qiskit/providers/ibm/__init__.py
@@ -0,0 +1,5 @@
+"""IBM Quantum Provider for PLANQK SDK."""
+
+from planqk.quantum.sdk.qiskit.planqk_qiskit_runtime_service import PlanqkQiskitRuntimeService
+
+__all__ = ['PlanqkQiskitRuntimeService']
